apache/hadoop

HDDS-1720 : Add ability to configure RocksDB logs for Ozone Manager.

hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/db/DBStoreBuilder.java
@@ -22,11 +22,13 @@ package org.apache.hadoop.hdds.utils.db;
 import com.google.common.base.Preconditions;
 import org.apache.hadoop.conf.Configuration;
 
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdfs.DFSUtil;
 import org.eclipse.jetty.util.StringUtil;
 import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyOptions;
 import org.rocksdb.DBOptions;
+import org.rocksdb.InfoLogLevel;
 import org.rocksdb.RocksDB;
 import org.rocksdb.Statistics;
 import org.rocksdb.StatsLevel;
@@ -63,8 +65,9 @@ public final class DBStoreBuilder {
   private Configuration configuration;
   private CodecRegistry registry;
   private String rocksDbStat;
+  private RocksDBConfiguration rocksDBConfiguration;
 
-  private DBStoreBuilder(Configuration configuration) {
+  private DBStoreBuilder(OzoneConfiguration configuration) {
     tables = new HashSet<>();
     tableNames = new LinkedList<>();
     this.configuration = configuration;
@@ -72,9 +75,11 @@ public final class DBStoreBuilder {
     this.rocksDbStat = configuration.getTrimmed(
         OZONE_METADATA_STORE_ROCKSDB_STATISTICS,
         OZONE_METADATA_STORE_ROCKSDB_STATISTICS_DEFAULT);
+    this.rocksDBConfiguration =
+        configuration.getObject(RocksDBConfiguration.class);
   }
 
-  public static DBStoreBuilder newBuilder(Configuration configuration) {
+  public static DBStoreBuilder newBuilder(OzoneConfiguration configuration) {
     return new DBStoreBuilder(configuration);
   }
 
@@ -197,6 +202,21 @@ public final class DBStoreBuilder {
     if (option == null) {
       LOG.info("Using default options. {}", dbProfile.toString());
       option = dbProfile.getDBOptions();
+    }
+
+    if (rocksDBConfiguration.isRocksdbLoggingEnabled()) {
+      Logger rocksdbLogger =
+          LoggerFactory.getLogger(RocksDB.class);
+      org.rocksdb.Logger logger = new org.rocksdb.Logger(option) {
+        @Override
+        protected void log(InfoLogLevel infoLogLevel, String s) {
+          rocksdbLogger.info(s);
+        }
+      };
+      InfoLogLevel level = InfoLogLevel.valueOf(rocksDBConfiguration
+          .getRocksdbLogLevel() + "_LEVEL");
+      logger.setInfoLogLevel(level);
+      option.setLogger(logger);
     }
 
     if (!rocksDbStat.equals(OZONE_METADATA_STORE_ROCKSDB_STATISTICS_OFF)) {

hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/db/RocksDBConfiguration.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.hadoop.hdds.utils.db;
+
+import org.apache.hadoop.hdds.conf.Config;
+import org.apache.hadoop.hdds.conf.ConfigGroup;
+import org.apache.hadoop.hdds.conf.ConfigTag;
+import org.apache.hadoop.hdds.conf.ConfigType;
+
+@ConfigGroup(prefix = "hadoop.hdds.db")
+public class RocksDBConfiguration {
+
+  private boolean rocksdbLogEnabled;
+
+  @Config(key = "rocksdb.logging.enabled",
+      type = ConfigType.BOOLEAN,
+      defaultValue = "false",
+      tags = {ConfigTag.OM},
+      description = "Enable/Disable RocksDB logging for OM.")
+  public void setRocksdbLoggingEnabled(boolean rocksdbLogEnabled) {
+    this.rocksdbLogEnabled = rocksdbLogEnabled;
+  }
+
+  public boolean isRocksdbLoggingEnabled() {
+    return rocksdbLogEnabled;
+  }
+
+  private String rocksdbLogLevel;
+
+  @Config(key = "rocksdb.logging.level",
+      type = ConfigType.STRING,
+      defaultValue = "INFO",
+      tags = {ConfigTag.OM},
+      description = "OM RocksDB logging level (INFO/DEBUG/WARN/ERROR/FATAL)")
+  public void setRocksdbLogLevel(String rocksdbLogLevel) {
+    this.rocksdbLogLevel = rocksdbLogLevel;
+  }
+
+  public String getRocksdbLogLevel() {
+    return rocksdbLogLevel;
+  }
+
+}

apache/ozone

Signed-off-by: Anu Engineer <aengineer@apache.org>

hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/db/DBStoreBuilder.java
@@ -22,11 +22,13 @@ package org.apache.hadoop.hdds.utils.db;
 import com.google.common.base.Preconditions;
 import org.apache.hadoop.conf.Configuration;
 
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdfs.DFSUtil;
 import org.eclipse.jetty.util.StringUtil;
 import org.rocksdb.ColumnFamilyDescriptor;
 import org.rocksdb.ColumnFamilyOptions;
 import org.rocksdb.DBOptions;
+import org.rocksdb.InfoLogLevel;
 import org.rocksdb.RocksDB;
 import org.rocksdb.Statistics;
 import org.rocksdb.StatsLevel;
@@ -54,6 +56,8 @@ import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_METADATA_STORE_ROCKS
 public final class DBStoreBuilder {
   private static final Logger LOG =
       LoggerFactory.getLogger(DBStoreBuilder.class);
+  public static final Logger ROCKS_DB_LOGGER =
+      LoggerFactory.getLogger(RocksDB.class);
   private Set<TableConfig> tables;
   private DBProfile dbProfile;
   private DBOptions rocksDBOption;
@@ -63,8 +67,9 @@ public final class DBStoreBuilder {
   private Configuration configuration;
   private CodecRegistry registry;
   private String rocksDbStat;
+  private RocksDBConfiguration rocksDBConfiguration;
 
-  private DBStoreBuilder(Configuration configuration) {
+  private DBStoreBuilder(OzoneConfiguration configuration) {
     tables = new HashSet<>();
     tableNames = new LinkedList<>();
     this.configuration = configuration;
@@ -72,9 +77,11 @@ public final class DBStoreBuilder {
     this.rocksDbStat = configuration.getTrimmed(
         OZONE_METADATA_STORE_ROCKSDB_STATISTICS,
         OZONE_METADATA_STORE_ROCKSDB_STATISTICS_DEFAULT);
+    this.rocksDBConfiguration =
+        configuration.getObject(RocksDBConfiguration.class);
   }
 
-  public static DBStoreBuilder newBuilder(Configuration configuration) {
+  public static DBStoreBuilder newBuilder(OzoneConfiguration configuration) {
     return new DBStoreBuilder(configuration);
   }
 
@@ -197,6 +204,19 @@ public final class DBStoreBuilder {
     if (option == null) {
       LOG.info("Using default options. {}", dbProfile.toString());
       option = dbProfile.getDBOptions();
+    }
+
+    if (rocksDBConfiguration.isRocksdbLoggingEnabled()) {
+      org.rocksdb.Logger logger = new org.rocksdb.Logger(option) {
+        @Override
+        protected void log(InfoLogLevel infoLogLevel, String s) {
+          ROCKS_DB_LOGGER.info(s);
+        }
+      };
+      InfoLogLevel level = InfoLogLevel.valueOf(rocksDBConfiguration
+          .getRocksdbLogLevel() + "_LEVEL");
+      logger.setInfoLogLevel(level);
+      option.setLogger(logger);
     }
 
     if (!rocksDbStat.equals(OZONE_METADATA_STORE_ROCKSDB_STATISTICS_OFF)) {

hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/db/RocksDBConfiguration.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.hadoop.hdds.utils.db;
+
+import org.apache.hadoop.hdds.conf.Config;
+import org.apache.hadoop.hdds.conf.ConfigGroup;
+import org.apache.hadoop.hdds.conf.ConfigTag;
+import org.apache.hadoop.hdds.conf.ConfigType;
+
+/**
+ * Holds configuration items for OM RocksDB.
+ */
+@ConfigGroup(prefix = "hadoop.hdds.db")
+public class RocksDBConfiguration {
+
+  private boolean rocksdbLogEnabled;
+
+  @Config(key = "rocksdb.logging.enabled",
+      type = ConfigType.BOOLEAN,
+      defaultValue = "false",
+      tags = {ConfigTag.OM},
+      description = "Enable/Disable RocksDB logging for OM.")
+  public void setRocksdbLoggingEnabled(boolean enabled) {
+    this.rocksdbLogEnabled = enabled;
+  }
+
+  public boolean isRocksdbLoggingEnabled() {
+    return rocksdbLogEnabled;
+  }
+
+  private String rocksdbLogLevel;
+
+  @Config(key = "rocksdb.logging.level",
+      type = ConfigType.STRING,
+      defaultValue = "INFO",
+      tags = {ConfigTag.OM},
+      description = "OM RocksDB logging level (INFO/DEBUG/WARN/ERROR/FATAL)")
+  public void setRocksdbLogLevel(String level) {
+    this.rocksdbLogLevel = level;
+  }
+
+  public String getRocksdbLogLevel() {
+    return rocksdbLogLevel;
+  }
+
+}