27f5c0958ecde00c8fcb526a98c62b9894063a8d
Siddharth Wagle <swagle@hortonworks.com>
2019-10-17 22:58:32 -0700

    HDDS-1868. Checkstyle fix.

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/XceiverServerRatis.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/XceiverServerRatis.java
@@ -68,7 +68,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
