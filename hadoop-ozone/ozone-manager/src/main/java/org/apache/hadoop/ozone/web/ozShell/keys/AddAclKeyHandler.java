55011da2e791de168fbf743bc9b050b1d2716df7
cxorm <lianp964@gmail.com>
2019-10-29 18:45:04 +0800

    Improve AddAclHandler Message

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/web/ozShell/keys/AddAclKeyHandler.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/web/ozShell/keys/AddAclKeyHandler.java
@@ -94,7 +94,11 @@ public class AddAclKeyHandler extends Handler {
     boolean result = client.getObjectStore().addAcl(obj,
         OzoneAcl.parseAcl(acl));
 
-    System.out.printf("%s%n", "Acl added successfully: " + result);
+    String message = result
+        ? ("Acl added successfully: ") + result
+        : ("Acl added successfully: ") + result + (" (Acl already exists.)");
+
+    System.out.println(message);
 
     client.close();
     return null;
