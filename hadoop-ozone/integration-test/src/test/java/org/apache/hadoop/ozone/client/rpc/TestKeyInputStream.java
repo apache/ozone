9565cc56444e5daa151591a4fa6992db0ac12a6f
bshashikant <shashikant@apache.org>
2019-11-06 10:56:16 +0530

    HDDS-2359. Seeking randomly in a key with more than 2 blocks of data leads to inconsistent reads (#82)

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestKeyInputStream.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestKeyInputStream.java
@@ -37,6 +37,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -118,6 +119,107 @@ public class TestKeyInputStream {
         .createKey(keyName, type, size, objectStore, volumeName, bucketName);
   }
 
+
+  @Test
+  public void testSeekRandomly() throws Exception {
+    XceiverClientMetrics metrics = XceiverClientManager
+        .getXceiverClientMetrics();
+
+    String keyName = getKeyName();
+    OzoneOutputStream key = ContainerTestHelper.createKey(keyName,
+        ReplicationType.RATIS, 0, objectStore, volumeName, bucketName);
+
+    // write data of more than 2 blocks.
+    int dataLength = (2 * blockSize) + (chunkSize);
+
+    Random rd = new Random();
+    byte[] inputData = new byte[dataLength];
+    rd.nextBytes(inputData);
+    key.write(inputData);
+    key.close();
+
+
+    KeyInputStream keyInputStream = (KeyInputStream) objectStore
+        .getVolume(volumeName).getBucket(bucketName).readKey(keyName)
+        .getInputStream();
+
+    // Seek to some where end.
+    validate(keyInputStream, inputData, dataLength-200, 100);
+
+    // Now seek to start.
+    validate(keyInputStream, inputData, 0, 140);
+
+    validate(keyInputStream, inputData, 200, 300);
+
+    validate(keyInputStream, inputData, 30, 500);
+
+    randomSeek(dataLength, keyInputStream, inputData);
+
+    // Read entire key.
+    validate(keyInputStream, inputData, 0, dataLength);
+
+    // Repeat again and check.
+    randomSeek(dataLength, keyInputStream, inputData);
+
+    validate(keyInputStream, inputData, 0, dataLength);
+
+    keyInputStream.close();
+  }
+
+  /**
+   * This method does random seeks and reads and validates the reads are
+   * correct or not.
+   * @param dataLength
+   * @param keyInputStream
+   * @param inputData
+   * @throws Exception
+   */
+  private void randomSeek(int dataLength, KeyInputStream keyInputStream,
+      byte[] inputData) throws Exception {
+    // Do random seek.
+    for (int i=0; i<dataLength - 300; i+=20) {
+      validate(keyInputStream, inputData, i, 200);
+    }
+
+    // Seek to end and read in reverse order. And also this is partial chunks
+    // as readLength is 20, chunk length is 100.
+    for (int i=dataLength - 100; i>=100; i-=20) {
+      validate(keyInputStream, inputData, i, 20);
+    }
+
+    // Start from begin and seek such that we read partially chunks.
+    for (int i=0; i<dataLength - 300; i+=20) {
+      validate(keyInputStream, inputData, i, 90);
+    }
+
+  }
+
+  /**
+   * This method seeks to specified seek value and read the data specified by
+   * readLength and validate the read is correct or not.
+   * @param keyInputStream
+   * @param inputData
+   * @param seek
+   * @param readLength
+   * @throws Exception
+   */
+  private void validate(KeyInputStream keyInputStream, byte[] inputData,
+      long seek, int readLength) throws Exception {
+    keyInputStream.seek(seek);
+
+    byte[] expectedData = new byte[readLength];
+    keyInputStream.read(expectedData, 0, readLength);
+
+    byte[] dest = new byte[readLength];
+
+    System.arraycopy(inputData, (int)seek, dest, 0, readLength);
+
+    for (int i=0; i < readLength; i++) {
+      Assert.assertEquals(expectedData[i], dest[i]);
+    }
+  }
+
+
   @Test
   public void testSeek() throws Exception {
     XceiverClientMetrics metrics = XceiverClientManager
@@ -153,8 +255,6 @@ public class TestKeyInputStream {
     // Seek operation should not result in any readChunk operation.
     Assert.assertEquals(readChunkCount, metrics
         .getContainerOpsMetrics(ContainerProtos.Type.ReadChunk));
-    Assert.assertEquals(readChunkCount, metrics
-        .getContainerOpCountMetrics(ContainerProtos.Type.ReadChunk));
 
     byte[] readData = new byte[chunkSize];
     keyInputStream.read(readData, 0, chunkSize);
