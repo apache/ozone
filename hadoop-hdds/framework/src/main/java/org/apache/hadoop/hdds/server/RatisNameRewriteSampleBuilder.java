4116a9753caf57312f34ef3d22cdb326ee7ce707
Márton Elek <elek@apache.org>
2019-11-20 14:53:59 +0100

    delete unused code

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/RatisNameRewriteSampleBuilder.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/RatisNameRewriteSampleBuilder.java
@@ -101,17 +101,4 @@ public class RatisNameRewriteSampleBuilder extends DefaultSampleBuilder {
     return Strings.join(nameParts, '.');
   }
 
-  private String withoutSecondPart(String[] parts) {
-    StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < parts.length; i++) {
-      if (i != 2) {
-        if (i != 0) {
-          builder.append(".");
-        }
-        builder.append(parts[i]);
-      }
-    }
-    return builder.toString();
-  }
-
 }
