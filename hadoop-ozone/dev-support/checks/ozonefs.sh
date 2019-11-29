34c647b2dace24987b93cb5782b666afbe6e1378
Márton Elek <elek@apache.org>
2019-11-29 09:57:57 +0100

    Integration test separation and experimetal builds

--- hadoop-ozone/dev-support/checks/ozonefs.sh
+++ hadoop-ozone/dev-support/checks/ozonefs.sh
@@ -16,12 +16,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../../.." || exit 1
 
-REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/integration"}
-mkdir -p "$REPORT_DIR"
-
 export MAVEN_OPTS="-Xmx4096m"
 mvn -B install -DskipTests
-mvn -B -fn test -pl :hadoop-ozone-integration-test "$@" | tee $REPORT_DIR/output.log
+mvn -B -fn test -pl :hadoop-ozone-filesystem "$@"
+
+REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/ozonefs"}
+mkdir -p "$REPORT_DIR"
 
 # shellcheck source=hadoop-ozone/dev-support/checks/_mvn_unit_report.sh
 source "$DIR/_mvn_unit_report.sh"
--- hadoop-ozone/dev-support/checks/tools.sh
+++ hadoop-ozone/dev-support/checks/tools.sh
@@ -13,20 +13,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$DIR/../../.." || exit 1
-
-REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/integration"}
-mkdir -p "$REPORT_DIR"
 
 export MAVEN_OPTS="-Xmx4096m"
 mvn -B install -DskipTests
-mvn -B -fn test -pl :hadoop-ozone-integration-test "$@" | tee $REPORT_DIR/output.log
+mvn -B -fn test -pl :hadoop-ozone-tools "$@"
+
+REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/tools"}
+mkdir -p "$REPORT_DIR"
 
 # shellcheck source=hadoop-ozone/dev-support/checks/_mvn_unit_report.sh
 source "$DIR/_mvn_unit_report.sh"
 
-if [[ -s "$REPORT_DIR/summary.txt" ]] ; then
-    exit 1
+if [[ -s "$REPORT_DIR/summary.txt" ]]; then
+  exit 1
 fi
 exit 0
