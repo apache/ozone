3285415d589062849b3eb10cae056351c8aed610
cxorm <lianp964@gmail.com>
2019-12-09 23:34:38 +0800

    Update OzoneManager

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
@@ -2238,6 +2238,15 @@ public final class OzoneManager extends ServiceRuntimeInfoImpl
     }
   }
 
+  // TODO: HDDS-2424. recover-trash command server side handling.
+  @Override
+  public boolean recoverTrash(String volumeName, String bucketName,
+      String keyName, String destinationBucket) throws IOException {
+
+    boolean recoverOperation = true;
+    return recoverOperation;
+  }
+
   /**
    * Sets bucket property from args.
    *
