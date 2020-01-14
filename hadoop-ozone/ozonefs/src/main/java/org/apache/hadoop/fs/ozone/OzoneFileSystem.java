7953c354771178e2f0e732f790b1be862a55f883
cxorm <lianp964@gmail.com>
2020-01-14 18:33:43 +0800

    Add createFSInputStream(InputStream)

--- hadoop-ozone/ozonefs/src/main/java/org/apache/hadoop/fs/ozone/OzoneFileSystem.java
+++ hadoop-ozone/ozonefs/src/main/java/org/apache/hadoop/fs/ozone/OzoneFileSystem.java
@@ -19,6 +19,7 @@
 package org.apache.hadoop.fs.ozone;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 import org.apache.hadoop.classification.InterfaceAudience;
@@ -102,5 +103,10 @@ public class OzoneFileSystem extends BasicOzoneFileSystem
       return new OzoneClientAdapterImpl(omHost, omPort, conf,
           volumeStr, bucketStr, storageStatistics);
     }
+  }
+
+  @Override
+  protected InputStream createFSInputStream(InputStream inputStream) {
+    return new CapableOzoneFSInputStream(inputStream, statistics);
   }
 }
