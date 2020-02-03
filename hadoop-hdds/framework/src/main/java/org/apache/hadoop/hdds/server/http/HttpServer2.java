1f98b9bcf29868924ed4b550a58807217d43c05b
Márton Elek <elek@apache.org>
2020-02-03 10:27:32 +0100

    make private constants private

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/http/HttpServer2.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/http/HttpServer2.java
@@ -118,44 +118,44 @@ public final class HttpServer2 implements FilterContainer {
   public static final Logger LOG = LoggerFactory.getLogger(HttpServer2.class);
 
   private static final String HTTP_SCHEME = "http";
-  public static final String HTTPS_SCHEME = "https";
+  private static final String HTTPS_SCHEME = "https";
 
-  public static final String HTTP_MAX_REQUEST_HEADER_SIZE_KEY =
+  private static final String HTTP_MAX_REQUEST_HEADER_SIZE_KEY =
       "hadoop.http.max.request.header.size";
-  public static final int HTTP_MAX_REQUEST_HEADER_SIZE_DEFAULT = 65536;
-  public static final String HTTP_MAX_RESPONSE_HEADER_SIZE_KEY =
+  private static final int HTTP_MAX_REQUEST_HEADER_SIZE_DEFAULT = 65536;
+  private static final String HTTP_MAX_RESPONSE_HEADER_SIZE_KEY =
       "hadoop.http.max.response.header.size";
-  public static final int HTTP_MAX_RESPONSE_HEADER_SIZE_DEFAULT = 65536;
+  private static final int HTTP_MAX_RESPONSE_HEADER_SIZE_DEFAULT = 65536;
 
-  public static final String HTTP_SOCKET_BACKLOG_SIZE_KEY =
+  private static final String HTTP_SOCKET_BACKLOG_SIZE_KEY =
       "hadoop.http.socket.backlog.size";
-  public static final int HTTP_SOCKET_BACKLOG_SIZE_DEFAULT = 128;
-  public static final String HTTP_MAX_THREADS_KEY = "hadoop.http.max.threads";
-  public static final String HTTP_ACCEPTOR_COUNT_KEY =
+  private static final int HTTP_SOCKET_BACKLOG_SIZE_DEFAULT = 128;
+  private static final String HTTP_MAX_THREADS_KEY = "hadoop.http.max.threads";
+  private static final String HTTP_ACCEPTOR_COUNT_KEY =
       "hadoop.http.acceptor.count";
   // -1 to use default behavior of setting count based on CPU core count
-  public static final int HTTP_ACCEPTOR_COUNT_DEFAULT = -1;
-  public static final String HTTP_SELECTOR_COUNT_KEY =
+  private static final int HTTP_ACCEPTOR_COUNT_DEFAULT = -1;
+  private static final String HTTP_SELECTOR_COUNT_KEY =
       "hadoop.http.selector.count";
   // -1 to use default behavior of setting count based on CPU core count
-  public static final int HTTP_SELECTOR_COUNT_DEFAULT = -1;
+  private static final int HTTP_SELECTOR_COUNT_DEFAULT = -1;
   // idle timeout in milliseconds
-  public static final String HTTP_IDLE_TIMEOUT_MS_KEY =
+  private static final String HTTP_IDLE_TIMEOUT_MS_KEY =
       "hadoop.http.idle_timeout.ms";
-  public static final int HTTP_IDLE_TIMEOUT_MS_DEFAULT = 10000;
-  public static final String HTTP_TEMP_DIR_KEY = "hadoop.http.temp.dir";
+  private static final int HTTP_IDLE_TIMEOUT_MS_DEFAULT = 10000;
+  private static final String HTTP_TEMP_DIR_KEY = "hadoop.http.temp.dir";
 
-  public static final String FILTER_INITIALIZER_PROPERTY
+  private static final String FILTER_INITIALIZER_PROPERTY
       = "ozone.http.filter.initializers";
 
   // The ServletContext attribute where the daemon Configuration
   // gets stored.
-  public static final String CONF_CONTEXT_ATTRIBUTE = "hadoop.conf";
-  public static final String ADMINS_ACL = "admins.acl";
-  public static final String SPNEGO_FILTER = "SpnegoFilter";
-  public static final String NO_CACHE_FILTER = "NoCacheFilter";
+  private static final String CONF_CONTEXT_ATTRIBUTE = "hadoop.conf";
+  private static final String ADMINS_ACL = "admins.acl";
+  private static final String SPNEGO_FILTER = "SpnegoFilter";
+  private static final String NO_CACHE_FILTER = "NoCacheFilter";
 
-  public static final String BIND_ADDRESS = "bind.address";
+  private static final String BIND_ADDRESS = "bind.address";
 
   private final AccessControlList adminsAcl;
 
