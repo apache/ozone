0b00d07698d1d34c2ead56a9c31d91230dc90b66
Márton Elek <elek@apache.org>
2020-02-11 09:46:41 +0100

    fix shellcheck errors

--- .github/comment-commands/ready.sh
+++ .github/comment-commands/ready.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #doc: Dismiss all the blocking reviews by github-actions bot
+
 MESSAGE="Blocking review request is removed."
-URL="$(jq -r '.issue.pull_request.url' $GITHUB_EVENT_PATH)/reviews"
+URL="$(jq -r '.issue.pull_request.url' "$GITHUB_EVENT_PATH")/reviews"
 curl -s -o /dev/null "$URL" \
     | jq -r '.[] | [.user.login, .id] | @tsv' \
     | grep github-actions \
@@ -10,5 +26,5 @@ curl -s -o /dev/null "$URL" \
        -X PUT \
        --data "$(jq --arg message "$MESSAGE" -n '{message: $message}')" \
        --header "authorization: Bearer $GITHUB_TOKEN" \
-       $URL/ISSUE_ID/dismissals
+       "$URL"/ISSUE_ID/dismissals
 
--- .github/process-comment.sh
+++ .github/process-comment.sh
@@ -1,23 +1,38 @@
 #!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BODY=$(jq -r .comment.body $GITHUB_EVENT_PATH)
-LINES=$(printf "$BODY" | wc -l)
+BODY=$(jq -r .comment.body "$GITHUB_EVENT_PATH")
+LINES=$(printf "%s" "$BODY" | wc -l)
 if [ "$LINES" == "0" ]; then
    if  [[ "$BODY" == /* ]]; then
       echo "Command $BODY is received"
-      COMMAND=$(echo $BODY | awk '{print $1}' | sed 's/\///')
-      ARGS=$(echo $BODY | cut -d ' ' -f2-)
+      COMMAND=$(echo "$BODY" | awk '{print $1}' | sed 's/\///')
+      ARGS=$(echo "$BODY" | cut -d ' ' -f2-)
       if [ -f "$SCRIPT_DIR/comment-commands/$COMMAND.sh" ]; then
-         RESPONSE=$("$SCRIPT_DIR/comment-commands/$COMMAND.sh" $ARGS 2>1)
+         RESPONSE=$("$SCRIPT_DIR/comment-commands/$COMMAND.sh" "${ARGS[@]}" 2>&1)
       else
-         RESPONSE="No such command. \`$COMMAND\` $($SCRIPT_DIR/comment-commands/help.sh)"
+         RESPONSE="No such command. \`$COMMAND\` $("$SCRIPT_DIR/comment-commands/help.sh")"
       fi
       set +x #do not display the GITHUB_TOKEN
-      COMMENTS_URL=$(jq -r .issue.comments_url $GITHUB_EVENT_PATH)
+      COMMENTS_URL=$(jq -r .issue.comments_url "$GITHUB_EVENT_PATH")
       curl -s \
             --data "$(jq --arg body "$RESPONSE" -n '{body: $body}')" \
             --header "authorization: Bearer $GITHUB_TOKEN" \
             --header 'content-type: application/json' \
-            $COMMENTS_URL
+            "$COMMENTS_URL"
    fi
 fi
