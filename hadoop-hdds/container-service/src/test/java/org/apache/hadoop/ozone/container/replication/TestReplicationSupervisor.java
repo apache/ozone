453f8640f4ace0a7e316ab3c48f3381e16fb5a82
Wei-Chiu Chuang <weichiu@cloudera.com>
2020-02-14 11:27:35 +0100

    HDDS-3000. Update guava version to 28.2-jre
    Closes #545

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/replication/TestReplicationSupervisor.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/replication/TestReplicationSupervisor.java
@@ -38,7 +38,7 @@ import org.junit.Test;
 
 import javax.annotation.Nonnull;
 
-import static com.google.common.util.concurrent.MoreExecutors.sameThreadExecutor;
+import static com.google.common.util.concurrent.MoreExecutors.newDirectExecutorService;
 import static java.util.Collections.emptyList;
 
 /**
@@ -115,7 +115,7 @@ public class TestReplicationSupervisor {
   public void failureHandling() {
     // GIVEN
     ReplicationSupervisor supervisor = supervisorWith(
-        __ -> throwingReplicator, sameThreadExecutor());
+        __ -> throwingReplicator, newDirectExecutorService());
 
     try {
       //WHEN
@@ -159,7 +159,7 @@ public class TestReplicationSupervisor {
 
   private ReplicationSupervisor supervisorWithSuccessfulReplicator() {
     return supervisorWith(supervisor -> new FakeReplicator(supervisor, set),
-        sameThreadExecutor());
+        newDirectExecutorService());
   }
 
   private ReplicationSupervisor supervisorWith(
