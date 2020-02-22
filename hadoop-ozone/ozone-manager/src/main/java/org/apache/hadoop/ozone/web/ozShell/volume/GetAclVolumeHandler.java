48d187542836f2adae64b0e68a22092abc589262
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2020-02-22 15:27:52 -0800

    HDDS-3057. Improve Ozone Shell ACL operations' help text readability.

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/web/ozShell/volume/GetAclVolumeHandler.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/web/ozShell/volume/GetAclVolumeHandler.java
@@ -37,7 +37,7 @@ import static org.apache.hadoop.ozone.security.acl.OzoneObj.StoreType.OZONE;
  * Get acl handler for volume.
  */
 @Command(name = "getacl",
-    description = "List all acls.")
+    description = "List all ACLs.")
 public class GetAclVolumeHandler extends Handler {
 
   @Parameters(arity = "1..1", description = Shell.OZONE_BUCKET_URI_DESCRIPTION)
@@ -45,7 +45,7 @@ public class GetAclVolumeHandler extends Handler {
 
   @CommandLine.Option(names = {"--store", "-s"},
       required = false,
-      description = "store type. i.e OZONE or S3")
+      description = "Store type. i.e OZONE or S3")
   private String storeType;
 
   /**
