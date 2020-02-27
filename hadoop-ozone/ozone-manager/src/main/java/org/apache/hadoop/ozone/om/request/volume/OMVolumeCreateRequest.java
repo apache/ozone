c8fa92af7ef5df5cc68193d60d858cf3ff7bbfbe
Bharat Viswanadham <bharat@apache.org>
2020-02-27 15:23:18 -0800

    rebase

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/volume/OMVolumeCreateRequest.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/volume/OMVolumeCreateRequest.java
@@ -115,14 +115,15 @@ public class OMVolumeCreateRequest extends OMVolumeRequest {
     Map<String, String> auditMap = new HashMap<>();
     Collection<String> ozAdmins = ozoneManager.getOzoneAdmins();
     try {
-      long objectID = OMFileRequest.getObjIDFromTxId(transactionLogIndex);
       omVolumeArgs = OmVolumeArgs.getFromProtobuf(volumeInfo);
-      // when you create a volume, we set both Object ID and update ID to the
-      // same ratis transaction ID. The Object ID will never change, but update
+      // when you create a volume, we set both Object ID and update ID.
+      // The Object ID will never change, but update
       // ID will be set to transactionID each time we update the object.
+      omVolumeArgs.setObjectID(
+          OMFileRequest.getObjIDFromTxId(transactionLogIndex));
       omVolumeArgs.setUpdateID(transactionLogIndex,
           ozoneManager.isRatisEnabled());
-      omVolumeArgs.setObjectID(transactionLogIndex);
+
 
       auditMap = omVolumeArgs.toAuditMap();
 
