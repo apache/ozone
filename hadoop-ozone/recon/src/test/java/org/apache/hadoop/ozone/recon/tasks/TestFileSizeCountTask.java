97bb601e43f4e56d8a2a9736e84b79cb7908f98f
avijayanhwx <14299376+avijayanhwx@users.noreply.github.com>
2020-03-16 14:32:16 -0700

    HDDS-3170. Fix issues in File count by size task. (#675)
    * HDDS-3170. Fix issues in File count by size task.
    
    * HDDS-3170. Address review comments.

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/tasks/TestFileSizeCountTask.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/tasks/TestFileSizeCountTask.java
@@ -18,28 +18,51 @@
 
 package org.apache.hadoop.ozone.recon.tasks;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.ozone.om.OMMetadataManager;
 import org.apache.hadoop.ozone.om.OmMetadataManagerImpl;
 import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
 import org.apache.hadoop.hdds.utils.db.TypedTable;
+import org.apache.hadoop.ozone.recon.persistence.AbstractSqlDatabaseTest;
+import org.apache.hadoop.ozone.recon.tasks.OMDBUpdateEvent.OMUpdateEventBuilder;
+import org.hadoop.ozone.recon.schema.UtilizationSchemaDefinition;
+import org.hadoop.ozone.recon.schema.tables.daos.FileCountBySizeDao;
+import org.jooq.Configuration;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Arrays;
 
+import static org.apache.hadoop.ozone.recon.tasks.OMDBUpdateEvent.OMDBUpdateAction.DELETE;
 import static org.apache.hadoop.ozone.recon.tasks.OMDBUpdateEvent.OMDBUpdateAction.PUT;
+import static org.apache.hadoop.ozone.recon.tasks.OMDBUpdateEvent.OMDBUpdateAction.UPDATE;
 import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 /**
  * Unit test for File Size Count Task.
  */
-public class TestFileSizeCountTask {
+public class TestFileSizeCountTask extends AbstractSqlDatabaseTest {
+
+  private FileCountBySizeDao fileCountBySizeDao;
+
+  @Before
+  public void setUp() throws SQLException {
+    UtilizationSchemaDefinition schemaDefinition =
+        getInjector().getInstance(UtilizationSchemaDefinition.class);
+    schemaDefinition.initializeSchema();
+    Configuration sqlConfiguration =
+        getInjector().getInstance((Configuration.class));
+    fileCountBySizeDao = new FileCountBySizeDao(sqlConfiguration);
+  }
+
   @Test
   public void testCalculateBinIndex() {
     FileSizeCountTask fileSizeCountTask = mock(FileSizeCountTask.class);
@@ -94,14 +117,13 @@ public class TestFileSizeCountTask {
   }
 
   @Test
-  public void testFileCountBySizeReprocess() throws IOException {
+  public void testReprocess() throws IOException {
     OmKeyInfo omKeyInfo1 = mock(OmKeyInfo.class);
     given(omKeyInfo1.getKeyName()).willReturn("key1");
     given(omKeyInfo1.getDataSize()).willReturn(1000L);
 
     OMMetadataManager omMetadataManager = mock(OmMetadataManagerImpl.class);
     TypedTable<String, OmKeyInfo> keyTable = mock(TypedTable.class);
-
 
     TypedTable.TypedTableIterator mockKeyIter = mock(TypedTable
         .TypedTableIterator.class);
@@ -114,17 +136,88 @@ public class TestFileSizeCountTask {
     when(mockKeyIter.next()).thenReturn(mockKeyValue);
     when(mockKeyValue.getValue()).thenReturn(omKeyInfo1);
 
-    FileSizeCountTask fileSizeCountTask = mock(FileSizeCountTask.class);
-    when(fileSizeCountTask.getMaxFileSizeUpperBound()).
-        thenReturn(4096L);
-    when(fileSizeCountTask.getOneKB()).thenReturn(1024L);
+    FileSizeCountTask fileSizeCountTask =
+        new FileSizeCountTask(fileCountBySizeDao);
+    Pair<String, Boolean> result =
+        fileSizeCountTask.reprocess(omMetadataManager);
+    assertTrue(result.getRight());
 
-    when(fileSizeCountTask.reprocess(omMetadataManager)).thenCallRealMethod();
-    //call reprocess()
-    fileSizeCountTask.reprocess(omMetadataManager);
-    verify(fileSizeCountTask, times(1)).
-        updateUpperBoundCount(omKeyInfo1, PUT);
-    verify(fileSizeCountTask,
-        times(1)).populateFileCountBySizeDB();
+    long[] upperBoundCount = fileSizeCountTask.getUpperBoundCount();
+    assertEquals(1, upperBoundCount[0]);
+    for (int i = 1; i < upperBoundCount.length; i++) {
+      assertEquals(0, upperBoundCount[i]);
+    }
+  }
+
+  @Test
+  public void testProcess() {
+    FileSizeCountTask fileSizeCountTask =
+        new FileSizeCountTask(fileCountBySizeDao);
+
+    // Write 2 keys.
+    OmKeyInfo toBeDeletedKey = mock(OmKeyInfo.class);
+    given(toBeDeletedKey.getKeyName()).willReturn("deletedKey");
+    given(toBeDeletedKey.getDataSize()).willReturn(2000L); // Bin 1
+    OMDBUpdateEvent event = new OMUpdateEventBuilder()
+        .setAction(PUT)
+        .setKey("deletedKey")
+        .setValue(toBeDeletedKey)
+        .build();
+
+    OmKeyInfo toBeUpdatedKey = mock(OmKeyInfo.class);
+    given(toBeUpdatedKey.getKeyName()).willReturn("updatedKey");
+    given(toBeUpdatedKey.getDataSize()).willReturn(10000L); // Bin 4
+    OMDBUpdateEvent event2 = new OMUpdateEventBuilder()
+        .setAction(PUT)
+        .setKey("updatedKey")
+        .setValue(toBeUpdatedKey)
+        .build();
+
+    OMUpdateEventBatch omUpdateEventBatch =
+        new OMUpdateEventBatch(Arrays.asList(event, event2));
+    fileSizeCountTask.process(omUpdateEventBatch);
+
+    // Verify 2 keys are in correct bins.
+    long[] upperBoundCount = fileSizeCountTask.getUpperBoundCount();
+    assertEquals(1, upperBoundCount[4]); // updatedKey
+    assertEquals(1, upperBoundCount[1]); // deletedKey
+
+    // Add new key.
+    OmKeyInfo newKey = mock(OmKeyInfo.class);
+    given(newKey.getKeyName()).willReturn("newKey");
+    given(newKey.getDataSize()).willReturn(1000L); // Bin 0
+    OMDBUpdateEvent putEvent = new OMUpdateEventBuilder()
+        .setAction(PUT)
+        .setKey("newKey")
+        .setValue(newKey)
+        .build();
+
+    // Update existing key.
+    OmKeyInfo updatedKey = mock(OmKeyInfo.class);
+    given(updatedKey.getKeyName()).willReturn("updatedKey");
+    given(updatedKey.getDataSize()).willReturn(50000L); // Bin 6
+    OMDBUpdateEvent updateEvent = new OMUpdateEventBuilder()
+        .setAction(UPDATE)
+        .setKey("updatedKey")
+        .setValue(updatedKey)
+        .setOldValue(toBeUpdatedKey)
+        .build();
+
+    // Delete another existing key.
+    OMDBUpdateEvent deleteEvent = new OMUpdateEventBuilder()
+        .setAction(DELETE)
+        .setKey("deletedKey")
+        .setValue(toBeDeletedKey)
+        .build();
+
+    omUpdateEventBatch = new OMUpdateEventBatch(
+        Arrays.asList(updateEvent, putEvent, deleteEvent));
+    fileSizeCountTask.process(omUpdateEventBatch);
+
+    upperBoundCount = fileSizeCountTask.getUpperBoundCount();
+    assertEquals(1, upperBoundCount[0]); // newKey
+    assertEquals(0, upperBoundCount[1]); // deletedKey
+    assertEquals(0, upperBoundCount[4]); // updatedKey old
+    assertEquals(1, upperBoundCount[6]); // updatedKey new
   }
 }
