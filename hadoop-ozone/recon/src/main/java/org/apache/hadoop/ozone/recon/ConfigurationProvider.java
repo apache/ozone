f66d6e16033c7f5ca81ccc67acb268e41f4f69a3
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2020-03-18 18:04:01 -0700

    HDDS-3183. Deprecate old Recon HTTP Server Keytab config key. (#689)

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/ConfigurationProvider.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/ConfigurationProvider.java
@@ -19,6 +19,7 @@ package org.apache.hadoop.ozone.recon;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Provider;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 
 /**
@@ -30,7 +31,17 @@ import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 public class ConfigurationProvider implements
     Provider<OzoneConfiguration> {
 
+  static {
+    addDeprecations();
+  }
+
   private static OzoneConfiguration configuration;
+
+  private static void addDeprecations() {
+    Configuration.addDeprecation(
+        ReconServerConfigKeys.OZONE_RECON_HTTP_KEYTAB_FILE_OLD,
+        ReconServerConfigKeys.OZONE_RECON_HTTP_KEYTAB_FILE);
+  }
 
   @VisibleForTesting
   public static void setConfiguration(OzoneConfiguration conf) {
