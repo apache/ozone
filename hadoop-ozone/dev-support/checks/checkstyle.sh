6b3fe1e76e99790da56731584511839e1b44b6b6
Elek Márton <elek@apache.org>
2020-04-03 13:27:54 +0200

    HDDS-3316. Checkstyle check fails silently in case of mvn related errors
    Closes #746

--- hadoop-ozone/dev-support/checks/checkstyle.sh
+++ hadoop-ozone/dev-support/checks/checkstyle.sh
@@ -21,7 +21,9 @@ REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/checkstyle"}
 mkdir -p "$REPORT_DIR"
 REPORT_FILE="$REPORT_DIR/summary.txt"
 
-mvn -B -fn checkstyle:check
+set -e
+mvn -B checkstyle:check -D checkstyle.failOnViolation=false
+set +e
 
 #Print out the exact violations with parsing XML results with sed
 find "." -name checkstyle-errors.xml -print0 \
