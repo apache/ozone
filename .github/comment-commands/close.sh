e9742db5d85194409eea0d37d3b774ee143a68f7
Elek Márton <elek@apache.org>
2020-04-03 14:11:01 +0200

    HDDS-3318. Support /close command in the Github comments
    Closes #748

--- .github/comment-commands/close.sh
+++ .github/comment-commands/close.sh
@@ -14,25 +14,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#doc: Add a REQUESTED_CHANGE type review to mark issue non-mergeable: `/pending <reason>`
+#doc: Close pending pull request temporary
 # shellcheck disable=SC2124
-MESSAGE="Marking this issue as un-mergeable as requested.
+MESSAGE="Thank you very much for the patch. I am closing this PR __temporarily__ as there was no 
+activity recently and it is waiting for response from its author.
 
-Please use \`/ready\` comment when it's resolved.
+It doesn't mean that this PR is not important or ignored: feel free to reopen the PR at any time.
 
-> $@"
+It only means that attention of committers is not required. We prefer to keep the review queue clean. This ensures PRs in need of review are more visible, which results in faster feedback for all PRs.
 
-URL="$(jq -r '.issue.pull_request.url' "$GITHUB_EVENT_PATH")/reviews"
+If you need ANY help to finish this PR, please [contact the community](https://github.com/apache/hadoop-ozone#contact) on the mailing list or the slack channel.
+
 set +x #GITHUB_TOKEN
 curl -s -o /dev/null \
-  --data "$(jq --arg body "$MESSAGE" -n '{event: "REQUEST_CHANGES", body: $body}')" \
+  -X POST \
+  --data "$(jq --arg body "$MESSAGE" -n '{body: $body}')" \
   --header "authorization: Bearer $GITHUB_TOKEN" \
   --header 'content-type: application/json' \
-  "$URL"
+  "$(jq -r '.issue.comments_url' "$GITHUB_EVENT_PATH")"
 
 curl -s -o /dev/null \
-  -X POST \
-  --data '{"labels": [ "pending" ]}' \
+  -X PATCH \
+  --data '{"state": "close"}' \
   --header "authorization: Bearer $GITHUB_TOKEN" \
-  "$(jq -r '.issue.url' "$GITHUB_EVENT_PATH")/labels"
-
+  --header 'content-type: application/json' \
+  "$(jq -r '.issue.pull_request.url' "$GITHUB_EVENT_PATH")"
