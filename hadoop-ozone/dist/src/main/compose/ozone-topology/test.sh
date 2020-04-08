30ff33881d40e312de1a3a5e9b89accf612b3e9f
S O'Donnell <sodonnell@cloudera.com>
2020-04-08 18:55:51 +0100

    Enabled test topology read test and increase min DNs to 6

--- hadoop-ozone/dist/src/main/compose/ozone-topology/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozone-topology/test.sh
@@ -33,6 +33,24 @@ execute_robot_test scm basic/basic.robot
 
 execute_robot_test scm topology/cli.robot
 
+# Ensure data can be read even when a full rack
+# is stopped.
+execute_robot_test scm topology/loaddata.robot
+
+stop_containers datanode_1 datanode_2 datanode_3
+
+execute_robot_test scm topology/readdata.robot
+
+start_containers datanode_1 datanode_2 datanode_3
+
+wait_for_port datanode_1 9858 60
+wait_for_port datanode_2 9858 60
+wait_for_port datanode_3 9858 60
+
+stop_containers datanode_4 datanode_5 datanode_6
+
+execute_robot_test scm topology/readdata.robot
+
 stop_docker_env
 
 generate_report
