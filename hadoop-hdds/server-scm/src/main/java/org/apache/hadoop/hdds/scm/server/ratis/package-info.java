61a9bb49dcca4999b780108c61183258274ea0b8
Li Cheng <bloodhell2004@gmail.com>
2020-04-17 15:39:03 +0800

    HDDS-3187 Construct SCM StateMachine. (#819)
    Co-authored-by: Li Cheng <timmycheng@tencent.com>

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/ratis/package-info.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/ratis/package-info.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hdds.scm.ratis;
+package org.apache.hadoop.hdds.scm.server.ratis;
 
 /**
  * This package contains classes related to Apache Ratis for SCM.
