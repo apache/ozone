9aaaef8240fdccf507a06d1de809816c7517f196
Bharat Viswanadham <bharat@apache.org>
2020-04-20 15:39:22 -0700

    fix rat and checkstyle

--- hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/endpoint/TestCompleteMultipartUploadRequestUnmarshaller.java
+++ hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/endpoint/TestCompleteMultipartUploadRequestUnmarshaller.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.ozone.s3.endpoint;
 
 import org.junit.Assert;
@@ -57,16 +75,14 @@ public class TestCompleteMultipartUploadRequestUnmarshaller {
     checkContent(completeMultipartUploadRequest);
   }
 
-  private void checkContent(CompleteMultipartUploadRequest completeMultipartUploadRequest) {
-    Assert.assertEquals(2, completeMultipartUploadRequest.getPartList().size());
+  private void checkContent(CompleteMultipartUploadRequest request) {
+    Assert.assertEquals(2, request.getPartList().size());
 
     List<CompleteMultipartUploadRequest.Part> parts =
-        completeMultipartUploadRequest.getPartList();
+        request.getPartList();
 
-    Assert.assertEquals(part1,
-        parts.get(0).geteTag());
-    Assert.assertEquals(part2,
-        completeMultipartUploadRequest.getPartList().get(1).geteTag());
+    Assert.assertEquals(part1, parts.get(0).geteTag());
+    Assert.assertEquals(part2, parts.get(1).geteTag());
   }
 
   private CompleteMultipartUploadRequest unmarshall(
