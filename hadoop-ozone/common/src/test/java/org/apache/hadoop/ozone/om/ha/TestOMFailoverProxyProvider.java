510f511b12473edf493ac7ff3e5664b53c2c9c2c
Uma Maheswara Rao Gangumalla <umamahesh@apache.org>
2020-04-23 15:18:01 -0700

    HDDS-3465. OM Failover retry happens too quickly when new leader suggested and retrying on same OM.: Added few more tests and fixed comments.

--- hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/om/ha/TestOMFailoverProxyProvider.java
+++ hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/om/ha/TestOMFailoverProxyProvider.java
@@ -19,8 +19,9 @@ package org.apache.hadoop.ozone.om.ha;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
-import java.io.IOException;
+import java.util.StringJoiner;
 
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.ozone.OmUtils;
@@ -34,57 +35,121 @@ import static org.apache.hadoop.ozone.OzoneConfigKeys.
     OZONE_CLIENT_WAIT_BETWEEN_RETRIES_MILLIS_DEFAULT;
 
 /**
- * Test OMFailoverProxyProvider failover behaviour.
+ * Tests OMFailoverProxyProvider failover behaviour.
  */
 public class TestOMFailoverProxyProvider {
   private final static String OM_SERVICE_ID = "om-service-test1";
   private final static String NODE_ID_BASE_STR = "omNode-";
   private final static String DUMMY_NODE_ADDR = "0.0.0.0:8080";
+  private OMFailoverProxyProvider provider;
+  private long waitBetweenRetries;
 
-  /**
-   * 1. Create FailoverProvider with 3 OMs.
-   * 2. Try failover to different node: WaitTime should be 0.
-   * 3. Try failover to same node: wiatTime should increment
-   * attempts*waitBetweenRetries
-   */
-  @Test
-  public void testWaitTimeWithSameNodeFailover() throws IOException {
-    String nodeId1 = NODE_ID_BASE_STR + 1;
-    String nodeId2 = NODE_ID_BASE_STR + 2;
-    String nodeId3 = NODE_ID_BASE_STR + 3;
+  @Before
+  public void init() throws Exception {
     OzoneConfiguration config = new OzoneConfiguration();
-    config.set(OmUtils.addKeySuffixes(OZONE_OM_NODES_KEY, OM_SERVICE_ID),
-        String.join(",", nodeId1, nodeId2, nodeId3));
-    config.set(OmUtils.addKeySuffixes(OZONE_OM_ADDRESS_KEY, OM_SERVICE_ID,
-        nodeId1), DUMMY_NODE_ADDR);
-    config.set(OmUtils.addKeySuffixes(OZONE_OM_ADDRESS_KEY, OM_SERVICE_ID,
-        nodeId2), DUMMY_NODE_ADDR);
-    config.set(OmUtils.addKeySuffixes(OZONE_OM_ADDRESS_KEY, OM_SERVICE_ID,
-        nodeId3), DUMMY_NODE_ADDR);
-    long waitBetweenRetries = config.getLong(
+    long numNodes = 3;
+    waitBetweenRetries = config.getLong(
         OZONE_CLIENT_WAIT_BETWEEN_RETRIES_MILLIS_KEY,
         OZONE_CLIENT_WAIT_BETWEEN_RETRIES_MILLIS_DEFAULT);
-    OMFailoverProxyProvider provider = new OMFailoverProxyProvider(config,
+    StringJoiner allNodeIds = new StringJoiner(",");
+    for (int i = 1; i <= numNodes; i++) {
+      String nodeId = NODE_ID_BASE_STR + i;
+      config.set(OmUtils.addKeySuffixes(OZONE_OM_ADDRESS_KEY, OM_SERVICE_ID,
+          nodeId), DUMMY_NODE_ADDR);
+      allNodeIds.add(nodeId);
+    }
+    config.set(OmUtils.addKeySuffixes(OZONE_OM_NODES_KEY, OM_SERVICE_ID),
+        allNodeIds.toString());
+    provider = new OMFailoverProxyProvider(config,
         UserGroupInformation.getCurrentUser(), OM_SERVICE_ID);
-    provider.performFailoverToNextProxy(); // Failover attempt 1
-    Assert.assertEquals(0, provider.getWaitTime());
-    provider.performFailoverToNextProxy(); // Failover attempt 2
-    Assert.assertEquals(0, provider.getWaitTime());
-    // Failover attempt 3 to same OM, waitTime should increase
-    provider.performFailoverIfRequired(provider.getCurrentProxyOMNodeId());
-    Assert.assertEquals(waitBetweenRetries, provider.getWaitTime());
-    // Failover attempt 4 to same OM, waitTime should further increase
-    provider.performFailoverIfRequired(provider.getCurrentProxyOMNodeId());
-    Assert.assertEquals(waitBetweenRetries * 2, provider.getWaitTime());
-    // Failover attempt 5 to same OM, waitTime should further increase
-    provider.performFailoverIfRequired(provider.getCurrentProxyOMNodeId());
-    Assert.assertEquals(waitBetweenRetries * 3, provider.getWaitTime());
-    // Failover attempt 6 to same OM, waitTime should further increase
-    provider.performFailoverIfRequired(provider.getCurrentProxyOMNodeId());
-    Assert.assertEquals(waitBetweenRetries * 4, provider.getWaitTime());
+  }
 
-    // Failover attempt 7, will go to new OM and waitTime should be 0
-    provider.performFailoverToNextProxy();
-    Assert.assertEquals(0, provider.getWaitTime());
+  /**
+   * Tests waitTime when fail over to next node.
+   */
+  @Test
+  public void testWaitTimeWithNextNode() {
+    failoverToNextNode(2, 0);
+    // After 3 attempts done, wait time should be waitBetweenRetries.
+    failoverToNextNode(1, waitBetweenRetries);
+    // From 4th Attempt waitTime should reset to 0.
+    failoverToNextNode(2, 0);
+    // After 2nd round of 3attempts done, wait time should be
+    // waitBetweenRetries.
+    failoverToNextNode(1, waitBetweenRetries);
+  }
+
+  /**
+   * 1. Try failover to same node: wiatTime should increment
+   * attempts*waitBetweenRetries.
+   * 2. Try failover to next node: waitTime should be 0.
+   */
+  @Test
+  public void testWaitTimeWithSameNodeFailover() {
+    // Failover attempt 1 to same OM, waitTime should increase.
+    failoverToSameNode(4);
+    // 2 same node failovers, waitTime should be 0.
+    failoverToNextNode(2, 0);
+  }
+
+  /**
+   * Tests failover to next node and same node.
+   */
+  @Test
+  public void testWaitTimeWithNextNodeAndSameNodeFailover() {
+    failoverToNextNode(1, 0);
+    // 1 Failover attempt to same OM, waitTime should increase.
+    failoverToSameNode(2);
+  }
+
+  /**
+   * Tests wait time should reset in thr following case:
+   * 1. Do a couple same node failover attempts.
+   * 2. Next node failover should reset wait time to 0.
+   */
+  @Test
+  public void testWaitTimeResetWhenNextNodeFailoverAfterSameNode() {
+    // 2 failover attempts to same OM, waitTime should increase.
+    failoverToSameNode(2);
+    // Failover to next node, should reset waitTime to 0.
+    failoverToNextNode(1, 0);
+  }
+
+  /**
+   * Tests waitTime reset after same node failover.
+   */
+  @Test
+  public void testWaitTimeResetWhenAllNodeFailoverAndSameNode() {
+    // Next node failover wait time should be 0.
+    failoverToNextNode(2, 0);
+    // Once all numNodes failover done, waitTime should be waitBetweenRetries
+    failoverToNextNode(1, waitBetweenRetries);
+    // 1 failover attempt to same OM, waitTime should increase.
+    failoverToSameNode(2);
+    // Next node failover should reset wait time.
+    failoverToNextNode(2, 0);
+    failoverToNextNode(1, waitBetweenRetries);
+  }
+
+  /**
+   * Failover to next node and wait time should be same as waitTimeAfter.
+   */
+  private void failoverToNextNode(int numNextNodeFailoverTimes,
+      long waitTimeAfter) {
+    for (int attempt = 0; attempt < numNextNodeFailoverTimes; attempt++) {
+      provider.performFailoverToNextProxy();
+      Assert.assertEquals(waitTimeAfter, provider.getWaitTime());
+    }
+  }
+
+  /**
+   * Failover to same node and wait time will be attempt*waitBetweenRetries.
+   */
+  private void failoverToSameNode(int numSameNodeFailoverTimes) {
+    for (int attempt = 1; attempt <= numSameNodeFailoverTimes; attempt++) {
+      provider.performFailoverIfRequired(provider.getCurrentProxyOMNodeId());
+      Assert.assertEquals(attempt * waitBetweenRetries,
+          provider.getWaitTime());
+    }
   }
 }
