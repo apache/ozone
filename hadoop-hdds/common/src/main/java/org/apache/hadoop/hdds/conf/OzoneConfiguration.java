958494b3fc05433ee96d8ef508b83e071684ac68
Simon0806 <barley0806@gmail.com>
2020-05-06 11:53:17 +0800

    HDDS-3513. add conf to UGI when security is enable

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/conf/OzoneConfiguration.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/conf/OzoneConfiguration.java
@@ -211,7 +211,6 @@ public class OzoneConfiguration extends Configuration
     Configuration.addDefaultResource("hdfs-default.xml");
     Configuration.addDefaultResource("hdfs-site.xml");
     Configuration.addDefaultResource("ozone-default.xml");
-    Configuration.addDefaultResource("ozone-site.xml");
   }
 
   /**
