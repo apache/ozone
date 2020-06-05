e4469d6a0c649dadbc2f357600c418df58d3efbc
avijayanhwx <14299376+avijayanhwx@users.noreply.github.com>
2020-06-05 10:10:49 -0700

    Update hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RDBStore.java
    Co-authored-by: Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RDBStore.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RDBStore.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
