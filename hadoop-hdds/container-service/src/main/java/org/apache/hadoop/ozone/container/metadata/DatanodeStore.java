40f680a8bb72330c979e83d2f472a020770e04f0
Ethan Rose <erose@cloudera.com>
2020-06-29 09:32:15 -0400

    Add DatanodeStore interface based on the SCMMetadataStore interface

---
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/metadata/DatanodeStore.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.ozone.container.metadata;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.utils.db.BatchOperationHandler;
+import org.apache.hadoop.hdds.utils.db.DBStore;
+import org.apache.hadoop.hdds.utils.db.Table;
+
+import java.io.IOException;
+
+public interface DatanodeStore {
+
+  /**
+   * Start datanode manager.
+   *
+   * @param configuration - Configuration
+   * @throws IOException - Unable to start datanode store.
+   */
+  void start(OzoneConfiguration configuration) throws IOException;
+
+  /**
+   * Stop datanode manager.
+   */
+  void stop() throws Exception;
+
+  /**
+   * Get datanode store.
+   *
+   * @return datanode store.
+   */
+  @VisibleForTesting
+  DBStore getStore();
+
+  /**
+   * A Table that keeps the block data.
+   *
+   * @return Table
+   */
+  Table<Long, Long> getBlockDataTable();
+
+  /**
+   * A Table that keeps the metadata.
+   *
+   * @return Table
+   */
+  Table<Long, Long> getMetadataTable();
+
+  /**
+   * Helper to create and write batch transactions.
+   */
+  BatchOperationHandler getBatchHandler();
+}
