e219aae7c7bcd8eacd592ca2ccfcf58626477f37
avijayanhwx <14299376+avijayanhwx@users.noreply.github.com>
2020-07-31 22:38:34 -0700

    HDDS-4021. Organize Recon DBs into a 'DBDefinition'. (#1255)

---
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/spi/impl/ReconDBDefinition.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.apache.hadoop.ozone.recon.spi.impl;
+
+import org.apache.hadoop.hdds.utils.db.DBColumnFamilyDefinition;
+import org.apache.hadoop.hdds.utils.db.DBDefinition;
+import org.apache.hadoop.hdds.utils.db.IntegerCodec;
+import org.apache.hadoop.hdds.utils.db.LongCodec;
+import org.apache.hadoop.ozone.recon.ReconServerConfigKeys;
+import org.apache.hadoop.ozone.recon.api.types.ContainerKeyPrefix;
+
+/**
+ * RocksDB definition for the DB internal to Recon.
+ */
+public class ReconDBDefinition implements DBDefinition {
+
+  private String dbName;
+
+  public ReconDBDefinition(String dbName) {
+    this.dbName = dbName;
+  }
+
+  public static final DBColumnFamilyDefinition<ContainerKeyPrefix, Integer>
+      CONTAINER_KEY =
+      new DBColumnFamilyDefinition<>(
+          "containerKeyTable",
+          ContainerKeyPrefix.class,
+          new ContainerKeyPrefixCodec(),
+          Integer.class,
+          new IntegerCodec());
+
+  public static final DBColumnFamilyDefinition<Long, Long>
+      CONTAINER_KEY_COUNT =
+      new DBColumnFamilyDefinition<>(
+          "containerKeyCountTable",
+          Long.class,
+          new LongCodec(),
+          Long.class,
+          new LongCodec());
+
+  @Override
+  public String getName() {
+    return dbName;
+  }
+
+  @Override
+  public String getLocationConfigKey() {
+    return ReconServerConfigKeys.OZONE_RECON_DB_DIR;
+  }
+
+  @Override
+  public DBColumnFamilyDefinition[] getColumnFamilies() {
+    return new DBColumnFamilyDefinition[] {CONTAINER_KEY, CONTAINER_KEY_COUNT};
+  }
+}
