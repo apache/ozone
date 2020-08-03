3e141bc32e4c77ecb0e2696204ad5e992e161d2d
avijayanhwx <14299376+avijayanhwx@users.noreply.github.com>
2020-08-03 09:20:28 -0700

    HDDS-4029. Recon unable to add a new container which is in CLOSED state. (#1258)
    (cherry picked from commit c07ccd7c23dd63745124f8b509fe4d90c4d5c966)

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/scm/TestReconIncrementalContainerReportHandler.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/scm/TestReconIncrementalContainerReportHandler.java
@@ -19,23 +19,28 @@
 package org.apache.hadoop.ozone.recon.scm;
 
 import static org.apache.hadoop.hdds.protocol.MockDatanodeDetails.randomDatanodeDetails;
+import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.LifeCycleState.OPEN;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.UUID;
 
 import org.apache.hadoop.hdds.HddsConfigKeys;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.protocol.DatanodeDetails;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos.LifeCycleState;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.ContainerReplicaProto;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.ContainerReplicaProto.State;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.IncrementalContainerReportProto;
 import org.apache.hadoop.hdds.scm.container.ContainerID;
+import org.apache.hadoop.hdds.scm.container.common.helpers.ContainerWithPipeline;
 import org.apache.hadoop.hdds.scm.net.NetworkTopology;
 import org.apache.hadoop.hdds.scm.net.NetworkTopologyImpl;
 import org.apache.hadoop.hdds.scm.node.NodeManager;
@@ -90,6 +95,64 @@ public class TestReconIncrementalContainerReportHandler
     nodeManager.addContainer(datanodeDetails, containerID);
     assertTrue(containerManager.exists(containerID));
     assertEquals(1, containerManager.getContainerReplicas(containerID).size());
+    assertEquals(OPEN, containerManager.getContainer(containerID).getState());
+  }
+
+  @Test
+  public void testProcessICRStateMismatch() throws IOException {
+
+    // Recon container state is "OPEN".
+    // Replica state could be any Non OPEN state.
+    long containerId = 11;
+    for (State state : Arrays.asList(State.CLOSING, State.QUASI_CLOSED,
+        State.CLOSED)) {
+      ContainerWithPipeline containerWithPipeline = getTestContainer(
+          containerId++, OPEN);
+      ContainerID containerID =
+          containerWithPipeline.getContainerInfo().containerID();
+
+      ReconContainerManager containerManager = getContainerManager();
+      containerManager.addNewContainer(containerID.getId(),
+          containerWithPipeline);
+
+      DatanodeDetails datanodeDetails =
+          containerWithPipeline.getPipeline().getFirstNode();
+      NodeManager nodeManagerMock = mock(NodeManager.class);
+      when(nodeManagerMock.getNodeByUuid(any())).thenReturn(datanodeDetails);
+      IncrementalContainerReportFromDatanode reportMock =
+          mock(IncrementalContainerReportFromDatanode.class);
+      when(reportMock.getDatanodeDetails())
+          .thenReturn(containerWithPipeline.getPipeline().getFirstNode());
+
+      IncrementalContainerReportProto containerReport =
+          getIncrementalContainerReportProto(containerID, state,
+              datanodeDetails.getUuidString());
+      when(reportMock.getReport()).thenReturn(containerReport);
+      ReconIncrementalContainerReportHandler reconIcr =
+          new ReconIncrementalContainerReportHandler(nodeManagerMock,
+              containerManager);
+
+      reconIcr.onMessage(reportMock, mock(EventPublisher.class));
+      assertTrue(containerManager.exists(containerID));
+      assertEquals(1,
+          containerManager.getContainerReplicas(containerID).size());
+      LifeCycleState expectedState = getContainerStateFromReplicaState(state);
+      LifeCycleState actualState =
+          containerManager.getContainer(containerID).getState();
+      assertEquals(String.format("Expecting %s in " +
+              "container state for replica state %s", expectedState,
+          state), expectedState, actualState);
+    }
+  }
+
+  private LifeCycleState getContainerStateFromReplicaState(
+      State state) {
+    switch (state) {
+    case CLOSING: return LifeCycleState.CLOSING;
+    case QUASI_CLOSED: return LifeCycleState.QUASI_CLOSED;
+    case CLOSED: return LifeCycleState.CLOSED;
+    default: return null;
+    }
   }
 
   private static IncrementalContainerReportProto
