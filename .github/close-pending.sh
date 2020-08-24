2ee814ad4dee09e686aeeb310c860462d9b7cde7
Elek Márton <elek@apache.org>
2020-08-24 17:57:04 +0200

    make it work with apache repo

--- .github/close-pending.sh
+++ .github/close-pending.sh
@@ -22,22 +22,20 @@ MESSAGE=$(cat $SCRIPT_DIR/closing-message.txt)
 while IFS= read -r number &&
       IFS= read -r title; do
       echo "Closing PR ($number): $title"
-      curl -v \
+      curl -s -o /dev/null \
         -X POST \
         --data "$(jq --arg body "$MESSAGE" -n '{body: $body}')" \
         --header "authorization: Bearer $GITHUB_TOKEN" \
         --header 'content-type: application/json' \
-        "https://api.github.com/repos/elek/hadoop-ozone/issues/$number/comments"
+        "https://api.github.com/repos/apache/hadoop-ozone/issues/$number/comments"
 
-      curl -v \
+      curl -s -o /dev/null \
         -X PATCH \
         --data '{"state": "close"}' \
         --header "authorization: Bearer $GITHUB_TOKEN" \
         --header 'content-type: application/json' \
-        "https://api.github.com/repos/elek/hadoop-ozone/pulls/$number"
-
-#     "https://api.github.com/search/issues?q=repo:apache/hadoop-ozone+type:pr+updated:<$(date -d "-1 month" +%Y-%m-%d)+label:pending+is:open" \
+        "https://api.github.com/repos/apache/hadoop-ozone/pulls/$number"
 done < <(curl -H "Content-Type: application/json" \
      --header "authorization: Bearer $GITHUB_TOKEN" \
-     "https://api.github.com/search/issues?q=repo:elek/hadoop-ozone+type:pr+label:pending+is:open" \
-    | jq -r '.items[] | (.number,.title)')
+     "https://api.github.com/search/issues?q=repo:apache/hadoop-ozone+type:pr+updated:<$(date -d "-1 month" +%Y-%m-%d)+label:pending+is:open" \
+     | jq -r '.items[] | (.number,.title)')
