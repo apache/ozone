ead6371b584b7246ed1304782c16f561a46b0356
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2020-09-03 16:43:31 +0200

    HDDS-3840. Use OFS in MapReduce acceptance test (#1365)

--- hadoop-ozone/dist/src/main/compose/ozone-mr/hadoop32/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozone-mr/hadoop32/test.sh
@@ -31,9 +31,10 @@ export OZONE_DIR=/opt/ozone
 # shellcheck source=/dev/null
 source "$COMPOSE_DIR/../../testlib.sh"
 
-execute_robot_test rm ozonefs/hadoopo3fs.robot
-
-execute_robot_test rm mapreduce.robot
+for scheme in o3fs ofs; do
+  execute_robot_test rm -v "SCHEME:${scheme}" -N "hadoopfs-${scheme}" ozonefs/hadoopo3fs.robot
+  execute_robot_test rm -v "SCHEME:${scheme}" -N "mapreduce-${scheme}" mapreduce.robot
+done
 
 stop_docker_env
 
