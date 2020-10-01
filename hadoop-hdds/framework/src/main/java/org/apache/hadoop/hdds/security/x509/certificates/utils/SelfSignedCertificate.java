0fb7ae70a0bfa8c10f91b767000dd39e4b5c6c01
Xiaoyu Yao <xyao@apache.org>
2020-10-01 15:05:09 -0700

    HDDS-4301. SCM CA certificate does not encode KeyUsage extension propertly.

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificates/utils/SelfSignedCertificate.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificates/utils/SelfSignedCertificate.java
@@ -145,8 +145,7 @@ public final class SelfSignedCertificate {
           new BasicConstraints(true));
       int keyUsageFlag = KeyUsage.keyCertSign | KeyUsage.cRLSign;
       KeyUsage keyUsage = new KeyUsage(keyUsageFlag);
-      builder.addExtension(Extension.keyUsage, false,
-          new DEROctetString(keyUsage));
+      builder.addExtension(Extension.keyUsage, true, keyUsage);
       if (altNames != null && altNames.size() >= 1) {
         builder.addExtension(new Extension(Extension.subjectAlternativeName,
             false, new GeneralNames(altNames.toArray(
