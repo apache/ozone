4f118f387261dd26562e32c6d193848fd15f6a2f
Ethan Rose <errose28@gmail.com>
2020-10-12 15:32:20 -0400

    Implement open key creation method and remove unused methods

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/TestOpenKeyCleanupService.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/TestOpenKeyCleanupService.java
@@ -19,9 +19,16 @@
 
 package org.apache.hadoop.ozone.om;
 
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Random;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
 import org.apache.hadoop.ozone.common.BlockGroup;
+import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
+import org.apache.hadoop.ozone.om.request.TestOMRequestUtils;
 import org.apache.hadoop.test.GenericTestUtils;
+import org.apache.htrace.core.MilliSpan;
 import org.apache.ratis.util.TimeDuration;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,6 +38,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -42,29 +50,28 @@ public class TestOpenKeyCleanupService {
   // Lower service interval to speed up testing.
   private static final TimeDuration serviceInterval = TimeDuration.valueOf(100,
       TimeUnit.MILLISECONDS);
+  // High expiration time used keys without modified creation time will not
+  // expire during the test.
+  private static final TimeDuration expireThreshold = TimeDuration.valueOf(24,
+      TimeUnit.HOURS);
   // Maximum number of keys to be cleaned up per run of the service.
   private static final int taskLimit = 10;
 
   private OMMetadataManager metadataManager;
   private KeyManager keyManager;
   private OzoneConfiguration conf;
-  private TimeDuration expireThreshold;
   private OpenKeyCleanupService service;
 
   @Before
   public void setup() throws Exception {
     conf = new OzoneConfiguration();
     conf.set(OMConfigKeys.OZONE_OM_DB_DIRS, folder.getRoot().getAbsolutePath());
+
+    // Set custom configurations for testing.
     conf.setTimeDuration(OMConfigKeys.OZONE_OPEN_KEY_CLEANUP_SERVICE_INTERVAL,
         serviceInterval.getDuration(), serviceInterval.getUnit());
-
-    long expireDuration = conf.getTimeDuration(
-        OMConfigKeys.OZONE_OPEN_KEY_EXPIRE_THRESHOLD,
-        OMConfigKeys.OZONE_OPEN_KEY_EXPIRE_THRESHOLD_DEFAULT.getDuration(),
-        TimeUnit.MILLISECONDS);
-
-    expireThreshold =
-        TimeDuration.valueOf(expireDuration, TimeUnit.MILLISECONDS);
+    conf.setTimeDuration(OMConfigKeys.OZONE_OPEN_KEY_EXPIRE_THRESHOLD,
+        expireThreshold.getDuration(), expireThreshold.getUnit());
 
     metadataManager = new OmMetadataManagerImpl(conf);
 
@@ -164,17 +171,6 @@ public class TestOpenKeyCleanupService {
     return keyNames;
   }
 
-  private void runServiceUntilKeysDeleted(int numKeys) throws Exception {
-    int serviceIntervalMillis =
-        serviceInterval.toIntExact(TimeUnit.MILLISECONDS);
-
-    GenericTestUtils.waitFor(
-        () -> service.getSubmittedOpenKeyCount().get() >= numKeys,
-        serviceIntervalMillis, serviceIntervalMillis * 10);
-
-    Assert.assertTrue(service.getRunCount().get() > 1);
-  }
-
   private void runService() throws Exception {
     runService(1);
   }
@@ -188,23 +184,56 @@ public class TestOpenKeyCleanupService {
         serviceIntervalMillis, serviceIntervalMillis * 10);
   }
 
-  private void createExpiredOpenKeys(int numKeys) {
+  private void createExpiredOpenKeys(int numKeys) throws Exception {
     createOpenKeys(numKeys, 0, true);
   }
 
-  private void createExpiredOpenKeys(int numKeys, int numBlocks) {
+  private void createExpiredOpenKeys(int numKeys, int numBlocks)
+      throws Exception {
     createOpenKeys(numKeys, numBlocks, true);
   }
 
-  private void createOpenKeys(int numKeys) {
+  private void createOpenKeys(int numKeys) throws Exception {
     createOpenKeys(numKeys, 0, false);
   }
 
-  private void createOpenKeys(int numKeys, int numBlocks) {
-    createOpenKeys(numKeys, numBlocks, false);
-  }
+  private void createOpenKeys(int numKeys, int numBlocks, boolean expired)
+      throws Exception {
 
-  private void createOpenKeys(int numKeys, int numBlocks, boolean expired) {
+    long creationTime = Instant.now().toEpochMilli();
 
+    // Simulate expired keys by creating them with age twice that of the
+    // expiration age.
+    if (expired) {
+      long ageMillis =
+          expireThreshold.add(expireThreshold).toLong(TimeUnit.MILLISECONDS);
+      creationTime -= ageMillis;
+    }
+
+    for (int i = 0; i < numKeys; i++) {
+      String volume = UUID.randomUUID().toString();
+      String bucket = UUID.randomUUID().toString();
+      String key = UUID.randomUUID().toString();
+      long clientID = new Random().nextLong();
+
+      TestOMRequestUtils.addVolumeToDB(volume, metadataManager);
+      TestOMRequestUtils.addBucketToDB(volume, bucket, metadataManager);
+
+      OmKeyInfo keyInfo = TestOMRequestUtils.createOmKeyInfo(volume,
+          bucket, key, HddsProtos.ReplicationType.RATIS,
+          HddsProtos.ReplicationFactor.ONE, 0L, creationTime);
+
+      if (numBlocks > 0) {
+        TestOMRequestUtils.addKeyLocationInfo(keyInfo, 0, numBlocks);
+      }
+
+      TestOMRequestUtils.addKeyToTable(true, false,
+          keyInfo, clientID, 0L, metadataManager);
+
+      // For returning created keys.
+//      String groupID = metadataManager.getOpenKey(volumeName, bucketName,
+//          keyInfo.getKeyName(), clientID);
+//      expiredKeys.add(groupID);
+    }
   }
 }
