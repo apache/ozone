e432d7d68285ea989264a2c9013753b92c05b83f
Elek Márton <elek@apache.org>
2020-11-24 14:00:51 +0100

    fix tests + checkstyle

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/ozoneimpl/OzoneContainer.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/ozoneimpl/OzoneContainer.java
@@ -146,7 +146,7 @@ public class OzoneContainer {
     writeChannel = XceiverServerRatis.newXceiverServerRatis(
         datanodeDetails, config, hddsDispatcher, controller, certClient,
         context);
-    ;
+
     replicationServer = new ReplicationServer(
         controller,
         conf.getObject(ReplicationConfig.class),
--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/replication/ReplicationServer.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/replication/ReplicationServer.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.ozone.container.replication;
 
 import java.io.IOException;
@@ -102,7 +119,7 @@ public class ReplicationServer {
   @ConfigGroup(prefix = "datanode.replication")
   public static final class ReplicationConfig {
 
-    @Config(key = "port", defaultValue = "1111", description = "Port used for"
+    @Config(key = "port", defaultValue = "9886", description = "Port used for"
         + " the server2server replication server", tags = {
         ConfigTag.MANAGEMENT})
     private int port;
@@ -111,8 +128,9 @@ public class ReplicationServer {
       return port;
     }
 
-    public void setPort(int port) {
+    public ReplicationConfig setPort(int port) {
       this.port = port;
+      return this;
     }
   }
 
