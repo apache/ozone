ddda6ec8707938f1b01457e10b7d807db5ed35cd
Siyao Meng <siyao@apache.org>
2020-12-02 23:07:31 -0800

    Clean up ContainerReplicaWithTimestamp.
    Change-Id: Ia68a91ee2d3fb815b880a75822c34028547ffc46

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/scm/ContainerReplicaWithTimestamp.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/scm/ContainerReplicaWithTimestamp.java
@@ -18,25 +18,13 @@
 
 package org.apache.hadoop.ozone.recon.scm;
 
-import org.apache.hadoop.hdds.protocol.DatanodeDetails;
-import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos;
-import org.apache.hadoop.hdds.scm.container.ContainerID;
 import org.apache.hadoop.hdds.scm.container.ContainerReplica;
-
-import java.util.HashSet;
-import java.util.UUID;
 
 public class ContainerReplicaWithTimestamp {
 
-  // ContainerReplica is final so we are not directly extending it
   private final ContainerReplica containerReplica;
-//  Long firstSeenTime;
+//  private Long firstSeenTime;
   private Long lastSeenTime;
-
-  ContainerReplicaWithTimestamp(ContainerReplica containerReplica) {
-    this.containerReplica = containerReplica;
-    lastSeenTime = System.currentTimeMillis();
-  }
 
   ContainerReplicaWithTimestamp(ContainerReplica containerReplica,
       Long lastSeenTime) {
@@ -54,53 +42,5 @@ public class ContainerReplicaWithTimestamp {
 
   public void setLastSeenTime(Long lastSeenTime) {
     this.lastSeenTime = lastSeenTime;
-  }
-
-  @Override
-  public int hashCode() {
-    return containerReplica.getDatanodeDetails().getUuid().hashCode();
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this) {
-      return true;
-    }
-
-    if (obj instanceof ContainerReplica) {
-      final ContainerReplica replica = (ContainerReplica) obj;
-      return containerReplica.getDatanodeDetails().getUuid().equals(
-          replica.getDatanodeDetails().getUuid());
-    } else if (!(obj instanceof ContainerReplicaWithTimestamp)) {
-      return false;
-    } else {
-      final ContainerReplicaWithTimestamp replicaTS =
-          (ContainerReplicaWithTimestamp) obj;
-      return containerReplica.getDatanodeDetails().getUuid().equals(
-          replicaTS.containerReplica.getDatanodeDetails().getUuid());
-    }
-  }
-
-  public static void main(String[] args) {
-    DatanodeDetails dn1 = DatanodeDetails.newBuilder()
-        .setUuid(UUID.randomUUID())
-        .setHostName("localhost")
-        .setIpAddress("127.0.0.1")
-        .build();
-    ContainerReplica replica1 = ContainerReplica.newBuilder()
-        .setContainerID(new ContainerID(2L))
-        .setContainerState(StorageContainerDatanodeProtocolProtos.
-            ContainerReplicaProto.State.CLOSED)
-        .setDatanodeDetails(dn1)
-        .build();
-    ContainerReplicaWithTimestamp ts1 =
-        new ContainerReplicaWithTimestamp(replica1);
-    ContainerReplicaWithTimestamp ts2 =
-        new ContainerReplicaWithTimestamp(replica1);
-
-//    System.out.println(ts1.equals(ts2));
-    HashSet<ContainerReplicaWithTimestamp> hashSet = new HashSet<>();
-    hashSet.add(ts1);
-    System.out.println(hashSet.contains(ts2));
   }
 }
