375b1692fce238815e2779e8698568e243a9d20f
Huang-Mu Zheng <flirmnave@gmail.com>
2020-12-08 22:24:35 +0800

    HDDS-4444. Remove KeyManagerImpl#refreshPipeline because it is the same as refresh(). (#1593)

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/KeyManagerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/KeyManagerImpl.java
@@ -699,22 +699,12 @@ public class KeyManagerImpl implements KeyManager {
     // Refresh container pipeline info from SCM
     // based on OmKeyArgs.refreshPipeline flag
     // value won't be null as the check is done inside try/catch block.
-    refreshPipeline(value);
+    refresh(value);
 
     if (args.getSortDatanodes()) {
       sortDatanodes(clientAddress, value);
     }
     return value;
-  }
-
-  /**
-   * Refresh pipeline info in OM by asking SCM.
-   * @param value OmKeyInfo
-   */
-  @VisibleForTesting
-  protected void refreshPipeline(OmKeyInfo value) throws IOException {
-    Preconditions.checkNotNull(value, "OMKeyInfo cannot be null");
-    refreshPipeline(Arrays.asList(value));
   }
 
   /**
@@ -1828,7 +1818,7 @@ public class KeyManagerImpl implements KeyManager {
         // refreshPipeline flag check has been removed as part of
         // https://issues.apache.org/jira/browse/HDDS-3658.
         // Please refer this jira for more details.
-        refreshPipeline(fileKeyInfo);
+        refresh(fileKeyInfo);
         if (sortDatanodes) {
           sortDatanodes(clientAddress, fileKeyInfo);
         }
