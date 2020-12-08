2479d124e92509621e49e2a224f98f7a942d0f1a
Siyao Meng <siyao@apache.org>
2020-12-07 17:50:20 -0800

    Checkstyle.
    Change-Id: I1353458f1ac8eea4ac7e0ecbf91ec06043ae9a76

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
@@ -106,10 +106,10 @@ public class TestContainerEndpoint {
   private PipelineID pipelineID;
   private long keyCount = 5L;
 
-  UUID uuid1;
-  UUID uuid2;
-  UUID uuid3;
-  UUID uuid4;
+  private UUID uuid1;
+  private UUID uuid2;
+  private UUID uuid3;
+  private UUID uuid4;
 
   private void initializeInjector() throws Exception {
     reconOMMetadataManager = getTestReconOmMetadataManager(
@@ -427,14 +427,14 @@ public class TestContainerEndpoint {
 
     putContainerInfos(1);
     // Add container history for id 1
-    final UUID uuid1 = newDatanode("host1", "127.0.0.1");
-    final UUID uuid2 = newDatanode("host2", "127.0.0.2");
-    final UUID uuid3 = newDatanode("host3", "127.0.0.3");
-    final UUID uuid4 = newDatanode("host4", "127.0.0.4");
-    containerSchemaManager.upsertContainerHistory(1L, uuid1, 1L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid2, 2L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid3, 3L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid4, 4L);
+    final UUID u1 = newDatanode("host1", "127.0.0.1");
+    final UUID u2 = newDatanode("host2", "127.0.0.2");
+    final UUID u3 = newDatanode("host3", "127.0.0.3");
+    final UUID u4 = newDatanode("host4", "127.0.0.4");
+    containerSchemaManager.upsertContainerHistory(1L, u1, 1L);
+    containerSchemaManager.upsertContainerHistory(1L, u2, 2L);
+    containerSchemaManager.upsertContainerHistory(1L, u3, 3L);
+    containerSchemaManager.upsertContainerHistory(1L, u4, 4L);
 
     response = containerEndpoint.getMissingContainers();
     responseObject = (MissingContainersResponse) response.getEntity();
@@ -659,28 +659,27 @@ public class TestContainerEndpoint {
   @Test
   public void testGetReplicaHistoryForContainer() throws IOException {
     // Add container history for container id 1
-    final UUID uuid1 = newDatanode("host1", "127.0.0.1");
-    final UUID uuid2 = newDatanode("host2", "127.0.0.2");
-    final UUID uuid3 = newDatanode("host3", "127.0.0.3");
-    final UUID uuid4 = newDatanode("host4", "127.0.0.4");
-    containerSchemaManager.upsertContainerHistory(1L, uuid1, 1L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid2, 2L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid3, 3L);
-    containerSchemaManager.upsertContainerHistory(1L, uuid4, 4L);
+    final UUID u1 = newDatanode("host1", "127.0.0.1");
+    final UUID u2 = newDatanode("host2", "127.0.0.2");
+    final UUID u3 = newDatanode("host3", "127.0.0.3");
+    final UUID u4 = newDatanode("host4", "127.0.0.4");
+    containerSchemaManager.upsertContainerHistory(1L, u1, 1L);
+    containerSchemaManager.upsertContainerHistory(1L, u2, 2L);
+    containerSchemaManager.upsertContainerHistory(1L, u3, 3L);
+    containerSchemaManager.upsertContainerHistory(1L, u4, 4L);
 
-    containerSchemaManager.upsertContainerHistory(1L, uuid1, 5L);
+    containerSchemaManager.upsertContainerHistory(1L, u1, 5L);
 
     Response response = containerEndpoint.getReplicaHistoryForContainer(1L);
     List<ContainerHistory> histories =
         (List<ContainerHistory>) response.getEntity();
     Set<String> datanodes = Collections.unmodifiableSet(
         new HashSet<>(Arrays.asList(
-            uuid1.toString(), uuid2.toString(),
-            uuid3.toString(), uuid4.toString())));
+            u1.toString(), u2.toString(), u3.toString(), u4.toString())));
     Assert.assertEquals(4, histories.size());
     histories.forEach(history -> {
       Assert.assertTrue(datanodes.contains(history.getDatanodeUuid()));
-      if (history.getDatanodeUuid().equals(uuid1.toString())) {
+      if (history.getDatanodeUuid().equals(u1.toString())) {
         Assert.assertEquals("host1", history.getDatanodeHost());
         Assert.assertEquals(1L, history.getFirstSeenTime());
         Assert.assertEquals(5L, history.getLastSeenTime());
