ca031fdce6e0b33b5643fd58de23bfed69771b0b
Doroszlai, Attila <adoroszlai@apache.org>
2020-12-15 11:43:52 +0100

    simplify ozone_bootstrap call

--- hadoop-ozone/dist/src/shell/ozone/start-ozone.sh
+++ hadoop-ozone/dist/src/shell/ozone/start-ozone.sh
@@ -44,11 +44,7 @@ if ! declare -F ozone_bootstrap >& /dev/null; then
   exit 1
 fi
 
-if ! ozone_bootstrap; then
-  echo "ERROR: Cannot find ozone-config.sh." 2>&1
-  exit 1
-fi
-
+ozone_bootstrap
 . "${OZONE_LIBEXEC_DIR}/ozone-config.sh"
 
 # get arguments
