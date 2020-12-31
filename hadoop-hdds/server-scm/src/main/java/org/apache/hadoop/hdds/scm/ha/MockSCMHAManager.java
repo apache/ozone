f4d7a23ca557dd9863d2d878bf690c40d6d0cb3e
amaliujia <amaliujia@163.com>
2020-12-30 21:59:18 -0800

    fixup! address comments

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/MockSCMHAManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/MockSCMHAManager.java
@@ -35,7 +35,25 @@
 package org.apache.hadoop.hdds.scm.ha;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.hadoop.hdds.protocol.proto.SCMRatisProtocol.RequestType;
+import org.apache.ratis.protocol.ClientId;
+import org.apache.ratis.protocol.Message;
+import org.apache.ratis.protocol.RaftClientReply;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.protocol.RaftGroupMemberId;
+import org.apache.ratis.protocol.RaftPeerId;
+import org.apache.ratis.protocol.exceptions.NotLeaderException;
+import org.apache.ratis.protocol.exceptions.StateMachineException;
+import org.apache.ratis.server.RaftServer;
 
 // TODO: Move this class to test package after fixing Recon
 /**
@@ -89,5 +107,127 @@ public final class MockSCMHAManager implements SCMHAManager {
   @Override
   public void shutdown() throws IOException {
     ratisServer.stop();
+  }
+
+  /**
+   * Mock RatisServer implementation for testing.
+   */
+  private class MockRatisServer implements SCMRatisServer {
+
+    private Map<RequestType, Object> handlers =
+        new EnumMap<>(RequestType.class);
+
+    private MockSCMHAManager scmhaManager;
+
+    MockRatisServer(MockSCMHAManager scmhaManager) {
+      this.scmhaManager = scmhaManager;
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void registerStateMachineHandler(final RequestType handlerType,
+                                            final Object handler) {
+      handlers.put(handlerType, handler);
+    }
+
+    @Override
+    public SCMRatisResponse submitRequest(final SCMRatisRequest request)
+        throws IOException {
+      final RaftGroupMemberId raftId = RaftGroupMemberId.valueOf(
+          RaftPeerId.valueOf("peer"), RaftGroupId.randomId());
+      RaftClientReply reply;
+      if (scmhaManager != null && scmhaManager.isLeader().isPresent()) {
+        try {
+          final Message result = process(request);
+          reply = RaftClientReply.newBuilder()
+              .setClientId(ClientId.randomId())
+              .setServerId(raftId)
+              .setGroupId(RaftGroupId.emptyGroupId())
+              .setCallId(1L)
+              .setSuccess(true)
+              .setMessage(result)
+              .setException(null)
+              .setLogIndex(1L)
+              .build();
+        } catch (Exception ex) {
+          reply = RaftClientReply.newBuilder()
+              .setClientId(ClientId.randomId())
+              .setServerId(raftId)
+              .setGroupId(RaftGroupId.emptyGroupId())
+              .setCallId(1L)
+              .setSuccess(false)
+              .setMessage(Message.EMPTY)
+              .setException(new StateMachineException(raftId, ex))
+              .setLogIndex(1L)
+              .build();
+        }
+      } else {
+        reply = RaftClientReply.newBuilder()
+            .setClientId(ClientId.randomId())
+            .setServerId(raftId)
+            .setGroupId(RaftGroupId.emptyGroupId())
+            .setCallId(1L)
+            .setSuccess(false)
+            .setMessage(Message.EMPTY)
+            .setException(triggerNotLeaderException())
+            .setLogIndex(1L)
+            .build();
+      }
+      return SCMRatisResponse.decode(reply);
+    }
+
+    private Message process(final SCMRatisRequest request)
+        throws Exception {
+      try {
+        final Object handler = handlers.get(request.getType());
+
+        if (handler == null) {
+          throw new IOException("No handler found for request type " +
+              request.getType());
+        }
+
+        final List<Class<?>> argumentTypes = new ArrayList<>();
+        for(Object args : request.getArguments()) {
+          argumentTypes.add(args.getClass());
+        }
+        final Object result = handler.getClass().getMethod(
+            request.getOperation(), argumentTypes.toArray(new Class<?>[0]))
+            .invoke(handler, request.getArguments());
+
+        return SCMRatisResponse.encode(result);
+      } catch (NoSuchMethodException | SecurityException ex) {
+        throw new InvalidProtocolBufferException(ex.getMessage());
+      } catch (InvocationTargetException e) {
+        final Exception targetEx = (Exception) e.getTargetException();
+        throw targetEx != null ? targetEx : e;
+      }
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public RaftServer.Division getDivision() {
+      return null;
+    }
+
+    @Override
+    public List<String> getRatisRoles() {
+      return Arrays.asList(
+          "180.3.14.5:9865",
+          "180.3.14.21:9865",
+          "180.3.14.145:9865");
+    }
+
+    @Override
+    public NotLeaderException triggerNotLeaderException() {
+      return new NotLeaderException(RaftGroupMemberId.valueOf(
+          RaftPeerId.valueOf("peer"), RaftGroupId.randomId()),
+          null, new ArrayList<>());
+    }
   }
 }
--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/ha/TestReplicationAnnotation.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/ha/TestReplicationAnnotation.java
@@ -16,27 +16,112 @@
  */
 package org.apache.hadoop.hdds.scm.ha;
 
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
 import org.apache.hadoop.hdds.protocol.proto.SCMRatisProtocol.RequestType;
-import org.apache.hadoop.hdds.scm.metadata.Replicate;
+import org.apache.hadoop.hdds.scm.container.ContainerID;
+import org.apache.hadoop.hdds.scm.container.ContainerInfo;
+import org.apache.hadoop.hdds.scm.container.ContainerReplica;
+import org.apache.hadoop.hdds.scm.container.ContainerStateManagerV2;
+import org.apache.hadoop.hdds.scm.pipeline.PipelineID;
+import org.apache.hadoop.ozone.common.statemachine.InvalidStateTransitionException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.NavigableSet;
+import java.util.Set;
 
 /**
  * Tests on {@link org.apache.hadoop.hdds.scm.metadata.Replicate}.
  */
 public class TestReplicationAnnotation {
   private SCMHAInvocationHandler scmhaInvocationHandler;
+  private ContainerStateManagerV2 mockCSM;
 
   @Before
   public void setup() {
     SCMHAManager mock = MockSCMHAManager.getInstance(true);
-    TestReplicationAnnotation testReplicationAnnotation =
-        new TestReplicationAnnotation();
+    mockCSM = new ContainerStateManagerV2() {
+      @Override
+      public boolean contains(HddsProtos.ContainerID containerID) {
+        return false;
+      }
+
+      @Override
+      public Set<ContainerID> getContainerIDs() {
+        return null;
+      }
+
+      @Override
+      public Set<ContainerID> getContainerIDs(
+          HddsProtos.LifeCycleState state) {
+        return null;
+      }
+
+      @Override
+      public ContainerInfo getContainer(HddsProtos.ContainerID id) {
+        return null;
+      }
+
+      @Override
+      public Set<ContainerReplica> getContainerReplicas(
+          HddsProtos.ContainerID id) {
+        return null;
+      }
+
+      @Override
+      public void updateContainerReplica(
+          HddsProtos.ContainerID id, ContainerReplica replica) {
+
+      }
+
+      @Override
+      public void removeContainerReplica(
+          HddsProtos.ContainerID id, ContainerReplica replica) {
+
+      }
+
+      @Override
+      public void addContainer(HddsProtos.ContainerInfoProto containerInfo)
+          throws IOException {
+
+      }
+
+      @Override
+      public void updateContainerState(
+          HddsProtos.ContainerID id, HddsProtos.LifeCycleEvent event)
+          throws IOException, InvalidStateTransitionException {
+
+      }
+
+      @Override
+      public void updateDeleteTransactionId(
+          Map<ContainerID, Long> deleteTransactionMap) throws IOException {
+
+      }
+
+      @Override
+      public ContainerInfo getMatchingContainer(
+          long size, String owner, PipelineID pipelineID,
+          NavigableSet<ContainerID> containerIDs) {
+        return null;
+      }
+
+      @Override
+      public void removeContainer(HddsProtos.ContainerID containerInfo)
+          throws IOException {
+      }
+
+      @Override
+      public void close() throws IOException {
+
+      }
+    };
+
     scmhaInvocationHandler = new SCMHAInvocationHandler(
-        RequestType.CONTAINER,
-        testReplicationAnnotation,
-        mock.getRatisServer());
+        RequestType.CONTAINER, mockCSM, mock.getRatisServer());
   }
 
   @Test
@@ -44,12 +129,15 @@ public class TestReplicationAnnotation {
     // test whether replicatedOperation will hit the Ratis based replication
     // code path in SCMHAInvocationHandler. The invoke() can return means the
     // request is handled properly thus response is successful. Expected
-    // result is null because replicatedOperation returns nothing.
+    // result is null because the funciton returns nothing.
+    Object[] arguments = {HddsProtos.ContainerInfoProto.getDefaultInstance()};
     Assert.assertEquals(null, scmhaInvocationHandler.invoke(new Object(),
-        this.getClass().getMethod("replicatedOperation"), new Object[0]));
+        mockCSM.getClass().getMethod(
+            "addContainer", HddsProtos.ContainerInfoProto.class),
+        arguments));
   }
 
-  @Replicate
-  public void replicatedOperation() {
-  }
+//  @Replicate
+//  public void replicatedOperation() {
+//  }
 }
