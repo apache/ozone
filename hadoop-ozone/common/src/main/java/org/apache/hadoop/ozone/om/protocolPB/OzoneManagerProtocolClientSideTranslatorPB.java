72c215cd07ba90fcfcf0e8aecbfe41d99a11f2a9
Ethan Rose <33912936+errose28@users.noreply.github.com>
2021-01-08 06:17:21 -0500

    HDDS-4612. Create OMCancelPrepareRequest and Response to cancel the prepared state of an OM. (#1765)

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocolPB/OzoneManagerProtocolClientSideTranslatorPB.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/protocolPB/OzoneManagerProtocolClientSideTranslatorPB.java
@@ -1587,6 +1587,17 @@ public final class OzoneManagerProtocolClientSideTranslatorPB
     return prepareStatusResponse;
   }
 
+  @Override
+  public CancelPrepareResponse cancelOzoneManagerPrepare() throws IOException {
+    CancelPrepareRequest cancelPrepareRequest =
+        CancelPrepareRequest.newBuilder().build();
+
+    OMRequest omRequest = createOMRequest(Type.CancelPrepare)
+        .setCancelPrepareRequest(cancelPrepareRequest).build();
+
+    return handleError(submitRequest(omRequest)).getCancelPrepareResponse();
+  }
+
   @VisibleForTesting
   public OmTransport getTransport() {
     return transport;
