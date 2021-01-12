5436f13b2ce79ff1641ace596055b801b7e56c91
Siyao Meng <siyao@apache.org>
2021-01-12 10:19:17 -0800

    Checkstyle.
    Change-Id: Ic35fb9f8a37ff615f086ad7a86d0bbec0dc0e7af

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/tasks/TestReconTaskControllerImpl.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/tasks/TestReconTaskControllerImpl.java
@@ -27,7 +27,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
 import java.util.HashSet;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
