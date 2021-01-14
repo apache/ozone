159b0c61c3264c9c3c3e1e6e94ef853e31138557
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2021-01-14 15:36:36 +0100

    HDDS-4556. Add smoketest for ozonescripts environment (#1671)

--- hadoop-ozone/dist/src/main/compose/ozonescripts/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozonescripts/test.sh
@@ -14,4 +14,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-docker-compose exec -T scm /opt/hadoop/sbin/stop-ozone.sh
+
+#suite:misc
+
+COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export COMPOSE_DIR
+
+export SECURITY_ENABLED=false
+export OZONE_REPLICATION_FACTOR=1
+
+# shellcheck source=/dev/null
+source "$COMPOSE_DIR/../testlib.sh"
+
+wait_for_safemode_exit() {
+  wait_for_port scm 22 30
+  wait_for_port om 22 30
+  wait_for_port datanode 22 30
+}
+
+start_docker_env 1
+
+${COMPOSE_DIR}/start.sh
+${COMPOSE_DIR}/ps.sh
+
+execute_robot_test scm admincli/pipeline.robot
+
+${COMPOSE_DIR}/stop.sh
+
+stop_docker_env
+
+generate_report
