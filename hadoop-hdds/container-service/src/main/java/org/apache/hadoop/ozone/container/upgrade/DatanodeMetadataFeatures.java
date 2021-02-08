c9d3a1d3f30ff2b41b57a410b067edcf094f3318
Ethan Rose <errose28@gmail.com>
2021-02-08 14:41:56 -0500

    Fix checkstyle, add docs, and fix typo

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/upgrade/DatanodeMetadataFeatures.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/upgrade/DatanodeMetadataFeatures.java
@@ -3,19 +3,27 @@ package org.apache.hadoop.ozone.container.upgrade;
 import org.apache.hadoop.hdds.upgrade.HDDSLayoutVersionManager;
 import org.apache.hadoop.ozone.OzoneConsts;
 
+/**
+ * Utility class to retrieve the version of a feature that corresponds to the
+ * metadata layout version specified by the provided
+ * {@link HDDSLayoutVersionManager}.
+ */
 public final class DatanodeMetadataFeatures {
   private static HDDSLayoutVersionManager versionManager;
 
-  public static void setHDDSLayoutVersionManager(HDDSLayoutVersionManager manager) {
+  private DatanodeMetadataFeatures() { }
+
+  public static synchronized void setHDDSLayoutVersionManager(
+      HDDSLayoutVersionManager manager) {
     versionManager = manager;
   }
 
-  public static String getSchemaVersion() {
+  public static synchronized String getSchemaVersion() {
     if (versionManager == null ||
         versionManager.getMetadataLayoutVersion() < 1) {
       return OzoneConsts.SCHEMA_V1;
     } else {
-      return OzoneConsts.SCHEMA_V1;
+      return OzoneConsts.SCHEMA_V2;
     }
   }
 }
