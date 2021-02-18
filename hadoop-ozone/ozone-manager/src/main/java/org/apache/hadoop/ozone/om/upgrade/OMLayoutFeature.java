e6ba183a761c6143435a3930cb3fbbd637e12383
Aravindan Vijayan <avijayan@cloudera.com>
2021-02-18 06:53:13 -0800

    Test adding a new layout feature.

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
@@ -27,8 +27,9 @@ import org.apache.hadoop.ozone.upgrade.LayoutFeature;
  */
 public enum OMLayoutFeature implements LayoutFeature {
   //////////////////////////////  //////////////////////////////
-  INITIAL_VERSION(0, "Initial Layout Version");
-
+  INITIAL_VERSION(0, "Initial Layout Version"),
+  ERASURE_CODING(1, "", new ECFeatureOnFinalizeAction()); // Mock Feature. To
+  // be removed later.
   //////////////////////////////  //////////////////////////////
 
   private int layoutVersion;
