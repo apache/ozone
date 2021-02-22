cbd65dc03f9aa0d3580b5367fca77d21d4727d85
Doroszlai, Attila <adoroszlai@apache.org>
2021-02-22 16:54:54 +0100

    remove leftover debug

--- hadoop-ozone/dist/src/main/compose/upgrade/test.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/test.sh
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -x
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 ALL_RESULT_DIR="$SCRIPT_DIR/result"
 mkdir -p "$ALL_RESULT_DIR"
