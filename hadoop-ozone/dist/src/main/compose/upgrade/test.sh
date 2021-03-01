bc791c3079d18d9aa4ce61af6f8d9d955b35b6aa
Ethan Rose <erose@apache.org>
2021-03-01 16:47:03 -0500

    Run all upgrades even if one fails
    Also allows all results to be saved if a CI run fails.

--- hadoop-ozone/dist/src/main/compose/upgrade/test.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/test.sh
@@ -30,9 +30,13 @@ export COMPOSE_DIR="$TEST_DIR"
 RESULT_DIR="$ALL_RESULT_DIR" create_results_dir
 
 # Upgrade tests to be run.
+# Run all upgrades even if one fails.
+# Any failure will save a failing return code to $RESULT.
+set +e
 run_test manual-upgrade 0.5.0 1.1.0
 run_test non-rolling-upgrade 1.0.0 1.1.0
+set -e
 
 generate_report "upgrade" "${ALL_RESULT_DIR}"
 
-exit ${RESULT}
+exit "$RESULT"
