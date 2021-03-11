825da787d028aee21cb5ff2650bb115157f1bb27
Doroszlai, Attila <adoroszlai@apache.org>
2021-03-11 18:13:41 +0100

    HDDS-4969. Extract check dependency installation from Github Actions workflow

--- hadoop-ozone/dev-support/checks/_lib.sh
+++ hadoop-ozone/dev-support/checks/_lib.sh
@@ -14,33 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+check_name="$(basename "${BASH_SOURCE[1]}")"
+check_name="${check_name%.sh}"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${DIR}/../../.." || exit 1
-
-_install_tool_callback() {
-  curl -LSs https://github.com/bats-core/bats-core/archive/v1.2.1.tar.gz | tar xzf -
+install_tool() {
+  if [[ ! -d ${TOOL_DIR} ]]; then
+    mkdir -pv "${TOOL_DIR}"
+    pushd "${TOOL_DIR}"
+    _install_tool_callback
+    popd
+  fi
 }
 
-source "${DIR}/_lib.sh"
-install_tool
-export PATH="${PATH}:${TOOL_DIR}/bats-core-1.2.1/bin"
-
-REPORT_DIR=${OUTPUT_DIR:-"${DIR}/../../../target/bats"}
-mkdir -p "${REPORT_DIR}"
-REPORT_FILE="${REPORT_DIR}/summary.txt"
-
-rm -f "${REPORT_DIR}/output.log"
-
-find * -path '*/src/test/shell/*' -name '*.bats' -print0 \
-  | xargs -0 -n1 bats --formatter tap \
-  | tee -a "${REPORT_DIR}/output.log"
-
-grep '^\(not ok\|#\)' "${REPORT_DIR}/output.log" > "${REPORT_FILE}"
-
-grep -c '^not ok' "${REPORT_FILE}" > "${REPORT_DIR}/failures"
-
-if [[ -s "${REPORT_FILE}" ]]; then
-   exit 1
-fi
+: ${TOOLS_DIR:=$(pwd)/.dev-tools}
+: ${TOOL_DIR:=${TOOLS_DIR}/${check_name}}
--- hadoop-ozone/dev-support/checks/kubernetes.sh
+++ hadoop-ozone/dev-support/checks/kubernetes.sh
@@ -13,8 +13,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+set -x
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../../.." || exit 1
+
+if [[ "$(uname -s)" = "Darwin" ]]; then
+  echo "k3s is not supported on Mac" >&2
+  exit 1
+fi
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+_install_tool_callback() {
+  # Install k3s
+  curl -sfL https://get.k3s.io | sh -
+
+  sudo chmod a+r $KUBECONFIG
+
+  # Install flekszible
+  wget https://github.com/elek/flekszible/releases/download/v1.8.1/flekszible_1.8.1_Linux_x86_64.tar.gz -O - | tar -zx
+  chmod +x flekszible
+  mkdir -p ${TOOL_DIR}/bin
+  mv -iv flekszible ${TOOL_DIR}/bin/
+
+  # Install robotframework
+  sudo pip install robotframework
+}
+
+source "${DIR}/_lib.sh"
+install_tool
+export PATH="${PATH}:${TOOL_DIR}/bin"
 
 REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/kubernetes"}
 
