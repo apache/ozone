c694a0027600a48196ecde65f3c2ca911e44d34e
Bharat Viswanadham <bviswanadham@cloudera.com>
2021-03-12 23:26:11 +0530

    fix ci issue.

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/ha/io/TestX509CertificateCodec.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/ha/io/TestX509CertificateCodec.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.apache.hadoop.hdds.scm.ha.io;
 
 import com.google.protobuf.ByteString;
@@ -39,7 +56,6 @@ public class TestX509CertificateCodec {
     X509CertificateCodec x509CertificateCodec = new X509CertificateCodec();
     ByteString byteString = ByteString.copyFrom("dummy".getBytes(UTF_8));
 
-    X509Certificate actual = (X509Certificate)
-        x509CertificateCodec.deserialize(X509Certificate.class, byteString);
+    x509CertificateCodec.deserialize(X509Certificate.class, byteString);
   }
 }
