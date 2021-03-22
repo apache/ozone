0a97040b6d7a2ee2dfd8bbc61d2dce6052def518
Elek Márton <elek@apache.org>
2021-03-22 12:28:26 +0100

    introduce Java replication config

---
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/client/ReplicationConfig.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hdds.client;
+
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
+
+/**
+ * Replication configuration for any ReplicationType with all the required
+ * parameters..
+ */
+public interface ReplicationConfig {
+
+  /**
+   * Helper method to create proper replication method from old-style
+   * factor+type definition.
+   * <p>
+   * Note: it's never used for EC replication where config is created.
+   */
+  static ReplicationConfig fromTypeAndFactor(
+      HddsProtos.ReplicationType type,
+      HddsProtos.ReplicationFactor factor
+  ) {
+    switch (type) {
+    case RATIS:
+      return new RatisReplicationConfig(factor);
+    case STAND_ALONE:
+      return new StandaloneReplicationConfig(factor);
+    default:
+      throw new UnsupportedOperationException(
+          "Not supported replication: " + type);
+    }
+  }
+
+  /**
+   * Helper method to serialize from proto.
+   * <p>
+   * This uses either the old type/factor or the new ecConfig depends on the
+   * type.
+   */
+  static ReplicationConfig fromProto(
+      HddsProtos.ReplicationType type,
+      HddsProtos.ReplicationFactor factor,
+      HddsProtos.ECReplicationConfig ecConfig) {
+    switch (type) {
+    case EC:
+      return new ECReplicationConfig(ecConfig);
+    case RATIS:
+    case STAND_ALONE:
+      return fromTypeAndFactor(type, factor);
+    default:
+      throw new UnsupportedOperationException(
+          "Not supported replication: " + type);
+    }
+  }
+
+  /**
+   * Replication type supported by the replication config.
+   */
+  HddsProtos.ReplicationType getReplicationType();
+
+}
