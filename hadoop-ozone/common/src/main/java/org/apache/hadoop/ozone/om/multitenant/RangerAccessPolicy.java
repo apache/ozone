c47afd03ea7049ef8c3ade2bc0cf9888194e884e
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2021-03-31 06:42:54 -0700

    Checkstyle: RangerAccessPolicy.
    Change-Id: I7b59d17948c1e43552979ba80b889f1c5faf4dba

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/multitenant/RangerAccessPolicy.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/multitenant/RangerAccessPolicy.java
@@ -17,35 +17,31 @@
  */
 package org.apache.hadoop.ozone.om.multitenant;
 
-import static org.apache.hadoop.ozone.om.multitenant.AccessPolicy.AccessPolicyType.RANGER_POLICY;
-import static org.apache.hadoop.ozone.om.multitenant.OzoneMultiTenantPrincipal.OzonePrincipalType.GROUP_PRINCIPAL;
+import org.apache.hadoop.ozone.security.acl.IAccessAuthorizer.ACLType;
+import org.apache.hadoop.ozone.security.acl.OzoneObj;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.collections.iterators.EntrySetMapIterator;
-import org.apache.hadoop.ozone.security.acl.IAccessAuthorizer.ACLType;
-import org.apache.hadoop.ozone.security.acl.OzoneObj;
-
-import com.google.common.collect.Multiset;
+import static org.apache.hadoop.ozone.om.multitenant.AccessPolicy.AccessPolicyType.RANGER_POLICY;
+import static org.apache.hadoop.ozone.om.multitenant.OzoneMultiTenantPrincipal.OzonePrincipalType.GROUP_PRINCIPAL;
 
 public class RangerAccessPolicy implements AccessPolicy {
 
   // For now RangerAccessPolicy supports only one object per policy
-  OzoneObj accessObject;
-  Map<String, List<AccessPolicyElem>> policyMap;
-  String policyID;
-  String policyJsonString;
-  String policyName;
+  private OzoneObj accessObject;
+  private Map<String, List<AccessPolicyElem>> policyMap;
+  private String policyID;
+  private String policyJsonString;
+  private String policyName;
 
-  public RangerAccessPolicy(String Name) {
+  public RangerAccessPolicy(String name) {
     policyMap = new ConcurrentHashMap<>();
-    policyName = Name;
+    policyName = name;
   }
 
   public void setPolicyID(String id) {
@@ -79,11 +75,11 @@ public class RangerAccessPolicy implements AccessPolicy {
     if (accessObject == null) {
       accessObject = object;
     } else if (!object.toString().equals(accessObject.toString())) {
-      throw new IOException("RangerAccessPolicy supports only one object per" +
-          " policy");
+      throw new IOException(
+          "RangerAccessPolicy supports only one object per" + " policy");
     }
     AccessPolicyElem elem = new AccessPolicyElem(object, principal, acl, grant);
-    if(!policyMap.containsKey(principal.toString())) {
+    if (!policyMap.containsKey(principal.toString())) {
       List<AccessPolicyElem> elemList = new ArrayList<>();
       elemList.add(elem);
       policyMap.put(principal.toString(), elemList);
@@ -92,8 +88,9 @@ public class RangerAccessPolicy implements AccessPolicy {
     List<AccessPolicyElem> elemList = policyMap.get(principal.toString());
     for (AccessPolicyElem e : elemList) {
       if (e.getAclType() == acl) {
-        throw new IOException("RangerAccessPolicy: Principal " +
-            principal.toString() + " already exists with access " + acl);
+        throw new IOException(
+            "RangerAccessPolicy: Principal " + principal.toString()
+                + " already exists with access " + acl);
       }
     }
     elemList.add(elem);
@@ -102,27 +99,25 @@ public class RangerAccessPolicy implements AccessPolicy {
   @Override
   public List<AccessPolicyElem> getAccessPolicyElem() {
     List<AccessPolicyElem> list = new ArrayList<>();
-    for(Map.Entry<String, List<AccessPolicyElem>> entry :
-        policyMap.entrySet()) {
+    for (Map.Entry<String, List<AccessPolicyElem>> entry : policyMap
+        .entrySet()) {
       list.addAll(entry.getValue());
     }
     return list;
   }
 
-  @Override
-  public void removeAccessPolicyElem(OzoneObj object,
-                                     OzoneMultiTenantPrincipal principal,
-                                     ACLType acl, AccessGrantType grant)
+  @Override public void removeAccessPolicyElem(OzoneObj object,
+      OzoneMultiTenantPrincipal principal, ACLType acl, AccessGrantType grant)
       throws IOException {
     if (accessObject == null) {
       throw new IOException("removeAccessPolicyElem: Invalid Arguments.");
     } else if (!object.toString().equals(accessObject.toString())) {
-      throw new IOException("removeAccessPolicyElem:  Object not found." +
-          object.toString());
+      throw new IOException(
+          "removeAccessPolicyElem:  Object not found." + object.toString());
     }
-    if(!policyMap.containsKey(principal.toString())) {
-      throw new IOException("removeAccessPolicyElem:  Principal not found." +
-          object.toString());
+    if (!policyMap.containsKey(principal.toString())) {
+      throw new IOException(
+          "removeAccessPolicyElem:  Principal not found." + object.toString());
     }
     List<AccessPolicyElem> elemList = policyMap.get(principal.toString());
     for (AccessPolicyElem e : elemList) {
@@ -133,10 +128,9 @@ public class RangerAccessPolicy implements AccessPolicy {
     if (elemList.isEmpty()) {
       policyMap.remove(principal.toString());
     }
-    throw new IOException("removeAccessPolicyElem:  aclType not found." +
-        object.toString());
+    throw new IOException(
+        "removeAccessPolicyElem:  aclType not found." + object.toString());
   }
-
 
   private String createRangerResourceItems() throws IOException {
     StringBuilder resourceItems = new StringBuilder();
@@ -151,7 +145,7 @@ public class RangerAccessPolicy implements AccessPolicy {
     if ((accessObject.getResourceType() == OzoneObj.ResourceType.BUCKET) ||
         (accessObject.getResourceType() == OzoneObj.ResourceType.KEY)) {
       resourceItems.append(
-      ",\"bucket\":{" +
+          ",\"bucket\":{" +
           "\"values\":[\"");
       resourceItems.append(accessObject.getBucketName());
       resourceItems.append("\"]," +
@@ -160,9 +154,8 @@ public class RangerAccessPolicy implements AccessPolicy {
           "}");
     }
     if (accessObject.getResourceType() == OzoneObj.ResourceType.KEY) {
-      resourceItems.append(
-          ",\"key\":{" +
-              "\"values\":[\"");
+      resourceItems.append(",\"key\":{" +
+          "\"values\":[\"");
       resourceItems.append(accessObject.getKeyName());
       resourceItems.append("\"]," +
           "\"isRecursive\":true," +
@@ -177,15 +170,16 @@ public class RangerAccessPolicy implements AccessPolicy {
     StringBuilder policyItems = new StringBuilder();
     policyItems.append("\"policyItems\":[");
     int mapRemainingSize = policyMap.size();
-    for (Map.Entry<String,List<AccessPolicyElem>> mapElem :
-        policyMap.entrySet()) {
+    for (Map.Entry<String, List<AccessPolicyElem>> mapElem : policyMap
+        .entrySet()) {
       mapRemainingSize--;
       List<AccessPolicyElem> list = mapElem.getValue();
       if (list.isEmpty()) {
         continue;
       }
       policyItems.append("{");
-      if (list.get(0).getPrincipal().getUserPrincipalType() == GROUP_PRINCIPAL) {
+      if (list.get(0).getPrincipal().getUserPrincipalType()
+          == GROUP_PRINCIPAL) {
         policyItems.append("\"groups\":[\"" + mapElem.getKey() + "\"],");
       } else {
         policyItems.append("\"users\":[\"" + mapElem.getKey() + "\"],");
@@ -220,45 +214,37 @@ public class RangerAccessPolicy implements AccessPolicy {
 
   private String getRangerAclString(ACLType aclType) throws IOException {
     switch (aclType) {
-      case ALL:
-        return "All";
-      case LIST:
-        return "List";
-      case READ:
-        return "Read";
-      case WRITE:
-        return "Write";
-      case CREATE:
-        return "Create";
-      case DELETE:
-        return "Delete";
-      case READ_ACL:
-        return "Read_ACL";
-      case WRITE_ACL:
-        return "Write_ACL";
-      case NONE:
-        return "";
-      default:
-        throw new IOException("Unknown ACLType");
+    case ALL:
+      return "All";
+    case LIST:
+      return "List";
+    case READ:
+      return "Read";
+    case WRITE:
+      return "Write";
+    case CREATE:
+      return "Create";
+    case DELETE:
+      return "Delete";
+    case READ_ACL:
+      return "Read_ACL";
+    case WRITE_ACL:
+      return "Write_ACL";
+    case NONE:
+      return "";
+    default:
+      throw new IOException("Unknown ACLType");
     }
   }
 
-  private void updatePolicyJsonString()
-      throws Exception {
-    policyJsonString = "{\"policyType\":\"0\"," +
-        "\"name\":\"" + policyName +"\"," +
-        "\"isEnabled\":true," +
-        "\"policyPriority\":0," +
-        "\"policyLabels\":[]," +
-        "\"description\":\"\"," +
-        "\"isAuditEnabled\":true," +
-        createRangerResourceItems() +
-        "\"isDenyAllElse\":false," +
-        createRangerPolicyItems() +
-        "\"allowExceptions\":[]," +
-        "\"denyPolicyItems\":[]," +
-        "\"denyExceptions\":[]," +
-        "\"service\":\"cm_ozone\"" +
-        "}";
+  private void updatePolicyJsonString() throws Exception {
+    policyJsonString =
+        "{\"policyType\":\"0\"," + "\"name\":\"" + policyName + "\","
+            + "\"isEnabled\":true," + "\"policyPriority\":0,"
+            + "\"policyLabels\":[]," + "\"description\":\"\","
+            + "\"isAuditEnabled\":true," + createRangerResourceItems()
+            + "\"isDenyAllElse\":false," + createRangerPolicyItems()
+            + "\"allowExceptions\":[]," + "\"denyPolicyItems\":[],"
+            + "\"denyExceptions\":[]," + "\"service\":\"cm_ozone\"" + "}";
   }
 }
