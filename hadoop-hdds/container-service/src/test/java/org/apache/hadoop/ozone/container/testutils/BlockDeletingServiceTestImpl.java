c5ae02f7b110a8bf1a1b51e76412c812dfc29a89
Neil Joshi <neil2021.gres@gmail.com>
2021-04-23 19:35:56 -0600

    Small change to blockDeleting unit tests affecting TestSchemaBackwardsCompatiblity and TestBlockDeletingService.  Current implementation expect background service deleting blocks to run background threads and complete prior to checking expected test results.  The caller does NOT block until worker threads are complete therefore at times unit tests fail as expected results are not present when caller checks for results.  Inserted executer thread pool implementation into test implementation that completes all worker tasks prior to returning control to caller.

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/testutils/BlockDeletingServiceTestImpl.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/testutils/BlockDeletingServiceTestImpl.java
@@ -16,17 +16,28 @@
  */
 package org.apache.hadoop.ozone.container.testutils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.hadoop.hdds.conf.ConfigurationSource;
+import org.apache.hadoop.hdds.scm.container.common.helpers.AllocatedBlock;
+import org.apache.hadoop.hdds.utils.BackgroundTask;
+import org.apache.hadoop.hdds.utils.BackgroundTaskQueue;
+import org.apache.hadoop.hdds.utils.BackgroundTaskResult;
 import org.apache.hadoop.ozone.container.keyvalue.statemachine.background.BlockDeletingService;
 import org.apache.hadoop.ozone.container.ozoneimpl.OzoneContainer;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.apache.ratis.util.TimeDuration;
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A test class implementation for {@link BlockDeletingService}.
@@ -41,6 +52,9 @@ public class BlockDeletingServiceTestImpl
   private CountDownLatch latch;
   private Thread testingThread;
   private AtomicInteger numOfProcessed = new AtomicInteger(0);
+  private static final Logger LOG =
+          LoggerFactory.getLogger(BlockDeletingServiceTestImpl.class);
+
 
   public BlockDeletingServiceTestImpl(OzoneContainer container,
       int serviceInterval, ConfigurationSource conf) {
@@ -66,10 +80,60 @@ public class BlockDeletingServiceTestImpl
     return numOfProcessed.get();
   }
 
+  public class PeriodicalTaskTestImpl extends PeriodicalTask {
+    @Override
+    public synchronized void run() {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Running background service : {}", "BlockDeletingServiceTestImpl");
+      }
+      long serviceTimeoutInNanos = TimeDuration.valueOf(SERVICE_TIMEOUT_IN_MILLISECONDS, TimeUnit.MILLISECONDS)
+              .toLong(TimeUnit.NANOSECONDS);
+      BackgroundTaskQueue tasks = getTasks();
+      if (tasks.isEmpty()) {
+        // No task found, or some problems to init tasks
+        // return and retry in next interval.
+        return;
+      }
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Number of background tasks to execute : {}", tasks.size());
+      }
+      List<CompletableFuture<?> >futureList =
+              new ArrayList<>();
+
+      while (tasks.size() > 0) {
+        BackgroundTask task = tasks.poll();
+        futureList.add(CompletableFuture.runAsync(() -> {
+          long startTime = System.nanoTime();
+          try {
+            BackgroundTaskResult result = task.call();
+            if (LOG.isDebugEnabled()) {
+              LOG.debug("task execution result size {}", result.getSize());
+            }
+          } catch (Exception e) {
+            LOG.warn("Background task execution failed", e);
+          } finally {
+            long endTime = System.nanoTime();
+            if (endTime - startTime > serviceTimeoutInNanos) {
+              LOG.warn("{} Background task execution took {}ns > {}ns(timeout)",
+                      "BlockDeletingServiceTestImpl", endTime - startTime, serviceTimeoutInNanos);
+            }
+          }
+        }, getExecutorService()) );
+      }
+      try {
+        CompletableFuture
+                .allOf(futureList.toArray(new CompletableFuture[futureList.size()]))
+                .get();
+      } catch (Exception e) {
+        Assert.fail("testAllocateBlockInParallel failed");
+      }
+    }
+  }
+
   // Override the implementation to start a single on-call control thread.
   @Override
   public void start() {
-    PeriodicalTask svc = new PeriodicalTask();
+    PeriodicalTask svc = new PeriodicalTaskTestImpl();
     // In test mode, relies on a latch countdown to runDeletingTasks tasks.
     Runnable r = () -> {
       while (true) {
