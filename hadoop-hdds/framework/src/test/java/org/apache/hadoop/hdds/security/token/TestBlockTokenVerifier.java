48628993891a9103bb1cb262a7bdedd407363558
Doroszlai, Attila <adoroszlai@apache.org>
2021-04-26 17:54:10 +0200

    Implement TestBlockTokenVerifier

--- hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/token/TestBlockTokenVerifier.java
+++ hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/token/TestBlockTokenVerifier.java
@@ -17,9 +17,53 @@
  */
 package org.apache.hadoop.hdds.security.token;
 
+import org.apache.hadoop.hdds.client.BlockID;
+import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.ContainerCommandRequestProto;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos.BlockTokenSecretProto.AccessModeProto;
+import org.apache.hadoop.hdds.scm.pipeline.MockPipeline;
+import org.apache.hadoop.hdds.scm.pipeline.Pipeline;
+import org.apache.hadoop.hdds.security.x509.SecurityConfig;
+import org.apache.hadoop.hdds.security.x509.certificate.client.CertificateClient;
+import org.apache.hadoop.ozone.container.ContainerTestHelper;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.EnumSet;
+
+import static org.apache.hadoop.ozone.container.ContainerTestHelper.getWriteChunkRequest;
+
 /**
  * Tests for {@link BlockTokenVerifier}.
  */
-public class TestBlockTokenVerifier {
+public class TestBlockTokenVerifier
+    extends TokenVerifierTests<OzoneBlockTokenIdentifier> {
 
+  @Override
+  protected TokenVerifier newTestSubject(SecurityConfig secConf,
+      CertificateClient caClient) {
+    return new BlockTokenVerifier(secConf, caClient);
+  }
+
+  @Override
+  protected ContainerCommandRequestProto unverifiedRequest()
+      throws IOException {
+    Pipeline pipeline = MockPipeline.createPipeline(1);
+    return ContainerTestHelper.getCloseContainer(pipeline, 1);
+  }
+
+  @Override
+  protected ContainerCommandRequestProto verifiedRequest(
+      OzoneBlockTokenIdentifier tokenId) throws IOException {
+    Pipeline pipeline = MockPipeline.createPipeline(1);
+    return getWriteChunkRequest(pipeline, new BlockID(1, 0), 1024, null);
+  }
+
+  @Override
+  protected OzoneBlockTokenIdentifier newTokenId() {
+    return new OzoneBlockTokenIdentifier("any user",
+        new BlockID(1, 0),
+        EnumSet.allOf(AccessModeProto.class),
+        Instant.now().plusSeconds(3600).toEpochMilli(),
+        CERT_ID, 100);
+  }
 }
