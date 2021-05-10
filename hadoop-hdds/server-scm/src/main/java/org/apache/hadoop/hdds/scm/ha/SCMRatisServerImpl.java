97e50d2406bb64683d2861ae44d3f5a8bd95707d
Bharat Viswanadham <bviswanadham@cloudera.com>
2021-05-10 15:43:20 +0530

    HDDS-5200. Fix scm roles command if one of the host is unresolvable.

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/SCMRatisServerImpl.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/SCMRatisServerImpl.java
@@ -229,10 +229,19 @@ public class SCMRatisServerImpl implements SCMRatisServer {
   public List<String> getRatisRoles() throws IOException {
     Collection<RaftPeer> peers = division.getGroup().getPeers();
     List<String> ratisRoles = new ArrayList<>();
+    InetAddress peerInetAddress = null;
     for (RaftPeer peer : peers) {
-      InetAddress peerInetAddress = InetAddress.getByName(
-              HddsUtils.getHostName(peer.getAddress()).get());
-      boolean isLocal = NetUtils.isLocalAddress(peerInetAddress);
+      try {
+       peerInetAddress = InetAddress.getByName(
+            HddsUtils.getHostName(peer.getAddress()).get());
+      } catch (IOException ex) {
+        LOG.error("SCM Ratis PeerInetAddress {} is unresolvable",
+            peer.getAddress());
+      }
+      boolean isLocal = false;
+      if (peerInetAddress != null) {
+        isLocal = NetUtils.isLocalAddress(peerInetAddress);
+      }
       ratisRoles.add((peer.getAddress() == null ? "" :
               peer.getAddress().concat(isLocal ?
                       ":".concat(RaftProtos.RaftPeerRole.LEADER
