974136d78231dce54f34057add7d79d2f2e9de01
Rakesh Radhakrishnan <rakeshr@apache.org>
2021-05-14 12:32:22 +0530

    HDDS-4923. [FSO]S3Multipart: Run S3 acceptance test with prefix layout (#2208)

--- hadoop-ozone/dist/src/main/compose/ozone/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozone/test.sh
@@ -58,12 +58,18 @@ export OZONE_KEEP_RESULTS=true
 stop_docker_env
 
 ## Restarting the cluster with prefix-layout enabled (FSO)
-OZONE_OM_METADATA_LAYOUT=PREFIX
-OZONE_OM_ENABLE_FILESYSTEM_PATHS=true
+export OZONE_OM_METADATA_LAYOUT=PREFIX
+export OZONE_OM_ENABLE_FILESYSTEM_PATHS=true
 start_docker_env
 
 execute_robot_test scm -v SCHEME:ofs -v BUCKET_TYPE:link -N ozonefs-prefix-ofs-link ozonefs/ozonefs.robot
 execute_robot_test scm -v SCHEME:o3fs -v BUCKET_TYPE:bucket -N ozonefs-prefix-o3fs-bucket ozonefs/ozonefs.robot
+
+execute_robot_test scm -v BUCKET:${bucket} -N s3-${bucket}-prefix-layout-objectputget s3/objectputget.robot
+execute_robot_test scm -v BUCKET:${bucket} -N s3-${bucket}-prefix-layout-objectdelete s3/objectdelete.robot
+execute_robot_test scm -v BUCKET:${bucket} -N s3-${bucket}-prefix-layout-objectcopy s3/objectcopy.robot
+execute_robot_test scm -v BUCKET:${bucket} -N s3-${bucket}-prefix-layout-objectmultidelete s3/objectmultidelete.robot
+execute_robot_test scm -v BUCKET:${bucket} -N s3-${bucket}-prefix-layout-MultipartUpload s3/MultipartUpload.robot
 
 stop_docker_env
 
