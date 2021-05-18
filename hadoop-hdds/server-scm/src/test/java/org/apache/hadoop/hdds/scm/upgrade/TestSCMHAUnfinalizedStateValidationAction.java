91a101f92137eb7699b5b54aaf08e0309bf9c292
Ethan Rose <erose@apache.org>
2021-05-18 18:34:15 -0400

    Update unit tests to not test init after upgrade

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/upgrade/TestSCMHAUnfinalizedStateValidationAction.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/upgrade/TestSCMHAUnfinalizedStateValidationAction.java
@@ -23,7 +23,6 @@ import org.apache.hadoop.hdds.scm.ScmConfigKeys;
 import org.apache.hadoop.hdds.scm.TestUtils;
 import org.apache.hadoop.hdds.scm.server.StorageContainerManager;
 import org.apache.hadoop.hdds.upgrade.HDDSLayoutFeature;
-import org.apache.hadoop.hdds.utils.TransactionInfo;
 import org.apache.hadoop.ozone.OzoneConfigKeys;
 import org.apache.hadoop.ozone.upgrade.UpgradeException;
 import org.apache.hadoop.test.LambdaTestUtils;
@@ -39,31 +38,24 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-import static org.apache.hadoop.ozone.OzoneConsts.TRANSACTION_INFO_KEY;
-
 @RunWith(Parameterized.class)
 public class TestSCMHAUnfinalizedStateValidationAction {
-
   @Rule
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-  private final boolean initAfterUpgrade;
   private final boolean haEnabledBefore;
-  private final boolean haEnabledAfter;
+  private final boolean haEnabledPreFinalized;
   private final boolean shouldFail;
   private final String dataPath;
 
-  @Parameterized.Parameters(name = "habefore={0} haafter={1} init={2}")
+  @Parameterized.Parameters(name = "haEnabledBefore={0} " +
+      "haEnabledPreFinalized={1}")
   public static Collection<Object[]> cases() {
     List<Object[]> params = new ArrayList<>();
 
-
     for (boolean haBefore: Arrays.asList(true, false)) {
       for (boolean haAfter: Arrays.asList(true, false)) {
-        for (boolean initAfter: Arrays.asList(true, false)) {
-          params.add(new Object[]{haBefore, haAfter, initAfter});
-
-        }
+        params.add(new Object[]{haBefore, haAfter});
       }
     }
 
@@ -71,34 +63,21 @@ public class TestSCMHAUnfinalizedStateValidationAction {
   }
 
   public TestSCMHAUnfinalizedStateValidationAction(
-      boolean haEnabledBefore, boolean haEnabledAfter, boolean initAfterUpgrade)
+      boolean haEnabledBefore, boolean haEnabledPreFinalized)
       throws Exception {
-    this.initAfterUpgrade = initAfterUpgrade;
     this.haEnabledBefore = haEnabledBefore;
-    this.haEnabledAfter = haEnabledAfter;
+    this.haEnabledPreFinalized = haEnabledPreFinalized;
 
-    shouldFail = !this.haEnabledBefore && this.haEnabledAfter;
+    shouldFail = !this.haEnabledBefore && this.haEnabledPreFinalized;
 
     temporaryFolder.create();
     dataPath = temporaryFolder.newFolder().getAbsolutePath();
   }
 
-  /*
-  ha/no upgrade-pre-f+ha/no noinit+start/init+start(if init passes)
-  haEnabledBefore, haEnabledAfter, initAfterUpgrade
-
-  Do not need to test when action runs, only conditions around it running.
-  Do not need to test fresh install, as other tests do that.
-  fresh init ha/no start
-   */
-
   @Test
   public void testUpgrade() throws Exception {
     // Write version file for original version.
     OzoneConfiguration conf = new OzoneConfiguration();
-    // Write the lowest layout version to the file, since clusters using
-    // scm ha before upgrades was introduced will not have a version
-    // associated with it.
     conf.setInt(ScmConfig.ConfigStrings.HDDS_SCM_INIT_DEFAULT_LAYOUT_VERSION,
         HDDSLayoutFeature.INITIAL_VERSION.layoutVersion());
     conf.setBoolean(ScmConfigKeys.OZONE_SCM_HA_ENABLE_KEY, haEnabledBefore);
@@ -106,23 +85,12 @@ public class TestSCMHAUnfinalizedStateValidationAction {
     conf.set(OzoneConfigKeys.OZONE_METADATA_DIRS, dataPath);
     StorageContainerManager.scmInit(conf, UUID.randomUUID().toString());
 
-
-    conf.setBoolean(ScmConfigKeys.OZONE_SCM_HA_ENABLE_KEY, haEnabledAfter);
-
-    if (initAfterUpgrade) {
-      if (shouldFail) {
-        LambdaTestUtils.intercept(UpgradeException.class,
-            () -> StorageContainerManager.scmInit(conf, ""));
-      } else {
-        StorageContainerManager.scmInit(conf, UUID.randomUUID().toString());
-      }
-    }
-
+    // Set up new pre-finalized SCM.
+    conf.setBoolean(ScmConfigKeys.OZONE_SCM_HA_ENABLE_KEY, haEnabledPreFinalized);
     StorageContainerManager scm = TestUtils.getScm(conf);
 
     if (shouldFail) {
-      LambdaTestUtils.intercept(UpgradeException.class,
-          scm::start);
+      LambdaTestUtils.intercept(UpgradeException.class, scm::start);
     } else {
       scm.start();
       scm.stop();
