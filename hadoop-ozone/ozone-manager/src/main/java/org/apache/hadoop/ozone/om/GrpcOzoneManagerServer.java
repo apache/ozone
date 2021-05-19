95ef9232d41514753ade69dd0ac960c6318f5a49
Neil Joshi <neil2021.gres@gmail.com>
2021-05-19 00:07:44 -0600

    Added changes to GrpcOzoneManagerServer to satisfy creating ratis client requests - fixes reported limitation on disabled om ratis configuration.  S3g gRPC NOW functioning with om ratis.  Added maven pom file dependency to s3gateway to fix problem with runtime not finding grpc netty ChannelFactory when run in docker cluster mode.  Minor cleanup to maven pom files.

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/GrpcOzoneManagerServer.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/GrpcOzoneManagerServer.java
@@ -19,6 +19,7 @@ package org.apache.hadoop.ozone.om;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.protobuf.RpcController;
 import org.apache.hadoop.hdds.conf.Config;
@@ -28,6 +29,9 @@ import org.apache.hadoop.ozone.OzoneConsts;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerServiceGrpc.OzoneManagerServiceImplBase;
 import org.apache.hadoop.ozone.protocolPB.OzoneManagerProtocolServerSideTranslatorPB;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.OMResponse;
+import org.apache.hadoop.ipc.Server.Call;
+import org.apache.hadoop.ipc.RPC;
+import org.apache.hadoop.ipc.ClientId;
 import io.grpc.Server;
 import io.grpc.netty.NettyServerBuilder;
 import com.google.protobuf.ServiceException;
@@ -58,8 +62,16 @@ class OzoneManagerServiceGrpc extends OzoneManagerServiceImplBase {
                                 responseObserver) {
     LOG.debug("GrpcOzoneManagerServer: OzoneManagerServiceImplBase " +
         "processing s3g client submit request");
+    AtomicInteger callCount = new AtomicInteger(0);
     try {
         // need to look into handling the error path, trapping exception
+      org.apache.hadoop.ipc.Server.getCurCall().set(new Call(1,
+          callCount.incrementAndGet(),
+          null,
+          null,
+          RPC.RpcKind.RPC_PROTOCOL_BUFFER,
+          ClientId.getClientId()));
+
       OMResponse omResponse = this.omTranslator.
           submitRequest(NULL_RPC_CONTROLLER, request);
       responseObserver.onNext(omResponse);
