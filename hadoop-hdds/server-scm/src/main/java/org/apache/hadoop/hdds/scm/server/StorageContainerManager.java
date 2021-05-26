fe201571f496453c3472dcafa8e88eb7bb1ca7b8
Sadanand Shenoy <sadanand.shenoy4898@gmail.com>
2021-05-26 22:02:45 +0530

    HDDS-5241. SCM UI should have leader/follower and Primordial SCM information (#2260)
    (cherry picked from commit d9ae1b3b03b2ae322dac56031087fc0c130c0de1)
    Change-Id: Ifb75e2c1062edce2934618697bd23bbd13b4b510

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/StorageContainerManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/StorageContainerManager.java
@@ -139,6 +139,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -1744,6 +1745,35 @@ public final class StorageContainerManager extends ServiceRuntimeInfoImpl
     return containerTokenMgr != null
         ? containerTokenMgr
         : ContainerTokenGenerator.DISABLED;
+  }
+
+  @Override
+  public String getScmRatisRoles() throws IOException {
+    return HddsUtils.format(getScmHAManager().getRatisServer().getRatisRoles());
+  }
+
+  /**
+   * @return hostname of primordialNode
+   */
+  @Override
+  public String getPrimordialNode() {
+    if (SCMHAUtils.isSCMHAEnabled(configuration)) {
+      String primordialNode = SCMHAUtils.getPrimordialSCM(configuration);
+      // primordialNode can be nodeId too . If it is then return hostname.
+      if (SCMHAUtils.getSCMNodeIds(configuration).contains(primordialNode)) {
+        List<SCMNodeDetails> localAndPeerNodes =
+            new ArrayList<>(scmHANodeDetails.getPeerNodeDetails());
+        localAndPeerNodes.add(getSCMHANodeDetails().getLocalNodeDetails());
+        for (SCMNodeDetails nodes : localAndPeerNodes) {
+          if (nodes.getNodeId().equals(primordialNode)) {
+            return nodes.getHostName();
+          }
+        }
+
+      }
+      return primordialNode;
+    }
+    return null;
   }
 
 }
