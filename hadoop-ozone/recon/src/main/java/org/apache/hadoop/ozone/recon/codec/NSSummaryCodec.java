49223153635748239343f24c9cbbce63aa861791
Yuan Gu <53324985+yuangu002@users.noreply.github.com>
2021-06-25 18:47:13 -0400

    assert fix

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/codec/NSSummaryCodec.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/codec/NSSummaryCodec.java
@@ -61,7 +61,7 @@ public class NSSummaryCodec implements Codec<NSSummary> {
     res.setNumOfFiles(in.readInt());
     res.setSizeOfFiles(in.readInt());
     short len = in.readShort();
-    assert(len == (short) NUM_OF_BINS);
+    assert(len == (short) ReconConstants.NUM_OF_BINS);
     int[] fileSizeBucket = new int[len];
     for (int i = 0; i < len; ++i) {
       fileSizeBucket[i] = in.readInt();
