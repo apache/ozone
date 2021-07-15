faf39b6b2e37e80f8c41c0826c9ef73f8351f958
lihanran <lihanran@jd.com>
2021-07-15 16:35:59 +0800

    HDDS-5445 Wrong formatting of comments

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/fsck/package-info.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/fsck/package-info.java
@@ -19,7 +19,7 @@
  * fsck tool.
  */
 
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
