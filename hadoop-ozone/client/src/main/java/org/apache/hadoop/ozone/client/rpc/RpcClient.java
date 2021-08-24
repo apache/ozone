b1eaf8f5e27a4fd1bb4e3d7cf98a09f9ae5d4a2d
Bharat Viswanadham <bharat@apache.org>
2021-08-24 12:02:36 +0530

    HDDS-5393. Return latest version of key location for client on createKey/createFile (#2432)

--- hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/rpc/RpcClient.java
+++ hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/rpc/RpcClient.java
@@ -780,7 +780,8 @@ public class RpcClient implements ClientProtocol {
         .setDataSize(size)
         .setReplicationConfig(replicationConfig)
         .addAllMetadata(metadata)
-        .setAcls(getAclList());
+        .setAcls(getAclList())
+        .setLatestVersionLocation(getLatestVersionLocation);
 
     if (Boolean.parseBoolean(metadata.get(OzoneConsts.GDPR_FLAG))) {
       try{
@@ -1253,6 +1254,7 @@ public class RpcClient implements ClientProtocol {
         .setDataSize(size)
         .setReplicationConfig(replicationConfig)
         .setAcls(getAclList())
+        .setLatestVersionLocation(getLatestVersionLocation)
         .build();
     OpenKeySession keySession =
         ozoneManagerClient.createFile(keyArgs, overWrite, recursive);
