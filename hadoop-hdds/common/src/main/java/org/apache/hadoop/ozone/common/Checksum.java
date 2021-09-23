cc1daa9c00aefc1c9384c817ab5b987d519437ef
micah zhao <micahzhao@tencent.com>
2021-09-23 11:26:43 +0800

    HDDS-5742. Avoid unnecessary Bytebuffer conversions (#2673)

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/common/Checksum.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/common/Checksum.java
@@ -139,6 +139,11 @@ public class Checksum {
    */
   public ChecksumData computeChecksum(ByteBuffer data)
       throws OzoneChecksumException {
+    // If type is set to NONE, we do not need to compute the checksums. We also
+    // need to avoid unnecessary conversions.
+    if (checksumType == ChecksumType.NONE) {
+      return new ChecksumData(checksumType, bytesPerChecksum);
+    }
     if (!data.isReadOnly()) {
       data = data.asReadOnlyBuffer();
     }
