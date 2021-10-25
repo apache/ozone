14c5b5707293a50f56af4b3785f05854f56a79dc
Aswin Shakil Balasubramanian <aswinshakilbalu@gmail.com>
2021-10-25 16:41:04 -0700

    HDDS-5843. SCM terminates when adding container to a pipeline during startup (#2745)

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/pipeline/MockPipelineManager.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/pipeline/MockPipelineManager.java
@@ -133,6 +133,12 @@ public final class MockPipelineManager implements PipelineManager {
   }
 
   @Override
+  public void addContainerToPipelineSCMStart(
+          PipelineID pipelineID, ContainerID containerID) throws IOException {
+    stateManager.addContainerToPipelineSCMStart(pipelineID, containerID);
+  }
+
+  @Override
   public void removeContainerFromPipeline(final PipelineID pipelineID,
                                           final ContainerID containerID)
       throws IOException {
