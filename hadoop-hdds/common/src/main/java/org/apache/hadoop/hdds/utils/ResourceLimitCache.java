ba02f4a429b26605bb7158e7cd7ce7a3363fe208
Doroszlai, Attila <adoroszlai@apache.org>
2021-12-08 07:58:28 +0100

    Remove ArrayList

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/ResourceLimitCache.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/ResourceLimitCache.java
@@ -17,7 +17,6 @@
  */
 package org.apache.hadoop.hdds.utils;
 
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
@@ -79,7 +78,7 @@ public class ResourceLimitCache<K, V> implements Cache<K, V> {
   @Override
   public void removeIf(Predicate<K> predicate) {
     Objects.requireNonNull(predicate);
-    for (K key : new ArrayList<>(map.keySet())) {
+    for (K key : map.keySet()) {
       if (predicate.test(key)) {
         remove(key);
       }
