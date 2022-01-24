93d21981e8609b76625889118d992c4702d50ad1
Stephen O'Donnell <stephen.odonnell@gmail.com>
2022-01-24 22:11:30 +0000

    Update hadoop-hdds/common/src/test/java/org/apache/hadoop/hdds/scm/container/package-info.java
    Co-authored-by: Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>

--- hadoop-hdds/common/src/test/java/org/apache/hadoop/hdds/scm/container/package-info.java
+++ hadoop-hdds/common/src/test/java/org/apache/hadoop/hdds/scm/container/package-info.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hdds.scm.container;
 /**
  Test cases for SCM container client classes.
  */
+package org.apache.hadoop.hdds.scm.container;
