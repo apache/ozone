3c277f33fe7247c91abf18bb43a8f83fd6f50a2c
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2022-02-09 02:15:10 -0800

    Revert regex for column. Doesn't work on all checkstyle warnings (works on whitespacearound though)
    Change-Id: Ic5529bc97a7fce75b72b51701fca7e3c4203613c

--- hadoop-ozone/dev-support/checks/checkstyle.sh
+++ hadoop-ozone/dev-support/checks/checkstyle.sh
@@ -42,7 +42,7 @@ find "." -name checkstyle-errors.xml -print0 \
       -e '/<checkstyle.*/d' \
       -e '/<\/.*/d' \
       -e 's/<file name="\([^"]*\)".*/\1/' \
-      -e 's/<error.*line="\([[:digit:]]*\)".*column="\([[:digit:]]*\)".*message="\([^"]*\)".*/ \1,\2: \3/' \
+      -e 's/<error.*line="\([[:digit:]]*\)".*message="\([^"]*\)".*/ \1: \2/' \
       -e "s!^${BASE_DIR}/!!" \
       -e "s/&apos;/'/g" \
       -e "s/&lt;/</g" \
