d70d9fc1e704780746a446e52089a892905cbf45
Jackson Yao <jacksonyao@tencent.com>
2022-02-10 10:47:35 +0800

    fix stop

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/ratis/OzoneManagerStateMachine.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/ratis/OzoneManagerStateMachine.java
@@ -470,7 +470,7 @@ public class OzoneManagerStateMachine extends BaseStateMachine {
     LOG.info("StateMachine has shutdown. Shutdown OzoneManager if not " +
         "already shutdown.");
     super.close();
-    ozoneManager.stop();
+    stop();
     try{
       ExitUtils.terminate(1,
           "OM state machine is shutdown by Ratis server", LOG);
