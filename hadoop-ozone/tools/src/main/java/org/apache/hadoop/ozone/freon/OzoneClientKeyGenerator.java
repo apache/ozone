b6f7c4d7aa43d03927f4a217ce01bc8070614c23
Kaijie Chen <chen@kaijie.org>
2022-02-18 19:43:57 +0800

    Update help strings for replication config

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/OzoneClientKeyGenerator.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/OzoneClientKeyGenerator.java
@@ -85,17 +85,18 @@ public class OzoneClientKeyGenerator extends BaseFreonGenerator
   private String omServiceID;
 
   @Option(names = {"--replication"},
-      description =
-          "Replication configuration of the new key."
-              + "(ONE, THREE) for RATIS or STAND_ALONE, "
-              + "(rs-3-2-1024k, rs-6-3-1024k or rs-10-4-1024k) for EC."
+      description = "Replication level of the new keys. Example: THREE"
+          + " (for RATIS) or ONE (for STAND_ALONE). In case of EC, pass"
+          + " CODEC-DATA-PARITY-CHUNKSIZE, eg rs-3-2-1024k, rs-6-3-1024k,"
+          + " rs-10-4-1024k. Defaults to bucket or cluster config"
   )
   private String replication;
 
   @Option(names = {"--type"},
-      description = "Replication type of the new key. (RATIS, STAND_ALONE, EC)"
+      description = "Replication type of the new key. Supported types are"
+          + " RATIS, STANDALONE, EC. Defaults to bucket or cluster config"
   )
-  private ReplicationType replicationType;
+  private ReplicationType type;
 
   private Timer timer;
 
@@ -119,7 +120,7 @@ public class OzoneClientKeyGenerator extends BaseFreonGenerator
           .fromTypeAndFactor(ReplicationType.RATIS, factor);
     } else {
       replicationConfig = OzoneClientUtils
-          .validateAndGetClientReplicationConfig(replicationType, replication,
+          .validateAndGetClientReplicationConfig(type, replication,
               ozoneConfiguration);
     }
 
