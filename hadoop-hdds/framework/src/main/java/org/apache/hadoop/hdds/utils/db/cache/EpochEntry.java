1f2b874e7399b158eeb292b56bd43db6ee32eec0
Xushaohong <axcsd3692@qq.com>
2022-02-22 17:06:52 +0800

    fix checkstyle

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/cache/EpochEntry.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/cache/EpochEntry.java
@@ -64,7 +64,8 @@ public class EpochEntry<CACHEKEY> implements Comparable<CACHEKEY> {
   @Override
   public int compareTo(Object o) {
     if (this.epoch == ((EpochEntry<?>)o).epoch) {
-      return Integer.compare(this.cachekey.hashCode(), ((EpochEntry<?>) o).cachekey.hashCode());
+      return Integer.compare(
+          this.cachekey.hashCode(), ((EpochEntry<?>) o).cachekey.hashCode());
     } else if (this.epoch < ((EpochEntry<?>)o).epoch) {
       return -1;
     } else {
