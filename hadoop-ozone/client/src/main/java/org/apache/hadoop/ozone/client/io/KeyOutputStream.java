cc22755b6674bea21b302ce8b7176d5017f56f5b
Gui Hecheng <markgui@tencent.com>
2022-03-01 01:23:09 +0800

    HDDS-6373. EC: Exclude pipeline upon container close instead of exclude DNs. (#3137)

--- hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/KeyOutputStream.java
+++ hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/KeyOutputStream.java
@@ -431,7 +431,7 @@ public class KeyOutputStream extends OutputStream {
 
   // Every container specific exception from datatnode will be seen as
   // StorageContainerException
-  boolean checkIfContainerToExclude(Throwable t) {
+  protected boolean checkIfContainerToExclude(Throwable t) {
     return t instanceof StorageContainerException;
   }
 
