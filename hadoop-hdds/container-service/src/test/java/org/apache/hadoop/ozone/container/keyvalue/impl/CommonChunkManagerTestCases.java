dc9f3b87446ce0a4983a6dd686fe03a95cd30393
UENISHI Kota <kuenishi@users.noreply.github.com>
2022-03-02 23:36:32 +0900

    Update hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/keyvalue/impl/CommonChunkManagerTestCases.java
    Co-authored-by: Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/keyvalue/impl/CommonChunkManagerTestCases.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/keyvalue/impl/CommonChunkManagerTestCases.java
@@ -225,8 +225,6 @@ public abstract class CommonChunkManagerTestCases
 
     // THEN
     checkWriteIOStats(len * count, count);
-    assertTrue(getHddsVolume().getVolumeIOStats().getWriteBytes() > 0);
-    assertTrue(getHddsVolume().getVolumeIOStats().getWriteOpCount() > 0);
 
     // WHEN
     for (int i = 0; i < count; i++) {
