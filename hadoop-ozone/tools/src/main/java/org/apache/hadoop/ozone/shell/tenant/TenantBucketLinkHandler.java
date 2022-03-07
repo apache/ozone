ecb685a6cf8aa463e9a3e1836900e56c52ed5597
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2022-03-07 12:09:51 -0800

    HDDS-6275. [Multi-Tenant] Add feature documentation and CLI quick start guide (#3095)

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/shell/tenant/TenantBucketLinkHandler.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/shell/tenant/TenantBucketLinkHandler.java
@@ -30,14 +30,13 @@ import picocli.CommandLine.Parameters;
 import java.io.IOException;
 
 /**
- * ozone tenant bucket-link.
+ * ozone tenant linkbucket.
  *
  * Note: Currently this command is exactly the same as `ozone sh bucket link`.
  * We might expand this to add more functionality in the future, and different
  * ObjectStore API(s) would be used by then.
  */
-@CommandLine.Command(name = "bucket-link",
-    aliases = {"link-bucket", "bucketlink", "linkbucket"},
+@CommandLine.Command(name = "linkbucket",
     description = "Create a symlink to another bucket")
 public class TenantBucketLinkHandler extends TenantHandler {
 
