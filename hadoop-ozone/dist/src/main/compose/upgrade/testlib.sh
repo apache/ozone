6a8c97dff554136e21cdc8e1c84e8210920e224e
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2022-03-09 14:15:18 -0800

    Add extra steps in upgrade acceptance test that triggers the precondition check that exists in OM (released) versions <= 1.1.0

--- hadoop-ozone/dist/src/main/compose/upgrade/testlib.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/testlib.sh
@@ -100,13 +100,13 @@ run_test() {
 ## @description Generates data on the cluster.
 ## @param The prefix to use for data generated.
 generate() {
-    execute_robot_test scm -v PREFIX:"$1" upgrade/generate.robot
+    execute_robot_test scm -v PREFIX:"$1" ${@:2} upgrade/generate.robot
 }
 
 ## @description Validates that data exists on the cluster.
 ## @param The prefix of the data to be validated.
 validate() {
-    execute_robot_test scm -v PREFIX:"$1" upgrade/validate.robot
+    execute_robot_test scm -v PREFIX:"$1" ${@:2} upgrade/validate.robot
 }
 
 ## @description Checks that the metadata layout version of the provided node matches what is expected.
