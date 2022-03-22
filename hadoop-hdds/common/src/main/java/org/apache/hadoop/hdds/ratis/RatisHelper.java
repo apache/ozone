55bdd8a2383b049bbc114149b5da8dc966642e28
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2022-03-22 20:11:25 +0800

    Fix checkstyle.

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/ratis/RatisHelper.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/ratis/RatisHelper.java
@@ -25,7 +25,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
