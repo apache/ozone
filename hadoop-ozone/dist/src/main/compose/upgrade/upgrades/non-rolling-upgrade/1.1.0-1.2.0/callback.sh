b1d12cdb4849cc84d9eb21b48dcf778307a9d416
Istvan Fajth <pifta@cloudera.com>
2022-04-01 23:26:49 +0200

    Fix test expectations with new LayoutFeature added in HDDS.

--- hadoop-ozone/dist/src/main/compose/upgrade/upgrades/non-rolling-upgrade/1.1.0-1.2.0/callback.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/upgrades/non-rolling-upgrade/1.1.0-1.2.0/callback.sh
@@ -69,7 +69,7 @@ with_old_version_downgraded() {
 }
 
 with_new_version_finalized() {
-  _check_hdds_mlvs 2
+  _check_hdds_mlvs 3
   # OM currently only has one layout version.
   _check_om_mlvs 0
 
