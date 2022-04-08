76f809f3ec1a826abd7717eb7f5100089a2fda2f
tanvipenumudy <46785609+tanvipenumudy@users.noreply.github.com>
2022-04-08 18:01:46 +0530

    HDDS-6436. Add write lock waiting and held time metrics (#3271)

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/lock/ActiveLock.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/lock/ActiveLock.java
@@ -138,6 +138,27 @@ public final class ActiveLock {
   }
 
   /**
+   * Returns the number of reentrant write holds on this lock by the current
+   * thread.
+   *
+   * @return the number of holds on the write lock by the current thread,
+   *         or zero if the write lock is not held by the current thread
+   */
+  int getWriteHoldCount() {
+    return lock.getWriteHoldCount();
+  }
+
+  /**
+   * Queries if the write lock is held by the current thread.
+   *
+   * @return {@code true} if the current thread holds the write lock and
+   *         {@code false} otherwise
+   */
+  boolean isWriteLockedByCurrentThread() {
+    return lock.isWriteLockedByCurrentThread();
+  }
+
+  /**
    * Resets the active count on the lock.
    */
   void resetCounter() {
