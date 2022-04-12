4c4a7bb0c2bdae80a35d503757443b5b3698a818
Symious <yiyang0203@gmail.com>
2022-04-12 11:14:47 +0800

    HDDS-6525. Fix ClassNotFound issue

--- hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/util/S3Utils.java
+++ hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/util/S3Utils.java
@@ -17,7 +17,7 @@
  */
 package org.apache.hadoop.ozone.s3.util;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
