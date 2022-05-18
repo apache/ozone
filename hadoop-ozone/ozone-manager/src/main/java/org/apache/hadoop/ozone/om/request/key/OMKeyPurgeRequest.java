285f49c0f79c0a72ddabcad29f9dbd20c1ecc479
Jyotinder Singh <jyotinder.singh@cloudera.com>
2022-05-18 17:49:31 +0530

    address reviews

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/key/OMKeyPurgeRequest.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/key/OMKeyPurgeRequest.java
@@ -18,22 +18,12 @@
 
 package org.apache.hadoop.ozone.om.request.key;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.hadoop.ozone.om.OzoneManager;
-import org.apache.hadoop.ozone.om.exceptions.OMException;
 import org.apache.hadoop.ozone.om.ratis.utils.OzoneManagerDoubleBufferHelper;
 import org.apache.hadoop.ozone.om.request.util.OmResponseUtil;
-import org.apache.hadoop.ozone.om.request.validation.RequestFeatureValidator;
-import org.apache.hadoop.ozone.om.request.validation.RequestProcessingPhase;
-import org.apache.hadoop.ozone.om.request.validation.ValidationCondition;
-import org.apache.hadoop.ozone.om.request.validation.ValidationContext;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.ozone.om.response.OMClientResponse;
 import org.apache.hadoop.ozone.om.response.key.OMKeyPurgeResponse;
-import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.Type;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.DeletedKeys;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.OMRequest;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.OMResponse;
@@ -41,7 +31,6 @@ import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.PurgeKe
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -83,47 +72,4 @@ public class OMKeyPurgeRequest extends OMKeyRequest {
     return omClientResponse;
   }
 
-  /**
-   * Validates key purge requests.
-   * We do not want to allow older clients to purge keys in buckets which use
-   * non LEGACY layouts.
-   *
-   * @param req - the request to validate
-   * @param ctx - the validation context
-   * @return the validated request
-   * @throws OMException if the request is invalid
-   */
-  @RequestFeatureValidator(
-      conditions = ValidationCondition.OLDER_CLIENT_REQUESTS,
-      processingPhase = RequestProcessingPhase.PRE_PROCESS,
-      requestType = Type.PurgeKeys
-  )
-  public static OMRequest blockPurgeKeysWithBucketLayoutFromOldClient(
-      OMRequest req, ValidationContext ctx) throws IOException {
-    List<DeletedKeys> bucketDeletedKeysList =
-        req.getPurgeKeysRequest().getDeletedKeysList();
-
-    // Add volume and bucket pairs to a hashset to avoid duplicate RPC
-    // calls when querying bucket layout.
-    HashSet<Pair<String, String>> volumeAndBucketList = new HashSet<>();
-    for (DeletedKeys bucketWithDeleteKeys : bucketDeletedKeysList) {
-      volumeAndBucketList.add(
-          new ImmutablePair<>(bucketWithDeleteKeys.getVolumeName(),
-              bucketWithDeleteKeys.getBucketName()));
-    }
-
-    // Validate each pair of volume and bucket.
-    for (Pair<String, String> volAndBuck : volumeAndBucketList) {
-      if (!ctx.getBucketLayout(volAndBuck.getLeft(), volAndBuck.getRight())
-          .isLegacy()) {
-        throw new OMException(
-            "Client is attempting to purge one or more keys in a bucket" +
-                " which uses non-LEGACY bucket layout features. Please " +
-                "upgrade the client to a compatible version to perform this" +
-                " operation.",
-            OMException.ResultCodes.NOT_SUPPORTED_OPERATION);
-      }
-    }
-    return req;
-  }
 }
