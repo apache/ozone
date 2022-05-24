70a0f50b3a18a512a909e662c527b5a44002db8a
Jackson Yao <jacksonyao@tencent.com>
2022-05-25 00:07:55 +0800

    fix test

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/ScmConfigKeys.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/ScmConfigKeys.java
@@ -361,12 +361,12 @@ public final class ScmConfigKeys {
 
   public static final String
       OZONE_SCM_EXPIRED_CONTAINER_REPLICA_OP_SCRUB_INTERVAL =
-      "ozone.scm.expired.container.replica.operation.scrub.interval";
+      "ozone.scm.expired.container.replica.op.scrub.interval";
   public static final String
       OZONE_SCM_EXPIRED_CONTAINER_REPLICA_OP_SCRUB_INTERVAL_DEFAULT =
       "5m";
   public static final String OZONE_SCM_CONTAINER_REPLICA_OP_TIME_OUT =
-      "ozone.scm.expired.container.replica.operation.time.out";
+      "ozone.scm.expired.container.replica.op.time.out";
   public static final String
       OZONE_SCM_CONTAINER_REPLICA_OP_TIME_OUT_DEFAULT = "30m";
 
