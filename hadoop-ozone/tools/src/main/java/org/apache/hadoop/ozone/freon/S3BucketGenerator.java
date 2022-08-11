5dcf0b8dd8c703051c04ffdce3290220b2eec9cf
zeddling <victorzeddys@gmail.com>
2022-08-11 16:47:31 +0300

    Add sanity test for freon s3bg. Add setup commands to s3bg description

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/S3BucketGenerator.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/S3BucketGenerator.java
@@ -37,7 +37,10 @@ import java.util.concurrent.Callable;
  */
 @Command(name = "s3bg",
     aliases = "s3-bucket-generator",
-    description = "Create buckets via the s3 interface.",
+    description = "Create buckets via the s3 interface.\n" +
+        "Run following commands beforehand:\n" +
+        "kinit -k {user}/{instance}@EXAMPLE.COM -t /etc/security/keytabs/{keytab}\n" +
+        "eval $(ozone s3 getsecret -e)",
     versionProvider = HddsVersionProvider.class,
     mixinStandardHelpOptions = true,
     showDefaultValues = true)
