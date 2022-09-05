b48975ab68ac25b3a0e98f1c0a1452e631f26848
navinko <nakumr@cloudera.com>
2022-09-05 15:49:14 +0530

    HDDS-7075 Checkstyle issue fixed.

--- hadoop-ozone/ozonefs-common/src/main/java/org/apache/hadoop/fs/ozone/FileStatusListing.java
+++ hadoop-ozone/ozonefs-common/src/main/java/org/apache/hadoop/fs/ozone/FileStatusListing.java
@@ -1,21 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.hadoop.fs.ozone;
 
 import org.apache.hadoop.classification.InterfaceAudience;
@@ -24,7 +6,7 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 
 /**
- * This class defines a partial listing of a directory to support
+ * This class defines a partial listing of a directory to support.
  * iterative directory listing.
  */
 @InterfaceAudience.Private
@@ -34,7 +16,7 @@ public class FileStatusListing {
 
   /**
    *
-   * @param partialListing a partial listing of a directory
+   * @param partialListing a partial listing of a directory.
    */
   public FileStatusListing(FileStatus[] partialListing) {
     if (partialListing == null) {
@@ -44,8 +26,8 @@ public class FileStatusListing {
   }
 
   /**
+   * Get the partial listing of file status.
    *
-   * Get the partial listing of file status
    * @return the partial listing of file status
    */
   public FileStatus[] getPartialListing() {
@@ -53,9 +35,9 @@ public class FileStatusListing {
   }
 
   /**
+   * Get the last name in this list.
    *
-   * Get the last name in this list
-   * @return the last name in the list if it is not empty; otherwise return null
+   * @return the last name in the list if it is not empty otherwise return null.
    */
   public Path getLastName() {
     if (partialListing.length == 0) {
