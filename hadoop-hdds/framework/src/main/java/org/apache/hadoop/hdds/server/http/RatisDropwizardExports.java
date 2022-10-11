bb46766e2365628320f7d03ca1b8d2652d48a2e0
Doroszlai, Attila <adoroszlai@apache.org>
2022-10-11 18:54:39 +0200

    Revert "HDDS-7271. Ozone Integration test shows memory leak (graceful shutdown cleanup) (#3787)"
    This reverts commit afb47176b773072c46e748756b42e951f6427e57.

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/http/RatisDropwizardExports.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/server/http/RatisDropwizardExports.java
@@ -54,13 +54,6 @@ public class RatisDropwizardExports extends DropwizardExports {
         r2 -> deregisterDropwizard(r2, ratisMetricsMap));
   }
 
-  public static void clear(Map<String, RatisDropwizardExports>
-                               ratisMetricsMap) {
-    MetricRegistries.global().clear();
-    CollectorRegistry.defaultRegistry.clear();
-    ratisMetricsMap.clear();
-  }
-
   private static void registerDropwizard(RatisMetricRegistry registry,
       Map<String, RatisDropwizardExports> ratisMetricsMap) {
     RatisDropwizardExports rde = new RatisDropwizardExports(
