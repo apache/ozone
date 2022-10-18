a88ed4056551829c921c03f14723a86243236611
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2022-10-18 09:06:47 -0700

    Fix indentation
    Change-Id: Icd7dc5ef4d8e714bd44d2bbe5f36faf8d3560cf5

--- hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/views/diskUsage/diskUsage.tsx
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/views/diskUsage/diskUsage.tsx
@@ -172,13 +172,13 @@ export class DiskUsage extends React.Component<Record<string, object>, IDUState>
           return (subpath.isKey || subpathName === OTHER_PATH_NAME) ? subpathName : subpathName + '/';
         });
 
-       values = subpaths.map(subpath => {
-        return subpath.size / dataSize;
-      });
+        values = subpaths.map(subpath => {
+          return subpath.size / dataSize;
+        });
 
-       percentage = values.map(value => {
-        return (value * 100).toFixed(2);
-      });
+        percentage = values.map(value => {
+          return (value * 100).toFixed(2);
+        });
 
         sizeStr = subpaths.map(subpath => {
           return this.byteToSize(subpath.size, 1);
