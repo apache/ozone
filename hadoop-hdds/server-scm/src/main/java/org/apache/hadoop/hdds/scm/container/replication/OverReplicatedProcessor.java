2d47106d6df8abca86e64349e200bb1c5de011a0
Swaminathan Balachandran <47532440+swamirishi@users.noreply.github.com>
2022-12-16 07:59:37 -0700

    HDDS-7616. EC: Refactor Unhealthy Replicated Processor (#4063)

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/OverReplicatedProcessor.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/OverReplicatedProcessor.java
@@ -17,11 +17,8 @@
  */
 package org.apache.hadoop.hdds.scm.container.replication;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.hdds.protocol.DatanodeDetails;
 import org.apache.hadoop.ozone.protocol.commands.SCMCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -31,89 +28,32 @@ import java.util.Map;
  * queue, calculate the delete commands and assign to the datanodes
  * via the eventQueue.
  */
-public class OverReplicatedProcessor implements Runnable {
-
-  private static final Logger LOG = LoggerFactory
-      .getLogger(OverReplicatedProcessor.class);
-  private final ReplicationManager replicationManager;
-  private volatile boolean runImmediately = false;
-  private final long intervalInMillis;
+public class OverReplicatedProcessor extends UnhealthyReplicationProcessor
+        <ContainerHealthResult.OverReplicatedHealthResult> {
 
   public OverReplicatedProcessor(ReplicationManager replicationManager,
       long intervalInMillis) {
-    this.replicationManager = replicationManager;
-    this.intervalInMillis = intervalInMillis;
-  }
+    super(replicationManager, intervalInMillis);
 
-  /**
-   * Read messages from the ReplicationManager over replicated queue and,
-   * form commands to correct the over replication. The commands are added
-   * to the event queue and the PendingReplicaOps are adjusted.
-   *
-   * Note: this is a temporary implementation of this feature. A future
-   * version will need to limit the amount of messages assigned to each
-   * datanode, so they are not assigned too much work.
-   */
-  public void processAll() {
-    int processed = 0;
-    int failed = 0;
-    while (true) {
-      if (!replicationManager.shouldRun()) {
-        break;
-      }
-      ContainerHealthResult.OverReplicatedHealthResult overRep =
-          replicationManager.dequeueOverReplicatedContainer();
-      if (overRep == null) {
-        break;
-      }
-      try {
-        processContainer(overRep);
-        processed++;
-      } catch (Exception e) {
-        LOG.error("Error processing over replicated container {}",
-            overRep.getContainerInfo(), e);
-        failed++;
-        replicationManager.requeueOverReplicatedContainer(overRep);
-      }
-    }
-    LOG.info("Processed {} over replicated containers, failed processing {}",
-        processed, failed);
-  }
-
-  protected void processContainer(ContainerHealthResult
-      .OverReplicatedHealthResult overRep) throws IOException {
-    Map<DatanodeDetails, SCMCommand<?>> cmds = replicationManager
-        .processOverReplicatedContainer(overRep);
-    for (Map.Entry<DatanodeDetails, SCMCommand<?>> cmd : cmds.entrySet()) {
-      SCMCommand<?> scmCmd = cmd.getValue();
-      replicationManager.sendDatanodeCommand(scmCmd, overRep.getContainerInfo(),
-          cmd.getKey());
-    }
   }
 
   @Override
-  public void run() {
-    try {
-      while (!Thread.currentThread().isInterrupted()) {
-        if (replicationManager.shouldRun()) {
-          processAll();
-        }
-        synchronized (this) {
-          if (!runImmediately) {
-            wait(intervalInMillis);
-          }
-          runImmediately = false;
-        }
-      }
-    } catch (InterruptedException e) {
-      LOG.warn("{} interrupted. Exiting...", Thread.currentThread().getName());
-      Thread.currentThread().interrupt();
-    }
+  protected ContainerHealthResult.OverReplicatedHealthResult
+      dequeueHealthResultFromQueue(ReplicationManager replicationManager) {
+    return replicationManager.dequeueOverReplicatedContainer();
   }
 
-  @VisibleForTesting
-  synchronized void runImmediately() {
-    runImmediately = true;
-    notify();
+  @Override
+  protected void requeueHealthResultFromQueue(
+          ReplicationManager replicationManager,
+          ContainerHealthResult.OverReplicatedHealthResult healthResult) {
+    replicationManager.requeueOverReplicatedContainer(healthResult);
+  }
+  @Override
+  protected Map<DatanodeDetails, SCMCommand<?>> getDatanodeCommands(
+      ReplicationManager replicationManager,
+      ContainerHealthResult.OverReplicatedHealthResult healthResult)
+      throws IOException {
+    return replicationManager.processOverReplicatedContainer(healthResult);
   }
 }
