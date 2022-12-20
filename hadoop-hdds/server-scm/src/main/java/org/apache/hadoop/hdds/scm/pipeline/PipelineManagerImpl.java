3bee89f12a520068fab934ad08f1e1a250a4e38f
arafat <mohammad.khan@cloudera.com>
2022-12-21 02:53:31 +0530

    Refined the format of metrics

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineManagerImpl.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineManagerImpl.java
@@ -700,8 +700,7 @@ public class PipelineManagerImpl implements PipelineManager {
   }
 
   @Override
-  public Map<String, Map<String, Map<String, String>>> getPipelineLeaders()
-      throws NotLeaderException {
+  public Map<String, Map<String, Map<String, String>>> getPipelineLeaders() {
     List<Pipeline> pipelines = getPipelines();
     return pipelineLeaderFormat(pipelines);
   }
