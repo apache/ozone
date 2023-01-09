f060d5164d653aa364e9d766c7ba609904242454
Sammi Chen <sammichen@apache.org>
2023-01-09 10:43:40 +0800

    HDDS-7628. Intermittent failure in TestOzoneContainerWithTLS (#4142)

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/ozoneimpl/TestOzoneContainerWithTLS.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/ozoneimpl/TestOzoneContainerWithTLS.java
@@ -59,6 +59,8 @@ import java.io.File;
 import java.nio.file.Path;
 import java.security.cert.CertificateExpiredException;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -99,7 +101,7 @@ public class TestOzoneContainerWithTLS {
   private ContainerTokenSecretManager secretManager;
   private CertificateClientTestImpl caClient;
   private boolean containerTokenEnabled;
-  private int certLifetime = 10 * 1000; // 10s
+  private int certLifetime = 15 * 1000; // 15s
 
   public TestOzoneContainerWithTLS(boolean enableToken) {
     this.containerTokenEnabled = enableToken;
@@ -152,12 +154,13 @@ public class TestOzoneContainerWithTLS {
   @Test(expected = CertificateExpiredException.class)
   public void testCertificateLifetime() throws Exception {
     // Sleep to wait for certificate expire
-    Thread.sleep(certLifetime);
-    caClient.getCertificate().checkValidity();
+    LocalDateTime now = LocalDateTime.now();
+    now = now.plusSeconds(certLifetime / 1000);
+    caClient.getCertificate().checkValidity(Date.from(
+        now.atZone(ZoneId.systemDefault()).toInstant()));
   }
 
   @Test
-  @org.junit.Ignore("HDDS-7628")
   public void testCreateOzoneContainer() throws Exception {
     LOG.info("testCreateOzoneContainer with TLS and containerToken enabled: {}",
         containerTokenEnabled);
@@ -203,7 +206,6 @@ public class TestOzoneContainerWithTLS {
   }
 
   @Test
-  @org.junit.Ignore("HDDS-7628")
   public void testContainerDownload() throws Exception {
     DatanodeDetails dn = MockDatanodeDetails.createDatanodeDetails(
         UUID.randomUUID().toString(), "localhost", "0.0.0.0",
@@ -228,29 +230,26 @@ public class TestOzoneContainerWithTLS {
 
       // Create containers
       long containerId = ContainerTestHelper.getTestContainerID();
-      int count = 5;
       List<Long> containerIdList = new ArrayList<>();
       XceiverClientGrpc client = new XceiverClientGrpc(pipeline, conf,
           Collections.singletonList(caClient.getCACertificate()));
       client.connect();
-      for (int i = 0; i < count; i++, containerId++) {
-        if (containerTokenEnabled) {
-          Token<ContainerTokenIdentifier> token = secretManager.generateToken(
-              UserGroupInformation.getCurrentUser().getUserName(),
-              ContainerID.valueOf(containerId));
-          createSecureContainer(client, containerId, token);
-          closeSecureContainer(client, containerId, token);
-        } else {
-          createContainer(client, containerId);
-          closeContainer(client, containerId);
-        }
-        containerIdList.add(containerId);
+      if (containerTokenEnabled) {
+        Token<ContainerTokenIdentifier> token = secretManager.generateToken(
+            UserGroupInformation.getCurrentUser().getUserName(),
+            ContainerID.valueOf(containerId));
+        createSecureContainer(client, containerId, token);
+        closeSecureContainer(client, containerId, token);
+      } else {
+        createContainer(client, containerId);
+        closeContainer(client, containerId);
       }
+      containerIdList.add(containerId++);
 
       // Wait certificate to expire
       GenericTestUtils.waitFor(() ->
               caClient.getCertificate().getNotAfter().before(new Date()),
-          500, certLifetime);
+          100, certLifetime);
 
       List<DatanodeDetails> sourceDatanodes = new ArrayList<>();
       sourceDatanodes.add(dn);
