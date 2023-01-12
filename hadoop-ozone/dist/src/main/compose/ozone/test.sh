6be2f4a2ecea2684b7ede1093a15d6b1f93e0306
Aswin Shakil Balasubramanian <aswin.shakil47@gmail.com>
2023-01-11 19:18:33 -0800

    HDDS-7768. Update test.

--- hadoop-ozone/dist/src/main/compose/ozone/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozone/test.sh
@@ -50,6 +50,8 @@ execute_robot_test scm recon
 
 execute_robot_test scm om-ratis
 
+execute_robot_test scm snapshot/basic.robot
+
 execute_robot_test scm freon
 
 execute_robot_test scm cli
@@ -61,8 +63,6 @@ execute_robot_test scm -v SCHEME:ofs -v BUCKET_TYPE:link -N ozonefs-ofs-link ozo
 execute_robot_test scm -v SCHEME:o3fs -v BUCKET_TYPE:bucket -N ozonefs-o3fs-bucket ozonefs/ozonefs.robot
 
 execute_robot_test scm ec/basic.robot
-
-execute_robot_test scm snapshot/basic.robot
 
 stop_docker_env
 
