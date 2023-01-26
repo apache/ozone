ec19c502e2fcf9359daaa82cf4b02b63a52cebe0
Doroszlai, Attila <adoroszlai@apache.org>
2023-01-26 20:55:55 +0100

    Also fix unbuffer, and BlockInputStream

--- hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/KeyOutputStream.java
+++ hadoop-ozone/client/src/main/java/org/apache/hadoop/ozone/client/io/KeyOutputStream.java
@@ -332,7 +332,7 @@ public class KeyOutputStream extends OutputStream implements Syncable {
       excludeList.addPipeline(pipelineId);
     }
     // just clean up the current stream.
-    streamEntry.cleanup(retryFailure);
+    streamEntry.cleanup(!retryFailure);
 
     // discard all subsequent blocks the containers and pipelines which
     // are in the exclude list so that, the very next retry should never
