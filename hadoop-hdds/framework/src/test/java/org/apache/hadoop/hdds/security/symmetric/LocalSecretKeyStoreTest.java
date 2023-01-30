b3a438240fa8c2f0ccf93e971ce6a12560a43358
Duong Nguyen <duongnt.is@gmail.com>
2023-01-30 09:03:57 -0800

    Use json as string constant to fix rat violate in the json file.

--- hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/symmetric/LocalSecretKeyStoreTest.java
+++ hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/symmetric/LocalSecretKeyStoreTest.java
@@ -34,10 +34,12 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.PosixFilePermission;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -119,18 +121,23 @@ public class LocalSecretKeyStoreTest {
    * The intention of this is to ensure a saved file can be loaded after
    * future changes to {@link ManagedSecretKey} schema.
    *
-   * Please don't just change the content of test_secret_keys.json if this
+   * Please don't just change the content of test json if this
    * test fails, instead, analyse the backward-compatibility of the change.
    */
   @Test
   public void testLoadExistingFile() throws Exception {
     // copy test file content to the backing file.
-    ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    try (
-        InputStream is = cl.getResourceAsStream("test_secret_keys.json")
-    ) {
-      Files.copy(is, testSecretFile, StandardCopyOption.REPLACE_EXISTING);
-    }
+    String test_json = "[\n" +
+        "  {\n" +
+        "    \"id\":\"78864cfb-793b-4157-8ad6-714c9f950a16\",\n" +
+        "    \"creationTime\":\"2007-12-03T10:15:30Z\",\n" +
+        "    \"expiryTime\":\"2007-12-03T11:15:30Z\",\n" +
+        "    \"algorithm\":\"HmacSHA256\",\n" +
+        "    \"encoded\":\"YSeCdJRB4RclxoeE69ENmTe2Cv8ybyKhHP3mq4M1r8o=\"\n" +
+        "  }\n" +
+        "]";
+    Files.write(testSecretFile, Collections.singletonList(test_json),
+        StandardOpenOption.WRITE);
 
     Instant date = Instant.parse("2007-12-03T10:15:30.00Z");
     ManagedSecretKey secretKey = new ManagedSecretKey(
