aea5edfe99ea92687c604378bec4bfbf1773ba80
Hemant Kumar <hemantadhvaniya@gmail.com>
2023-01-31 10:56:00 -0800

    HDDS-7845. [Snapshot] Wait for RocksDB checkpoint directory creation (#4214)

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/snapshot/TestOzoneSnapshotRestore.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/snapshot/TestOzoneSnapshotRestore.java
@@ -25,7 +25,6 @@ import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.scm.HddsWhiteboxTestUtils;
 import org.apache.hadoop.ozone.MiniOzoneCluster;
 import org.apache.hadoop.ozone.MiniOzoneHAClusterImpl;
-import org.apache.hadoop.ozone.TestDataUtil;
 import org.apache.hadoop.ozone.client.ObjectStore;
 import org.apache.hadoop.ozone.client.OzoneBucket;
 import org.apache.hadoop.ozone.client.OzoneClient;
@@ -39,14 +38,12 @@ import org.apache.hadoop.ozone.om.OmSnapshotManager;
 import org.apache.hadoop.ozone.om.OzoneManager;
 import org.apache.hadoop.ozone.om.helpers.BucketLayout;
 import org.apache.hadoop.ozone.om.helpers.SnapshotInfo;
-import org.apache.hadoop.ozone.om.protocol.OzoneManagerProtocol;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.ozone.test.GenericTestUtils;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.rules.Timeout;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -55,7 +52,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
 
@@ -65,26 +61,18 @@ import static org.apache.hadoop.ozone.OzoneConsts.OM_DB_NAME;
 import static org.apache.hadoop.ozone.OzoneConsts.OM_KEY_PREFIX;
 import static org.apache.hadoop.ozone.OzoneConsts.OM_SNAPSHOT_DIR;
 import static org.apache.hadoop.ozone.OzoneConsts.OZONE_OFS_URI_SCHEME;
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * Tests Snapshot Restore function.
  */
-
+@Timeout(value = 300)
 public class TestOzoneSnapshotRestore {
-  private static final String OM_SERVICE_ID = "om-service-test1";
-  private static BucketLayout bucketLayout = BucketLayout.LEGACY;
-  private static MiniOzoneCluster cluster = null;
-  private static String volumeName;
-  private static String bucketName;
-  private static OzoneManagerProtocol writeClient;
-  private static ObjectStore store;
-  private static File metaDir;
-  private static OzoneManager leaderOzoneManager;
-  private static OzoneBucket ozoneBucket;
-  @Rule
-  public Timeout timeout = new Timeout(500, TimeUnit.SECONDS);
+  private static final String OM_SERVICE_ID = "om-service-test-1";
+  private MiniOzoneCluster cluster;
+  private ObjectStore store;
+  private File metaDir;
+  private OzoneManager leaderOzoneManager;
   private OzoneConfiguration clientConf;
 
   private static Stream<Arguments> bucketTypes() {
@@ -102,7 +90,7 @@ public class TestOzoneSnapshotRestore {
   }
 
   @BeforeEach
-  private void init() throws Exception {
+  public void init() throws Exception {
     OzoneConfiguration conf = new OzoneConfiguration();
     String clusterId = UUID.randomUUID().toString();
     String scmId = UUID.randomUUID().toString();
@@ -116,12 +104,6 @@ public class TestOzoneSnapshotRestore {
             .build();
     cluster.waitForClusterToBeReady();
 
-    // create a volume and a bucket to be used by OzoneFileSystem
-    ozoneBucket = TestDataUtil
-            .createVolumeAndBucket(cluster, bucketLayout);
-    volumeName = ozoneBucket.getVolumeName();
-    bucketName = ozoneBucket.getName();
-
     leaderOzoneManager = ((MiniOzoneHAClusterImpl) cluster).getOMLeader();
     OzoneConfiguration leaderConfig = leaderOzoneManager.getConfiguration();
     cluster.setConf(leaderConfig);
@@ -132,7 +114,6 @@ public class TestOzoneSnapshotRestore {
 
     OzoneClient client = cluster.getClient();
     store = client.getObjectStore();
-    writeClient = store.getClientProxy().getOzoneManagerClient();
 
     KeyManagerImpl keyManager = (KeyManagerImpl) HddsWhiteboxTestUtils
             .getInternalState(leaderOzoneManager, "keyManager");
@@ -204,7 +185,7 @@ public class TestOzoneSnapshotRestore {
       // Copy key from source to destination path
       int res = ToolRunner.run(shell,
               new String[]{"-cp", sourcePath, destPath});
-      assertEquals(0, res);
+      Assertions.assertEquals(0, res);
     } finally {
       shell.close();
     }
@@ -232,11 +213,11 @@ public class TestOzoneSnapshotRestore {
     String snapshotKeyPrefix = createSnapshot(volume, bucket);
 
     int volBucketKeyCount = keyCount(buck, snapshotKeyPrefix + keyPrefix);
-    Assert.assertEquals(5, volBucketKeyCount);
+    Assertions.assertEquals(5, volBucketKeyCount);
 
     deleteKeys(buck);
     int delKeyCount = keyCount(buck, keyPrefix);
-    Assert.assertEquals(0, delKeyCount);
+    Assertions.assertEquals(0, delKeyCount);
 
     String sourcePath = OM_KEY_PREFIX + volume + OM_KEY_PREFIX + bucket
             + OM_KEY_PREFIX + snapshotKeyPrefix;
@@ -247,8 +228,7 @@ public class TestOzoneSnapshotRestore {
     }
 
     int finalKeyCount = keyCount(buck, keyPrefix);
-    Assert.assertEquals(5, finalKeyCount);
-
+    Assertions.assertEquals(5, finalKeyCount);
   }
 
   @ParameterizedTest
@@ -276,7 +256,7 @@ public class TestOzoneSnapshotRestore {
     String snapshotKeyPrefix = createSnapshot(volume, bucket);
 
     int volBucketKeyCount = keyCount(buck, snapshotKeyPrefix + keyPrefix);
-    Assert.assertEquals(5, volBucketKeyCount);
+    Assertions.assertEquals(5, volBucketKeyCount);
 
     String sourcePath = OM_KEY_PREFIX + volume + OM_KEY_PREFIX + bucket
             + OM_KEY_PREFIX + snapshotKeyPrefix;
@@ -287,8 +267,7 @@ public class TestOzoneSnapshotRestore {
     }
 
     int finalKeyCount = keyCount(buck2, keyPrefix);
-    Assert.assertEquals(5, finalKeyCount);
-
+    Assertions.assertEquals(5, finalKeyCount);
   }
 
   @ParameterizedTest
@@ -320,7 +299,7 @@ public class TestOzoneSnapshotRestore {
     String snapshotKeyPrefix = createSnapshot(volume, bucket);
 
     int volBucketKeyCount = keyCount(buck, snapshotKeyPrefix + keyPrefix);
-    Assert.assertEquals(5, volBucketKeyCount);
+    Assertions.assertEquals(5, volBucketKeyCount);
 
     String sourcePath = OM_KEY_PREFIX + volume + OM_KEY_PREFIX + bucket
             + OM_KEY_PREFIX + snapshotKeyPrefix;
@@ -331,8 +310,6 @@ public class TestOzoneSnapshotRestore {
     }
 
     int finalKeyCount = keyCount(buck2, keyPrefix);
-    Assert.assertEquals(5, finalKeyCount);
-
+    Assertions.assertEquals(5, finalKeyCount);
   }
-
 }
