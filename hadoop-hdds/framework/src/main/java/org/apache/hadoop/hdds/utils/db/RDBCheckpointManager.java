d594dccda08d6c4eafc10c850b3277d5128d83f1
Hemant Kumar <hemantadhvaniya@gmail.com>
2023-01-31 10:56:00 -0800

    HDDS-7845. [Snapshot] Wait for RocksDB checkpoint directory creation (#4214)

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RDBCheckpointManager.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RDBCheckpointManager.java
@@ -20,28 +20,33 @@
 package org.apache.hadoop.hdds.utils.db;
 
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hdds.utils.db.RocksDatabase.RocksCheckpoint;
-
+import org.awaitility.core.ConditionTimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.awaitility.Awaitility.with;
 
 /**
  * RocksDB Checkpoint Manager, used to create and cleanup checkpoints.
  */
 public class RDBCheckpointManager implements Closeable {
-
+  private final RocksDatabase db;
   private final RocksCheckpoint checkpoint;
   public static final String RDB_CHECKPOINT_DIR_PREFIX = "checkpoint_";
   private static final Logger LOG =
       LoggerFactory.getLogger(RDBCheckpointManager.class);
   private final String checkpointNamePrefix;
+  private static final Duration POLL_DELAY_DURATION = Duration.ZERO;
+  private static final Duration POLL_INTERVAL_DURATION = Duration.ofMillis(100);
+  private static final Duration POLL_MAX_DURATION = Duration.ofSeconds(5);
 
   /**
    * Create a checkpoint manager with a prefix to be added to the
@@ -50,6 +55,7 @@ public class RDBCheckpointManager implements Closeable {
    * @param checkpointPrefix prefix string.
    */
   public RDBCheckpointManager(RocksDatabase db, String checkpointPrefix) {
+    this.db = db;
     this.checkpointNamePrefix = checkpointPrefix;
     this.checkpoint = db.createCheckpoint();
   }
@@ -76,14 +82,21 @@ public class RDBCheckpointManager implements Closeable {
 
       Path checkpointPath = Paths.get(parentDir, checkpointDir);
       Instant start = Instant.now();
-      checkpoint.createCheckpoint(checkpointPath);
-      //Best guesstimate here. Not accurate.
-      final long latest = checkpoint.getLatestSequenceNumber();
-      Instant end = Instant.now();
 
+      // Flush the DB WAL and mem table.
+      db.flushWal(true);
+      db.flush();
+
+      checkpoint.createCheckpoint(checkpointPath);
+      // Best guesstimate here. Not accurate.
+      final long latest = checkpoint.getLatestSequenceNumber();
+
+      Instant end = Instant.now();
       long duration = Duration.between(start, end).toMillis();
-      LOG.info("Created checkpoint at {} in {} milliseconds",
+      LOG.info("Created checkpoint in rocksDB at {} in {} milliseconds",
               checkpointPath, duration);
+
+      waitForCheckpointDirectoryExist(checkpointPath.toFile());
 
       return new RocksDBCheckpoint(
           checkpointPath,
@@ -94,6 +107,28 @@ public class RDBCheckpointManager implements Closeable {
       LOG.error("Unable to create RocksDB Snapshot.", e);
     }
     return null;
+  }
+
+  /**
+   * Wait for checkpoint directory to be created for 5 secs with 100 millis
+   * poll interval.
+   */
+  private void waitForCheckpointDirectoryExist(File file) throws IOException {
+    Instant start = Instant.now();
+    try {
+      with().atMost(POLL_MAX_DURATION)
+          .pollDelay(POLL_DELAY_DURATION)
+          .pollInterval(POLL_INTERVAL_DURATION)
+          .await()
+          .until(file::exists);
+      LOG.info("Waited for {} milliseconds for checkpoint directory {}" +
+              " availability.",
+          Duration.between(start, Instant.now()).toMillis(),
+          file.getAbsoluteFile());
+    } catch (ConditionTimeoutException exception) {
+      LOG.info("Checkpoint directory: {} didn't get created in 5 secs.",
+          file.getAbsolutePath());
+    }
   }
 
   /**
