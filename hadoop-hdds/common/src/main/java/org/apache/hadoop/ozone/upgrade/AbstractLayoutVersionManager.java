63847fd03b2ff15f8307e0c200c395b35246e620
Ivan Andika <ivan_andika3@rocketmail.com>
2023-01-31 13:53:19 +0800

    Ensure LayoutVersion is strictly increasing in LayoutVersionManager

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/upgrade/AbstractLayoutVersionManager.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/upgrade/AbstractLayoutVersionManager.java
@@ -64,6 +64,7 @@ public abstract class AbstractLayoutVersionManager<T extends LayoutFeature>
   protected void init(int version, T[] lfs) throws IOException {
     lock.writeLock().lock();
     try {
+      validateLayoutFeatureVersions(lfs);
       metadataLayoutVersion = version;
       initializeFeatures(lfs);
       softwareLayoutVersion = features.lastKey();
@@ -155,6 +156,24 @@ public abstract class AbstractLayoutVersionManager<T extends LayoutFeature>
       return metadataLayoutVersion > softwareLayoutVersion;
     } finally {
       lock.readLock().unlock();
+    }
+  }
+
+  private void validateLayoutFeatureVersions(T[] lfs) throws IOException {
+    if (lfs.length <= 1) {
+      return;
+    }
+    for (int i = 0; i < lfs.length - 1; i++) {
+      if (lfs[i + 1].layoutVersion() <= lfs[i].layoutVersion()) {
+        throw new IOException(
+          String.format(
+            "Cannot initialize VersionManager. " +
+              "Layout features' versions (%d) and (%d) are not " +
+              "strictly increasing",
+            lfs[i].layoutVersion(), lfs[i + 1].layoutVersion()
+          )
+        );
+      }
     }
   }
 
