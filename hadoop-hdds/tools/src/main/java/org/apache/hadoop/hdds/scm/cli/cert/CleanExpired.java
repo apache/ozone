d75a1a55be909e9a044f541a58162d0bb22fd250
nibiruxu <axcsd3692@qq.com>
2023-01-31 11:54:03 +0800

    fix CleanExpired

--- hadoop-hdds/tools/src/main/java/org/apache/hadoop/hdds/scm/cli/cert/CleanExpired.java
+++ hadoop-hdds/tools/src/main/java/org/apache/hadoop/hdds/scm/cli/cert/CleanExpired.java
@@ -79,7 +79,7 @@ public class CleanExpired implements Callable<Void>, SubcommandWithParent {
     try {
       DBStore dbStore = HAUtils.loadDB(
           configuration, db.getParentFile(),
-          db.getName(), new SCMDBDefinition());
+          db.getName(), new SCMDBDefinition(), false);
       removeExpiredCertificates(dbStore);
     } catch (Exception e) {
       LOG.error("Error trying to open file: " + dbFilePath +
