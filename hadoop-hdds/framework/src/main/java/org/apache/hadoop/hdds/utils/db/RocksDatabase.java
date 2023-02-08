1db8b735c40db32d7a4bf740bbec336eabac02ea
Sammi Chen <sammichen@apache.org>
2023-02-08 14:10:49 +0800

    HDDS-7321. Auto rocksDB small sst files compaction. (#3908)
    * HDDS-7321. Auto rocksDB small sst files compaction.
    
    Co-authored-by: Ritesh H Shukla <kerneltime@gmail.com>

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RocksDatabase.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/RocksDatabase.java
@@ -515,21 +515,6 @@ public final class RocksDatabase {
     }
   }
 
-  public void compactRange(ColumnFamily family, final byte[] begin,
-                           final byte[] end,
-                           final ManagedCompactRangeOptions options)
-      throws IOException {
-    try {
-      counter.incrementAndGet();
-      db.get().compactRange(family.getHandle(), begin, end, options);
-    } catch (RocksDBException e) {
-      closeOnError(e, true);
-      throw toIOException(this, "compactRange", e);
-    } finally {
-      counter.decrementAndGet();
-    }
-  }
-
   public void compactDB(ManagedCompactRangeOptions options) throws IOException {
     compactRangeDefault(options);
     for (RocksDatabase.ColumnFamily columnFamily : getExtraColumnFamilies()) {
@@ -579,6 +564,29 @@ public final class RocksDatabase {
       }
     }
     return null;
+  }
+
+  public void compactRange(ColumnFamily family, final byte[] begin,
+      final byte[] end, final ManagedCompactRangeOptions options)
+      throws IOException {
+    try {
+      counter.incrementAndGet();
+      db.get().compactRange(family.getHandle(), begin, end, options);
+    } catch (RocksDBException e) {
+      closeOnError(e, true);
+      throw toIOException(this, "compactRange", e);
+    } finally {
+      counter.decrementAndGet();
+    }
+  }
+
+  public List<LiveFileMetaData> getLiveFilesMetaData() {
+    try {
+      counter.incrementAndGet();
+      return db.get().getLiveFilesMetaData();
+    } finally {
+      counter.decrementAndGet();
+    }
   }
 
   RocksCheckpoint createCheckpoint() {
