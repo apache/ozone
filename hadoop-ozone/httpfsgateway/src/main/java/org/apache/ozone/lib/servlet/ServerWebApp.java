ea96fe7abdd270c108195cb03ce3456fab20d431
Zita Dombi <zitadombi98@gmail.com>
2023-02-24 01:10:59 +0100

    Change VisibleForTesting import
    Change-Id: Ifa8130d7176d4b84143e1bd457897fe2f0c54cd0

--- hadoop-ozone/httpfsgateway/src/main/java/org/apache/ozone/lib/servlet/ServerWebApp.java
+++ hadoop-ozone/httpfsgateway/src/main/java/org/apache/ozone/lib/servlet/ServerWebApp.java
@@ -18,9 +18,9 @@
 
 package org.apache.ozone.lib.servlet;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdds.annotation.InterfaceAudience;
-import org.apache.ratis.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.ozone.lib.server.Server;
 import org.apache.ozone.lib.server.ServerException;
 
