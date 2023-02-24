70bff85fbee9bd81b8e5e8d80208d8b3cc482460
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2023-02-24 21:09:21 +0100

    Add smoketest for Snapshot with ACLs

--- hadoop-ozone/dist/src/main/compose/ozonesecure/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure/test.sh
@@ -53,6 +53,7 @@ execute_robot_test scm recon
 
 execute_robot_test scm admincli
 execute_robot_test scm spnego
+execute_robot_test scm snapshot/snapshot-acls.robot
 
 # test replication
 docker-compose up -d --scale datanode=2
