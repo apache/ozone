af54100c797f5f755ce7f5c43e92df38985a1ad4
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2023-02-28 20:08:54 +0100

    HDDS-8044. Ensure GrpcOutputStream is closed (#4327)

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/replication/GrpcReplicationService.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/replication/GrpcReplicationService.java
@@ -27,6 +27,7 @@ import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.SendContai
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.SendContainerResponse;
 import org.apache.hadoop.hdds.protocol.datanode.proto.IntraDatanodeProtocolServiceGrpc;
 
+import org.apache.hadoop.hdds.utils.IOUtils;
 import org.apache.ratis.thirdparty.io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,13 +61,17 @@ public class GrpcReplicationService extends
     CopyContainerCompression compression = fromProto(request.getCompression());
     LOG.info("Streaming container data ({}) to other datanode " +
         "with compression {}", containerID, compression);
+    OutputStream outputStream = null;
     try {
-      OutputStream outputStream = new CopyContainerResponseStream(
+      outputStream = new CopyContainerResponseStream(
           responseObserver, containerID, BUFFER_SIZE);
       source.copyData(containerID, outputStream, compression);
     } catch (IOException e) {
       LOG.error("Error streaming container {}", containerID, e);
       responseObserver.onError(e);
+    } finally {
+      // output may have already been closed, ignore such errors
+      IOUtils.cleanupWithLogger(LOG, outputStream);
     }
   }
 
