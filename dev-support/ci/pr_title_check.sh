5042dc14f03234d9b844c498a9e3ab41137db128
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2023-03-03 20:15:16 -0800

    Print a message when the title is stripped
    Change-Id: Ic954363037a331e97f366ea14910c56fa3d2fdda

--- dev-support/ci/pr_title_check.sh
+++ dev-support/ci/pr_title_check.sh
@@ -37,6 +37,10 @@ assertNotMatch() {
 
 # strip starting 'Revert "', if any
 TITLE=${TITLE#Revert \"}
+if [ "$1" != "${TITLE}" ]; then
+  echo "Leading 'Revert \"' in the PR title has been stripped solely for this title checking purpose. Performing actual title check on:"
+  echo "${TITLE}"
+fi
 
 assertMatch    '^HDDS'                             'Fail: must start with HDDS'
 assertMatch    '^HDDS-'                            'Fail: missing dash in Jira'
