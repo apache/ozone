66c515f361015ca421b1b07cf2a470d3bc6caa33
S O'Donnell <sodonnell@cloudera.com>
2023-03-07 17:32:12 +0000

    Make legacy replication manager always do replication pull to fix failing tests after changing default to push

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/LegacyReplicationManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/LegacyReplicationManager.java
@@ -1450,14 +1450,12 @@ public class LegacyReplicationManager {
     final ContainerID id = container.containerID();
     final long containerID = id.getId();
     final boolean push = rmConf.isPush();
-    final ReplicateContainerCommand replicateCommand = push
-        ? ReplicateContainerCommand.toTarget(containerID, target)
-        : ReplicateContainerCommand.fromSources(containerID, sources);
+    final ReplicateContainerCommand replicateCommand =
+        ReplicateContainerCommand.fromSources(containerID, sources);
     final DatanodeDetails source = sources.get(0); // TODO randomize
-    final DatanodeDetails receiver = push ? source : target;
-    LOG.info("Sending {} to {}", replicateCommand, receiver);
+    LOG.info("Sending {} to {}", replicateCommand, target);
 
-    final boolean sent = sendAndTrackDatanodeCommand(receiver, replicateCommand,
+    final boolean sent = sendAndTrackDatanodeCommand(target, replicateCommand,
         action -> addInflight(InflightType.REPLICATION, id, action));
 
     if (sent) {
