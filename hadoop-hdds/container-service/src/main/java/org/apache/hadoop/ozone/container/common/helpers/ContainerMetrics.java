962f306ac916b1a2da9730ec69cc76caecccbbe4
Ritesh H Shukla <kerneltime@gmail.com>
2023-03-10 18:44:22 -0800

    HDDS-8142. Check if no entries in Block DB for a container on container delete

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/helpers/ContainerMetrics.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/helpers/ContainerMetrics.java
@@ -52,6 +52,7 @@ public class ContainerMetrics {
   @Metric private MutableCounterLong containerDeleteFailedNonEmptyDir;
   @Metric private MutableCounterLong containerDeleteFailedBlockCountNotZero;
   @Metric private MutableCounterLong containerForceDelete;
+  @Metric private MutableCounterLong containerDeleteFailedNonEmptyBlockDB;
 
   private MutableCounterLong[] numOpsArray;
   private MutableCounterLong[] opsBytesArray;
@@ -151,5 +152,13 @@ public class ContainerMetrics {
 
   public long getContainerForceDelete() {
     return containerForceDelete.value();
+  }
+
+  public void incContainerDeleteFailedNonEmptyBlocksDB() {
+    containerDeleteFailedNonEmptyBlockDB.incr();
+  }
+
+  public long getContainerDeleteFailedNonEmptyBlockDB() {
+    return containerDeleteFailedNonEmptyBlockDB.value();
   }
 }
