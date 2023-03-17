73a5810b94c88ff59266dee0cda3c9cb5130d73f
Sammi Chen <sammichen@apache.org>
2023-03-17 17:35:15 +0800

    HDDS-8149. Refactor the way to notify keyStoreFactory about certificate renewed (#4390)

--- hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/ssl/TestReloadingX509KeyManager.java
+++ hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/ssl/TestReloadingX509KeyManager.java
@@ -20,13 +20,12 @@ package org.apache.hadoop.hdds.security.ssl;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.security.x509.certificate.client.CertificateClientTestImpl;
-import org.apache.ozone.test.GenericTestUtils;
 import org.apache.ozone.test.GenericTestUtils.LogCapturer;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.net.ssl.KeyManager;
 import java.security.PrivateKey;
-import java.util.Timer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -49,36 +48,25 @@ public class TestReloadingX509KeyManager {
 
   @Test
   public void testReload() throws Exception {
-    int reloadInterval = 1000;
-    Timer fileMonitoringTimer = new Timer(true);
-    ReloadingX509KeyManager km = new ReloadingX509KeyManager("jks", caClient);
-    try {
-      fileMonitoringTimer.schedule(new MonitoringTimerTask(caClient,
-          km::loadFrom, null), reloadInterval, reloadInterval);
-      PrivateKey privateKey1 = caClient.getPrivateKey();
-      assertEquals(privateKey1,
-          km.getPrivateKey(caClient.getComponentName() + "_key"));
+    KeyManager km =
+        caClient.getServerKeyStoresFactory().getKeyManagers()[0];
+    PrivateKey privateKey1 = caClient.getPrivateKey();
+    assertEquals(privateKey1, ((ReloadingX509KeyManager)km).getPrivateKey(
+        caClient.getComponentName() + "_key"));
 
-      caClient.renewRootCA();
-      caClient.renewKey();
-      PrivateKey privateKey2 = caClient.getPrivateKey();
-      assertNotEquals(privateKey1, privateKey2);
+    caClient.renewRootCA();
+    caClient.renewKey();
+    PrivateKey privateKey2 = caClient.getPrivateKey();
+    assertNotEquals(privateKey1, privateKey2);
 
-      // Wait so that the new certificate get reloaded
-      Thread.sleep((reloadInterval + 1000));
-      assertEquals(privateKey2,
-          km.getPrivateKey(caClient.getComponentName() + "_key"));
+    assertEquals(privateKey2, ((ReloadingX509KeyManager)km).getPrivateKey(
+        caClient.getComponentName() + "_key"));
 
-      assertTrue(reloaderLog.getOutput().contains(
-          "ReloadingX509KeyManager is reloaded"));
+    assertTrue(reloaderLog.getOutput().contains(
+        "ReloadingX509KeyManager is reloaded"));
 
-      // Make sure there is only one reload happened.
-      GenericTestUtils.waitFor(
-          () -> 1 == StringUtils.countMatches(reloaderLog.getOutput(),
-              "ReloadingX509KeyManager is reloaded"),
-          1000, reloadInterval + 1000);
-    } finally {
-      fileMonitoringTimer.cancel();
-    }
+    // Make sure there is two reloads happened, one for server, one for client
+    assertEquals(2, StringUtils.countMatches(reloaderLog.getOutput(),
+        "ReloadingX509KeyManager is reloaded"));
   }
 }
