fd2164c59918c43ac600af9b711465eb2c9bed81
Ethan Rose <erose@apache.org>
2023-03-22 23:07:12 -0700

    Only test upgrade/downgrade with latest version

--- hadoop-ozone/dist/src/main/compose/upgrade/test.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/test.sh
@@ -37,8 +37,8 @@ RESULT_DIR="$ALL_RESULT_DIR" create_results_dir
 # code that was built. Other versions will pull images from docker hub.
 export OZONE_CURRENT_VERSION=1.4.0
 run_test ha non-rolling-upgrade 1.3.0 "$OZONE_CURRENT_VERSION"
-run_test ha non-rolling-upgrade 1.2.1 "$OZONE_CURRENT_VERSION"
-run_test om-ha non-rolling-upgrade 1.1.0 "$OZONE_CURRENT_VERSION"
+# run_test ha non-rolling-upgrade 1.2.1 "$OZONE_CURRENT_VERSION"
+# run_test om-ha non-rolling-upgrade 1.1.0 "$OZONE_CURRENT_VERSION"
 
 generate_report "upgrade" "$ALL_RESULT_DIR"
 
