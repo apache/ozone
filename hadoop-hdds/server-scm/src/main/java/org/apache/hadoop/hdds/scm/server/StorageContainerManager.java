23e0ce72d24e02f41b93d91b28bebc9bf061a3a3
hao guo <guohao1@360.cn>
2023-03-24 17:24:55 +0800

    HDDS-8223. SCM delete block service shoud run wait for safemode to exit. (#4432)

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/StorageContainerManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/StorageContainerManager.java
@@ -303,6 +303,8 @@ public final class StorageContainerManager extends ServiceRuntimeInfoImpl
           OZONE_ADMINISTRATORS
       );
 
+  private Clock systemClock;
+
   /**
    * Creates a new StorageContainerManager. Configuration will be
    * updated with information on the actual listening addresses used
@@ -597,7 +599,7 @@ public final class StorageContainerManager extends ServiceRuntimeInfoImpl
       SCMConfigurator configurator) throws IOException {
     // Use SystemClock when data is persisted
     // and used again after system restarts.
-    Clock systemClock = Clock.system(ZoneOffset.UTC);
+    systemClock = Clock.system(ZoneOffset.UTC);
 
     if (configurator.getNetworkTopology() != null) {
       clusterMap = configurator.getNetworkTopology();
@@ -904,6 +906,10 @@ public final class StorageContainerManager extends ServiceRuntimeInfoImpl
   @VisibleForTesting
   public void setScmCertificateClient(CertificateClient client) {
     scmCertificateClient = client;
+  }
+
+  public Clock getSystemClock() {
+    return systemClock;
   }
 
   private ContainerTokenSecretManager createContainerTokenSecretManager(
