dfa9d20bb8d3f60f53bd105c27068a5304b7ed80
Doroszlai, Attila <adoroszlai@apache.org>
2023-03-28 18:49:09 +0200

    Fix checkstyle

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/server/TestSCMCertStore.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/server/TestSCMCertStore.java
@@ -51,7 +51,6 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
