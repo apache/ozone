de7d1cde44d91b86eadcbf6da0b71d8dc894393c
deveshsingh <devmadhu@devesh_76@hotmail.com>
2023-03-31 21:51:40 +0530

    HDDS-8326. Container Level Info - OM DB Insights.

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
@@ -772,7 +772,7 @@ public class TestContainerEndpoint {
   }
 
   @Test
-  public void testGetContainerInsights_NonSCMContainers()
+  public void testGetContainerInsightsNonSCMContainers()
       throws IOException, TimeoutException {
     Map<Long, ContainerMetadata> omContainers =
         reconContainerMetadataManager.getContainers(-1, 0);
@@ -792,7 +792,7 @@ public class TestContainerEndpoint {
   }
 
   @Test
-  public void testGetContainerInsights_NonOMContainers()
+  public void testGetContainerInsightsNonOMContainers()
       throws IOException, TimeoutException {
     putContainerInfos(2);
     List<ContainerKeyPrefix> deletedContainerKeyList =
