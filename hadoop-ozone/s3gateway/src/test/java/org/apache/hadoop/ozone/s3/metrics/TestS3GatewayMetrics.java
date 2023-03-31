c2d75d373ad1172a9d923224715a466806424d35
tanvipenumudy <tanvi.penumudy@cloudera.com>
2023-03-31 09:49:20 +0530

    fix TestS3GatewayMetrics.testGetAclFailure post upstream/master merge

--- hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/metrics/TestS3GatewayMetrics.java
+++ hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/metrics/TestS3GatewayMetrics.java
@@ -241,10 +241,11 @@ public class TestS3GatewayMetrics {
           null, null, 0, null,
           null, null, null, "acl", null);
       fail();
-    } catch (OS3Exception ex) {
-      assertEquals(S3ErrorTable.NO_SUCH_BUCKET.getCode(), ex.getCode());
+    } catch (Exception ex) {
+      OS3Exception os3Ex = (OS3Exception) ex.getCause();
+      assertEquals(S3ErrorTable.NO_SUCH_BUCKET.getCode(), os3Ex.getCode());
       assertEquals(S3ErrorTable.NO_SUCH_BUCKET.getErrorMessage(),
-          ex.getErrorMessage());
+          os3Ex.getErrorMessage());
     }
     long curMetric = metrics.getGetAclFailure();
     assertEquals(1L, curMetric - oriMetric);
