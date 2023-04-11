bf35dc4cffa53bbfff5ace4dfa13d3262b6cd355
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2023-04-11 13:23:01 +0200

    HDDS-8337. ReplicationManager: MisReplicationHandler should throw an exception if partially successful (#4536)

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/MisReplicationHandler.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/container/replication/MisReplicationHandler.java
@@ -28,6 +28,7 @@ import org.apache.hadoop.hdds.scm.container.ContainerInfo;
 import org.apache.hadoop.hdds.scm.container.ContainerReplica;
 import org.apache.hadoop.hdds.scm.exceptions.SCMException;
 import org.apache.hadoop.hdds.scm.node.states.NodeNotFoundException;
+import org.apache.hadoop.hdds.scm.pipeline.InsufficientDatanodesException;
 import org.apache.hadoop.ozone.protocol.commands.ReplicateContainerCommand;
 import org.apache.ratis.protocol.exceptions.NotLeaderException;
 import org.slf4j.Logger;
@@ -195,17 +196,23 @@ public abstract class MisReplicationHandler implements
     List<DatanodeDetails> excludedDns = replicasToBeReplicated.stream()
             .map(ContainerReplica::getDatanodeDetails)
             .collect(Collectors.toList());
+    int requiredNodes = replicasToBeReplicated.size();
     List<DatanodeDetails> targetDatanodes = getTargetDatanodes(usedDns,
-           excludedDns, container, replicasToBeReplicated.size());
-    if (targetDatanodes.size() < replicasToBeReplicated.size()) {
+           excludedDns, container, requiredNodes);
+
+    int count = sendReplicateCommands(container, replicasToBeReplicated,
+        targetDatanodes);
+
+    int found = targetDatanodes.size();
+    if (found < requiredNodes) {
       LOG.warn("Placement Policy {} found only {} nodes for Container: {}," +
-               " number of required nodes: {}, usedNodes : {}",
-              containerPlacement.getClass(), targetDatanodes.size(),
-              container.getContainerID(), replicasToBeReplicated.size(),
-              usedDns);
+          " number of required nodes: {}, usedNodes : {}",
+          containerPlacement.getClass(), found,
+          container.getContainerID(), requiredNodes,
+          usedDns);
+      throw new InsufficientDatanodesException(requiredNodes, found);
     }
 
-    return sendReplicateCommands(container, replicasToBeReplicated,
-        targetDatanodes);
+    return count;
   }
 }
