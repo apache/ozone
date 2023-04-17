71694af0d98ba8af838395e770e2e9e48f7ed7e4
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2023-04-17 15:35:28 -0700

    HDDS-8253. Fix checkstyle issue

--- hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/Gateway.java
+++ hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/Gateway.java
@@ -20,7 +20,6 @@ package org.apache.hadoop.ozone.s3;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
