145cddbc6c8561f47fd9ae8bb6509f90ee109341
sumitagrawl <sumit.jecrc@gmail.com>
2023-04-26 09:08:33 +0530

    HDDS-8417. Cap on queue of commands at DN

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/statemachine/commandhandler/DeleteContainerCommandHandler.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/statemachine/commandhandler/DeleteContainerCommandHandler.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.time.Clock;
 import java.util.OptionalLong;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
