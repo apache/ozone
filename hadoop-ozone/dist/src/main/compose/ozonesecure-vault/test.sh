1532e0a3fc9cbe32da7469181901d5ab3f9903b3
Mikhail Pochatkin <MAlPochatkin@sberbank.ru>
2023-05-02 12:30:22 +0300

    HDDS-7815. Extract S3 secret layer
    Fix comments

--- hadoop-ozone/dist/src/main/compose/ozonesecure-vault/test.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure-vault/test.sh
@@ -25,35 +25,9 @@ source "$COMPOSE_DIR/../testlib.sh"
 
 export SECURITY_ENABLED=true
 
-: ${OZONE_BUCKET_KEY_NAME:=key1}
-
 start_docker_env
 
-execute_command_in_container kms hadoop key create ${OZONE_BUCKET_KEY_NAME}
-
-execute_robot_test scm kinit.robot
-
-execute_robot_test scm basic
-
-execute_robot_test scm security
-
-execute_robot_test scm -v SCHEME:ofs -v BUCKET_TYPE:bucket -N ozonefs-ofs-bucket ozonefs/ozonefs.robot
-
-#expects 4 pipelines, should be run before
-#admincli which creates STANDALONE pipeline
-execute_robot_test scm recon
-
-execute_robot_test scm admincli
-execute_robot_test scm spnego
-execute_robot_test scm snapshot/snapshot-acls.robot
-
-execute_robot_test scm httpfs
-
-# test replication
-docker-compose up -d --scale datanode=2
-execute_robot_test scm -v container:1 -v count:2 replication/wait.robot
-docker-compose up -d --scale datanode=3
-execute_robot_test scm -v container:1 -v count:3 replication/wait.robot
+execute_robot_test scm s3
 
 stop_docker_env
 
