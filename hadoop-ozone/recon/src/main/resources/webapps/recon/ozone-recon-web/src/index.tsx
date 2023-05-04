c4d348c30d982be0353e048f4a524ff47c57ba8e
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2023-05-04 22:45:37 +0500

    HDDS-8393. [Recon] Include Roboto font using fontsource instead (#4551)

--- hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/index.tsx
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/index.tsx
@@ -18,6 +18,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
 import './index.less';
 import App from './app';
 
