289c2071650ddfebb40ed4cc2e9cd2ec741d0fab
arafat <mohammad.khan@cloudera.com>
2023-05-26 12:23:15 +0530

    Rebased the PR

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/tasks/TableCountTask.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/tasks/TableCountTask.java
@@ -24,6 +24,8 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.hdds.utils.db.Table;
 import org.apache.hadoop.hdds.utils.db.TableIterator;
 import org.apache.hadoop.ozone.om.OMMetadataManager;
+import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
+import org.apache.hadoop.ozone.om.helpers.RepeatedOmKeyInfo;
 import org.apache.hadoop.ozone.recon.recovery.ReconOMMetadataManager;
 import org.hadoop.ozone.recon.schema.tables.daos.GlobalStatsDao;
 import org.hadoop.ozone.recon.schema.tables.pojos.GlobalStats;
@@ -39,8 +41,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
 import java.util.Map.Entry;
 
+import static org.apache.hadoop.ozone.om.OmMetadataManagerImpl.*;
 import static org.jooq.impl.DSL.currentTimestamp;
 import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.using;
@@ -76,19 +80,69 @@ public class TableCountTask implements ReconOmTask {
   @Override
   public Pair<String, Boolean> reprocess(OMMetadataManager omMetadataManager) {
     HashMap<String, Long> objectCountMap = initializeCountMap();
+    HashMap<String, Long> sizeCountMap = initializeSizeMap();
+
     for (String tableName : getTaskTables()) {
       Table table = omMetadataManager.getTable(tableName);
-      try (TableIterator keyIter = table.iterator()) {
-        long count = getCount(keyIter);
-        objectCountMap.put(getRowKeyFromTable(tableName), count);
+      if (table == null) {
+        LOG.error("Table " + tableName + " not found in OM Metadata.");
+        return new ImmutablePair<>(getTaskName(), false);
+      }
+
+      try {
+        if (getTablesRequiringSizeCalculation().contains(tableName)) {
+          Pair<Long, Long> details = getTableSizeAndCount(table);
+          objectCountMap.put(getRowKeyFromTable(tableName), details.getLeft());
+          sizeCountMap.put(getSizeKeyFromTable(tableName), details.getRight());
+        } else {
+          long count = getCount(table.iterator());
+          objectCountMap.put(getRowKeyFromTable(tableName), count);
+        }
       } catch (IOException ioEx) {
         LOG.error("Unable to populate Table Count in Recon DB.", ioEx);
         return new ImmutablePair<>(getTaskName(), false);
       }
     }
+
     writeCountsToDB(objectCountMap);
+    writeCountsToDB(sizeCountMap);
+
     LOG.info("Completed a 'reprocess' run of TableCountTask.");
     return new ImmutablePair<>(getTaskName(), true);
+  }
+
+  public Pair<Long, Long> getTableSizeAndCount(Table table) throws IOException {
+    long size = 0;
+    long count = 0;
+
+    if (table == null) {
+      return Pair.of(count, size);
+    }
+
+    TableIterator<String, ? extends Table.KeyValue<String, ?>> iterator =
+        table.iterator();
+    while (iterator.hasNext()) {
+      Table.KeyValue<String, ?> kv = iterator.next();
+
+      if (kv.getValue() instanceof OmKeyInfo) {
+        size += ((OmKeyInfo) kv.getValue()).getDataSize();
+      }
+
+      if (kv.getValue() instanceof RepeatedOmKeyInfo) {
+        size += ((RepeatedOmKeyInfo) kv.getValue()).getDataSize();
+      }
+
+      count++;  // Increment count for each row
+    }
+
+    return Pair.of(count, size);
+  }
+
+  public Collection<String> getTablesRequiringSizeCalculation() {
+    List<String> taskTables = new ArrayList<>();
+    taskTables.add(OPEN_KEY_TABLE);
+    taskTables.add(OPEN_FILE_TABLE);
+    return taskTables;
   }
 
   private long getCount(Iterator iterator) {
@@ -120,52 +174,78 @@ public class TableCountTask implements ReconOmTask {
   public Pair<String, Boolean> process(OMUpdateEventBatch events) {
     Iterator<OMDBUpdateEvent> eventIterator = events.getIterator();
     HashMap<String, Long> objectCountMap = initializeCountMap();
+    HashMap<String, Long> objectSizeMap =
+        initializeSizeMap();  // Initialize a new map for sizes
     final Collection<String> taskTables = getTaskTables();
+    final Collection<String> sizeRelatedTables =
+        getTablesRequiringSizeCalculation();
 
     while (eventIterator.hasNext()) {
       OMDBUpdateEvent<String, Object> omdbUpdateEvent = eventIterator.next();
-      // Filter event inside process method to avoid duping
+
       if (!taskTables.contains(omdbUpdateEvent.getTable())) {
         continue;
       }
+
       String rowKey = getRowKeyFromTable(omdbUpdateEvent.getTable());
+      String sizeKey = getSizeKeyFromTable(omdbUpdateEvent.getTable());
+
       try {
         switch (omdbUpdateEvent.getAction()) {
         case PUT:
           objectCountMap.computeIfPresent(rowKey, (k, count) -> count + 1L);
+
+          // Compute size if the table is size-related
+          if (sizeRelatedTables.contains(omdbUpdateEvent.getTable()) &&
+              omdbUpdateEvent.getValue() instanceof OmKeyInfo) {
+            objectSizeMap.computeIfPresent(sizeKey, (k, size) -> size +
+                ((OmKeyInfo) omdbUpdateEvent.getValue()).getDataSize());
+          }
           break;
 
         case DELETE:
-          // if value is null, it means that the volume / bucket / key
-          // is already deleted and does not exist in the OM database anymore.
           if (omdbUpdateEvent.getValue() != null) {
             String key = getRowKeyFromTable(omdbUpdateEvent.getTable());
             objectCountMap.computeIfPresent(key,
                 (k, count) -> count > 0 ? count - 1L : 0L);
+
+            // Compute size if the table is size-related
+            if (sizeRelatedTables.contains(omdbUpdateEvent.getTable()) &&
+                omdbUpdateEvent.getValue() instanceof OmKeyInfo) {
+              objectSizeMap.computeIfPresent(sizeKey, (k, size) -> size >
+                  ((OmKeyInfo) omdbUpdateEvent.getValue()).getDataSize() ?
+                  size -
+                      ((OmKeyInfo) omdbUpdateEvent.getValue()).getDataSize() :
+                  0L);
+            }
           }
           break;
 
-        default: LOG.trace("Skipping DB update event : Table: {}, Action: {}",
-            omdbUpdateEvent.getTable(), omdbUpdateEvent.getAction());
+        default:
+          LOG.trace("Skipping DB update event : Table: {}, Action: {}",
+              omdbUpdateEvent.getTable(), omdbUpdateEvent.getAction());
         }
       } catch (Exception e) {
-        LOG.error("Unexpected exception while processing the table {}, " +
-                "Action: {}", omdbUpdateEvent.getTable(),
-            omdbUpdateEvent.getAction(), e);
+        LOG.error(
+            "Unexpected exception while processing the table {}, Action: {}",
+            omdbUpdateEvent.getTable(), omdbUpdateEvent.getAction(), e);
         return new ImmutablePair<>(getTaskName(), false);
       }
     }
+
     writeCountsToDB(objectCountMap);
+    writeCountsToDB(objectSizeMap);  // Write size data to DB
 
     LOG.info("Completed a 'process' run of TableCountTask.");
     return new ImmutablePair<>(getTaskName(), true);
   }
 
+
   private void writeCountsToDB(Map<String, Long> objectCountMap) {
     List<GlobalStats> insertGlobalStats = new ArrayList<>();
     List<GlobalStats> updateGlobalStats = new ArrayList<>();
 
-    for (Entry<String, Long> entry: objectCountMap.entrySet()) {
+    for (Entry<String, Long> entry : objectCountMap.entrySet()) {
       Timestamp now =
           using(sqlConfiguration).fetchValue(select(currentTimestamp()));
       GlobalStats record = globalStatsDao.fetchOneByKey(entry.getKey());
@@ -187,15 +267,29 @@ public class TableCountTask implements ReconOmTask {
   private HashMap<String, Long> initializeCountMap() {
     Collection<String> tables = getTaskTables();
     HashMap<String, Long> objectCountMap = new HashMap<>(tables.size());
-    for (String tableName: tables) {
+    for (String tableName : tables) {
       String key = getRowKeyFromTable(tableName);
       objectCountMap.put(key, getCountForKey(key));
     }
     return objectCountMap;
   }
 
+  private HashMap<String, Long> initializeSizeMap() {
+    Collection<String> tables = getTablesRequiringSizeCalculation();
+    HashMap<String, Long> sizeCountMap = new HashMap<>(tables.size());
+    for (String tableName : tables) {
+      String key = getSizeKeyFromTable(tableName);
+      sizeCountMap.put(key, getCountForKey(key));
+    }
+    return sizeCountMap;
+  }
+
   public static String getRowKeyFromTable(String tableName) {
     return tableName + "Count";
+  }
+
+  public static String getSizeKeyFromTable(String tableName) {
+    return tableName + "DataSize";
   }
 
   /**
