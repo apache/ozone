f1c7b87a2e6cb542d63b068deae530e975942b5e
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2023-05-30 23:35:22 +0800

    Revert SchemaOneChunkInfoListCodec.

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/metadata/SchemaOneChunkInfoListCodec.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/metadata/SchemaOneChunkInfoListCodec.java
@@ -1,13 +1,13 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ *  with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +17,15 @@
  */
 package org.apache.hadoop.ozone.container.metadata;
 
+import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos;
 import org.apache.hadoop.hdds.utils.db.Codec;
 import org.apache.hadoop.ozone.container.common.helpers.ChunkInfoList;
+import org.apache.ratis.thirdparty.com.google.protobuf.InvalidProtocolBufferException;
+
+import java.io.IOException;
 
 /**
- * Codec for parsing {@link ChunkInfoList} objects from data
+ * Codec for parsing {@link ContainerProtos.ChunkInfoList} objects from data
  * that may have been written using schema version one. Before upgrading
  * schema versions, deleted block IDs were stored with a duplicate copy of
  * their ID as the value in the database. After upgrading the code, any
@@ -39,13 +43,38 @@ import org.apache.hadoop.ozone.container.common.helpers.ChunkInfoList;
  * future, callers should be aware that the associated chunk information may not
  * always be present.
  */
-public final class SchemaOneChunkInfoListCodec {
-  /** @return {@link ChunkInfoList#getCodec()}. */
+public final class SchemaOneChunkInfoListCodec implements Codec<ChunkInfoList> {
+
+  private static final Codec<ChunkInfoList> INSTANCE =
+      new SchemaOneChunkInfoListCodec();
+
   public static Codec<ChunkInfoList> get() {
-    return ChunkInfoList.getCodec();
+    return INSTANCE;
   }
 
   private SchemaOneChunkInfoListCodec() {
     // singleton
+  }
+
+  @Override
+  public byte[] toPersistedFormat(ChunkInfoList chunkList) {
+    return chunkList.getProtoBufMessage().toByteArray();
+  }
+
+  @Override
+  public ChunkInfoList fromPersistedFormat(byte[] rawData) throws IOException {
+    try {
+      return ChunkInfoList.getFromProtoBuf(
+              ContainerProtos.ChunkInfoList.parseFrom(rawData));
+    } catch (InvalidProtocolBufferException ex) {
+      throw new IOException("Invalid chunk information. " +
+              "This data may have been written using datanode " +
+              "schema version one, which did not save chunk information.", ex);
+    }
+  }
+
+  @Override
+  public ChunkInfoList copyObject(ChunkInfoList object) {
+    throw new UnsupportedOperationException();
   }
 }
