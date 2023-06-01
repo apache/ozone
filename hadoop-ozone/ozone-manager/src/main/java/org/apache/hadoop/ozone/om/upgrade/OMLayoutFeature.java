761531a4f5418af8ca11f7deff866fcc2c540fca
sumitagrawl <sumit.jecrc@gmail.com>
2023-06-01 19:24:37 +0530

    HDDS-8742. Quota repair counts of namespace and space usages

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
@@ -40,7 +40,7 @@ public enum OMLayoutFeature implements LayoutFeature {
 
   MULTITENANCY_SCHEMA(3, "Multi-Tenancy Schema"),
 
-  HSYNC(4, "Support hsync"),  
+  HSYNC(4, "Support hsync"),
 
   FILESYSTEM_SNAPSHOT(5, "Ozone version supporting snapshot"),
 
