febb54a723b964b1a5a07e1a9b45e8b5ce8be2de
deveshsingh <devmadhu@devesh_76@hotmail.com>
2023-06-06 16:38:31 +0530

    HDDS-8327. Fix Review Comments.

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/tasks/CleanUpTask.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/tasks/CleanUpTask.java
@@ -24,8 +24,10 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.hdds.utils.db.DBStore;
 import org.apache.hadoop.hdds.utils.db.Table;
 import org.apache.hadoop.ozone.om.OMMetadataManager;
+import org.apache.hadoop.ozone.om.helpers.OmBucketInfo;
 import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
-import org.apache.hadoop.ozone.om.helpers.WithParentObjectId;
+import org.apache.hadoop.ozone.om.helpers.WithObjectID;
+import org.apache.hadoop.ozone.recon.api.types.NSSummary;
 import org.apache.hadoop.ozone.recon.api.types.OrphanKeyMetaData;
 import org.apache.hadoop.ozone.recon.spi.ReconNamespaceSummaryManager;
 import org.apache.hadoop.ozone.recon.spi.impl.ReconDBProvider;
@@ -38,22 +40,25 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import static org.apache.hadoop.ozone.om.OmMetadataManagerImpl.BUCKET_TABLE;
 import static org.apache.hadoop.ozone.om.OmMetadataManagerImpl.DELETED_DIR_TABLE;
 import static org.apache.hadoop.ozone.recon.spi.impl.ReconDBDefinition.ORPHAN_KEYS_METADATA;
 
 /**
- * Task class to iterate over the OM DB and detect the orphan keys metadata.
+ * Task class to iterate over the OM DB and cleanup below data:.
+ *  1. namespaceSummaryTable data.
+ *  2. orphan_keys_metadata table data.
  */
-public class OrphanKeyDetectionTask implements ReconOmTask {
+public class CleanUpTask implements ReconOmTask {
 
   private static final Logger LOG =
-      LoggerFactory.getLogger(OrphanKeyDetectionTask.class);
+      LoggerFactory.getLogger(CleanUpTask.class);
   private final DBStore reconDbStore;
   private ReconNamespaceSummaryManager reconNamespaceSummaryManager;
   private final Table<Long, OrphanKeyMetaData> orphanKeysMetaDataTable;
 
   @Inject
-  public OrphanKeyDetectionTask(
+  public CleanUpTask(
       ReconDBProvider reconDBProvider,
       ReconNamespaceSummaryManager reconNamespaceSummaryManager)
       throws IOException {
@@ -71,6 +76,7 @@ public class OrphanKeyDetectionTask implements ReconOmTask {
   public Collection<String> getTaskTables() {
     List<String> taskTables = new ArrayList<>();
     taskTables.add(DELETED_DIR_TABLE);
+    taskTables.add(BUCKET_TABLE);
     return taskTables;
   }
 
@@ -86,34 +92,51 @@ public class OrphanKeyDetectionTask implements ReconOmTask {
     final Collection<String> taskTables = getTaskTables();
     while (eventIterator.hasNext()) {
       OMDBUpdateEvent<String, ? extends
-          WithParentObjectId> omdbUpdateEvent = eventIterator.next();
+          WithObjectID> omdbUpdateEvent = eventIterator.next();
       OMDBUpdateEvent.OMDBUpdateAction action = omdbUpdateEvent.getAction();
       // we only process updates on OM's deletedDirectoryTable
       String table = omdbUpdateEvent.getTable();
       if (!taskTables.contains(table)) {
         continue;
       }
+      boolean updateOnBucketTable = table.equals(BUCKET_TABLE);
 
       try {
-        // key update on deletedDirectoryTable
-        OMDBUpdateEvent<String, OmKeyInfo> keyTableUpdateEvent =
-            (OMDBUpdateEvent<String, OmKeyInfo>) omdbUpdateEvent;
-        OmKeyInfo updatedKeyInfo = keyTableUpdateEvent.getValue();
+        if (table.equals(DELETED_DIR_TABLE)) {
+          // key update on deletedDirectoryTable
+          OMDBUpdateEvent<String, OmKeyInfo> deletedDirTableUpdateEvent =
+              (OMDBUpdateEvent<String, OmKeyInfo>) omdbUpdateEvent;
+          OmKeyInfo updatedKeyInfo = deletedDirTableUpdateEvent.getValue();
 
-        switch (action) {
-        case PUT:
-          handlePutDeleteDirEvent(updatedKeyInfo);
-          break;
+          switch (action) {
+          case PUT:
+            handlePutDeleteDirEvent(updatedKeyInfo);
+            break;
+          case DELETE:
+          case UPDATE:
+            break;
+          default:
+            LOG.debug("Skipping DB update event : {}",
+                omdbUpdateEvent.getAction());
+          }
+        }
+        if (table.equals(BUCKET_TABLE)) {
+          // key update on Bucket Table
+          OMDBUpdateEvent<String, OmBucketInfo> bucketTableUpdateEvent =
+              (OMDBUpdateEvent<String, OmBucketInfo>) omdbUpdateEvent;
+          OmBucketInfo updatedBucketInfo = bucketTableUpdateEvent.getValue();
 
-        case DELETE:
-          break;
-
-        case UPDATE:
-          break;
-
-        default:
-          LOG.debug("Skipping DB update event : {}",
-              omdbUpdateEvent.getAction());
+          switch (action) {
+          case PUT:
+          case UPDATE:
+            break;
+          case DELETE:
+            handleBucketDeleteEvent(updatedBucketInfo);
+            break;
+          default:
+            LOG.debug("Skipping DB update event : {}",
+                omdbUpdateEvent.getAction());
+          }
         }
       } catch (Exception ex) {
         LOG.error("Unable to process Namespace Summary data in Recon DB. ", ex);
@@ -123,21 +146,34 @@ public class OrphanKeyDetectionTask implements ReconOmTask {
     return new ImmutablePair<>(getTaskName(), true);
   }
 
-  private void handlePutDeleteDirEvent(OmKeyInfo updatedKeyInfo) {
-    long objectID = updatedKeyInfo.getObjectID();
+  private void handleBucketDeleteEvent(OmBucketInfo updatedBucketInfo) {
+    long objectID = updatedBucketInfo.getObjectID();
+    removeOrphanAndNSSummaryParentEntry(objectID);
+  }
+
+  private void removeOrphanAndNSSummaryParentEntry(long objectID) {
     try {
       OrphanKeyMetaData orphanKeyMetaData =
           orphanKeysMetaDataTable.get(objectID);
       if (null != orphanKeyMetaData) {
         orphanKeysMetaDataTable.delete(objectID);
       }
+      NSSummary nsSummary = reconNamespaceSummaryManager.getNSSummary(objectID);
+      if (null != nsSummary) {
+        reconNamespaceSummaryManager.deleteNSSummary(objectID);
+      }
     } catch (IOException e) {
       // Logging as Info as we don't want to log as error when any dir not
       // found in orphan candidate metadata set. This is done to avoid 2
       // rocks DB operations - check if present and then delete operation.
-      LOG.info("Deleted Dir objectId {} may not be found in orphan map.",
-          objectID);
+      LOG.info("ObjectId {} may not be found in orphan metadata table or " +
+          "namespaceSummaryTable.", objectID);
     }
+  }
+
+  private void handlePutDeleteDirEvent(OmKeyInfo updatedKeyInfo) {
+    long objectID = updatedKeyInfo.getObjectID();
+    removeOrphanAndNSSummaryParentEntry(objectID);
   }
 
   /**
