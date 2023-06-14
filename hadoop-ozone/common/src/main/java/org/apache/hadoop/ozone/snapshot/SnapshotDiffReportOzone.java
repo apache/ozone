4c53136d1d1f2d28610304d0662ae742e6034008
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2023-06-13 17:48:44 -0700

    HDDS-8739. Fix Diff entry representation

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/snapshot/SnapshotDiffReportOzone.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/snapshot/SnapshotDiffReportOzone.java
@@ -24,12 +24,16 @@ import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.utils.db.Codec;
 import org.apache.hadoop.hdds.utils.db.DelegatedCodec;
 import org.apache.hadoop.hdds.utils.db.Proto2Codec;
+import org.apache.hadoop.hdfs.DFSUtilClient;
+import org.apache.hadoop.hdfs.protocol.SnapshotDiffReport;
 import org.apache.hadoop.ozone.OFSPath;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.DiffReportEntryProto;
 import org.apache.hadoop.ozone.protocol.proto.OzoneManagerProtocolProtos.SnapshotDiffReportProto;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -204,6 +208,44 @@ public class SnapshotDiffReportOzone
    */
   public void aggregate(SnapshotDiffReportOzone diffReport) {
     this.getDiffList().addAll(diffReport.getDiffList());
+  }
+
+  public static class DiffReportEntryOzone extends DiffReportEntry {
+
+    public DiffReportEntryOzone(DiffType type, byte[] sourcePath) {
+      super(type, sourcePath);
+    }
+
+    public DiffReportEntryOzone(DiffType type, byte[][] sourcePathComponents) {
+      super(type, sourcePathComponents);
+    }
+
+    public DiffReportEntryOzone(DiffType type, byte[] sourcePath,
+                                byte[] targetPath) {
+      super(type, sourcePath, targetPath);
+    }
+
+    public DiffReportEntryOzone(DiffType type, byte[][] sourcePathComponents,
+                                byte[][] targetPathComponents) {
+      super(type, sourcePathComponents, targetPathComponents);
+    }
+
+    static String getPathString(byte[] path) {
+      String pathStr = DFSUtilClient.bytes2String(path);
+      return pathStr.isEmpty() ? "." : Paths.get(pathStr)
+          .toAbsolutePath().toString();
+    }
+
+    @Override
+    public String toString() {
+      String str = this.getType().getLabel() + "\t" +
+          getPathString(this.getSourcePath());
+      if (this.getType() == SnapshotDiffReport.DiffType.RENAME) {
+        str = str + " -> " + getPathString(this.getTargetPath());
+      }
+
+      return str;
+    }
   }
 
 
