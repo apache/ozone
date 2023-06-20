46de8b7e9bc90295c3fd30621ed46362fc9874bf
smitajoshi <smitha.joshi@cloudera.com>
2023-06-20 12:55:37 +0530

    HDDS-8851. Recon Heatmap UI - User cannot navigate back from No Data Bug Fix in Review

--- hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/views/heatMap/heatMapConfiguration.tsx
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/views/heatMap/heatMapConfiguration.tsx
@@ -82,8 +82,7 @@ export default class HeatMapConfiguration extends Component {
           tileStrokeWidth: 1,
           colorDomain: [0.000, 0.050, 0.100, 0.150, 0.200, 0.250, 0.300, 0.350, 0.400, 0.450, 0.500, 0.550, 0.600, 0.650, 0.700, 0.750, 0.800, 0.850, 0.900, 0.950, 1.000],
           colorRange: [...colorRange1],
-          //groupFill: 'black',
-          nodePadding: 1.5, //Disatnce between two nodes
+          nodePadding: 1.5, //Distance between two nodes
           nodeGap: '10',
           labelShadow: { enabled: false }, //labels shadow
           highlightStyle: {
