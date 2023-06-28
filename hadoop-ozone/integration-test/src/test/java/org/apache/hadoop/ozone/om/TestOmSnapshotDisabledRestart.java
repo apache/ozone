e4930cbabb4f24085b1e9028afc4f312fd8bfbaa
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2023-06-27 20:48:17 -0700

    Disable added integration test due to potential Ratis issue: HDDS-8945.

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestOmSnapshotDisabledRestart.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestOmSnapshotDisabledRestart.java
@@ -29,6 +29,7 @@ import org.apache.ozone.test.LambdaTestUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -38,6 +39,7 @@ import java.util.UUID;
  * Integration test to verify that if snapshot feature is disabled, OM start up
  * will fail when there are still snapshots remaining.
  */
+@Disabled("HDDS-8945")
 public class TestOmSnapshotDisabledRestart {
 
   private static MiniOzoneHAClusterImpl cluster = null;
