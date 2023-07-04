0e0b8e45db40ed05afe0a68c4a9bab9548b9bdb9
Sammi Chen <sammichen@apache.org>
2023-07-04 22:35:16 +0800

    increase max certificate lifetime to reduce the possibility of random failure because of rotation is fast and some daemons start slowly

--- hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-root-ca-rotation.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-root-ca-rotation.sh
@@ -40,7 +40,7 @@ execute_robot_test scm1.org kinit.robot
 wait_for_execute_command scm1.org 30 "jps | grep StorageContainerManagerStarter | sed 's/StorageContainerManagerStarter//' | xargs  | xargs -I {} jstack {} | grep 'RootCARotationManager-Active'"
 
 # wait and verify root CA is rotated
-wait_for_execute_command scm1.org 180 "ozone admin cert info 2"
+wait_for_execute_command scm1.org 240 "ozone admin cert info 2"
 
 # transfer leader to scm2.org
 execute_robot_test scm1.org scmha/scm-leader-transfer.robot
@@ -53,7 +53,7 @@ execute_commands_in_container scm1.org "ozone sh volume create /r-v1 && ozone sh
 execute_robot_test scm1.org admincli/pipeline.robot
 
 # wait for next root CA rotation
-wait_for_execute_command scm1.org 180 "ozone admin cert info 3"
+wait_for_execute_command scm1.org 240 "ozone admin cert info 3"
 
 # bootstrap new SCM4 and verify certificate
 docker-compose up -d scm4.org
@@ -63,7 +63,7 @@ wait_for_execute_command scm4.org 120 "ozone admin scm roles | grep scm4.org"
 wait_for_execute_command scm4.org 30 "ozone admin cert list --role=scm | grep scm4.org"
 
 # wait for next root CA rotation
-wait_for_execute_command scm4.org 180 "ozone admin cert info 4"
+wait_for_execute_command scm4.org 240 "ozone admin cert info 4"
 
 #transfer leader to scm4.org
 execute_robot_test scm4.org -v "TARGET_SCM:scm4.org" scmha/scm-leader-transfer.robot
@@ -78,7 +78,7 @@ execute_robot_test scm3.org kinit.robot
 execute_robot_test scm4.org  -v "TARGET_SCM:scm3.org" scmha/scm-leader-transfer.robot
 
 # wait for next root CA rotation
-wait_for_execute_command scm3.org 180 "ozone admin cert info 5"
+wait_for_execute_command scm3.org 240 "ozone admin cert info 5"
 
 #decomission scm1.org
 execute_robot_test scm3.org scmha/scm-decommission.robot
