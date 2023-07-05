3030f7423c84981403e733b7f86cff695daac1b2
guohao1 <guohao1@360.cn>
2023-07-05 17:22:51 +0800

    change config(default datastream port not open)

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/OzoneConfigKeys.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/OzoneConfigKeys.java
@@ -97,7 +97,7 @@ public final class OzoneConfigKeys {
   public static final String DFS_CONTAINER_RATIS_DATASTREAM_ENABLED
       = "dfs.container.ratis.datastream.enabled";
   public static final boolean DFS_CONTAINER_RATIS_DATASTREAM_ENABLED_DEFAULT
-      = false;
+      = true;
   public static final String DFS_CONTAINER_RATIS_DATASTREAM_PORT
       = "dfs.container.ratis.datastream.port";
   public static final int DFS_CONTAINER_RATIS_DATASTREAM_PORT_DEFAULT
