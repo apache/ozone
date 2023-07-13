bff7281fca54c9355b0d745acf7f85d83e45748b
Istvan Fajth <fapifta@gmail.com>
2023-07-13 11:32:48 +0200

    Fix container reference in ozonesecure-ha test. Check for all OMs sees the 4th root ca before checking client operations.

--- hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-root-ca-rotation.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-root-ca-rotation.sh
@@ -66,7 +66,9 @@ wait_for_execute_command scm4.org 30 "ozone admin cert list --role=scm | grep sc
 # wait for next root CA rotation
 wait_for_execute_command scm4.org 240 "ozone admin cert info 4"
 
-wait_for_execute_command om 30 "find /data/metadata/om/certs/ROOTCA-4.crt"
+wait_for_execute_command om1 30 "find /data/metadata/om/certs/ROOTCA-4.crt"
+wait_for_execute_command om2 30 "find /data/metadata/om/certs/ROOTCA-4.crt"
+wait_for_execute_command om3 30 "find /data/metadata/om/certs/ROOTCA-4.crt"
 execute_robot_test scm4.org -v PREFIX:"rootca2" certrotation/root-ca-rotation-client-checks.robot
 
 #transfer leader to scm4.org
