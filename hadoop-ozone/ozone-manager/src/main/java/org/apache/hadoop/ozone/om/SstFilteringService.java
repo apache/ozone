cfbc54aefa0c910233617daf432dc35f8505ba3c
Swaminathan Balachandran <sbalachandran@cloudera.com>
2023-08-07 15:50:27 -0700

    HDDS-9129. Removed Filtered Filename usage

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/SstFilteringService.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/SstFilteringService.java
@@ -41,8 +41,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
