71039732410221000eb4714e3a7144dffa4e69af
tejaskriya <tejaskriya.madhan@cloudera.com>
2023-08-08 15:34:21 +0530

    HDDS-8011. IllegalArgumentException logged for invalid user-defined metadata

--- hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/exception/S3ErrorTable.java
+++ hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/exception/S3ErrorTable.java
@@ -126,6 +126,10 @@ public final class S3ErrorTable {
   public static final OS3Exception NO_OVERWRITE = new OS3Exception(
       "Conflict", "Cannot overwrite file with directory", HTTP_CONFLICT);
 
+  public static final OS3Exception METADATA_TOO_LARGE = new OS3Exception(
+      "MetadataTooLarge", "Your metadata headers exceed the maximum allowed " +
+      "metadata size.", HTTP_BAD_REQUEST);
+
   public static OS3Exception newError(OS3Exception e, String resource) {
     return newError(e, resource, null);
   }
