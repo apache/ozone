4125e66f97445cd74eb9fe1669cbc92ea3b2167b
smitajoshi <smitha.joshi@cloudera.com>
2023-08-31 23:50:58 +0530

    HDDS-9161. Recon Pipelines datanode columns search does not work After rebasing with Master

--- hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/utils/columnSearch.tsx
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/utils/columnSearch.tsx
@@ -72,7 +72,7 @@ class ColumnSearch extends React.PureComponent {
     onFilter: (value: string, record: any) => {
       if (record[dataIndex] !== undefined || record[dataIndex] !== null) {
           return typeof (record[dataIndex]) === typeof {}
-            ? Boolean (record[dataIndex].find(item => Object.values(item).toString().toLowerCase().includes(value.toLowerCase())) ? true :false)
+            ? Boolean (record[dataIndex].find(item => Object.values(item).toString().toLowerCase().includes(value.toLowerCase())))
             : record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
         }
         else {
