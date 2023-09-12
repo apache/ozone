bdef45c175af0e4ea1c5c84a15989533a2a45470
tejaskriya <tejaskriya.madhan@cloudera.com>
2023-09-12 11:04:38 +0530

    remove extra character

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/OzoneConsts.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ozone/OzoneConsts.java
@@ -436,7 +436,7 @@ public final class OzoneConsts {
 
   // Supported characters for resource names when isStrictS3 is set as false
   public static final Pattern SUPPORTED_CHARACTER_CHECK_REGEX  =
-      Pattern.compile("[a-z0-9.-]]");
+      Pattern.compile("[a-z0-9.-]");
 
   public static final String FS_FILE_COPYING_TEMP_SUFFIX = "._COPYING_";
 
