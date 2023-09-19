041fed250fe7dc3189a78c45e208854a00a0decb
Devesh Kumar Singh <devesh_76@hotmail.com>
2023-09-20 01:27:45 +0530

    HDDS-6618. Require successful basic checks for long-running tests (#5267)

--- hadoop-ozone/dev-support/checks/unit.sh
+++ hadoop-ozone/dev-support/checks/unit.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#checks:unit
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CHECK=unit
 source "${DIR}/junit.sh" -pl \!:ozone-integration-test,\!:mini-chaos-tests -DexcludedGroups=native "$@"
