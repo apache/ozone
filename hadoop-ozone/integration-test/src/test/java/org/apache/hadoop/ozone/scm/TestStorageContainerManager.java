9943c3e8d77cf37965934a2108ca51355376d519
deveshsingh <devmadhu@devesh_76@hotmail.com>
2023-10-06 09:11:56 +0530

    HDDS-8492. Intermittent timeout in TestStorageContainerManager#testBlockDeletionTransactions.

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/scm/TestStorageContainerManager.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/scm/TestStorageContainerManager.java
@@ -302,7 +302,8 @@ public class TestStorageContainerManager {
     // is created by default.
     conf.setInt(ScmConfigKeys.OZONE_SCM_PIPELINE_OWNER_CONTAINER_COUNT,
         numKeys);
-
+    conf.setTimeDuration(OZONE_SCM_HA_DBTRANSACTIONBUFFER_FLUSH_INTERVAL,
+        1000, TimeUnit.MILLISECONDS);
     MiniOzoneCluster cluster = MiniOzoneCluster.newBuilder(conf)
         .setHbInterval(100)
         .build();
