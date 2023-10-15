5582b49bd25d02f048571a0930305ecde96f1502
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2023-10-15 06:34:19 -0700

    HDDS-9454. TypedTable prefix iterator may leak CodecBuffer. (#5442)

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/TypedTable.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/db/TypedTable.java
@@ -414,7 +414,12 @@ public class TypedTable<KEY, VALUE> implements Table<KEY, VALUE> {
       throws IOException {
     if (supportCodecBuffer) {
       final CodecBuffer prefixBuffer = encodeKeyCodecBuffer(prefix);
-      return newCodecBufferTableIterator(rawTable.iterator(prefixBuffer));
+      try {
+        return newCodecBufferTableIterator(rawTable.iterator(prefixBuffer));
+      } catch (Throwable t) {
+        prefixBuffer.release();
+        throw t;
+      }
     } else {
       final byte[] prefixBytes = encodeKey(prefix);
       return new TypedTableIterator(rawTable.iterator(prefixBytes));
