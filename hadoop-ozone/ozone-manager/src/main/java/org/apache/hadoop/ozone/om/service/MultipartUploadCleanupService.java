c9cb576346f93311fd7b2629f22fd9fa6d0ff7a5
arafat <mohammad.khan@cloudera.com>
2023-10-16 18:27:10 +0530

    Rebased the PR with the latest changes

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/service/MultipartUploadCleanupService.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/service/MultipartUploadCleanupService.java
@@ -74,7 +74,7 @@ public class MultipartUploadCleanupService extends BackgroundService {
   public MultipartUploadCleanupService(long interval, TimeUnit unit,
         long timeout, OzoneManager ozoneManager, ConfigurationSource conf) {
     super("MultipartUploadCleanupService", interval, unit,
-        MPU_INFO_DELETING_CORE_POOL_SIZE, timeout);
+        MPU_INFO_DELETING_CORE_POOL_SIZE, timeout, ozoneManager.getNodeDetails().threadNamePrefix());
     this.ozoneManager = ozoneManager;
     this.keyManager = ozoneManager.getKeyManager();
 
