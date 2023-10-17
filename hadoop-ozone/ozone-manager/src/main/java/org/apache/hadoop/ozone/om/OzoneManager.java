c94ad2d43031b835f57f77dacee6cb6fcb53f55c
arafat <mohammad.khan@cloudera.com>
2023-10-17 13:25:51 +0530

    Changed string

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
@@ -3246,7 +3246,7 @@ public final class OzoneManager extends ServiceRuntimeInfoImpl
     // Trigger Ranger BG Sync
     if (noWait) {
       final Thread t = new Thread(bgSync::triggerRangerSyncOnce,
-          threadPrefix + "TriggerRangerSync");
+          threadPrefix + "RangerSync");
       t.start();
       LOG.info("User '{}' manually triggered Multi-Tenancy Ranger Sync "
           + "in a new thread, tid={}", ugi, t.getId());
