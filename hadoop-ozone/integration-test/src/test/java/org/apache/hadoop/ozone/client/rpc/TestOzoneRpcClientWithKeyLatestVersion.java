7f7bcd0387e6814a0159e1392f01bc7906f15ff4
Doroszlai, Attila <adoroszlai@apache.org>
2023-11-23 22:01:58 +0100

    Migrate TestReadRetries to JUnit5
    [skip ci]

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestOzoneRpcClientWithKeyLatestVersion.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestOzoneRpcClientWithKeyLatestVersion.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -135,8 +135,8 @@ class TestOzoneRpcClientWithKeyLatestVersion {
     }
   }
 
-  private void assertKeyContent(OzoneBucket bucket, String key, byte[] expected)
-      throws Exception {
+  public static void assertKeyContent(OzoneBucket bucket, String key,
+      byte[] expected) throws Exception {
     try (InputStream in = bucket.readKey(key)) {
       assertArrayEquals(expected, IOUtils.readFully(in, expected.length));
     }
