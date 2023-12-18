48ad482c640bd1f7998ac720e3adb2f23c02db23
deveshsingh <devmadhu@devesh_76@hotmail.com>
2023-12-18 11:19:13 +0530

    HDDS-5604.Intermittent failure in TestPipelineClose.

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestPipelineClose.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestPipelineClose.java
@@ -247,7 +247,7 @@ public class TestPipelineClose {
     xceiverRatis.handleNodeLogFailure(groupId, null);
 
     // verify SCM receives a pipeline action report "immediately"
-    Mockito.verify(pipelineActionTest, Mockito.timeout(100))
+    Mockito.verify(pipelineActionTest, Mockito.timeout(1000))
         .onMessage(
             actionCaptor.capture(),
             Mockito.any(EventPublisher.class));
