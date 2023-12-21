e4c250087ebf60a63aec54417e588af8423a763a
Duong Nguyen <duongnt.is@gmail.com>
2023-12-21 13:19:58 -0800

    Checksylte.

--- hadoop-hdds/managed-rocksdb/src/main/java/org/apache/hadoop/hdds/utils/db/managed/ManagedRocksObjectUtils.java
+++ hadoop-hdds/managed-rocksdb/src/main/java/org/apache/hadoop/hdds/utils/db/managed/ManagedRocksObjectUtils.java
@@ -18,14 +18,12 @@
  */
 package org.apache.hadoop.hdds.utils.db.managed;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.hdds.HddsUtils;
 import org.apache.hadoop.hdds.resource.LeakDetector;
 import org.apache.hadoop.hdds.resource.LeakTracker;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionTimeoutException;
 import org.rocksdb.RocksDB;
-import org.rocksdb.RocksObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
