fee40027544f7ddfece84facfb186a043a452dcc
Doroszlai, Attila <adoroszlai@apache.org>
2024-01-07 11:02:29 +0100

    HDDS-10074. Migrate FS contract tests to JUnit5

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/contract/TestOzoneContractLegacy.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/contract/TestOzoneContractLegacy.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,50 +6,38 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.apache.hadoop.fs.ozone.contract.rooted;
-
-import java.io.IOException;
+package org.apache.hadoop.fs.ozone.contract;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.contract.AbstractContractGetFileStatusTest;
 import org.apache.hadoop.fs.contract.AbstractFSContract;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_DEFAULT_BUCKET_LAYOUT;
+import static org.apache.hadoop.ozone.om.helpers.BucketLayout.LEGACY;
 
 /**
- * Ozone contract tests covering getFileStatus.
+ * Tests O3FS with LEGACY bucket.
  */
-public class ITestRootedOzoneContractGetFileStatus
-    extends AbstractContractGetFileStatusTest {
+class TestOzoneContractLegacy extends AbstractOzoneContractTest {
 
-  @BeforeClass
-  public static void createCluster() throws IOException {
-    RootedOzoneContract.createCluster();
-  }
-
-  @AfterClass
-  public static void teardownCluster() {
-    RootedOzoneContract.destroyCluster();
+  @Override
+  OzoneConfiguration createOzoneConfig() {
+    OzoneConfiguration conf = createBaseConfiguration();
+    conf.set(OZONE_DEFAULT_BUCKET_LAYOUT, LEGACY.name());
+    return conf;
   }
 
   @Override
-  protected AbstractFSContract createContract(Configuration conf) {
-    return new RootedOzoneContract(conf);
-  }
-
-  @Override
-  protected Configuration createConfiguration() {
-    return super.createConfiguration();
+  AbstractFSContract createOzoneContract(Configuration conf) {
+    return new OzoneContract(getCluster());
   }
 }
