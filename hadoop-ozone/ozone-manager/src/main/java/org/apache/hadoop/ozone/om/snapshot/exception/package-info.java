13b9f2771deccf765eb39e4b2728d3588440e223
Swaminathan Balachandran <sbalachandran@cloudera.com>
2024-01-08 13:46:05 -0800

    HDDS-8202. Fix checkstyle

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/snapshot/exception/package-info.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/snapshot/exception/package-info.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,8 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.hadoop.ozone.om.snapshot.exception;
 
 /**
- * This package contains classes related to snapshot exceptions.
+ * This package contains OM Snapshot Exception related classes.
  */
