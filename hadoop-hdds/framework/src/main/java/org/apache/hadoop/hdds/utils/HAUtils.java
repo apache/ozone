df58ab3412829abd0a8bbe4354cea4bbe30805ff
George Jahad <george@georgejahad.com>
2024-01-22 13:53:58 -0800

    disabling compaction on loading trn #

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/HAUtils.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/HAUtils.java
@@ -316,6 +316,7 @@ public final class HAUtils {
     DBStoreBuilder dbStoreBuilder =
         DBStoreBuilder.newBuilder(configuration, rocksDBConfiguration)
             .setName(dbName)
+            .disableDefaultCFAutoCompaction(true)
             .setPath(Paths.get(metaDir.getPath()));
     // Add column family names and codecs.
     for (DBColumnFamilyDefinition columnFamily : definition
