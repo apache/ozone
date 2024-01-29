471f25f5baf6dd9aadf77f256695d90f32db112f
Doroszlai, Attila <adoroszlai@apache.org>
2024-01-29 11:36:01 +0100

    fix findbugs: remove unused items

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/SCMNodeDetails.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/SCMNodeDetails.java
@@ -19,8 +19,6 @@ package org.apache.hadoop.hdds.scm.ha;
 
 import org.apache.hadoop.net.NetUtils;
 import org.apache.hadoop.hdds.NodeDetails;
-import org.apache.ratis.protocol.RaftGroup;
-import org.apache.ratis.protocol.RaftPeerId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -84,8 +82,6 @@ public final class SCMNodeDetails extends NodeDetails {
     private String clientProtocolServerAddressKey;
     private InetSocketAddress datanodeProtocolServerAddress;
     private String datanodeAddressKey;
-    private RaftGroup raftGroup;
-    private RaftPeerId selfPeerId;
 
     public Builder setDatanodeAddressKey(String addressKey) {
       this.datanodeAddressKey = addressKey;
@@ -114,16 +110,6 @@ public final class SCMNodeDetails extends NodeDetails {
 
     public Builder setDatanodeProtocolServerAddress(InetSocketAddress address) {
       this.datanodeProtocolServerAddress = address;
-      return this;
-    }
-
-    public Builder setRaftGroup(RaftGroup group) {
-      this.raftGroup = group;
-      return this;
-    }
-
-    public Builder setSelfPeerId(RaftPeerId peerId) {
-      this.selfPeerId = peerId;
       return this;
     }
 
