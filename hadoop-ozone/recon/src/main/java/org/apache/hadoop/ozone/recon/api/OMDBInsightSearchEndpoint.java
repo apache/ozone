7f829bf5edeaabe1c90f9b178445f15e8c115eac
arafat <mohammad.khan@cloudera.com>
2024-02-18 15:55:35 +0530

    HDDS-10389 Implement a search feature for users to locate open keys within the Open Keys Insights section.

---
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/api/OMDBInsightSearchEndpoint.java
@@ -0,0 +1,240 @@
+package org.apache.hadoop.ozone.recon.api;
+
+import org.apache.hadoop.hdds.scm.server.OzoneStorageContainerManager;
+import org.apache.hadoop.hdds.utils.db.Table;
+import org.apache.hadoop.hdds.utils.db.TableIterator;
+import org.apache.hadoop.ozone.OmUtils;
+import org.apache.hadoop.ozone.om.helpers.BucketLayout;
+import org.apache.hadoop.ozone.om.helpers.OmBucketInfo;
+import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
+import org.apache.hadoop.ozone.recon.api.handlers.BucketHandler;
+import org.apache.hadoop.ozone.recon.recovery.ReconOMMetadataManager;
+import org.apache.hadoop.ozone.recon.scm.ReconContainerManager;
+import org.apache.hadoop.ozone.recon.spi.ReconContainerMetadataManager;
+import org.apache.hadoop.ozone.recon.spi.impl.ReconNamespaceSummaryManagerImpl;
+import org.hadoop.ozone.recon.schema.tables.daos.GlobalStatsDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.hadoop.ozone.OzoneConsts.OM_KEY_PREFIX;
+import static org.apache.hadoop.ozone.recon.api.handlers.BucketHandler.getBucketHandler;
+import static org.apache.hadoop.ozone.recon.api.handlers.EntityHandler.parseRequestPath;
+
+/**
+ * REST endpoint for search implementation in OM DB Insight.
+ */
+@Path("/insights")
+@Produces(MediaType.APPLICATION_JSON)
+@AdminOnly
+public class OMDBInsightSearchEndpoint {
+
+  @Inject
+  private ContainerEndpoint containerEndpoint;
+  private OzoneStorageContainerManager reconSCM;
+  @Inject
+  private ReconContainerMetadataManager reconContainerMetadataManager;
+  private final ReconOMMetadataManager omMetadataManager;
+  private final ReconContainerManager containerManager;
+  private static final Logger LOG =
+      LoggerFactory.getLogger(OMDBInsightSearchEndpoint.class);
+  private final GlobalStatsDao globalStatsDao;
+  private ReconNamespaceSummaryManagerImpl reconNamespaceSummaryManager;
+
+
+  @Inject
+  public OMDBInsightSearchEndpoint(OzoneStorageContainerManager reconSCM,
+                             ReconOMMetadataManager omMetadataManager,
+                             GlobalStatsDao globalStatsDao,
+                             ReconNamespaceSummaryManagerImpl
+                                 reconNamespaceSummaryManager) {
+    this.reconSCM = reconSCM;
+    this.containerManager =
+        (ReconContainerManager) reconSCM.getContainerManager();
+    this.omMetadataManager = omMetadataManager;
+    this.globalStatsDao = globalStatsDao;
+    this.reconNamespaceSummaryManager = reconNamespaceSummaryManager;
+  }
+
+
+  /**
+   * Search for open keys in the OM DB Insight.
+   *
+   * @param searchPrefix The prefix to search for.
+   * @param limit The maximum number of keys to return.
+   * @return The response containing the matched keys.
+   * @throws IOException if an error occurs during the search.
+   */
+  @GET
+  @Path("/openKeys/search")
+  public Response searchOpenKeys(
+      @QueryParam("searchPrefix") String searchPrefix,
+      @DefaultValue("10") @QueryParam("limit") int limit)
+      throws IOException {
+
+    if (searchPrefix == null || searchPrefix.isEmpty()) {
+      return createBadRequestResponse(
+          "searchPrefix query parameter is required.");
+    }
+    List<OmKeyInfo> matchedKeys = new ArrayList<>();
+
+    for (BucketLayout layout : Arrays.asList(
+        BucketLayout.LEGACY, BucketLayout.FILE_SYSTEM_OPTIMIZED)) {
+
+      Table<String, OmKeyInfo> openKeyTable =
+          omMetadataManager.getOpenKeyTable(layout);
+      try (
+          TableIterator<String, ? extends Table.KeyValue<String, OmKeyInfo>>
+              keyIter = openKeyTable.iterator()) {
+        if (layout == BucketLayout.FILE_SYSTEM_OPTIMIZED) {
+          searchPrefix = convertToObjectPath(searchPrefix);
+        }
+        keyIter.seek(searchPrefix);
+        while (keyIter.hasNext() && matchedKeys.size() < limit) {
+          Table.KeyValue<String, OmKeyInfo> entry = keyIter.next();
+          String key = entry.getKey();
+          // Break if the key no longer matches the prefix
+          if (!key.startsWith(searchPrefix)) {
+            break;
+          }
+          OmKeyInfo omKeyInfo = entry.getValue();
+          // Add it to the list of matched keys.
+          matchedKeys.add(omKeyInfo);
+        }
+      } catch (Exception e) {
+        return noMatchedKeysResponse(searchPrefix);
+      }
+    }
+    if (matchedKeys.isEmpty()) {
+      return noMatchedKeysResponse(searchPrefix);
+    } else {
+      return Response.ok(matchedKeys).build();
+    }
+  }
+
+  /**
+   * Converts a given key prefix to an object path for FSO buckets.
+   * The conversion is necessary because keys in FSO buckets are stored in a
+   * object format in the OpenFileTable,
+   * e.g., "/volumeId/bucketId/parentId/fileName/id -> KeyInfo".
+   *
+   * @param prevKeyPrefix The key prefix to convert.
+   * @return The object path for the given key prefix.
+   * @throws IOException if an error occurs during conversion.
+   */
+  public String convertToObjectPath(String prevKeyPrefix) throws IOException {
+    if (prevKeyPrefix.isEmpty()) {
+      return "";
+    }
+
+    // Fetch the volumeID
+    try {
+      String[] names = parseRequestPath(normalizePath(prevKeyPrefix));
+      String volumeName = names[0];
+      String volumeKey = omMetadataManager.getVolumeKey(volumeName);
+      long volumeId =
+          omMetadataManager.getVolumeTable().getSkipCache(volumeKey).getObjectID();
+
+      if (names.length == 1) {
+        return constructObjectPathWithPrefix(volumeId);
+      }
+
+      // Fetch the bucketID
+      String bucketName = names[1];
+      String bucketKey = omMetadataManager.getBucketKey(volumeName, bucketName);
+      OmBucketInfo bucketInfo =
+          omMetadataManager.getBucketTable().getSkipCache(bucketKey);
+      long bucketId = bucketInfo.getObjectID();
+
+      if (names.length == 2) {
+        return constructObjectPathWithPrefix(volumeId, bucketId);
+      }
+
+      // Fetch the intermediate parentID
+      BucketHandler handler =
+          getBucketHandler(reconNamespaceSummaryManager, omMetadataManager,
+              reconSCM, bucketInfo);
+      long parentId = getParentId(handler, names, bucketName, bucketId);
+      String keyName = names[names.length - 1];
+      return constructObjectPathWithPrefix(volumeId, bucketId, parentId) +
+          OM_KEY_PREFIX + keyName;
+    } catch (IOException e) {
+      LOG.error("Error converting key prefix to object path: {}", prevKeyPrefix,
+          e);
+      return prevKeyPrefix; // Fallback to original prefix in case of exception
+    } catch (Exception e) {
+      LOG.error("Unexpected error during conversion: {}", prevKeyPrefix, e);
+      return prevKeyPrefix;
+    }
+  }
+
+  private long getParentId(BucketHandler handler, String[] names,
+                           String bucketName, long bucketId)
+      throws IOException {
+    String parentName = names[names.length - 2];
+    if (bucketName.equals(parentName) && names.length == 3) {
+      return bucketId;
+    }
+    return handler.getDirObjectId(names, names.length - 1);
+  }
+
+  /**
+   * Constructs an object path with the given IDs.
+   *
+   * @param ids The IDs to construct the object path with.
+   * @return The constructed object path.
+   */
+  private String constructObjectPathWithPrefix(long... ids) {
+    StringBuilder pathBuilder = new StringBuilder();
+    for (long id : ids) {
+      pathBuilder.append(OM_KEY_PREFIX).append(id);
+    }
+    return pathBuilder.toString();
+  }
+
+  /**
+   * Returns a response indicating that no keys matched the search prefix.
+   *
+   * @param searchPrefix The search prefix that was used.
+   * @return The response indicating that no keys matched the search prefix.
+   */
+  private Response noMatchedKeysResponse(String searchPrefix) {
+    String message =
+        "{\"message\": \"No keys exist for the specified search prefix";
+    message += ".\"}";
+    return Response.status(Response.Status.NOT_FOUND)
+        .entity(message)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  /**
+   * Utility method to create a bad request response with a custom message.
+   * @param message The message to include in the response body.
+   * @return A Response object configured with the provided message.
+   */
+  private Response createBadRequestResponse(String message) {
+    String jsonResponse = String.format("{\"message\": \"%s\"}", message);
+    return Response.status(Response.Status.BAD_REQUEST)
+        .entity(jsonResponse)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  private static String normalizePath(String path) {
+    return OM_KEY_PREFIX + OmUtils.normalizeKey(path, false);
+  }
+
+}
---
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestOMDBInsightSearchEndpoint.java
@@ -0,0 +1,308 @@
+package org.apache.hadoop.ozone.recon.api;
+
+import org.apache.hadoop.hdds.client.StandaloneReplicationConfig;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
+import org.apache.hadoop.hdds.scm.pipeline.Pipeline;
+import org.apache.hadoop.hdds.scm.server.OzoneStorageContainerManager;
+import org.apache.hadoop.hdds.utils.db.Table;
+import org.apache.hadoop.ozone.om.OMMetadataManager;
+import org.apache.hadoop.ozone.om.helpers.BucketLayout;
+import org.apache.hadoop.ozone.om.helpers.OmBucketInfo;
+import org.apache.hadoop.ozone.om.helpers.OmKeyInfo;
+import org.apache.hadoop.ozone.om.helpers.OmVolumeArgs;
+import org.apache.hadoop.ozone.recon.ReconTestInjector;
+import org.apache.hadoop.ozone.recon.persistence.AbstractReconSqlDBTest;
+import org.apache.hadoop.ozone.recon.persistence.ContainerHealthSchemaManager;
+import org.apache.hadoop.ozone.recon.recovery.ReconOMMetadataManager;
+import org.apache.hadoop.ozone.recon.scm.ReconPipelineManager;
+import org.apache.hadoop.ozone.recon.scm.ReconStorageContainerManagerFacade;
+import org.apache.hadoop.ozone.recon.spi.ReconContainerMetadataManager;
+import org.apache.hadoop.ozone.recon.spi.StorageContainerServiceProvider;
+import org.apache.hadoop.ozone.recon.spi.impl.OzoneManagerServiceProviderImpl;
+import org.apache.hadoop.ozone.recon.spi.impl.StorageContainerServiceProviderImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import javax.ws.rs.core.Response;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import static org.apache.hadoop.ozone.recon.OMMetadataManagerTestUtils.getTestReconOmMetadataManager;
+import static org.apache.hadoop.ozone.recon.OMMetadataManagerTestUtils.initializeNewOmMetadataManager;
+import static org.apache.hadoop.ozone.recon.OMMetadataManagerTestUtils.getRandomPipeline;
+import static org.apache.hadoop.ozone.recon.OMMetadataManagerTestUtils.getBucketLayout;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+/**
+ * Test for OMDBInsightSearchEndpoint.
+ */
+public class TestOMDBInsightSearchEndpoint extends AbstractReconSqlDBTest {
+  @TempDir
+  private Path temporaryFolder;
+  private OzoneStorageContainerManager ozoneStorageContainerManager;
+  private ReconContainerMetadataManager reconContainerMetadataManager;
+  private OMMetadataManager omMetadataManager;
+  private ReconPipelineManager reconPipelineManager;
+  private ReconOMMetadataManager reconOMMetadataManager;
+  private OMDBInsightSearchEndpoint omdbInsightSearchEndpoint;
+  private Pipeline pipeline;
+  private Random random = new Random();
+  private OzoneConfiguration ozoneConfiguration;
+  private Set<Long> generatedIds = new HashSet<>();
+  public TestOMDBInsightSearchEndpoint() {
+    super();
+  }
+
+  private long generateUniqueRandomLong() {
+    long newValue;
+    do {
+      newValue = random.nextLong();
+    } while (generatedIds.contains(newValue));
+
+    generatedIds.add(newValue);
+    return newValue;
+  }
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    omMetadataManager = initializeNewOmMetadataManager(
+        Files.createDirectory(temporaryFolder.resolve(
+            "JunitOmMetadata")).toFile());
+    reconOMMetadataManager = getTestReconOmMetadataManager(omMetadataManager,
+        Files.createDirectory(temporaryFolder.resolve(
+            "JunitOmMetadataTest")).toFile());
+    ReconTestInjector reconTestInjector =
+        new ReconTestInjector.Builder(temporaryFolder.toFile())
+            .withReconSqlDb()
+            .withReconOm(reconOMMetadataManager)
+            .withOmServiceProvider(mock(OzoneManagerServiceProviderImpl.class))
+            .addBinding(OzoneStorageContainerManager.class,
+                ReconStorageContainerManagerFacade.class)
+            .withContainerDB()
+            .addBinding(StorageContainerServiceProvider.class,
+                mock(StorageContainerServiceProviderImpl.class))
+            .addBinding(OMDBInsightEndpoint.class)
+            .addBinding(ContainerHealthSchemaManager.class)
+            .build();
+    reconContainerMetadataManager =
+        reconTestInjector.getInstance(ReconContainerMetadataManager.class);
+    omdbInsightSearchEndpoint = reconTestInjector.getInstance(
+        OMDBInsightSearchEndpoint.class);
+    ozoneStorageContainerManager =
+        reconTestInjector.getInstance(OzoneStorageContainerManager.class);
+    reconPipelineManager = (ReconPipelineManager)
+        ozoneStorageContainerManager.getPipelineManager();
+    pipeline = getRandomPipeline();
+    reconPipelineManager.addPipeline(pipeline);
+    ozoneConfiguration = new OzoneConfiguration();
+    openMocks(this);
+  }
+
+  @Test
+  public void testSearchOpenKeys() throws Exception {
+    // Create 3 keys in 'bucketOne' and 2 keys in 'bucketTwo'
+    for (int i = 1; i <= 3; i++) {
+      OmKeyInfo omKeyInfo =
+          getOmKeyInfo("sampleVol", "bucketOne", "key_" + i, true);
+      String keyPath = String.format("/sampleVol/%s/key_%d", "bucketOne", i);
+      reconOMMetadataManager.getOpenKeyTable(getBucketLayout())
+          .put(keyPath, omKeyInfo);
+    }
+
+    for (int i = 1; i <= 2; i++) {
+      OmKeyInfo omKeyInfo =
+          getOmKeyInfo("sampleVol", "bucketTwo", "key_" + i, true);
+      String keyPath = String.format("/sampleVol/%s/key_%d", "bucketTwo", i);
+      reconOMMetadataManager.getOpenKeyTable(getBucketLayout())
+          .put(keyPath, omKeyInfo);
+    }
+
+    // Search for keys in 'bucketOne'
+    String searchPrefixBucketOne = "/sampleVol/bucketOne/";
+    Response responseBucketOne =
+        omdbInsightSearchEndpoint.searchOpenKeys(searchPrefixBucketOne, 10);
+
+    // Assert that the search response for 'bucketOne' is OK and verify the results
+    assertEquals(Response.Status.OK.getStatusCode(),
+        responseBucketOne.getStatus());
+    List<OmKeyInfo> searchResultsBucketOne =
+        (List<OmKeyInfo>) responseBucketOne.getEntity();
+    assertNotNull(searchResultsBucketOne);
+    assertEquals(3, searchResultsBucketOne.size());
+
+    searchResultsBucketOne.forEach(keyInfo -> {
+      assertTrue(keyInfo.getKeyName().startsWith("key_"));
+      assertTrue(keyInfo.getVolumeName().startsWith("sampleVol"));
+      assertTrue(keyInfo.getBucketName().startsWith("bucketOne"));
+    });
+
+    // Search for keys in 'bucketTwo'
+    String searchPrefixBucketTwo = "/sampleVol/bucketTwo/";
+    Response responseBucketTwo =
+        omdbInsightSearchEndpoint.searchOpenKeys(searchPrefixBucketTwo, 10);
+
+    // Assert that the search response for 'bucketTwo' is OK and verify the results
+    assertEquals(Response.Status.OK.getStatusCode(),
+        responseBucketTwo.getStatus());
+    List<OmKeyInfo> searchResultsBucketTwo =
+        (List<OmKeyInfo>) responseBucketTwo.getEntity();
+    assertNotNull(searchResultsBucketTwo);
+    assertEquals(2, searchResultsBucketTwo.size());
+
+    searchResultsBucketTwo.forEach(keyInfo -> {
+      assertTrue(keyInfo.getKeyName().startsWith("key_"));
+      assertTrue(keyInfo.getVolumeName().startsWith("sampleVol"));
+      assertTrue(keyInfo.getBucketName().startsWith("bucketTwo"));
+    });
+  }
+
+  @Test
+  public void testConvertToObjectPathForEmptyPrefix() throws Exception {
+    String result = omdbInsightSearchEndpoint.convertToObjectPath("");
+    assertEquals("", result, "Expected an empty string for empty input");
+  }
+
+  @Test
+  public void testConvertToObjectPathForVolumeOnly() throws Exception {
+    setupVolume("vol1", 100L);
+
+    String result = omdbInsightSearchEndpoint.convertToObjectPath("/vol1");
+    assertEquals("/100", result, "Incorrect conversion for volume only path");
+  }
+
+  @Test
+  public void testConvertToObjectPathForVolumeAndBucket() throws Exception {
+    setupVolume("vol1", 100L);
+    setupBucket("vol1", "bucketOne", 200L, BucketLayout.LEGACY);
+
+    String result =
+        omdbInsightSearchEndpoint.convertToObjectPath("vol1/bucketOne");
+    assertEquals("/100/200", result,
+        "Incorrect conversion for volume and bucket path");
+  }
+
+  @Test
+  public void testSearchOpenKeysWithDifferentBucketLayouts() throws Exception {
+    // Setup for LEGACY bucket layout
+    OmKeyInfo legacyKeyInfo =
+        getOmKeyInfo("sampleVol", "legacyBucket", "legacy_key_1", true);
+    String legacyKeyPath =
+        String.format("/sampleVol/%s/%s", "legacyBucket", "legacy_key_1");
+    reconOMMetadataManager.getOpenKeyTable(BucketLayout.LEGACY)
+        .put(legacyKeyPath, legacyKeyInfo);
+
+    // Setup for FSO bucket layout
+    OmKeyInfo fsoKeyInfo =
+        getOmKeyInfo("sampleVol", "fsoBucket", "fso_key_1", true);
+    setupBucket("sampleVol", "fsoBucket", 200L,
+        BucketLayout.FILE_SYSTEM_OPTIMIZED);
+    long fsoKeyObjectId = fsoKeyInfo.getObjectID();
+    long bucketId = 200L;
+    long volumeId = 0L;
+    String fsoKeyPath = String.format("/%s/%s/%s", volumeId, bucketId,
+        fsoKeyInfo.getObjectID());
+    reconOMMetadataManager.getOpenKeyTable(BucketLayout.FILE_SYSTEM_OPTIMIZED)
+        .put(fsoKeyPath, fsoKeyInfo);
+
+    // Search for keys under the LEGACY bucket
+    String searchPrefixLegacy = "/sampleVol/legacyBucket/";
+    Response responseLegacy =
+        omdbInsightSearchEndpoint.searchOpenKeys(searchPrefixLegacy, 10);
+
+    // Verify response for LEGACY bucket layout
+    assertEquals(Response.Status.OK.getStatusCode(),
+        responseLegacy.getStatus());
+    List<OmKeyInfo> searchResultsLegacy =
+        (List<OmKeyInfo>) responseLegacy.getEntity();
+    assertNotNull(searchResultsLegacy);
+    assertEquals(1, searchResultsLegacy.size());
+    assertTrue(
+        searchResultsLegacy.get(0).getKeyName().startsWith("legacy_key_"));
+    assertEquals("legacyBucket", searchResultsLegacy.get(0).getBucketName());
+
+    // Search for keys under the FSO bucket
+    String searchPrefixFso = "/sampleVol/fsoBucket/";
+    Response responseFso =
+        omdbInsightSearchEndpoint.searchOpenKeys(searchPrefixFso, 10);
+
+    // Verify response for FSO bucket layout
+    assertEquals(Response.Status.OK.getStatusCode(), responseFso.getStatus());
+    List<OmKeyInfo> searchResultsFso =
+        (List<OmKeyInfo>) responseFso.getEntity();
+    assertNotNull(searchResultsFso);
+    assertEquals(1, searchResultsFso.size());
+    assertTrue(searchResultsFso.get(0).getKeyName().startsWith("fso_key_"));
+    assertEquals("fsoBucket", searchResultsFso.get(0).getBucketName());
+
+    // Pass only the volume name and verify the response.
+    String searchPrefixVolume = "/sampleVol/";
+    Response responseVolume =
+        omdbInsightSearchEndpoint.searchOpenKeys(searchPrefixVolume, 10);
+
+    // Verify response for volume name
+    assertEquals(Response.Status.OK.getStatusCode(),
+        responseVolume.getStatus());
+    List<OmKeyInfo> searchResultsVolume =
+        (List<OmKeyInfo>) responseVolume.getEntity();
+    assertNotNull(searchResultsVolume);
+    assertEquals(2, searchResultsVolume.size());
+  }
+
+  private OmKeyInfo getOmKeyInfo(String volumeName, String bucketName,
+                                 String keyName, boolean isFile) {
+    return new OmKeyInfo.Builder()
+        .setVolumeName(volumeName)
+        .setBucketName(bucketName)
+        .setKeyName(keyName)
+        .setFile(isFile)
+        .setObjectID(generateUniqueRandomLong())
+        .setReplicationConfig(StandaloneReplicationConfig
+            .getInstance(HddsProtos.ReplicationFactor.ONE))
+        .setDataSize(random.nextLong())
+        .build();
+  }
+
+  public void setupVolume(String volumeName, long volumeId) throws Exception {
+    Table<String, OmVolumeArgs> volumeTable =
+        reconOMMetadataManager.getVolumeTable();
+
+    OmVolumeArgs omVolumeArgs = OmVolumeArgs.newBuilder()
+        .setVolume(volumeName)
+        .setAdminName("TestUser")
+        .setOwnerName("TestUser")
+        .setObjectID(volumeId)
+        .build();
+    // Insert the volume into the table
+    volumeTable.put(reconOMMetadataManager.getVolumeKey(volumeName),
+        omVolumeArgs);
+  }
+
+  private void setupBucket(String volumeName, String bucketName, long bucketId,
+                           BucketLayout layout)
+      throws Exception {
+    Table<String, OmBucketInfo> bucketTable =
+        reconOMMetadataManager.getBucketTable();
+
+    OmBucketInfo omBucketInfo = OmBucketInfo.newBuilder()
+        .setVolumeName(volumeName)
+        .setBucketName(bucketName)
+        .setObjectID(bucketId)
+        .setBucketLayout(layout)
+        .build();
+
+    String bucketKey =
+        reconOMMetadataManager.getBucketKey(volumeName, bucketName);
+    bucketTable.put(bucketKey, omBucketInfo);
+  }
+
+}
