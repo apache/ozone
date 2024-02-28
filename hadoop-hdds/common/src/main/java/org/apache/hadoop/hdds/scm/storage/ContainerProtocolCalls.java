6d94e1486e2c9ba1c4e30188bf1a1800b5a2700e
Wei-Chiu Chuang <weichiu@apache.org>
2024-02-28 09:30:06 -0800

    Add proto definition, client side and server side handlers.
    (cherry picked from commit 29b534d330e5614bc7fc46227a510fd3f20b10bd)
    
    Add freon benchmark dn-echo and its test.
    
    (cherry picked from commit 26c72a1a51994da169fd060caad689ebe12f0bc2)
    
    Support requeset payload and response payload.
    
    (cherry picked from commit 3951228a5f94753719e4be0d668e49c51c8454a3)
    
    Updating to address review comments.
    
    Address review comments.
    
    Change-Id: Id686e2bc5a373e17515dcd66534b30b12f1b970e
    
    Fix compilation error
    
    Change-Id: Ifda423ae5867a5df94c16ff31854811efa5aeaa5
    
    Address review comments.
    
    Added an option to let DataNode handler to pause for a certain duration for each request.
    
    Change-Id: Id5e6c8551da3b42ebd720ceb8ed86b539aa7e9c8
    
    Add a default value for sleepTimeMs.
    
    Change-Id: Iab3f5e4b3dee3b49bc9432d66a7e02f3499e3e6d
    
    Skip sleep if duration is equal or less than zero.
    
    Change-Id: I3fe21bbd5a76335a34c0db7e73eb67c108a9fb40
    
    Add --clients parameter to specify the number of xceiver clients.
    
    In addition, support secure cluster.
    
    Change-Id: Ib7074e45b4874d544636df741f265e9aed9c886f
    (cherry picked from commit 3b2e1e1cd43022a3ff8adf8aceb146ec951205da)
    
    Update test
    
    Change-Id: I92e1c63d94a1fc1300a6908aba8f0eec87fb5d70
    
    Fix findbugs
    
    Change-Id: Ie849551820f755a585999c10a1502cfb00de5298
    
    Add robot test.
    
    Change-Id: I8866254a09029f48cf55df3e211b02e6ba9d96a6
    
    Add the missing space in ozone freon robot test.
    
    Change-Id: I36e75c06bae193d56619e49428066251359ab3df

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/storage/ContainerProtocolCalls.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/storage/ContainerProtocolCalls.java
@@ -57,6 +57,8 @@ import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.ReadContai
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.Type;
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.WriteChunkRequestProto;
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.FinalizeBlockRequestProto;
+import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.EchoRequestProto;
+import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.EchoResponseProto;
 import org.apache.hadoop.hdds.scm.XceiverClientReply;
 import org.apache.hadoop.hdds.scm.XceiverClientSpi;
 import org.apache.hadoop.hdds.scm.XceiverClientSpi.Validator;
@@ -65,6 +67,7 @@ import org.apache.hadoop.hdds.scm.container.common.helpers.ContainerNotOpenExcep
 import org.apache.hadoop.hdds.scm.container.common.helpers.StorageContainerException;
 import org.apache.hadoop.hdds.scm.pipeline.Pipeline;
 import org.apache.hadoop.hdds.security.token.OzoneBlockTokenIdentifier;
+import org.apache.hadoop.hdds.tracing.TracingUtil;
 import org.apache.hadoop.ozone.common.Checksum;
 import org.apache.hadoop.ozone.common.ChecksumData;
 import org.apache.hadoop.security.token.Token;
@@ -659,6 +662,38 @@ public final class ContainerProtocolCalls  {
     ContainerCommandResponseProto response =
         client.sendCommand(request, getValidatorList());
     return response.getGetSmallFile();
+  }
+
+  /**
+   * Send an echo to DataNode.
+   *
+   * @return EchoResponseProto
+   */
+  public static EchoResponseProto echo(XceiverClientSpi client, long containerID, ByteString payloadReqBytes,
+      int payloadRespSizeKB, int sleepTimeMs) throws IOException {
+    ContainerProtos.EchoRequestProto getEcho =
+        EchoRequestProto
+            .newBuilder()
+            .setPayload(payloadReqBytes)
+            .setPayloadSizeResp(payloadRespSizeKB)
+            .setSleepTimeMs(sleepTimeMs)
+            .build();
+    String id = client.getPipeline().getClosestNode().getUuidString();
+
+    ContainerCommandRequestProto.Builder builder = ContainerCommandRequestProto
+        .newBuilder()
+        .setCmdType(Type.Echo)
+        .setContainerID(containerID)
+        .setDatanodeUuid(id)
+        .setEcho(getEcho);
+    String traceId = TracingUtil.exportCurrentSpan();
+    if (traceId != null) {
+      builder.setTraceID(traceId);
+    }
+    ContainerCommandRequestProto request = builder.build();
+    ContainerCommandResponseProto response =
+        client.sendCommand(request, getValidatorList());
+    return response.getEcho();
   }
 
   /**
