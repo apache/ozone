01f8d62fbee2be4c79c4b4d4bf759a81f5ea30bb
Sarveksha Yeshavantha Raju <79865743+sarvekshayr@users.noreply.github.com>
2024-02-28 22:10:30 +0530

    HDDS-10331. Rename Java constants of ex-DFS config keys (#6290)

--- hadoop-ozone/fault-injection-test/mini-chaos-tests/src/test/java/org/apache/hadoop/ozone/MiniOzoneChaosCluster.java
+++ hadoop-ozone/fault-injection-test/mini-chaos-tests/src/test/java/org/apache/hadoop/ozone/MiniOzoneChaosCluster.java
@@ -261,10 +261,10 @@ public class MiniOzoneChaosCluster extends MiniOzoneHAClusterImpl {
           TimeUnit.SECONDS);
       conf.setInt(
           OzoneConfigKeys
-              .DFS_CONTAINER_RATIS_NUM_WRITE_CHUNK_THREADS_PER_VOLUME_KEY,
+              .HDDS_CONTAINER_RATIS_NUM_WRITE_CHUNK_THREADS_PER_VOLUME_KEY,
           4);
       conf.setInt(
-          OzoneConfigKeys.DFS_CONTAINER_RATIS_NUM_CONTAINER_OP_EXECUTORS_KEY,
+          OzoneConfigKeys.HDDS_CONTAINER_RATIS_NUM_CONTAINER_OP_EXECUTORS_KEY,
           2);
       conf.setInt(OzoneConfigKeys.OZONE_CONTAINER_CACHE_SIZE, 2);
       ReplicationManagerConfiguration replicationConf =
@@ -273,8 +273,8 @@ public class MiniOzoneChaosCluster extends MiniOzoneHAClusterImpl {
       replicationConf.setEventTimeout(Duration.ofSeconds(20));
       replicationConf.setDatanodeTimeoutOffset(0);
       conf.setFromObject(replicationConf);
-      conf.setInt(OzoneConfigKeys.DFS_RATIS_SNAPSHOT_THRESHOLD_KEY, 100);
-      conf.setInt(OzoneConfigKeys.DFS_CONTAINER_RATIS_LOG_PURGE_GAP, 100);
+      conf.setInt(OzoneConfigKeys.HDDS_RATIS_SNAPSHOT_THRESHOLD_KEY, 100);
+      conf.setInt(OzoneConfigKeys.HDDS_CONTAINER_RATIS_LOG_PURGE_GAP, 100);
       conf.setInt(OMConfigKeys.OZONE_OM_RATIS_LOG_PURGE_GAP, 100);
 
       conf.setInt(OMConfigKeys.
