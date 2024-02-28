1eeaa0baa2e8eacb8be4c305b787bd9bfe7b187f
hao guo <guohao1@360.cn>
2024-02-29 02:09:58 +0800

    HDDS-10144. Zero-Copy in replication (#6049)

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/replication/TestGrpcReplicationService.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/replication/TestGrpcReplicationService.java
@@ -17,14 +17,50 @@
  */
 package org.apache.hadoop.ozone.container.replication;
 
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.protocol.DatanodeDetails;
+import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos;
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.CopyContainerRequestProto;
 import org.apache.hadoop.hdds.protocol.datanode.proto.ContainerProtos.CopyContainerResponseProto;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
+import org.apache.hadoop.hdds.security.SecurityConfig;
+import org.apache.hadoop.ozone.OzoneConfigKeys;
+import org.apache.hadoop.ozone.container.common.helpers.ContainerMetrics;
+import org.apache.hadoop.ozone.container.common.impl.ContainerLayoutVersion;
+import org.apache.hadoop.ozone.container.common.impl.ContainerSet;
+import org.apache.hadoop.ozone.container.common.interfaces.Handler;
+import org.apache.hadoop.ozone.container.common.volume.HddsVolume;
+import org.apache.hadoop.ozone.container.common.volume.MutableVolumeSet;
+import org.apache.hadoop.ozone.container.common.volume.RoundRobinVolumeChoosingPolicy;
+import org.apache.hadoop.ozone.container.keyvalue.KeyValueContainer;
+import org.apache.hadoop.ozone.container.keyvalue.KeyValueContainerData;
+import org.apache.hadoop.ozone.container.keyvalue.KeyValueHandler;
+import org.apache.hadoop.ozone.container.ozoneimpl.ContainerController;
 import org.apache.ratis.thirdparty.io.grpc.stub.CallStreamObserver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
+import static org.apache.hadoop.ozone.OzoneConsts.GB;
+import static org.apache.hadoop.ozone.protocol.commands.ReplicateContainerCommand.toTarget;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -33,6 +69,146 @@ import static org.mockito.Mockito.when;
  * Tests {@link GrpcReplicationService}.
  */
 class TestGrpcReplicationService {
+
+  @TempDir
+  private Path tempDir;
+
+  private ReplicationServer replicationServer;
+  private OzoneConfiguration conf;
+  private ContainerController containerController;
+  private DatanodeDetails datanode;
+  private static final long CONTAINER_ID = 123456L;
+  private final AtomicLong pushContainerId = new AtomicLong();
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    init(false);
+  }
+
+  public void init(boolean isZeroCopy) throws Exception {
+    conf = new OzoneConfiguration();
+
+    ReplicationServer.ReplicationConfig replicationConfig =
+        conf.getObject(ReplicationServer.ReplicationConfig.class);
+
+    replicationConfig.setZeroCopyEnable(isZeroCopy);
+
+    SecurityConfig secConf = new SecurityConfig(conf);
+
+    ContainerSet containerSet = new ContainerSet(1000);
+
+    DatanodeDetails.Builder dn =
+        DatanodeDetails.newBuilder().setUuid(UUID.randomUUID())
+            .setHostName("localhost").setIpAddress("127.0.0.1")
+            .setPersistedOpState(HddsProtos.NodeOperationalState.IN_SERVICE)
+            .setPersistedOpStateExpiry(0);
+    DatanodeDetails.Port containerPort =
+        DatanodeDetails.newPort(DatanodeDetails.Port.Name.STANDALONE,
+            OzoneConfigKeys.DFS_CONTAINER_IPC_PORT_DEFAULT);
+    DatanodeDetails.Port ratisPort =
+        DatanodeDetails.newPort(DatanodeDetails.Port.Name.RATIS,
+            OzoneConfigKeys.DFS_CONTAINER_RATIS_IPC_PORT_DEFAULT);
+    DatanodeDetails.Port replicationPort =
+        DatanodeDetails.newPort(DatanodeDetails.Port.Name.REPLICATION,
+            replicationConfig.getPort());
+    DatanodeDetails.Port streamPort =
+        DatanodeDetails.newPort(DatanodeDetails.Port.Name.RATIS_DATASTREAM,
+            OzoneConfigKeys.DFS_CONTAINER_RATIS_DATASTREAM_PORT_DEFAULT);
+    dn.addPort(containerPort);
+    dn.addPort(ratisPort);
+    dn.addPort(replicationPort);
+    dn.addPort(streamPort);
+
+    datanode = dn.build();
+
+    final String testDir =
+        Files.createDirectory(tempDir.resolve("VolumeDir")).toString();
+
+    MutableVolumeSet volumeSet = mock(MutableVolumeSet.class);
+    when(volumeSet.getVolumesList()).thenReturn(Collections.singletonList(
+        new HddsVolume.Builder(testDir).conf(conf).build()));
+
+    ContainerMetrics metrics = ContainerMetrics.create(conf);
+    Handler containerHandler =
+        new KeyValueHandler(conf, datanode.getUuidString(), containerSet,
+            volumeSet, metrics, c -> {
+        });
+
+    containerController = new ContainerController(containerSet,
+        Collections.singletonMap(
+            ContainerProtos.ContainerType.KeyValueContainer, containerHandler));
+
+    KeyValueContainerData data = new KeyValueContainerData(
+        CONTAINER_ID,
+        ContainerLayoutVersion.FILE_PER_BLOCK, GB, UUID.randomUUID().toString(),
+        datanode.getUuidString());
+    KeyValueContainer container = new KeyValueContainer(data, conf);
+    container.create(volumeSet, new RoundRobinVolumeChoosingPolicy(),
+        "test-replication");
+    containerSet.addContainer(container);
+    container.close();
+
+    ContainerImporter importer = mock(ContainerImporter.class);
+    doAnswer(invocation -> {
+      pushContainerId.set((long) invocation.getArguments()[0]);
+      return null;
+    }).when(importer).importContainer(anyLong(), any(), any(), any());
+    doReturn(true).when(importer).isAllowedContainerImport(eq(
+        CONTAINER_ID));
+    when(importer.chooseNextVolume()).thenReturn(new HddsVolume.Builder(
+        Files.createDirectory(tempDir.resolve("ImporterDir")).toString()).conf(
+        conf).build());
+
+    replicationServer =
+        new ReplicationServer(containerController, replicationConfig, secConf,
+            null, importer, datanode.threadNamePrefix());
+    replicationServer.start();
+  }
+
+  @AfterEach
+  public void cleanup() {
+    replicationServer.stop();
+  }
+
+  @Test
+  public void testDownload() throws IOException {
+    SimpleContainerDownloader downloader =
+        new SimpleContainerDownloader(conf, null);
+    Path downloadDir = Files.createDirectory(tempDir.resolve("DownloadDir"));
+    Path result = downloader.getContainerDataFromReplicas(
+        CONTAINER_ID,
+        Collections.singletonList(datanode), downloadDir,
+        CopyContainerCompression.NO_COMPRESSION);
+
+    assertTrue(result.toString().startsWith(downloadDir.toString()));
+
+    File[] files = downloadDir.toFile().listFiles();
+
+    assertNotNull(files);
+    assertEquals(files.length, 1);
+
+    assertTrue(files[0].getName().startsWith("container-" +
+        CONTAINER_ID + "-"));
+
+    downloader.close();
+  }
+
+  @Test
+  public void testUpload() {
+    ContainerReplicationSource source =
+        new OnDemandContainerReplicationSource(containerController);
+
+    GrpcContainerUploader uploader = new GrpcContainerUploader(conf, null);
+
+    PushReplicator pushReplicator = new PushReplicator(conf, source, uploader);
+
+    ReplicationTask task =
+        new ReplicationTask(toTarget(CONTAINER_ID, datanode), pushReplicator);
+
+    pushReplicator.replicate(task);
+
+    assertEquals(pushContainerId.get(), CONTAINER_ID);
+  }
 
   @Test
   void closesStreamOnError() {
@@ -51,7 +227,7 @@ class TestGrpcReplicationService {
     };
     ContainerImporter importer = mock(ContainerImporter.class);
     GrpcReplicationService subject =
-        new GrpcReplicationService(source, importer);
+        new GrpcReplicationService(source, importer, false);
 
     CopyContainerRequestProto request = CopyContainerRequestProto.newBuilder()
         .setContainerID(1)
