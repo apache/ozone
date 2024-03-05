ff1e4143ad22ae56a9476c180bb56cdb3556c487
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2024-03-05 09:50:50 +0100

    HDDS-10430. Race condition around Pipeline#nodesInOrder (#6316)

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/KeyManagerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/KeyManagerImpl.java
@@ -1880,7 +1880,9 @@ public class KeyManagerImpl implements KeyManager {
             LOG.debug("Found sorted datanodes for pipeline {} and client {} "
                 + "in cache", pipeline.getId(), clientMachine);
           }
-          pipeline.setNodesInOrder(sortedNodes);
+          if (!Objects.equals(pipeline.getNodesInOrder(), sortedNodes)) {
+            k.setPipeline(pipeline.copyWithNodesInOrder(sortedNodes));
+          }
         }
       }
     }
