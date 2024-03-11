8f465c148040850b257c3c37ccb13d415b9d2b96
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2024-03-11 18:44:11 +0500

    HDDS-10503. Remove redundant imports

--- hadoop-hdds/rocksdb-checkpoint-differ/src/test/java/org/apache/ozone/graph/TestPrintableGraph.java
+++ hadoop-hdds/rocksdb-checkpoint-differ/src/test/java/org/apache/ozone/graph/TestPrintableGraph.java
@@ -23,9 +23,7 @@ import org.apache.ozone.rocksdiff.CompactionNode;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
