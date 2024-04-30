67fb31f8ddf93ee46bbbf6db11c6278e319fe647
DaveTeng0 <dteng@cloudera.com>
2024-04-29 20:12:45 -0700

    fix checkstyle

--- hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/repair/TestOzoneRepair.java
+++ hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/repair/TestOzoneRepair.java
@@ -17,7 +17,6 @@
  */
 package org.apache.hadoop.ozone.repair;
 
-import org.jooq.meta.derby.sys.Sys;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
