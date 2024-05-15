e249a3ca9aeb04f50abb8d95af3a289a01807bd2
DaveTeng0 <dteng@cloudera.com>
2024-05-15 11:31:26 -0700

    fix NPE in TestTransactionInfoRepair

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/repair/TransactionInfoRepair.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/repair/TransactionInfoRepair.java
@@ -87,13 +87,13 @@ public class TransactionInfoRepair
     try (ManagedRocksDB db = ManagedRocksDB.open(dbPath, cfDescList, cfHandleList)) {
       ColumnFamilyHandle transactionInfoCfh = RocksDBUtils.getColumnFamilyHandle(TRANSACTION_INFO_TABLE, cfHandleList);
       if (transactionInfoCfh == null) {
-        err().println(TRANSACTION_INFO_TABLE + " is not in a column family in DB for the given path.");
+        System.err.println(TRANSACTION_INFO_TABLE + " is not in a column family in DB for the given path.");
         return null;
       }
       TransactionInfo originalTransactionInfo =
           RocksDBUtils.getValue(db, transactionInfoCfh, TRANSACTION_INFO_KEY, TransactionInfo.getCodec());
 
-      out().println("The original highest transaction Info was " + originalTransactionInfo.getTermIndex());
+      System.out.println("The original highest transaction Info was " + originalTransactionInfo.getTermIndex());
 
       TransactionInfo transactionInfo = TransactionInfo.valueOf(
           highestTransactionTermIndex);
@@ -102,12 +102,12 @@ public class TransactionInfoRepair
       db.get()
           .put(transactionInfoCfh, StringCodec.get().toPersistedFormat(TRANSACTION_INFO_KEY), transactionInfoBytes);
 
-      out().println("The highest transaction info has been updated to: " +
+      System.out.println("The highest transaction info has been updated to: " +
           RocksDBUtils.getValue(db, transactionInfoCfh, TRANSACTION_INFO_KEY,
               TransactionInfo.getCodec()).getTermIndex());
     } catch (RocksDBException exception) {
-      err().println("Failed to update the RocksDB for the given path: " + dbPath);
-      err().println(
+      System.err.println("Failed to update the RocksDB for the given path: " + dbPath);
+      System.err.println(
           "Make sure that Ozone entity (OM, SCM or DN) is not running for the give database path and current host.");
       LOG.error(exception.toString());
     } finally {
@@ -124,14 +124,6 @@ public class TransactionInfoRepair
   @Override
   public Class<?> getParentType() {
     return RDBRepair.class;
-  }
-
-  private static PrintWriter out() {
-    return spec.commandLine().getOut();
-  }
-
-  private static PrintWriter err() {
-    return spec.commandLine().getErr();
   }
 
 }
