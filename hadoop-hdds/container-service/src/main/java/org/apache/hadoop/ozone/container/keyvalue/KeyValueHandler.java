1c204287c0734b54dd0c25060eef7d1eac1d018c
ashishk <ashishk@cloudera.com>
2024-05-17 12:12:02 +0530

    Handle static issue

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueHandler.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueHandler.java
@@ -1469,20 +1469,22 @@ public class KeyValueHandler extends Handler {
   }
 
   private ContainerCommandResponseProto checkFaultInjector(ContainerCommandRequestProto request) {
-    if (injector != null && (request.getCmdType().equals(injector.getType()) || injector.getType() == null)) {
+    if (injector != null) {
       synchronized (injector) {
-        Throwable ex = injector.getException();
         ContainerProtos.Type type = injector.getType();
-        if (ex != null) {
-          if (type == null) {
-            injector = null;
+        if (request.getCmdType().equals(type) || type == null) {
+          Throwable ex = injector.getException();
+          if (ex != null) {
+            if (type == null) {
+              injector = null;
+            }
+            return ContainerUtils.logAndReturnError(LOG, (StorageContainerException) ex, request);
           }
-          return ContainerUtils.logAndReturnError(LOG, (StorageContainerException) ex, request);
-        }
-        try {
-          injector.pause();
-        } catch (IOException e) {
-          // do nothing
+          try {
+            injector.pause();
+          } catch (IOException e) {
+            // do nothing
+          }
         }
       }
     }
