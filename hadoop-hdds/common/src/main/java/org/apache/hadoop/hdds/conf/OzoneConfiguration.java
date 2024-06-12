080f8efaf7cb2249c4554ef22e4c504fa19b2840
Zita Dombi <zitadombi98@gmail.com>
2024-06-13 00:39:39 +0200

    Add compliance check to Ozoneconfiguration and LegacyHadoopConfigurationSource
    Change-Id: Ib6ff17f7ad48e43f9e69cebf5a25859e29f133d6

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/conf/OzoneConfiguration.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/conf/OzoneConfiguration.java
@@ -33,6 +33,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -55,6 +56,7 @@ import static java.util.Collections.unmodifiableSortedSet;
 import static java.util.stream.Collectors.toCollection;
 import static org.apache.hadoop.hdds.ratis.RatisHelper.HDDS_DATANODE_RATIS_PREFIX_KEY;
 import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_CONTAINER_COPY_WORKDIR;
+import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED;
 
 /**
  * Configuration for ozone.
@@ -63,6 +65,9 @@ import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_CONTAINER_COPY_WORKD
 public class OzoneConfiguration extends Configuration
     implements MutableConfigurationSource {
 
+  private static String complianceMode;
+  private static boolean checkCompliance;
+  private static Properties cryptoProperties;
   public static final SortedSet<String> TAGS = unmodifiableSortedSet(
       Arrays.stream(ConfigTag.values())
           .map(Enum::name)
@@ -106,6 +111,11 @@ public class OzoneConfiguration extends Configuration
   public OzoneConfiguration() {
     OzoneConfiguration.activate();
     loadDefaults();
+    cryptoProperties = getCryptoProperties();
+    complianceMode = getPropertyUnsafe(OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE,
+        OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+    checkCompliance = !complianceMode.equals(OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+    //cryptoProperties = getCryptoProperties();
   }
 
   public OzoneConfiguration(Configuration conf) {
@@ -116,6 +126,10 @@ public class OzoneConfiguration extends Configuration
       loadDefaults();
       addResource(conf);
     }
+    complianceMode = getPropertyUnsafe(OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE,
+        OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+    checkCompliance = !complianceMode.equals(OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+    cryptoProperties = getCryptoProperties();
   }
 
   private void loadDefaults() {
@@ -407,4 +421,79 @@ public class OzoneConfiguration extends Configuration
     }
     return Integer.parseInt(value);
   }
+
+  private Properties properties;
+
+  @Override
+  protected final synchronized Properties getProps() {
+    if (properties == null) {
+      properties = new Properties(super.getProps()) {
+        @Override
+        public String getProperty(String key) {
+          String value = super.getProperty(key);
+          return checkCompliance(key, value);
+        }
+
+        @Override
+        public synchronized Object setProperty(String key, String value) {
+          super.setProperty(key, value);
+          return defaults.setProperty(key, value);
+        }
+      };
+    }
+    return properties;
+  }
+
+  /**
+   * Get a property value without the compliance check. It's needed to get the compliance
+   * mode and the whitelist parameter values in the checkCompliance method.
+   *
+   * @param key property name
+   * @param defaultValue default value
+   * @return property value, without compliance check
+   */
+  private String getPropertyUnsafe(String key, String defaultValue) {
+    return super.getProps().getProperty(key, defaultValue);
+  }
+
+  private Properties getCryptoProperties() {
+    return super.getAllPropertiesByTag(ConfigTag.CRYPTO_COMPLIANCE.toString());
+  }
+
+  public String checkCompliance(String config, String value) {
+    // Don't check the ozone.security.crypto.compliance.mode config, even though it's tagged as a crypto config
+    if (checkCompliance && cryptoProperties.containsKey(config) && !config.equals(OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE)) {
+
+      String whitelistConfig = config + "." + complianceMode + ".whitelist";
+      String whitelistValue = getPropertyUnsafe(whitelistConfig, "");
+
+      if (whitelistValue != null) {
+        String[] whitelistOptions = whitelistValue.split(",");
+
+        if (!Arrays.asList(whitelistOptions).contains(value)) {
+          throw new ConfigurationException("Not allowed configuration value! Compliance mode is set to " +
+              complianceMode + " and " + config + " configuration's value is not allowed. Please check the " +
+              whitelistConfig + " configuration.");
+        }
+      }
+    }
+    return value;
+  }
+
+  @Override
+  public Iterator<Map.Entry<String, String>> iterator() {
+    Properties props = getProps();
+    Map<String, String> result = new HashMap<>();
+    synchronized (props) {
+      for (Map.Entry<Object, Object> item : props.entrySet()) {
+        if (item.getKey() instanceof String && item.getValue() instanceof String) {
+          checkCompliance((String) item.getKey(),(String) item.getValue());
+          result.put((String) item.getKey(), (String) item.getValue());
+        }
+      }
+    }
+    return result.entrySet().iterator();
+  }
 }
+
+
--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/LegacyHadoopConfigurationSource.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/utils/LegacyHadoopConfigurationSource.java
@@ -18,11 +18,15 @@
 package org.apache.hadoop.hdds.utils;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.net.URL;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hdds.conf.ConfigurationSource;
-import org.apache.hadoop.hdds.conf.MutableConfigurationSource;
+import org.apache.hadoop.hdds.conf.*;
+import org.apache.hadoop.hdds.fs.DUFactory;
+import org.apache.hadoop.ozone.OzoneConfigKeys;
+
+import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED;
 
 /**
  * Configuration source to wrap Hadoop Configuration object.
@@ -32,9 +36,85 @@ public class LegacyHadoopConfigurationSource
 
   private Configuration configuration;
 
-  public LegacyHadoopConfigurationSource(
-      Configuration configuration) {
-    this.configuration = configuration;
+  public LegacyHadoopConfigurationSource(Configuration configuration) {
+    this.configuration = new Configuration(configuration) {
+      private Properties properties;
+      private String complianceMode = getPropertyUnsafe(OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE,
+          OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+      private boolean checkCompliance = !complianceMode.equals(OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE_UNRESTRICTED);
+      Properties cryptoProperties = getCryptoProperties();
+
+      @Override
+      protected synchronized Properties getProps() {
+        if (properties == null) {
+          properties = new Properties(super.getProps()) {
+            @Override
+            public String getProperty(String key) {
+              String value = super.getProperty(key);
+              return checkCompliance(key, value);
+            }
+
+            @Override
+            public synchronized Object setProperty(String key, String value) {
+              super.setProperty(key, value);
+              return defaults.setProperty(key, value);
+            }
+          };
+        }
+        return properties;
+      }
+
+      /**
+       * Get a property value without the compliance check. It's needed to get the compliance
+       * mode and the whitelist parameter values in the checkCompliance method.
+       *
+       * @param key property name
+       * @param defaultValue default value
+       * @return property value, without compliance check
+       */
+      private String getPropertyUnsafe(String key, String defaultValue) {
+        return super.getProps().getProperty(key, defaultValue);
+      }
+
+      private Properties getCryptoProperties() {
+        return super.getAllPropertiesByTag(ConfigTag.CRYPTO_COMPLIANCE.toString());
+      }
+
+      public String checkCompliance(String config, String value) {
+        // Don't check the ozone.security.crypto.compliance.mode config, even though it's tagged as a crypto config
+        if (checkCompliance && cryptoProperties.containsKey(config) && !config.equals(OzoneConfigKeys.OZONE_SECURITY_CRYPTO_COMPLIANCE_MODE)) {
+
+          String whitelistConfig = config + "." + complianceMode + ".whitelist";
+          String whitelistValue = getPropertyUnsafe(whitelistConfig, "");
+
+          if (whitelistValue != null) {
+            String[] whitelistOptions = whitelistValue.split(",");
+
+            if (!Arrays.asList(whitelistOptions).contains(value)) {
+              throw new ConfigurationException("Not allowed configuration value! Compliance mode is set to " +
+                  complianceMode + " and " + config + " configuration's value is not allowed. Please check the " +
+                  whitelistConfig + " configuration.");
+            }
+          }
+        }
+        return value;
+      }
+
+      @Override
+      public Iterator<Map.Entry<String, String>> iterator() {
+        Properties props = getProps();
+        Map<String, String> result = new HashMap<>();
+        synchronized (props) {
+          for (Map.Entry<Object, Object> item : props.entrySet()) {
+            if (item.getKey() instanceof String && item.getValue() instanceof String) {
+              checkCompliance((String) item.getKey(),(String) item.getValue());
+              result.put((String) item.getKey(), (String) item.getValue());
+            }
+          }
+        }
+        return result.entrySet().iterator();
+      }
+    };
   }
 
   @Override
@@ -76,6 +156,10 @@ public class LegacyHadoopConfigurationSource
     }
   }
 
+  /**
+   *
+   * @return
+   */
   public Configuration getOriginalHadoopConfiguration() {
     return configuration;
   }
