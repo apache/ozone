532bd64ff6cfec7760b1bb41bb1051a75acb9542
tejaskriya <tejaskriya.madhan@cloudera.com>
2024-06-12 13:01:30 +0530

    HDDS-11005. Intermittent failure in TestEndPoint#testRegisterRpcTimeout

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/ozone/container/common/TestEndPoint.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/ozone/container/common/TestEndPoint.java
@@ -473,8 +473,8 @@ public class TestEndPoint {
   @Test
   public void testRegisterRpcTimeout() throws Exception {
     final long rpcTimeout = 1000;
-    final long tolerance = 200;
-    scmServerImpl.setRpcResponseDelay(1500);
+    final long tolerance = 900;
+    scmServerImpl.setRpcResponseDelay(2500);
     long start = Time.monotonicNow();
     registerTaskHelper(serverAddress, 1000, false).close();
     long end = Time.monotonicNow();
