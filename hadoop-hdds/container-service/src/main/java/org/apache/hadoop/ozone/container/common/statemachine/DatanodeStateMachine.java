99f18cf50394841f712423854cd79494b3711333
jianghuazhu <740087514@qq.com>
2024-06-28 18:55:44 +0800

    HDDS-11082. Code cleanup in the DatanodeStateMachine class

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/statemachine/DatanodeStateMachine.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/statemachine/DatanodeStateMachine.java
@@ -281,8 +281,7 @@ public class DatanodeStateMachine implements Closeable {
   private int getEndPointTaskThreadPoolSize() {
     // TODO(runzhiwang): current only support one recon, if support multiple
     //  recon in future reconServerCount should be the real number of recon
-    int reconServerCount = 1;
-    int totalServerCount = reconServerCount;
+    int totalServerCount = 1;
 
     try {
       totalServerCount += HddsUtils.getSCMAddressForDatanodes(conf).size();
@@ -329,13 +328,11 @@ public class DatanodeStateMachine implements Closeable {
    * Runs the state machine at a fixed frequency.
    */
   private void startStateMachineThread() throws IOException {
-    long now;
-
     reportManager.init();
     initCommandHandlerThread(conf);
 
     upgradeFinalizer.runPrefinalizeStateActions(layoutStorage, this);
-
+    LOG.info("The DatanodeStateMachine started.");
     while (context.getState() != DatanodeStates.SHUTDOWN) {
       try {
         LOG.debug("Executing cycle Number : {}", context.getExecutionCount());
@@ -352,7 +349,7 @@ public class DatanodeStateMachine implements Closeable {
         LOG.error("Unable to finish the execution.", e);
       }
 
-      now = Time.monotonicNow();
+      long now = Time.monotonicNow();
       if (now < nextHB.get()) {
         if (!Thread.interrupted()) {
           try {
@@ -542,7 +539,6 @@ public class DatanodeStateMachine implements Closeable {
   public void startDaemon() {
     Runnable startStateMachineTask = () -> {
       try {
-        LOG.info("Ozone container server started.");
         startStateMachineThread();
       } catch (Exception ex) {
         LOG.error("Unable to start the DatanodeState Machine", ex);
@@ -625,9 +621,9 @@ public class DatanodeStateMachine implements Closeable {
       context.setState(DatanodeStates.SHUTDOWN);
       reportManager.shutdown();
       this.close();
-      LOG.info("Ozone container server stopped.");
+      LOG.info("The DatanodeStateMachine stopped.");
     } catch (IOException e) {
-      LOG.error("Stop ozone container server failed.", e);
+      LOG.error("Stop DatanodeStateMachine failed.", e);
     }
   }
 
@@ -660,7 +656,6 @@ public class DatanodeStateMachine implements Closeable {
      * we have single command  queue process thread.
      */
     Runnable processCommandQueue = () -> {
-      long now;
       while (getContext().getState() != DatanodeStates.SHUTDOWN) {
         SCMCommand<?> command = getContext().getNextCommand();
         if (command != null) {
@@ -669,7 +664,7 @@ public class DatanodeStateMachine implements Closeable {
         } else {
           try {
             // Sleep till the next HB + 1 second.
-            now = Time.monotonicNow();
+            long now = Time.monotonicNow();
             if (nextHB.get() > now) {
               Thread.sleep((nextHB.get() - now) + 1000L);
             }
@@ -699,15 +694,6 @@ public class DatanodeStateMachine implements Closeable {
       getCommandHandlerThread(processCommandQueue).start();
     });
     return handlerThread;
-  }
-
-  /**
-   * Returns the number of commands handled  by the datanode.
-   * @return  count
-   */
-  @VisibleForTesting
-  public long getCommandHandled() {
-    return commandsHandled;
   }
 
   /**
