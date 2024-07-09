0611fe2848704fd7947403e995eb14b6fe6e925a
sarvekshayr <sarvekshayr@cloudera.com>
2024-07-09 12:45:07 +0530

    Fixed checkstyle errors

--- hadoop-hdds/common/src/main/java/com/google/protobuf/XmlToMarkdown.java
+++ hadoop-hdds/common/src/main/java/com/google/protobuf/XmlToMarkdown.java
@@ -5,7 +5,7 @@
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -39,11 +39,14 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Represents a property in the XML file.
+ */
 class Property {
-  String name;
-  String value;
-  String tag;
-  String description;
+  private final String name;
+  private final String value;
+  private final String tag;
+  private final String description;
 
   Property(String name, String value, String tag, String description) {
     this.name = name;
@@ -51,15 +54,42 @@ class Property {
     this.tag = tag;
     this.description = description;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public String getTag() {
+    return tag;
+  }
+
+  public String getDescription() {
+    return description;
+  }
 }
 
-public class XmlToMarkdown {
+/**
+ * Utility class for converting XML properties from ozone-default.xml and
+ * ozone-default-generated.xml into Markdown format. The converted content
+ * is saved to Configurations.md.
+ */
+public final class XmlToMarkdown {
+
+  private XmlToMarkdown() {
+    // Prevent instantiation
+  }
 
   public static void main(String[] args) {
     try {
       // List of XML files to process
-      String[] xmlFiles = {"hadoop-hdds/common/src/main/resources/ozone-default.xml",
-              "hadoop-hdds/config/target/test-classes/ozone-default-generated.xml"};
+      String[] xmlFiles = {
+          "hadoop-hdds/common/src/main/resources/ozone-default.xml",
+          "hadoop-hdds/config/target/test-classes/ozone-default-generated.xml"
+      };
 
       List<Property> properties = new ArrayList<>();
 
@@ -95,33 +125,33 @@ public class XmlToMarkdown {
       }
 
       // Sort properties by name
-      properties.sort(Comparator.comparing(p -> p.name));
+      properties.sort(Comparator.comparing(Property::getName));
 
       // Generate markdown content
       StringBuilder markdown = new StringBuilder();
       // Add Apache license header
       markdown.append("<!--\n")
-        .append("Licensed to the Apache Software Foundation (ASF) under one or more\n")
-        .append("contributor license agreements.  See the NOTICE file distributed with\n")
-        .append("this work for additional information regarding copyright ownership.\n")
-        .append("The ASF licenses this file to You under the Apache License, Version 2.0\n")
-        .append("(the \"License\"); you may not use this file except in compliance with\n")
-        .append("the License.  You may obtain a copy of the License at\n\n")
-        .append("   http://www.apache.org/licenses/LICENSE-2.0\n\n")
-        .append("Unless required by applicable law or agreed to in writing, software\n")
-        .append("distributed under the License is distributed on an \"AS IS\" BASIS,\n")
-        .append("WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n")
-        .append("See the License for the specific language governing permissions and\n")
-        .append("limitations under the License.\n")
-        .append("-->\n\n");
+          .append("Licensed to the Apache Software Foundation (ASF) under one or more\n")
+          .append("contributor license agreements.  See the NOTICE file distributed with\n")
+          .append("this work for additional information regarding copyright ownership.\n")
+          .append("The ASF licenses this file to You under the Apache License, Version 2.0\n")
+          .append("(the \"License\"); you may not use this file except in compliance with\n")
+          .append("the License.  You may obtain a copy of the License at\n\n")
+          .append("   http://www.apache.org/licenses/LICENSE-2.0\n\n")
+          .append("Unless required by applicable law or agreed to in writing, software\n")
+          .append("distributed under the License is distributed on an \"AS IS\" BASIS,\n")
+          .append("WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n")
+          .append("See the License for the specific language governing permissions and\n")
+          .append("limitations under the License.\n")
+          .append("-->\n\n");
 
       // Add properties
       for (Property prop : properties) {
-        markdown.append("| **Name**        | `").append(prop.name).append("` |\n");
+        markdown.append("| **Name**        | `").append(prop.getName()).append("` |\n");
         markdown.append("|:----------------|:----------------------------|\n");
-        markdown.append("| **Value**       | ").append(prop.value).append(" |\n");
-        markdown.append("| **Tag**         | ").append(prop.tag).append(" |\n");
-        markdown.append("| **Description** | ").append(prop.description).append(" |\n");
+        markdown.append("| **Value**       | ").append(prop.getValue()).append(" |\n");
+        markdown.append("| **Tag**         | ").append(prop.getTag()).append(" |\n");
+        markdown.append("| **Description** | ").append(prop.getDescription()).append(" |\n");
         markdown.append("--------------------------------------------------------------------------------\n");
       }
 
