4b6e27bfd56303e333e5b2369d8bd8a3d778cd86
Wei-Chiu Chuang <weichiu@apache.org>
2024-08-01 09:29:38 -0700

    Address review comments.
    Change-Id: I644ef2028aa7a3b6eee85994aba3c2300a0ca00d

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/key/OMKeyCommitRequest.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/key/OMKeyCommitRequest.java
@@ -545,9 +545,32 @@ public class OMKeyCommitRequest extends OMKeyRequest {
     return req;
   }
 
+  @RequestFeatureValidator(
+      conditions = ValidationCondition.CLUSTER_NEEDS_FINALIZATION,
+      processingPhase = RequestProcessingPhase.PRE_PROCESS,
+      requestType = Type.CommitKey
+  )
+  public static OMRequest disallowHsync(
+      OMRequest req, ValidationContext ctx) throws OMException {
+    if (!ctx.versionManager()
+        .isAllowed(OMLayoutFeature.HSYNC)) {
+      CommitKeyRequest commitKeyRequest = req.getCommitKeyRequest();
+      boolean isHSync = commitKeyRequest.hasHsync() &&
+          commitKeyRequest.getHsync();
+      if (isHSync) {
+        throw new OMException("Cluster does not have the hsync support "
+            + "feature finalized yet, but the request contains"
+            + " an hsync field. Rejecting the request,"
+            + " please finalize the cluster upgrade and then try again.",
+            OMException.ResultCodes.NOT_SUPPORTED_OPERATION_PRIOR_FINALIZATION);
+      }
+    }
+    return req;
+  }
+
   /**
    * Validates key commit requests.
-   * We do not want to allow clients to perform hsync or lease recovery requests
+   * We do not want to allow clients to perform lease recovery requests
    * until the cluster has finalized the HBase support feature.
    *
    * @param req - the request to validate
@@ -560,19 +583,18 @@ public class OMKeyCommitRequest extends OMKeyRequest {
       processingPhase = RequestProcessingPhase.PRE_PROCESS,
       requestType = Type.CommitKey
   )
-  public static OMRequest disallowHBase(
+
+  public static OMRequest disallowRecovery(
       OMRequest req, ValidationContext ctx) throws OMException {
     if (!ctx.versionManager()
         .isAllowed(OMLayoutFeature.HBASE_SUPPORT)) {
       CommitKeyRequest commitKeyRequest = req.getCommitKeyRequest();
-      boolean isHSync = commitKeyRequest.hasHsync() &&
-          commitKeyRequest.getHsync();
       boolean isRecovery = commitKeyRequest.hasRecovery() &&
           commitKeyRequest.getRecovery();
-      if (isHSync || isRecovery) {
+      if (isRecovery) {
         throw new OMException("Cluster does not have the HBase support "
             + "feature finalized yet, but the request contains"
-            + " an hsync or a recovery field. Rejecting the request,"
+            + " an recovery field. Rejecting the request,"
             + " please finalize the cluster upgrade and then try again.",
             OMException.ResultCodes.NOT_SUPPORTED_OPERATION_PRIOR_FINALIZATION);
       }
--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/upgrade/OMLayoutFeature.java
@@ -45,7 +45,7 @@ public enum OMLayoutFeature implements LayoutFeature {
   FILESYSTEM_SNAPSHOT(5, "Ozone version supporting snapshot"),
 
   QUOTA(6, "Ozone quota re-calculate"),
-  HBASE_SUPPORT(7, "Full support of hsync for HBase");
+  HBASE_SUPPORT(7, "Full support of hsync, lease recovery and listOpenFiles APIs for HBase");
 
   ///////////////////////////////  /////////////////////////////
   //    Example OM Layout Feature with Actions
