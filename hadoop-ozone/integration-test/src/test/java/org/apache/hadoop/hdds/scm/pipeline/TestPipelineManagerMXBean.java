482e374f8e5d5dc730b02cd559c37687def2c3a1
chungen0126 <wfps1210@gmail.com>
2024-08-29 12:16:43 +0800

    clean up unused code

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestPipelineManagerMXBean.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/scm/pipeline/TestPipelineManagerMXBean.java
@@ -21,7 +21,6 @@ package org.apache.hadoop.hdds.scm.pipeline;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.ozone.MiniOzoneCluster;
 import org.apache.ozone.test.GenericTestUtils;
-import org.apache.ozone.test.tag.Flaky;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,8 +46,6 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 @Timeout(3000)
 public class TestPipelineManagerMXBean {
 
-  private static final Logger LOG =
-      LoggerFactory.getLogger(TestPipelineManagerMXBean.class);
   private MiniOzoneCluster cluster;
   private MBeanServer mbs;
 
