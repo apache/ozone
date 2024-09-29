e4b4b27d5e9824612cae3f285e8c575481500087
arafat <mohammad.khan@cloudera.com>
2024-09-30 01:40:57 +0530

    Added log comments for testing

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/upgrade/Feature1UpgradeAction.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/upgrade/Feature1UpgradeAction.java
@@ -1,10 +1,14 @@
 package org.apache.hadoop.ozone.recon.upgrade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Feature1UpgradeAction implements ReconUpgradeAction {
+  private static final Logger LOG = LoggerFactory.getLogger(Feature1UpgradeAction.class);
   @Override
   public void execute() throws Exception {
     // Logic for upgrading to version 1
-    System.out.println("Executing Feature 1 upgrade: Adding new column to the table.");
+    LOG.info("Executing Feature 1 upgrade:");
     // Implement the database schema update or other upgrade logic here
   }
 }
--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/upgrade/Feature2UpgradeAction.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/upgrade/Feature2UpgradeAction.java
@@ -1,10 +1,15 @@
 package org.apache.hadoop.ozone.recon.upgrade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Feature2UpgradeAction implements ReconUpgradeAction {
+  private static final Logger LOG = LoggerFactory.getLogger(Feature2UpgradeAction.class);
+
   @Override
   public void execute() throws Exception {
     // Logic for upgrading to version 2
-    System.out.println("Executing Feature 2 upgrade: Creating a new table.");
+    LOG.info("Executing Feature 2 upgrade");
     // Implement the database schema update or other upgrade logic here
   }
 }
