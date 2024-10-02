7eb69a2e5becf9c0cabf1fc7b860ecd86400b5a4
harryteng9527 <zhimao.teng@gmail.com>
2024-10-02 18:56:05 +0800

    Fix checkstyle

--- hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/checksum/TestECFileChecksumHelper.java
+++ hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/checksum/TestECFileChecksumHelper.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.ozone.client.checksum;
 
 import jakarta.annotation.Nonnull;
@@ -18,7 +35,6 @@ import org.apache.hadoop.hdds.scm.pipeline.Pipeline;
 import org.apache.hadoop.hdds.scm.pipeline.PipelineID;
 import org.apache.hadoop.ozone.client.MockOmTransport;
 import org.apache.hadoop.ozone.client.MockXceiverClientFactory;
-import org.apache.hadoop.ozone.client.ObjectStore;
 import org.apache.hadoop.ozone.client.OzoneBucket;
 import org.apache.hadoop.ozone.client.OzoneClient;
 import org.apache.hadoop.ozone.client.OzoneVolume;
@@ -51,201 +67,204 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * Unit tests for ECFileChecksumHelper class.
+ */
 public class TestECFileChecksumHelper {
-    private OzoneClient client;
-    private RpcClient rpcClient;
+  private OzoneClient client;
+  private RpcClient rpcClient;
 
-    @BeforeEach
-    public void init() throws IOException {
-        ConfigurationSource config = new InMemoryConfiguration();
-        OzoneClientConfig clientConfig = config.getObject(OzoneClientConfig.class);
-        clientConfig.setChecksumType(ContainerProtos.ChecksumType.CRC32C);
+  @BeforeEach
+  public void init() throws IOException {
+    ConfigurationSource config = new InMemoryConfiguration();
+    OzoneClientConfig clientConfig = config.getObject(OzoneClientConfig.class);
+    clientConfig.setChecksumType(ContainerProtos.ChecksumType.CRC32C);
 
-        ((InMemoryConfiguration)config).setFromObject(clientConfig);
+    ((InMemoryConfiguration) config).setFromObject(clientConfig);
 
-        rpcClient = new RpcClient(config, null) {
+    rpcClient = new RpcClient(config, null) {
 
-            @Override
-            protected OmTransport createOmTransport(
-                    String omServiceId) {
-                return new MockOmTransport();
-            }
+      @Override
+      protected OmTransport createOmTransport(
+          String omServiceId) {
+        return new MockOmTransport();
+      }
 
-            @Nonnull
-            @Override
-            protected XceiverClientFactory createXceiverClientFactory(
-                    ServiceInfoEx serviceInfo) {
-                return new MockXceiverClientFactory();
-            }
+      @Nonnull
+      @Override
+      protected XceiverClientFactory createXceiverClientFactory(
+          ServiceInfoEx serviceInfo) {
+        return new MockXceiverClientFactory();
+      }
+    };
+
+    client = new OzoneClient(config, rpcClient);
+  }
+
+  @AfterEach
+  public void close() throws IOException {
+    client.close();
+  }
+
+
+  @Test
+  public void testEmptyBlock() throws IOException {
+    // test the file checksum of a file with an empty block.
+    RpcClient mockRpcClient = mock(RpcClient.class);
+    OzoneManagerProtocol om = mock(OzoneManagerProtocol.class);
+    OzoneVolume mockVolume = mock(OzoneVolume.class);
+    OzoneBucket mockBucket = mock(OzoneBucket.class);
+    when(mockRpcClient.getOzoneManagerClient()).thenReturn(om);
+    when(mockVolume.getName()).thenReturn("vol1");
+    when(mockBucket.getName()).thenReturn("bucket1");
+    OmKeyInfo omKeyInfo = new OmKeyInfo.Builder()
+        .setVolumeName(mockVolume.getName())
+        .setBucketName(mockBucket.getName())
+        .setKeyName("dummy key")
+        .addOmKeyLocationInfoGroup(new OmKeyLocationInfoGroup(0, new ArrayList<>()))
+        .setCreationTime(Time.now())
+        .setModificationTime(Time.now())
+        .setDataSize(0)
+        .setReplicationConfig(new ECReplicationConfig(6, 3))
+        .setFileEncryptionInfo(null)
+        .setAcls(null)
+        .build();
+    when(om.lookupKey(any())).thenReturn(omKeyInfo);
+    OzoneClientConfig.ChecksumCombineMode combineMode =
+        OzoneClientConfig.ChecksumCombineMode.MD5MD5CRC;
+    ECFileChecksumHelper helper = new ECFileChecksumHelper(
+        mockVolume, mockBucket, "dummy", 10, combineMode, mockRpcClient, omKeyInfo);
+
+    helper.compute();
+    FileChecksum fileChecksum = helper.getFileChecksum();
+    assertInstanceOf(MD5MD5CRC32GzipFileChecksum.class, fileChecksum);
+    assertEquals(DataChecksum.Type.CRC32,
+        ((MD5MD5CRC32GzipFileChecksum) fileChecksum).getCrcType());
+
+    // test negative length
+    helper = new ECFileChecksumHelper(
+        mockVolume, mockBucket, "dummy", -1, combineMode, mockRpcClient, omKeyInfo);
+    helper.compute();
+    assertNull(helper.getKeyLocationInfoList());
+  }
+
+  @Test
+  public void testOneBlock() throws IOException {
+    // test the file checksum of a file with one block.
+    OzoneConfiguration conf = new OzoneConfiguration();
+    List<DatanodeDetails> dns = Collections.singletonList(
+        DatanodeDetails.newBuilder().setUuid(UUID.randomUUID()).build());
+    Pipeline pipeline = Pipeline.newBuilder()
+        .setId(PipelineID.randomId())
+        .setReplicationConfig(new ECReplicationConfig(6, 3))
+        .setState(Pipeline.PipelineState.CLOSED)
+        .setNodes(dns)
+        .build();
+    RpcClient mockRpcClient = mock(RpcClient.class);
+    XceiverClientGrpc xceiverClientGrpc =
+        new XceiverClientGrpc(pipeline, conf) {
+          @Override
+          public XceiverClientReply sendCommandAsync(
+              ContainerProtos.ContainerCommandRequestProto request,
+              DatanodeDetails dn) {
+            return buildValidResponse();
+          }
         };
+    XceiverClientFactory factory = mock(XceiverClientFactory.class);
+    OzoneManagerProtocol om = mock(OzoneManagerProtocol.class);
+    OzoneVolume mockVolume = mock(OzoneVolume.class);
+    OzoneBucket mockBucket = mock(OzoneBucket.class);
+    when(mockVolume.getName()).thenReturn("vol1");
+    when(mockBucket.getName()).thenReturn("bucket1");
+    when(factory.acquireClientForReadData(any())).
+        thenReturn(xceiverClientGrpc);
+    when(mockRpcClient.getXceiverClientManager()).thenReturn(factory);
+    when(mockRpcClient.getOzoneManagerClient()).thenReturn(om);
 
-        client = new OzoneClient(config, rpcClient);
-    }
+    BlockID blockID = new BlockID(1, 1);
+    OmKeyLocationInfo omKeyLocationInfo =
+        new OmKeyLocationInfo.Builder().setPipeline(pipeline)
+            .setBlockID(blockID)
+            .build();
 
-    @AfterEach
-    public void close() throws IOException {
-        client.close();
-    }
+    List<OmKeyLocationInfo> omKeyLocationInfoList =
+        Collections.singletonList(omKeyLocationInfo);
 
+    OmKeyInfo omKeyInfo = new OmKeyInfo.Builder()
+        .setVolumeName(mockVolume.getName())
+        .setBucketName(mockBucket.getName())
+        .setKeyName("dummy")
+        .setCreationTime(Time.now())
+        .setModificationTime(Time.now())
+        .setDataSize(10)
+        .setReplicationConfig(new ECReplicationConfig(6, 3))
+        .setFileEncryptionInfo(null)
+        .setAcls(null)
+        .addOmKeyLocationInfoGroup(new OmKeyLocationInfoGroup(0, omKeyLocationInfoList))
+        .build();
 
-    @Test
-    public void testEmptyBlock() throws IOException {
-        // test the file checksum of a file with an empty block.
-        RpcClient mockRpcClient = mock(RpcClient.class);
-        OzoneManagerProtocol om = mock(OzoneManagerProtocol.class);
-        OzoneVolume mockVolume = mock(OzoneVolume.class);
-        OzoneBucket mockBucket = mock(OzoneBucket.class);
-        when(mockRpcClient.getOzoneManagerClient()).thenReturn(om);
-        when(mockVolume.getName()).thenReturn("vol1");
-        when(mockBucket.getName()).thenReturn("bucket1");
-        OmKeyInfo omKeyInfo = new OmKeyInfo.Builder()
-                .setVolumeName(mockVolume.getName())
-                .setBucketName(mockBucket.getName())
-                .setKeyName("dummy key")
-                .addOmKeyLocationInfoGroup(new OmKeyLocationInfoGroup(0, new ArrayList<>()))
-                .setCreationTime(Time.now())
-                .setModificationTime(Time.now())
-                .setDataSize(0)
-                .setReplicationConfig(new ECReplicationConfig(6, 3))
-                .setFileEncryptionInfo(null)
-                .setAcls(null)
-                .build();
-        when(om.lookupKey(any())).thenReturn(omKeyInfo);
-        OzoneClientConfig.ChecksumCombineMode combineMode =
-                OzoneClientConfig.ChecksumCombineMode.MD5MD5CRC;
-        ECFileChecksumHelper helper = new ECFileChecksumHelper(
-                mockVolume, mockBucket, "dummy", 10, combineMode, mockRpcClient, omKeyInfo);
+    when(om.lookupKey(any())).thenReturn(omKeyInfo);
 
-        helper.compute();
-        FileChecksum fileChecksum = helper.getFileChecksum();
-        assertInstanceOf(MD5MD5CRC32GzipFileChecksum.class, fileChecksum);
-        assertEquals(DataChecksum.Type.CRC32,
-                ((MD5MD5CRC32GzipFileChecksum)fileChecksum).getCrcType());
+    OzoneClientConfig.ChecksumCombineMode combineMode =
+        OzoneClientConfig.ChecksumCombineMode.MD5MD5CRC;
 
-        // test negative length
-        helper = new ECFileChecksumHelper(
-                mockVolume, mockBucket, "dummy", -1, combineMode, mockRpcClient, omKeyInfo);
-        helper.compute();
-        assertNull(helper.getKeyLocationInfoList());
-    }
+    ECFileChecksumHelper helper = new ECFileChecksumHelper(
+        mockVolume, mockBucket, "dummy", 10, combineMode, mockRpcClient, omKeyInfo);
 
-    @Test
-    public void testOneBlock() throws IOException {
-        // test the file checksum of a file with one block.
-        OzoneConfiguration conf = new OzoneConfiguration();
-        List<DatanodeDetails> dns = Collections.singletonList(
-                DatanodeDetails.newBuilder().setUuid(UUID.randomUUID()).build());
-        Pipeline pipeline = Pipeline.newBuilder()
-                .setId(PipelineID.randomId())
-                .setReplicationConfig(new ECReplicationConfig(6, 3))
-                .setState(Pipeline.PipelineState.CLOSED)
-                .setNodes(dns)
-                .build();;
-        RpcClient mockRpcClient = mock(RpcClient.class);
-        XceiverClientGrpc xceiverClientGrpc =
-                new XceiverClientGrpc(pipeline, conf) {
-                    @Override
-                    public XceiverClientReply sendCommandAsync(
-                            ContainerProtos.ContainerCommandRequestProto request,
-                            DatanodeDetails dn) {
-                        return buildValidResponse();
-                    }
-                };
-        XceiverClientFactory factory = mock(XceiverClientFactory.class);
-        OzoneManagerProtocol om = mock(OzoneManagerProtocol.class);
-        OzoneVolume mockVolume = mock(OzoneVolume.class);
-        OzoneBucket mockBucket = mock(OzoneBucket.class);
-        when(mockVolume.getName()).thenReturn("vol1");
-        when(mockBucket.getName()).thenReturn("bucket1");
-        when(factory.acquireClientForReadData(any())).
-                thenReturn(xceiverClientGrpc);
-        when(mockRpcClient.getXceiverClientManager()).thenReturn(factory);
-        when(mockRpcClient.getOzoneManagerClient()).thenReturn(om);
+    helper.compute();
+    FileChecksum fileChecksum = helper.getFileChecksum();
+    assertInstanceOf(MD5MD5CRC32GzipFileChecksum.class, fileChecksum);
+    assertEquals(1, helper.getKeyLocationInfoList().size());
+  }
 
-        BlockID blockID = new BlockID(1, 1);
-        OmKeyLocationInfo omKeyLocationInfo =
-                new OmKeyLocationInfo.Builder().setPipeline(pipeline)
-                        .setBlockID(blockID)
-                        .build();
+  private XceiverClientReply buildValidResponse() {
+    // return a GetBlockResponse message of a block and its chunk checksums.
+    ContainerProtos.DatanodeBlockID blockID =
+        ContainerProtos.DatanodeBlockID.newBuilder()
+            .setContainerID(1)
+            .setLocalID(1)
+            .setBlockCommitSequenceId(1).build();
 
-        List<OmKeyLocationInfo> omKeyLocationInfoList =
-                Collections.singletonList(omKeyLocationInfo);
+    byte[] stripeByteArray = new byte[12];
+    ByteString stripeString = ByteString.copyFrom(stripeByteArray);
 
-        OmKeyInfo omKeyInfo = new OmKeyInfo.Builder()
-                .setVolumeName(mockVolume.getName())
-                .setBucketName(mockBucket.getName())
-                .setKeyName("dummy")
-                .setCreationTime(Time.now())
-                .setModificationTime(Time.now())
-                .setDataSize(10)
-                .setReplicationConfig(new ECReplicationConfig(6, 3))
-                .setFileEncryptionInfo(null)
-                .setAcls(null)
-                .addOmKeyLocationInfoGroup(new OmKeyLocationInfoGroup(0, omKeyLocationInfoList))
-                .build();
+    ContainerProtos.ChecksumData checksumData =
+        ContainerProtos.ChecksumData.newBuilder()
+            .setType(CRC32)
+            .setBytesPerChecksum(1024)
+            .addChecksums(stripeString)
+            .build();
 
-        when(om.lookupKey(any())).thenReturn(omKeyInfo);
+    ContainerProtos.ChunkInfo chunkInfo =
+        ContainerProtos.ChunkInfo.newBuilder()
+            .setChunkName("dummy_chunk")
+            .setOffset(1)
+            .setLen(10)
+            .setChecksumData(checksumData)
+            .setStripeChecksum(stripeString)
+            .build();
 
-        OzoneClientConfig.ChecksumCombineMode combineMode =
-                OzoneClientConfig.ChecksumCombineMode.MD5MD5CRC;
+    ContainerProtos.BlockData blockData =
+        ContainerProtos.BlockData.newBuilder()
+            .setBlockID(blockID)
+            .addChunks(chunkInfo)
+            .build();
 
-        ECFileChecksumHelper helper = new ECFileChecksumHelper(
-                mockVolume, mockBucket, "dummy", 10, combineMode, mockRpcClient, omKeyInfo);
+    ContainerProtos.GetBlockResponseProto getBlockResponseProto
+        = ContainerProtos.GetBlockResponseProto.newBuilder()
+        .setBlockData(blockData)
+        .build();
 
-        helper.compute();
-        FileChecksum fileChecksum = helper.getFileChecksum();
-        assertInstanceOf(MD5MD5CRC32GzipFileChecksum.class, fileChecksum);
-        assertEquals(1, helper.getKeyLocationInfoList().size());
-    }
-
-    private XceiverClientReply buildValidResponse() {
-        // return a GetBlockResponse message of a block and its chunk checksums.
-        ContainerProtos.DatanodeBlockID blockID =
-                ContainerProtos.DatanodeBlockID.newBuilder()
-                        .setContainerID(1)
-                        .setLocalID(1)
-                        .setBlockCommitSequenceId(1).build();
-
-        byte[] stripeByteArray = new byte[12];
-        ByteString stripeString = ByteString.copyFrom(stripeByteArray);
-
-        ContainerProtos.ChecksumData checksumData =
-                ContainerProtos.ChecksumData.newBuilder()
-                        .setType(CRC32)
-                        .setBytesPerChecksum(1024)
-                        .addChecksums(stripeString)
-                        .build();
-
-        ContainerProtos.ChunkInfo chunkInfo =
-                ContainerProtos.ChunkInfo.newBuilder()
-                        .setChunkName("dummy_chunk")
-                        .setOffset(1)
-                        .setLen(10)
-                        .setChecksumData(checksumData)
-                        .setStripeChecksum(stripeString)
-                        .build();
-
-        ContainerProtos.BlockData blockData =
-                ContainerProtos.BlockData.newBuilder()
-                        .setBlockID(blockID)
-                        .addChunks(chunkInfo)
-                        .build();
-
-        ContainerProtos.GetBlockResponseProto getBlockResponseProto
-                = ContainerProtos.GetBlockResponseProto.newBuilder()
-                .setBlockData(blockData)
-                .build();
-
-        ContainerProtos.ContainerCommandResponseProto resp =
-                ContainerProtos.ContainerCommandResponseProto.newBuilder()
-                        .setCmdType(ContainerProtos.Type.GetBlock)
-                        .setResult(ContainerProtos.Result.SUCCESS)
-                        .setGetBlock(getBlockResponseProto)
-                        .build();
-        final CompletableFuture<ContainerProtos.ContainerCommandResponseProto>
-                replyFuture = new CompletableFuture<>();
-        replyFuture.complete(resp);
-        return new XceiverClientReply(replyFuture);
-    }
+    ContainerProtos.ContainerCommandResponseProto resp =
+        ContainerProtos.ContainerCommandResponseProto.newBuilder()
+            .setCmdType(ContainerProtos.Type.GetBlock)
+            .setResult(ContainerProtos.Result.SUCCESS)
+            .setGetBlock(getBlockResponseProto)
+            .build();
+    final CompletableFuture<ContainerProtos.ContainerCommandResponseProto>
+        replyFuture = new CompletableFuture<>();
+    replyFuture.complete(resp);
+    return new XceiverClientReply(replyFuture);
+  }
 
 }
