2c376bcadd82eaed5bdc70def203f943d3e0834e
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2024-11-07 13:11:40 -0800

    HDDS-11650. Fix checkstyle & rat
    Change-Id: I16cec52ea1c2853c80ee9a6e3279a23408d05651

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/volume/MutableVolumeSet.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/volume/MutableVolumeSet.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
