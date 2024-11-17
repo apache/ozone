af679d8af46d538584f4ccb4445c25d7fc72871f
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2024-11-17 11:14:24 -0800

    HDDS-11697. Fix whitespace change
    Change-Id: I10aa7afdefa2c3f37d0cfc553a06762448dfb6fb

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataReader.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataReader.java
@@ -264,7 +264,7 @@ public class OmMetadataReader implements IOmMetadataReader, Auditor {
         .map(OzoneFileStatusLight::fromOzoneFileStatus)
         .collect(Collectors.toList());
   }
- 
+  
   @Override
   public OzoneFileStatus getFileStatus(OmKeyArgs args) throws IOException {
     ResolvedBucket bucket = ozoneManager.resolveBucketLink(args);
