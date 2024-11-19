f7b5b6b1d12bef4fb319412c38e3c1919f9bba29
Doroszlai, Attila <adoroszlai@apache.org>
2024-11-19 08:20:48 +0100

    replace skip.installnpx with skip.installnodenpm

--- hadoop-ozone/dev-support/checks/checkstyle.sh
+++ hadoop-ozone/dev-support/checks/checkstyle.sh
@@ -24,7 +24,7 @@ REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../../target/checkstyle"}
 mkdir -p "$REPORT_DIR"
 REPORT_FILE="$REPORT_DIR/summary.txt"
 
-MAVEN_OPTIONS='-B -fae -Dskip.npx -Dskip.installnpx -Dcheckstyle.failOnViolation=false --no-transfer-progress'
+MAVEN_OPTIONS='-B -fae -Dskip.npx -Dskip.installnodenpm -Dcheckstyle.failOnViolation=false --no-transfer-progress'
 
 declare -i rc
 mvn ${MAVEN_OPTIONS} checkstyle:check > "${REPORT_DIR}/output.log"
