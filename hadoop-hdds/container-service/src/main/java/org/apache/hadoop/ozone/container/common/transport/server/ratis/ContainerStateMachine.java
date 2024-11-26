87a812eb597d3a9f1f59e75e99a7607ec00fb34b
Wei-Chiu Chuang <weichiu@apache.org>
2024-11-26 10:16:51 -0800

    HDDS-11785. DataNode aborts ContainerStateMachine if it does not know any follower next index (#7480)
    (cherry picked from commit 3f92663aed8acf023c09eb6d7f92e214c677b027)

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/ContainerStateMachine.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/ContainerStateMachine.java
@@ -964,12 +964,15 @@ public class ContainerStateMachine extends BaseStateMachine {
       try {
         RaftServer.Division division = ratisServer.getServer().getDivision(gid);
         if (division.getInfo().isLeader()) {
-          long minIndex = Arrays.stream(division.getInfo()
-              .getFollowerNextIndices()).min().getAsLong();
-          LOG.debug("Removing data corresponding to log index {} min index {} "
-                  + "from cache", index, minIndex);
-          removeCacheDataUpTo(Math.min(minIndex, index));
+          Arrays.stream(division.getInfo()
+              .getFollowerNextIndices()).min().ifPresent(minIndex -> {
+                removeCacheDataUpTo(Math.min(minIndex, index));
+                LOG.debug("Removing data corresponding to log index {} min index {} "
+                    + "from cache", index, minIndex);
+              });
         }
+      } catch (RuntimeException e) {
+        throw e;
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
