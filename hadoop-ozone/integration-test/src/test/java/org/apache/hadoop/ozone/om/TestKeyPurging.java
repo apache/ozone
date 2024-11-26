6e06d888d2f7792ad57d02f0f67ebf2463ddd01c
Aryan Gupta <aryan.gupta@cloudera.com>
2024-11-26 18:43:24 +0530

    HDDS-11808. Key deletion service should support multiple threads

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestKeyPurging.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestKeyPurging.java
@@ -140,7 +140,7 @@ public class TestKeyPurging {
     GenericTestUtils.waitFor(
         () -> {
           try {
-            return keyManager.getPendingDeletionKeys(Integer.MAX_VALUE)
+            return keyManager.getPendingDeletionKeys(Integer.MAX_VALUE, keyDeletingService.getDeletedKeySupplier())
                 .getKeyBlocksList().size() == 0;
           } catch (IOException e) {
             return false;
