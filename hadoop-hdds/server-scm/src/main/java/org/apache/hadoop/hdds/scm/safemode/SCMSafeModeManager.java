e1b57014b383ac8a56e103d8e3d1ebb629a92109
fanshilun <fanshilun@didiglobal.com>
2024-12-08 15:07:22 +0800

    HDDS-11843. Enhance DataNodeSafeModeRule to Use Pipeline DN List.

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/safemode/SCMSafeModeManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/safemode/SCMSafeModeManager.java
@@ -130,7 +130,7 @@ public class SCMSafeModeManager implements SafeModeManager {
           new ContainerSafeModeRule(CONT_EXIT_RULE, eventQueue, config,
               allContainers,  containerManager, this);
       DataNodeSafeModeRule dataNodeSafeModeRule =
-          new DataNodeSafeModeRule(DN_EXIT_RULE, eventQueue, config, this);
+          new DataNodeSafeModeRule(DN_EXIT_RULE, eventQueue, config, this, pipelineManager);
       exitRules.put(CONT_EXIT_RULE, containerSafeModeRule);
       exitRules.put(DN_EXIT_RULE, dataNodeSafeModeRule);
       preCheckRules.add(DN_EXIT_RULE);
