4c4e3a5be31dfe98694c808a47ced2b0a1039927
Rishabh Patel <ptlrshb@gmail.com>
2024-12-11 10:46:17 -0800

    HDDS-7990. add haproxy and s3g to a secure ozone cluster

--- hadoop-ozone/dist/src/main/compose/ozonesecure/test-haproxy-s3g.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure/test-haproxy-s3g.sh
@@ -24,6 +24,7 @@ export COMPOSE_DIR
 source "$COMPOSE_DIR/../testlib.sh"
 
 export SECURITY_ENABLED=true
+export COMPOSE_FILE=docker-compose.yaml:../common/s3-haproxy-secure.yaml
 
 : ${OZONE_BUCKET_KEY_NAME:=key1}
 
@@ -33,11 +34,7 @@ execute_command_in_container kms hadoop key create ${OZONE_BUCKET_KEY_NAME}
 
 execute_robot_test scm kinit.robot
 
-execute_robot_test scm basic
-
 execute_robot_test scm security
-
-execute_robot_test scm -v SCHEME:ofs -v BUCKET_TYPE:bucket -N ozonefs-ofs-bucket ozonefs/ozonefs.robot
 
 ## Exclude virtual-host tests. This is tested separately as it requires additional config.
 exclude="--exclude virtual-host"
@@ -48,21 +45,4 @@ for bucket in encrypted; do
   exclude="--exclude virtual-host --exclude no-bucket-type"
 done
 
-#expects 4 pipelines, should be run before
-#admincli which creates STANDALONE pipeline
-execute_robot_test scm recon
-
-execute_robot_test scm admincli
 execute_robot_test scm spnego
-execute_robot_test scm snapshot/snapshot-acls.robot
-
-execute_robot_test scm httpfs
-
-# test replication
-docker-compose up -d --scale datanode=2
-execute_robot_test scm -v container:1 -v count:2 replication/wait.robot
-docker-compose up -d --scale datanode=3
-execute_robot_test scm -v container:1 -v count:3 replication/wait.robot
-
-#test public key and certificate recovery
-source "$COMPOSE_DIR/public-key-cert-recovery-test.sh"
