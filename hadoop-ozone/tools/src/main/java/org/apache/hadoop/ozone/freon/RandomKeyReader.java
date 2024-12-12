42af9692850959b7b8f28c227566a6fa9e0c1def
Ren Koike <ren.ko1139@gmail.com>
2024-12-12 13:57:06 +0100

    Break down call method and replace kotlin.Pair with org.apache.commons.lang3.tuple.Pair

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/RandomKeyReader.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/RandomKeyReader.java
@@ -1,7 +1,7 @@
 package org.apache.hadoop.ozone.freon;
 
 import com.codahale.metrics.Timer;
-import kotlin.Pair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.hdds.cli.HddsVersionProvider;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
@@ -39,65 +39,88 @@ public class RandomKeyReader extends BaseFreonGenerator
   private int clientCount;
   private Timer timer;
   private AtomicInteger readKeyCount = new AtomicInteger();
+  private ArrayList<Pair<OzoneVolume, OzoneBucket>> volBuckPairs;
 
   @Override
   public Void call() throws Exception {
     init();
+    setupClients();
+    gatherVolumeBucketInfo();
+    runTests(this::readRandomKeys);
+    teardownClients();
+    report();
+    return null;
+  }
+
+  public void init() {
+    super.init();
+    timer = getMetrics().timer("key-read");
+  }
+
+  private void setupClients() throws Exception {
     OzoneConfiguration ozoneConfiguration = createOzoneConfiguration();
     clientCount = getThreadNo();
     ozoneClients = new OzoneClient[clientCount];
     for (int i = 0; i < clientCount; i++) {
       ozoneClients[i] = createOzoneClient(omServiceID, ozoneConfiguration);
     }
-    timer = getMetrics().timer("key-read");
-
-    runTests(this::readRandomKeys);
-    for (int i = 0; i < clientCount; i++) {
-      if (ozoneClients[i] != null) {
-        ozoneClients[i].close();
-      }
-    }
-    String report = "-- Result ----------------------------------------------------------------------\n" +
-            "The number of keys read: "
-            + readKeyCount.get();
-    System.out.println(report);
-    return null;
   }
 
-  // Get volume list,
-  public void readRandomKeys(long count) throws IOException {
-    int clientIndex = (int) (count % clientCount);
-    OzoneClient c = ozoneClients[clientIndex];
-    timer.time(() -> {
-      Iterator<? extends OzoneVolume> vols = null;
-      try {
-        vols = c.getObjectStore().listVolumes("");
-        if (!vols.hasNext()) {
-          throw new RuntimeException("There is no volume.");
-        }
-        ArrayList<Pair<OzoneVolume, OzoneBucket>> volBuckPairs = new ArrayList<>();
-        vols.forEachRemaining(vol ->
-                vol.listBuckets("").forEachRemaining(buck -> {
-                  Pair<OzoneVolume, OzoneBucket> p = new Pair<>(vol, buck);
-                  volBuckPairs.add(p);
-                })
-        );
-        if (volBuckPairs.isEmpty()) {
-          throw new RuntimeException("No bucket exists in any volume. Create a bucket ");
-        }
-        int randomPairInd = new Random().nextInt(volBuckPairs.size());
-        Pair<OzoneVolume, OzoneBucket> pair = volBuckPairs.get(randomPairInd);
-        Iterator<? extends OzoneKey> keyItereator = pair.getSecond().listKeys("", "");
+  private void gatherVolumeBucketInfo() throws IOException {
+    if (ozoneClients.length == 0) {
+      throw new IllegalStateException("No clients available for gathering volume-bucket information.");
+    }
 
-        while (keyItereator.hasNext()) {
-          String keyName = keyItereator.next().getName();
-          OzoneInputStream stream = null;
-          try {
-            stream = pair.getSecond().readKey(keyName);
+    OzoneClient client = ozoneClients[0];
+    Iterator<? extends OzoneVolume> volumes = client.getObjectStore().listVolumes("");
+
+    if (!volumes.hasNext()) {
+      throw new RuntimeException("There is no volume.");
+    }
+
+    volBuckPairs = new ArrayList<>();
+    volumes.forEachRemaining(vol ->
+            vol.listBuckets("").forEachRemaining(buck -> {
+              Pair<OzoneVolume, OzoneBucket> pair = Pair.of(vol, buck);
+              volBuckPairs.add(pair);
+            })
+    );
+
+    if (volBuckPairs.isEmpty()) {
+      throw new RuntimeException("No bucket exists in any volume. Create a bucket.");
+    }
+  }
+
+  private void teardownClients() {
+    for (int i = 0; i < clientCount; i++) {
+      try {
+        if (ozoneClients[i] != null) {
+          ozoneClients[i].close();
+        }
+      } catch (IOException e) {
+        System.err.println("Error while closing client " + i + ": " + e.getMessage());
+      }
+    }
+  }
+
+  private void report() {
+    String report = "-- Result ----------------------------------------------------------------------\n" +
+            "The number of keys read: " + readKeyCount.get();
+    System.out.println(report);
+  }
+
+  public void readRandomKeys(long count) {
+    timer.time(() -> {
+      try {
+        int randomPairIndex = new Random().nextInt(volBuckPairs.size());
+        Pair<OzoneVolume, OzoneBucket> pair = volBuckPairs.get(randomPairIndex);
+        Iterator<? extends OzoneKey> keyIterator = pair.getRight().listKeys("", "");
+
+        while (keyIterator.hasNext()) {
+          String keyName = keyIterator.next().getName();
+          try (OzoneInputStream stream = pair.getRight().readKey(keyName)) {
             IOUtils.consume(stream);
             readKeyCount.incrementAndGet();
-          } catch (IOException e) {
-            throw new RuntimeException(e);
           }
         }
       } catch (IOException e) {
