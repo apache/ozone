9a5de7dd0ade7aac92abdabe836c684b510b3faa
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2024-12-12 12:10:11 +0100

    HDDS-11909. Intermittent timeout building Hadoop in s3a test (#7559)
    (cherry picked from commit 6b9cbe0e47831c9e18cd64e7adfa86e383955938)

--- hadoop-ozone/dist/src/main/compose/common/s3a-test.sh
+++ hadoop-ozone/dist/src/main/compose/common/s3a-test.sh
@@ -96,7 +96,7 @@ EOF
   # - ITestS3AContractDistCp: HDDS-10616
   # - ITestS3AContractGetFileStatusV1List: HDDS-10617
   # - ITestS3AContractRename: HDDS-10665
-  mvn -B -V --fail-never --no-transfer-progress \
+  mvn ${MAVEN_ARGS:-} --fail-never \
     -Dtest='ITestS3AContract*, ITestS3ACommitterMRJob, !ITestS3AContractDistCp, !ITestS3AContractGetFileStatusV1List, !ITestS3AContractRename' \
     clean test
 
