3bf2963f9f2eae8b7adfc1de5efeea50ec680965
Chia-Chuan Yu <yujuan476@gmail.com>
2024-12-15 17:38:39 +0800

    Removed LegacyReplicationManager

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/balancer/TestContainerBalancerTask.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/balancer/TestContainerBalancerTask.java
@@ -75,7 +75,6 @@ import static org.apache.hadoop.hdds.scm.container.replication.ReplicationManage
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
@@ -142,7 +141,6 @@ public class TestContainerBalancerTask {
      as well as EC containers for balancing. Also, MoveManager will be used.
      */
     when(replicationManager.getConfig()).thenReturn(rmConf);
-    rmConf.setEnableLegacy(false);
     // these configs will usually be specified in each test
     balancerConfiguration =
         conf.getObject(ContainerBalancerConfiguration.class);
@@ -175,12 +173,6 @@ public class TestContainerBalancerTask {
     when(replicationManager
         .isContainerReplicatingOrDeleting(any(ContainerID.class)))
         .thenReturn(false);
-
-    when(replicationManager.move(any(ContainerID.class),
-        any(DatanodeDetails.class),
-        any(DatanodeDetails.class)))
-        .thenReturn(CompletableFuture.
-            completedFuture(MoveManager.MoveResult.COMPLETED));
 
     when(replicationManager.getClock())
         .thenReturn(Clock.system(ZoneId.systemDefault()));
@@ -366,40 +358,6 @@ public class TestContainerBalancerTask {
     // ensure the thread dies
     GenericTestUtils.waitFor(() -> !balancingThread.isAlive(), 1, 20);
     assertFalse(balancingThread.isAlive());
-  }
-
-  /**
-   * The expectation is that only RATIS containers should be selected for
-   * balancing when LegacyReplicationManager is enabled. This is because
-   * LegacyReplicationManager does not support moving EC containers.
-   */
-  @Test
-  public void balancerShouldExcludeECContainersWhenLegacyRmIsEnabled()
-      throws IllegalContainerBalancerStateException, IOException,
-      InvalidContainerBalancerConfigurationException, TimeoutException {
-    // Enable LegacyReplicationManager
-    rmConf.setEnableLegacy(true);
-    balancerConfiguration.setThreshold(10);
-    balancerConfiguration.setIterations(1);
-    balancerConfiguration.setMaxSizeEnteringTarget(10 * STORAGE_UNIT);
-    balancerConfiguration.setMaxSizeToMovePerIteration(100 * STORAGE_UNIT);
-    balancerConfiguration.setMaxDatanodesPercentageToInvolvePerIteration(100);
-
-    startBalancer(balancerConfiguration);
-
-    /*
-     Get all containers that were selected by balancer and assert none of
-     them is an EC container.
-     */
-    Map<ContainerID, DatanodeDetails> containerToSource =
-        containerBalancerTask.getContainerToSourceMap();
-    assertFalse(containerToSource.isEmpty());
-    for (Map.Entry<ContainerID, DatanodeDetails> entry :
-        containerToSource.entrySet()) {
-      ContainerInfo containerInfo = cidToInfoMap.get(entry.getKey());
-      assertNotSame(HddsProtos.ReplicationType.EC,
-          containerInfo.getReplicationType());
-    }
   }
 
   /**
