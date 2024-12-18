b9ef7d0c476b8f7f78ae4022d9184c280bc7bb5f
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2024-12-18 12:45:06 -0800

    HDDS-11149. Fix annotation processor
    Change-Id: I6a5e84b8053a9401ce4210849495af8254561f09

--- hadoop-hdds/annotations/src/main/java/org/apache/ozone/annotations/OmRequestFeatureValidatorProcessor.java
+++ hadoop-hdds/annotations/src/main/java/org/apache/ozone/annotations/OmRequestFeatureValidatorProcessor.java
@@ -28,7 +28,6 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.ExecutableType;
