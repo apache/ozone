df698f86d44d86932dca342f409321e8bad19753
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2024-12-18 16:13:22 -0800

    HDDS-11149. Fix package scanner prefix
    Change-Id: I8302abcfcaa70b07a85ef9505c7dabdc312955d4

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/request/validation/ValidatorRegistry.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/request/validation/ValidatorRegistry.java
@@ -101,7 +101,7 @@ public class ValidatorRegistry<RequestType extends Enum<RequestType>> {
     Class<RequestType[]> requestArrayClass = (Class<RequestType[]>) Array.newInstance(requestType, 0)
         .getClass();
     Set<Class<? extends Annotation>> validatorsToBeRegistered =
-        new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forPackage("org.apache"))
+        new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forPackage("org.apache.hadoop"))
             .setScanners(Scanners.TypesAnnotated)
             .setParallel(true)).getTypesAnnotatedWith(RegisterValidator.class).stream()
             .filter(annotationClass -> getReturnTypeOfAnnotationMethod((Class<? extends Annotation>) annotationClass,
