8cbf459e4f845d037d85947f396abec897eacfcc
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2025-02-03 07:51:43 +0100

    HDDS-12142. Save logs from build check (#7782)

--- hadoop-ozone/dev-support/checks/_build.sh
+++ hadoop-ozone/dev-support/checks/_build.sh
@@ -13,10 +13,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+set -u -o pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../../.." || exit 1
 
+: ${CHECK:="build"}
+: ${ERROR_PATTERN:="\[ERROR\]"}
 : ${OZONE_WITH_COVERAGE:="false"}
+
+BASE_DIR="$(pwd -P)"
+REPORT_DIR=${OUTPUT_DIR:-"${BASE_DIR}/target/${CHECK}"}
+REPORT_FILE="$REPORT_DIR/summary.txt"
 
 MAVEN_OPTIONS='-V -B -DskipTests -DskipDocs --no-transfer-progress'
 
@@ -27,5 +36,10 @@ else
 fi
 
 export MAVEN_OPTS="-Xmx4096m ${MAVEN_OPTS:-}"
-mvn ${MAVEN_OPTIONS} clean "$@"
+mvn ${MAVEN_OPTIONS} clean "$@" | tee output.log
 rc=$?
+
+mkdir -p "$REPORT_DIR" # after `mvn clean`
+mv output.log "$REPORT_DIR"/
+
+source "${DIR}/_post_process.sh"
