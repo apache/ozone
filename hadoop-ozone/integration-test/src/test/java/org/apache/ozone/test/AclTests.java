0f1768109159f95337e72505fbd094d9a1cf66d5
Doroszlai, Attila <adoroszlai@apache.org>
2025-02-07 08:56:14 +0100

    add cluster for ACL tests

---
+++ hadoop-ozone/integration-test/src/test/java/org/apache/ozone/test/AclTests.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ozone.test;
+
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.ozone.MiniOzoneCluster;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS;
+import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS_NATIVE;
+import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_ACL_ENABLED;
+
+/**
+ * Tests with ACL enabled using Ozone Native Authorizer.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class AclTests extends ClusterForTests<MiniOzoneCluster> {
+
+  public static final String ADMIN_USER = "om";
+  public static final String ADMIN_GROUP = "ozone";
+  public static final UserGroupInformation ADMIN_UGI =
+      UserGroupInformation.createUserForTesting(ADMIN_USER, new String[] {ADMIN_GROUP});
+
+  /** Hook method for subclasses. */
+  MiniOzoneCluster.Builder newClusterBuilder() {
+    return MiniOzoneCluster.newBuilder(createOzoneConfig())
+        .setNumDatanodes(3);
+  }
+
+  @Override
+  protected OzoneConfiguration createOzoneConfig() {
+    UserGroupInformation.setLoginUser(ADMIN_UGI);
+    OzoneConfiguration conf = super.createOzoneConfig();
+    conf.setBoolean(OZONE_ACL_ENABLED, true);
+    conf.set(OZONE_ACL_AUTHORIZER_CLASS, OZONE_ACL_AUTHORIZER_CLASS_NATIVE);
+    return conf;
+  }
+
+  /** Test cases for non-HA cluster should implement this. */
+  public interface TestCase {
+    MiniOzoneCluster cluster();
+  }
+
+}
