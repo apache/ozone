e2310a4f71d1093b5c9509bcc2dd9a72ddffb56c
Abhishek Pal <pal.abhishek03012001@gmail.com>
2025-02-12 15:20:44 +0530

    Added License header

--- hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/__tests__/pipelines/Pipelines.test.tsx
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/__tests__/pipelines/Pipelines.test.tsx
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 import {
   render,
@@ -5,7 +23,6 @@ import {
   waitFor,
   fireEvent
 } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
 import { vi } from 'vitest';
 
@@ -13,7 +30,7 @@ import Pipelines from '@/v2/pages/pipelines/pipelines';
 import * as commonUtils from '@/utils/common';
 import { pipelineServer } from '@tests/mocks/pipelineMocks/pipelinesServer';
 import { pipelineLocators, searchInputLocator } from '@tests/locators/locators';
-import { waitForPipelineTable } from '@/__tests__/utils/pipelines.utils';
+import { waitForPipelineTable } from '@tests/utils/pipelines.utils';
 
 // Mock utility functions
 vi.spyOn(commonUtils, 'showDataFetchError');
