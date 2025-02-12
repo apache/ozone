c0097fab1f21c28c5f957c714fc942bbee2dee06
Sreeja Chintalapati <schintalapati@Sreejas-MacBook-Pro.local>
2025-02-12 10:12:05 +0530

    HDDS-12179. Improve Container Log to also capture the Command fired to Datanode

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/utils/ContainerLogger.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/utils/ContainerLogger.java
@@ -51,7 +51,7 @@ public final class ContainerLogger {
    * @param containerData The container that was created and opened.
    */
   public static void logOpen(ContainerData containerData) {
-    LOG.info(getMessage(containerData));
+    LOG.info(getMessage(containerData,"CommandType=CreateContainer"));
   }
 
   /**
@@ -80,7 +80,7 @@ public final class ContainerLogger {
    * @param containerData The container that was closed.
    */
   public static void logClosed(ContainerData containerData) {
-    LOG.info(getMessage(containerData));
+    LOG.info(getMessage(containerData,"CommandType=CloseContainer"));
   }
 
   /**
@@ -116,9 +116,9 @@ public final class ContainerLogger {
    */
   public static void logDeleted(ContainerData containerData, boolean force) {
     if (force) {
-      LOG.info(getMessage(containerData, "Container force deleted"));
+      LOG.info(getMessage(containerData, "Container force deleted, CommandType=DeleteContainer"));
     } else {
-      LOG.info(getMessage(containerData, "Empty container deleted"));
+      LOG.info(getMessage(containerData, "Empty container deleted, CommandType=DeleteContainer"));
     }
   }
 
@@ -128,7 +128,7 @@ public final class ContainerLogger {
    * @param containerData The container that was imported to this datanode.
    */
   public static void logImported(ContainerData containerData) {
-    LOG.info(getMessage(containerData));
+    LOG.info(getMessage(containerData,"action=Imported"));
   }
 
   /**
@@ -137,7 +137,7 @@ public final class ContainerLogger {
    * @param containerData The container that was exported from this datanode.
    */
   public static void logExported(ContainerData containerData) {
-    LOG.info(getMessage(containerData));
+    LOG.info(getMessage(containerData,"action=Exported"));
   }
 
   /**
@@ -146,7 +146,7 @@ public final class ContainerLogger {
    * @param containerData The container that was recovered on this datanode.
    */
   public static void logRecovered(ContainerData containerData) {
-    LOG.info(getMessage(containerData));
+    LOG.info(getMessage(containerData,"action=Recovered"));
   }
 
   private static String getMessage(ContainerData containerData,
@@ -159,6 +159,7 @@ public final class ContainerLogger {
         "ID=" + containerData.getContainerID(),
         "Index=" + containerData.getReplicaIndex(),
         "BCSID=" + containerData.getBlockCommitSequenceId(),
-        "State=" + containerData.getState());
+        "State=" + containerData.getState(),
+        "Volume=" + containerData.getVolume());
   }
 }
