70d5784ed08ebf922c9e4e366229d7a7b91f6f00
sarvekshayr <sarvekshayr@cloudera.com>
2025-02-18 16:23:17 +0530

    Corrected TestOzoneConfigurationFields

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/TestOzoneConfigurationFields.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/TestOzoneConfigurationFields.java
@@ -134,7 +134,19 @@ public class TestOzoneConfigurationFields extends TestConfigurationFieldsBase {
         HddsConfigKeys.HDDS_DATANODE_VOLUME_MIN_FREE_SPACE_PERCENT,
         OzoneConfigKeys.HDDS_SCM_CLIENT_RPC_TIME_OUT,
         OzoneConfigKeys.HDDS_SCM_CLIENT_MAX_RETRY_TIMEOUT,
-        OzoneConfigKeys.HDDS_SCM_CLIENT_FAILOVER_MAX_RETRY
+        OzoneConfigKeys.HDDS_SCM_CLIENT_FAILOVER_MAX_RETRY,
+        OzoneConfigKeys.HDDS_DATANODE_DNS_INTERFACE_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_DNS_NAMESERVER_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_HOST_NAME_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_DATA_DIR_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_USE_DN_HOSTNAME,
+        OzoneConfigKeys.HDDS_XFRAME_OPTION_ENABLED,
+        OzoneConfigKeys.HDDS_XFRAME_OPTION_VALUE,
+        OzoneConfigKeys.HDDS_METRICS_SESSION_ID_KEY,
+        OzoneConfigKeys.NET_TOPOLOGY_NODE_SWITCH_MAPPING_IMPL_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_KERBEROS_PRINCIPAL_KEY,
+        OzoneConfigKeys.HDDS_DATANODE_KERBEROS_KEYTAB_FILE_KEY,
+        OzoneConfigKeys.HDDS_METRICS_PERCENTILES_INTERVALS_KEY
     ));
   }
 }
