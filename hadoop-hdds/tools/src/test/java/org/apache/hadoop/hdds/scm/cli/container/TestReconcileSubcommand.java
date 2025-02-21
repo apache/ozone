9ba8efda47184c5a09729b3bd125f074cadf9dc0
Ethan Rose <erose@apache.org>
2025-02-21 17:48:25 -0500

    Fix findbugs and rat

--- hadoop-hdds/tools/src/test/java/org/apache/hadoop/hdds/scm/cli/container/TestReconcileSubcommand.java
+++ hadoop-hdds/tools/src/test/java/org/apache/hadoop/hdds/scm/cli/container/TestReconcileSubcommand.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hdds.scm.cli.container;
 
 import org.apache.hadoop.hdds.client.RatisReplicationConfig;
@@ -26,6 +43,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.LifeCycleState.CLOSED;
@@ -163,7 +181,7 @@ public class TestReconcileSubcommand {
     List<String> args = Arrays.stream(containerIDs)
         .mapToObj(Long::toString)
         .collect(Collectors.toList());
-    // TODO status flag only works when it is first.
+    // TODO status flag only works when it is first in the argument list.
     args.add(0, "--status");
     cmdLine.parseArgs(args.toArray(new String[]{}));
     cmd.execute(scmClient);
@@ -255,12 +273,12 @@ public class TestReconcileSubcommand {
     // No extra commands should have been sent.
     verify(scmClient, times(containerIDs.length)).reconcileContainer(anyLong());
 
-    // TODO output content is empty
+    // TODO output content is empty for some reason
     String outputString = outContent.toString(DEFAULT_ENCODING);
     for (long id: containerIDs) {
       verify(scmClient, times(1)).reconcileContainer(id);
-//      Pattern p = Pattern.compile("Reconciliation has been triggered for container " + id);
-//      assertTrue(p.matcher(outputString).find());
+      Pattern p = Pattern.compile("Reconciliation has been triggered for container " + id);
+      assertTrue(p.matcher(outputString).find());
     }
   }
 
