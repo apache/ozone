14472d314607e451b1fcf8696d3c4aa0e4c737a0
Chia-Chuan Yu <yujuan476@gmail.com>
2025-02-23 14:39:42 +0800

    Fixed findingbugs

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestBucketOwner.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestBucketOwner.java
@@ -178,7 +178,7 @@ public abstract class TestBucketOwner implements NonHATests.TestCase {
       OzoneBucket ozoneBucket = volume.getBucket("bucket1");
       //Key Create
       byte[] bytes = new byte[10];
-      createKey(ozoneBucket, "key2", new String(bytes));
+      createKey(ozoneBucket, "key2", new String(bytes, StandardCharsets.UTF_8));
       //Key Delete
       ozoneBucket.deleteKey("key2");
       //List Keys
