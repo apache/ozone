dec8c61e3ff44ebf870715a5d6fac1237357e3d0
Rishabh Patel <1607531+ptlrs@users.noreply.github.com>
2025-03-10 20:15:07 -0700

    Update hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/TestContainerStateMachine.java
    Co-authored-by: Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/TestContainerStateMachine.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/common/transport/server/ratis/TestContainerStateMachine.java
@@ -61,7 +61,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
 /**
  * Test class to ContainerStateMachine class.
  */
