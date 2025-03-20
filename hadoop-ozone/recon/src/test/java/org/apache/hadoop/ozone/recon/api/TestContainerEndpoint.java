0edcad1cc9cca7d6ed2bb5ba5816c4ca98e96a40
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2025-03-19 19:48:01 -0700

    HDDS-12620. Fix checkstyle
    Change-Id: I1b163c81d4dedc1376ccf16467968190ef6b94e5

--- hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
+++ hadoop-ozone/recon/src/test/java/org/apache/hadoop/ozone/recon/api/TestContainerEndpoint.java
@@ -1585,8 +1585,7 @@ public class TestContainerEndpoint {
 
   @Test
   public void testGetOmContainersDeletedInSCMPagination() throws Exception {
-    Map<Long, ContainerMetadata> omContainers =
-      reconContainerMetadataManager.getContainers(-1, 0);
+    Map<Long, ContainerMetadata> omContainers = reconContainerMetadataManager.getContainers(-1, 0);
     putContainerInfos(2);
     List<ContainerInfo> scmContainers = reconContainerManager.getContainers();
     assertEquals(omContainers.size(), scmContainers.size());
@@ -1596,31 +1595,26 @@ public class TestContainerEndpoint {
 
     assertContainerCount(HddsProtos.LifeCycleState.DELETED, 1);
 
-    List<ContainerInfo> deletedSCMContainers =
-      reconContainerManager.getContainers(HddsProtos.LifeCycleState.DELETED);
+    List<ContainerInfo> deletedSCMContainers = reconContainerManager.getContainers(HddsProtos.LifeCycleState.DELETED);
     assertEquals(1, deletedSCMContainers.size());
 
     Response omContainersDeletedInSCMResponse =
-      containerEndpoint.getOmContainersDeletedInSCM(1, 0);
+        containerEndpoint.getOmContainersDeletedInSCM(1, 0);
     assertNotNull(omContainersDeletedInSCMResponse);
 
-    Map<String, Object> responseMap =
-      (Map<String, Object>) omContainersDeletedInSCMResponse.getEntity();
+    Map<String, Object> responseMap = (Map<String, Object>) omContainersDeletedInSCMResponse.getEntity();
 
     // Fetch the ContainerDiscrepancyInfo list from the response
     List<ContainerDiscrepancyInfo> containerDiscrepancyInfoList =
-      (List<ContainerDiscrepancyInfo>) responseMap.get(
-        "containerDiscrepancyInfo");
+        (List<ContainerDiscrepancyInfo>) responseMap.get("containerDiscrepancyInfo");
     assertEquals(1, containerDiscrepancyInfoList.size());
     // Check the prevKey is set correct in the response
     long responsePrevKey = (long) responseMap.get("lastKey");
-    assertEquals(containerDiscrepancyInfoList.get(
-        containerDiscrepancyInfoList.size() - 1).getContainerID(),
-      responsePrevKey);
+    assertEquals(containerDiscrepancyInfoList.get(containerDiscrepancyInfoList.size() - 1).getContainerID(),
+        responsePrevKey);
     assertEquals(2, responsePrevKey);
 
-    assertEquals(omContainers.get(2L).getNumberOfKeys(), containerDiscrepancyInfoList.get(0)
-      .getNumberOfKeys());
+    assertEquals(omContainers.get(2L).getNumberOfKeys(), containerDiscrepancyInfoList.get(0).getNumberOfKeys());
     assertEquals(1, containerDiscrepancyInfoList.size());
   }
 
