1372e04c61f183986fd769c62554ac683d900821
Doroszlai, Attila <adoroszlai@apache.org>
2025-03-26 17:23:09 +0100

    improve TestCheckNative

--- hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/checknative/TestCheckNative.java
+++ hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/checknative/TestCheckNative.java
@@ -17,19 +17,16 @@
 
 package org.apache.hadoop.ozone.checknative;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.hadoop.hdds.utils.NativeConstants.ROCKS_TOOLS_NATIVE_LIBRARY_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
+import org.apache.hadoop.hdds.utils.IOUtils;
 import org.apache.hadoop.ozone.debug.OzoneDebug;
 import org.apache.hadoop.ozone.shell.checknative.CheckNative;
+import org.apache.ozone.test.GenericTestUtils;
 import org.apache.ozone.test.tag.Native;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,53 +34,43 @@ import org.junit.jupiter.api.Test;
  */
 public class TestCheckNative {
 
-  private static PrintStream psBackup;
-  private static ByteArrayOutputStream outputStream;
+  private GenericTestUtils.PrintStreamCapturer out;
 
-  private static final String DEFAULT_ENCODING = UTF_8.name();
-
-  @BeforeAll
-  public static void init() throws UnsupportedEncodingException {
-    psBackup = System.out;
-    outputStream = new ByteArrayOutputStream();
-    PrintStream psOut = new PrintStream(outputStream, false, DEFAULT_ENCODING);
-    System.setOut(psOut);
+  @BeforeEach
+  void init() {
+    out = GenericTestUtils.captureOut();
   }
 
   @Test
-  public void testCheckNativeNotLoaded() throws UnsupportedEncodingException {
-    outputStream.reset();
-    new OzoneDebug().getCmd().execute("checknative");
-    // trims multiple spaces
-    String stdOut = outputStream.toString(DEFAULT_ENCODING)
-        .replaceAll(" +", " ");
-    assertThat(stdOut).contains("Native library checking:");
-    assertThat(stdOut).contains("hadoop: false");
-    assertThat(stdOut).contains("ISA-L: false");
-    assertThat(stdOut).contains("rocks-tools: false");
+  void testCheckNativeNotLoaded() {
+    executeCheckNative();
+    assertOutput(false);
   }
 
   @Native(ROCKS_TOOLS_NATIVE_LIBRARY_NAME)
   @Test
-  public void testCheckNativeRocksToolsLoaded() throws UnsupportedEncodingException {
-    outputStream.reset();
-    new OzoneDebug().getCmd().execute("checknative");
+  void testCheckNativeRocksToolsLoaded() {
+    executeCheckNative();
+    assertOutput(true);
+  }
+
+  private void assertOutput(boolean expectedRocksNative) {
     // trims multiple spaces
-    String stdOut = outputStream.toString(DEFAULT_ENCODING)
-        .replaceAll(" +", " ");
-    assertThat(stdOut).contains("Native library checking:");
-    assertThat(stdOut).contains("hadoop: false");
-    assertThat(stdOut).contains("ISA-L: false");
-    assertThat(stdOut).contains("rocks-tools: true");
+    String stdOut = out.get()
+        .replaceAll("  +", " ");
+    assertThat(stdOut)
+        .contains("Native library checking:")
+        .contains("hadoop: false")
+        .contains("ISA-L: false")
+        .contains("rocks-tools: " + expectedRocksNative);
   }
 
   @AfterEach
-  public void setUp() {
-    outputStream.reset();
+  void setUp() {
+    IOUtils.closeQuietly(out);
   }
 
-  @AfterAll
-  public static void tearDown() {
-    System.setOut(psBackup);
+  private static void executeCheckNative() {
+    new OzoneDebug().getCmd().execute("checknative");
   }
 }
