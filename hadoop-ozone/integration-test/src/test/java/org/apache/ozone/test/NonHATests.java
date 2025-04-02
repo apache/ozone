66da86bcc6f723febfd32e96cead5573062c06d9
Doroszlai, Attila <adoroszlai@apache.org>
2025-04-02 16:24:49 +0200

    HDDS-12756. Speed up TestReconfigShell

--- hadoop-ozone/integration-test/src/test/java/org/apache/ozone/test/NonHATests.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/ozone/test/NonHATests.java
@@ -55,6 +55,7 @@ import org.apache.hadoop.ozone.om.TestOzoneManagerRestInterface;
 import org.apache.hadoop.ozone.reconfig.TestDatanodeReconfiguration;
 import org.apache.hadoop.ozone.reconfig.TestOmReconfiguration;
 import org.apache.hadoop.ozone.reconfig.TestScmReconfiguration;
+import org.apache.hadoop.ozone.shell.TestReconfigShell;
 import org.apache.hadoop.ozone.shell.TestReplicationConfigPreference;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
@@ -364,6 +365,14 @@ public abstract class NonHATests extends ClusterForTests<MiniOzoneCluster> {
 
   @Nested
   class OmReconfiguration extends TestOmReconfiguration {
+    @Override
+    public MiniOzoneCluster cluster() {
+      return getCluster();
+    }
+  }
+
+  @Nested
+  class ReconfigShell extends TestReconfigShell {
     @Override
     public MiniOzoneCluster cluster() {
       return getCluster();
