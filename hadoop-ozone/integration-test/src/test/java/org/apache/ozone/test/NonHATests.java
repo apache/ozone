030c4f3cdec94975321e223197b9de21ba042f58
Doroszlai, Attila <adoroszlai@apache.org>
2025-04-02 18:59:05 +0200

    convert TestOzoneDebugShell

--- hadoop-ozone/integration-test/src/test/java/org/apache/ozone/test/NonHATests.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/ozone/test/NonHATests.java
@@ -55,6 +55,7 @@ import org.apache.hadoop.ozone.om.TestOzoneManagerRestInterface;
 import org.apache.hadoop.ozone.reconfig.TestDatanodeReconfiguration;
 import org.apache.hadoop.ozone.reconfig.TestOmReconfiguration;
 import org.apache.hadoop.ozone.reconfig.TestScmReconfiguration;
+import org.apache.hadoop.ozone.shell.TestOzoneDebugShell;
 import org.apache.hadoop.ozone.shell.TestReconfigShell;
 import org.apache.hadoop.ozone.shell.TestReplicationConfigPreference;
 import org.junit.jupiter.api.Nested;
@@ -365,6 +366,14 @@ public abstract class NonHATests extends ClusterForTests<MiniOzoneCluster> {
 
   @Nested
   class OmReconfiguration extends TestOmReconfiguration {
+    @Override
+    public MiniOzoneCluster cluster() {
+      return getCluster();
+    }
+  }
+
+  @Nested
+  class OzoneDebugShell extends TestOzoneDebugShell {
     @Override
     public MiniOzoneCluster cluster() {
       return getCluster();
