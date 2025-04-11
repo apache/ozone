e0c6f2e6185b67c5fdf3dcf584964fdba6fd128d
Sreeja Chintalapati <schintalapati@Sreejas-MacBook-Pro.local>
2025-04-11 08:01:53 +0530

    HDDS-12697. CLI command to display details of a single container
    # Conflicts:
    #	hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/containerlog/parser/ContainerDatanodeDatabase.java
    #	hadoop-ozone/tools/src/main/resources/container-log-db-queries.properties

---
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/debug/logs/container/ContainerInfoCommand.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.ozone.debug.logs.container;
+
+import java.util.concurrent.Callable;
+import org.apache.hadoop.ozone.containerlog.parser.ContainerDatanodeDatabase;
+import picocli.CommandLine;
+
+/**
+ * Command to display detailed information of a single container by ID.
+ */
+
+@CommandLine.Command(
+    name = "info",
+    description = "provides details of a single container"
+)
+public class ContainerInfoCommand implements Callable<Void> {
+
+  @CommandLine.Parameters(index = "0", description = "Container ID")
+  private Long containerId;
+
+  @CommandLine.ParentCommand
+  private ContainerLogController parent;
+
+  @Override
+  public Void call() throws Exception {
+
+    if (containerId < 0) {
+      System.err.println("Invalid container ID: " + containerId);
+      return null;
+    }
+
+    ContainerDatanodeDatabase cdd = new ContainerDatanodeDatabase();
+    try {
+      cdd.showContainerDetails(containerId);
+    } catch (Exception e) {
+      System.err.println("Error while retrieving container with id: " + containerId + " " + e.getMessage());
+    }
+
+    return null;
+  }
+
+}
