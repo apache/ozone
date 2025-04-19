9d80995da084f83086a503670801b1c3d6ff97dc
Chia-Chuan Yu <yujuan476@gmail.com>
2025-04-19 12:50:06 +0800

    Added missing getID()

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/HddsDatanodeService.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/HddsDatanodeService.java
@@ -234,7 +234,7 @@ public class HddsDatanodeService extends GenericCli implements Callable<Void>, S
       LOG.info("HddsDatanodeService {}", datanodeDetails);
       // Authenticate Hdds Datanode service if security is enabled
       if (OzoneSecurityUtil.isSecurityEnabled(conf)) {
-        component = "dn-" + datanodeDetails;
+        component = "dn-" + datanodeDetails.getID();
         secConf = new SecurityConfig(conf);
 
         if (SecurityUtil.getAuthenticationMethod(conf).equals(
