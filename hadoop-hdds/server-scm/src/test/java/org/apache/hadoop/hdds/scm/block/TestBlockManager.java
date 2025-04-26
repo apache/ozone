0ffb4302dbcd17da11f6da6d280c2579037cb141
Chia-Chuan Yu <yujuan476@gmail.com>
2025-04-26 17:35:56 +0800

    HDDS-12912. Remove the usage of deprecated closePipeline(Pipeline pipeline, boolean onTimeout) function.

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/block/TestBlockManager.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/block/TestBlockManager.java
@@ -575,7 +575,7 @@ public class TestBlockManager {
   public void testBlockAllocationWithNoAvailablePipelines()
       throws IOException {
     for (Pipeline pipeline : pipelineManager.getPipelines()) {
-      pipelineManager.closePipeline(pipeline, false);
+      pipelineManager.closePipeline(pipeline.getId());
     }
     assertEquals(0, pipelineManager.getPipelines(replicationConfig).size());
     assertNotNull(blockManager
