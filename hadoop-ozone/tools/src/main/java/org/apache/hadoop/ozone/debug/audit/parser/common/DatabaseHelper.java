a73e0529ba3067b083c1fead435a824ebfdfb2ad
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2025-04-28 12:08:38 +0500

    HDDS-12907. Enable FieldDeclarationsShouldBeAtStartOfClass PMD rule (#8344)

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/debug/audit/parser/common/DatabaseHelper.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/debug/audit/parser/common/DatabaseHelper.java
@@ -45,9 +45,6 @@ import org.slf4j.LoggerFactory;
  * Database helper for ozone audit parser tool.
  */
 public final class DatabaseHelper {
-  private DatabaseHelper() {
-    //Never constructed
-  }
 
   static {
     loadProperties();
@@ -56,6 +53,10 @@ public final class DatabaseHelper {
   private static final Logger LOG =
       LoggerFactory.getLogger(DatabaseHelper.class);
   private static Map<String, String> properties;
+
+  private DatabaseHelper() {
+    //Never constructed
+  }
 
   public static boolean setup(String dbName, String logs) throws Exception {
     if (createAuditTable(dbName)) {
