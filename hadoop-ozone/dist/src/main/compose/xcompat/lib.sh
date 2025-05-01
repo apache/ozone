aac8fa7241d523329397b3562b18f7bcc8f0f7e5
Chia-Chuan Yu <yujuan476@gmail.com>
2025-05-01 14:48:35 +0800

    HDDS-12728. Add Ozone 2.0.0 to compatibility acceptance tests (#8361)
    (cherry picked from commit 560fcdf90c04501b621b79339764a3585a4293e3)

--- hadoop-ozone/dist/src/main/compose/xcompat/lib.sh
+++ hadoop-ozone/dist/src/main/compose/xcompat/lib.sh
@@ -21,7 +21,7 @@ basename=$(basename ${COMPOSE_DIR})
 
 current_version="${OZONE_CURRENT_VERSION}"
 # TODO: debug acceptance test failures for client versions 1.0.0 on secure clusters
-old_versions="1.1.0 1.2.1 1.3.0 1.4.0 1.4.1" # container is needed for each version in clients.yaml
+old_versions="1.1.0 1.2.1 1.3.0 1.4.1 2.0.0" # container is needed for each version in clients.yaml
 
 # shellcheck source=hadoop-ozone/dist/src/main/compose/testlib.sh
 source "${COMPOSE_DIR}/../testlib.sh"
