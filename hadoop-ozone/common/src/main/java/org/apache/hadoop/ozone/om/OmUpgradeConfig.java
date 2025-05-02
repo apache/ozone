9fe1dba2aa3b4fdc487b5437c8e282bab3315a60
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2025-05-02 19:01:30 +0200

    HDDS-12942. Init layout version config should not be public (#8373)

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/OmUpgradeConfig.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/OmUpgradeConfig.java
@@ -42,37 +42,11 @@ public class OmUpgradeConfig {
   private long ratisBasedFinalizationTimeout =
       Duration.ofSeconds(30).getSeconds();
 
-  @Config(key = "init.default.layout.version",
-      defaultValue = "-1",
-      type = ConfigType.INT,
-      tags = {ConfigTag.OM, ConfigTag.UPGRADE},
-      description = "Default Layout Version to init the OM with. Intended to "
-          + "be used in tests to finalize from an older version of OM to the "
-          + "latest. By default, OM init uses the highest layout version."
-  )
-  private int defaultLayoutVersionOnInit = -1;
-
-  /**
-   * Config key class.
-   */
-  public static class ConfigStrings {
-    public static final String OZONE_OM_INIT_DEFAULT_LAYOUT_VERSION =
-        "ozone.om.init.default.layout.version";
-  }
-
   public long getRatisBasedFinalizationTimeout() {
     return ratisBasedFinalizationTimeout;
   }
 
   public void setRatisBasedFinalizationTimeout(long timeout) {
     this.ratisBasedFinalizationTimeout = timeout;
-  }
-
-  public int getDefaultLayoutVersionOnInit() {
-    return defaultLayoutVersionOnInit;
-  }
-
-  public void setDefaultLayoutVersionOnInit(int defaultLayoutVersionOnInit) {
-    this.defaultLayoutVersionOnInit = defaultLayoutVersionOnInit;
   }
 }
