f615275674740c98549c3c37b9fc7b29cfc2729b
Ethan Rose <erose@apache.org>
2025-05-12 23:32:08 -0400

    Add comment on failure type

--- hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/keyvalue/TestContainerCorruptions.java
+++ hadoop-hdds/container-service/src/test/java/org/apache/hadoop/ozone/container/keyvalue/TestContainerCorruptions.java
@@ -111,7 +111,10 @@ public enum TestContainerCorruptions {
   TRUNCATED_BLOCK((container, blockID) -> {
     File blockFile = getBlock(container, blockID);
     truncateFile(blockFile);
-  }, ContainerScanError.FailureType.MISSING_CHUNK);
+  },
+      // This test completely removes all content from the block file. The scanner will see this as all the chunks in
+      // the block missing, hence MISSING_CHUNK instead of INCONSISTENT_CHUNK_LENGTH.
+      ContainerScanError.FailureType.MISSING_CHUNK);
 
   private final BiConsumer<Container<?>, Long> corruption;
   private final ContainerScanError.FailureType expectedResult;
