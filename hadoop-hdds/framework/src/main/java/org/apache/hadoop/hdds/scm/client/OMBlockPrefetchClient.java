a75bce2331c058213e2cd06e548b2987862a5b2a
Penumudy Tanvi <tanvi.penumudy@Penumudys-MacBook-Pro.local>
2025-05-21 09:58:43 +0530

    Resolve pmd errors

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/scm/client/OMBlockPrefetchClient.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/scm/client/OMBlockPrefetchClient.java
@@ -68,15 +68,6 @@ import org.slf4j.LoggerFactory;
  */
 public class OMBlockPrefetchClient {
   private static final Logger LOG = LoggerFactory.getLogger(OMBlockPrefetchClient.class);
-  private final ScmBlockLocationProtocol scmBlockLocationProtocol;
-  private int maxBlocks;
-  private int minBlocks;
-  private boolean useHostname;
-  private DNSToSwitchMapping dnsToSwitchMapping;
-  private final Map<ReplicationConfig, ConcurrentLinkedDeque<ExpiringAllocatedBlock>> blockQueueMap =
-      new ConcurrentHashMap<>();
-  private long expiryDuration;
-  private OMBlockPrefetchMetrics metrics;
   private static final ReplicationConfig RATIS_THREE =
       ReplicationConfig.fromProtoTypeAndFactor(HddsProtos.ReplicationType.RATIS,
           HddsProtos.ReplicationFactor.THREE);
@@ -92,7 +83,15 @@ public class OMBlockPrefetchClient {
   private static final ReplicationConfig XOR_10_4_4096 =
       ReplicationConfig.fromProto(HddsProtos.ReplicationType.EC, null,
           toProto(10, 4, ECReplicationConfig.EcCodec.XOR, 4096));
-
+  private final ScmBlockLocationProtocol scmBlockLocationProtocol;
+  private int maxBlocks;
+  private int minBlocks;
+  private boolean useHostname;
+  private DNSToSwitchMapping dnsToSwitchMapping;
+  private final Map<ReplicationConfig, ConcurrentLinkedDeque<ExpiringAllocatedBlock>> blockQueueMap =
+      new ConcurrentHashMap<>();
+  private long expiryDuration;
+  private OMBlockPrefetchMetrics metrics;
   private ExecutorService prefetchExecutor;
   private final AtomicBoolean isPrefetching = new AtomicBoolean(false);
 
