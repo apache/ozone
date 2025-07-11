96081d0ef5c7f5869feeef1b89e3da920b001da8
Priyesh Karatha <priyeshkaratha@gmail.com>
2025-07-11 13:56:02 +0530

    adding EC related tests

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/service/TestDeletionService.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/service/TestDeletionService.java
@@ -18,14 +18,14 @@
 package org.apache.hadoop.ozone.om.service;
 
 import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_BLOCK_DELETING_SERVICE_INTERVAL;
-import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_REPLICATION;
-import static org.apache.hadoop.ozone.OzoneConfigKeys.OZONE_REPLICATION_TYPE;
+import static org.apache.hadoop.ozone.OzoneConsts.MB;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
@@ -34,8 +34,8 @@ import java.util.stream.Stream;
 import org.apache.hadoop.hdds.client.DeletedBlock;
 import org.apache.hadoop.hdds.client.ECReplicationConfig;
 import org.apache.hadoop.hdds.client.RatisReplicationConfig;
+import org.apache.hadoop.hdds.client.ReplicationConfig;
 import org.apache.hadoop.hdds.client.ReplicationFactor;
-import org.apache.hadoop.hdds.client.ReplicationType;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.scm.block.BlockManager;
 import org.apache.hadoop.hdds.scm.server.StorageContainerManager;
@@ -59,23 +59,27 @@ public class TestDeletionService {
   private static final String BUCKET_NAME = "bucket1";
   private static final String KEY_NAME = "testkey";
   private static final int KEY_SIZE = 5 * 1024; // 5 KB
-  private static final int ONEMB = 1024 * 1024;
 
   public static Stream<Arguments> replicaType() {
     return Stream.of(
         arguments("RATIS", "ONE"),
-        arguments("RATIS", "THREE"),
-        arguments("EC", "ONE")
+        arguments("RATIS", "THREE")
+    );
+  }
+
+  public static Stream<Arguments> ecType() {
+    return Stream.of(
+        arguments(ECReplicationConfig.EcCodec.RS, 3, 2, MB),
+        arguments(ECReplicationConfig.EcCodec.RS, 3, 2, 2 * MB),
+        arguments(ECReplicationConfig.EcCodec.RS, 6, 3, MB),
+        arguments(ECReplicationConfig.EcCodec.RS, 6, 3, 2 * MB)
     );
   }
 
   @ParameterizedTest
   @MethodSource("replicaType")
   public void testDeletedKeyBytesPropagatedToSCM(String type, String factor) throws Exception {
-    OzoneConfiguration conf = new OzoneConfiguration();
-    conf.setTimeDuration(OZONE_BLOCK_DELETING_SERVICE_INTERVAL, 100, TimeUnit.MILLISECONDS);
-    conf.set(OZONE_REPLICATION_TYPE, type);
-    conf.set(OZONE_REPLICATION, factor);
+    OzoneConfiguration conf = createBasicConfig();
     MiniOzoneCluster cluster = null;
 
     try {
@@ -83,52 +87,100 @@ public class TestDeletionService {
       cluster = MiniOzoneCluster.newBuilder(conf).setNumDatanodes(3).build();
       cluster.waitForClusterToBeReady();
 
+      ReplicationConfig replicationConfig = RatisReplicationConfig
+          .getInstance(ReplicationFactor.valueOf(factor).toProto());
+
       // Step 2: Create volume, bucket, and write a key
-      OzoneClient client = cluster.newClient();
-      client.getObjectStore().createVolume(VOLUME_NAME);
-      client.getObjectStore().getVolume(VOLUME_NAME).createBucket(BUCKET_NAME);
-      OzoneBucket bucket = client.getObjectStore().getVolume(VOLUME_NAME).getBucket(BUCKET_NAME);
-      byte[] data = new byte[KEY_SIZE];
-      try (OzoneOutputStream out = bucket.createKey(KEY_NAME, KEY_SIZE,
-          ReplicationType.valueOf(type), ReplicationFactor.valueOf(factor), new HashMap<>())) {
-        out.write(data);
-      }
-
+      OzoneBucket bucket = setupClientAndWriteKey(cluster, type, factor, replicationConfig);
       // Step 3: Spy on BlockManager and inject it into SCM
-      StorageContainerManager scm = cluster.getStorageContainerManager();
-      BlockManager realManager = scm.getScmBlockManager();
-      BlockManager spyManager = spy(realManager);
-
-      Field field = scm.getClass().getDeclaredField("scmBlockManager");
-      field.setAccessible(true);
-      field.set(scm, spyManager);
-
+      BlockManager spyManager = injectSpyBlockManager(cluster);
       // Step 4: Delete the key (which triggers deleteBlocks call)
       bucket.deleteKey(KEY_NAME);
-
       // Step 5: Verify deleteBlocks call and capture argument
-      ArgumentCaptor<List<BlockGroup>> captor =
-          ArgumentCaptor.forClass(List.class);
-      verify(spyManager, timeout(50000).atLeastOnce()).deleteBlocks(captor.capture());
-      ECReplicationConfig repConfig = new ECReplicationConfig(3, 2,
-          ECReplicationConfig.EcCodec.RS, ONEMB);
-      // Step 6: Calculate and assert used bytes
-      long totalUsedBytes = captor.getAllValues().get(0).stream()
-          .flatMap(group -> group.getAllBlocks().stream())
-          .mapToLong(DeletedBlock::getReplicatedSize)
-          .sum();
-      long totalUnreplicatedBytes = captor.getAllValues().get(0).stream()
-          .flatMap(group -> group.getAllBlocks().stream())
-          .mapToLong(DeletedBlock::getSize)
-          .sum();
-      assertEquals(QuotaUtil.getReplicatedSize(KEY_SIZE, type.equals("RC") ?
-          repConfig : RatisReplicationConfig.getInstance(
-          ReplicationFactor.valueOf(factor).toProto())), totalUsedBytes);
-      assertEquals(KEY_SIZE, totalUnreplicatedBytes);
+      verifyAndAssertQuota(spyManager, replicationConfig);
     } finally {
       if (cluster != null) {
         cluster.shutdown();
       }
     }
+  }
+
+  @ParameterizedTest
+  @MethodSource("ecType")
+  void testGetDefaultShouldCreateECReplicationConfFromConfValues(
+      ECReplicationConfig.EcCodec codec, int data, int parity, long chunkSize) throws IOException {
+
+    OzoneConfiguration conf = createBasicConfig();
+    MiniOzoneCluster cluster = null;
+
+    try {
+      ReplicationConfig replicationConfig = new ECReplicationConfig(data, parity, codec, (int) chunkSize);
+      // Step 1: Start MiniOzoneCluster
+      cluster = MiniOzoneCluster.newBuilder(conf).setNumDatanodes(data + parity).build();
+      cluster.waitForClusterToBeReady();
+      // Step 2: Create volume, bucket, and write a key
+      OzoneBucket bucket = setupClientAndWriteKey(cluster, "EC", null, replicationConfig);
+      // Step 3: Spy on BlockManager and inject it into SCM
+      BlockManager spyManager = injectSpyBlockManager(cluster);
+      // Step 4: Delete the key (which triggers deleteBlocks call)
+      bucket.deleteKey(KEY_NAME);
+      // Step 5: Verify deleteBlocks call and capture argument
+      verifyAndAssertQuota(spyManager, replicationConfig);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    } finally {
+      if (cluster != null) {
+        cluster.shutdown();
+      }
+    }
+  }
+
+  private OzoneConfiguration createBasicConfig() {
+    OzoneConfiguration conf = new OzoneConfiguration();
+    conf.setTimeDuration(OZONE_BLOCK_DELETING_SERVICE_INTERVAL, 100, TimeUnit.MILLISECONDS);
+    return conf;
+  }
+
+  private OzoneBucket setupClientAndWriteKey(MiniOzoneCluster cluster, String type,
+                                             String factor, ReplicationConfig replicationConfig)
+      throws IOException {
+    OzoneClient client = cluster.newClient();
+    client.getObjectStore().createVolume(VOLUME_NAME);
+    client.getObjectStore().getVolume(VOLUME_NAME).createBucket(BUCKET_NAME);
+    OzoneBucket bucket = client.getObjectStore().getVolume(VOLUME_NAME).getBucket(BUCKET_NAME);
+
+    byte[] data = new byte[KEY_SIZE];
+    try (OzoneOutputStream out = bucket.createKey(KEY_NAME, KEY_SIZE,
+        replicationConfig, new HashMap<>())) {
+      out.write(data);
+    }
+    return bucket;
+  }
+
+  private BlockManager injectSpyBlockManager(MiniOzoneCluster cluster) throws Exception {
+    StorageContainerManager scm = cluster.getStorageContainerManager();
+    BlockManager realManager = scm.getScmBlockManager();
+    BlockManager spyManager = spy(realManager);
+
+    Field field = scm.getClass().getDeclaredField("scmBlockManager");
+    field.setAccessible(true);
+    field.set(scm, spyManager);
+    return spyManager;
+  }
+
+  private void verifyAndAssertQuota(BlockManager spyManager, ReplicationConfig replicationConfig) throws IOException {
+    ArgumentCaptor<List<BlockGroup>> captor = ArgumentCaptor.forClass(List.class);
+    verify(spyManager, timeout(50000).atLeastOnce()).deleteBlocks(captor.capture());
+
+    long totalUsedBytes = captor.getAllValues().get(0).stream()
+        .flatMap(group -> group.getAllBlocks().stream())
+        .mapToLong(DeletedBlock::getReplicatedSize).sum();
+
+    long totalUnreplicatedBytes = captor.getAllValues().get(0).stream()
+        .flatMap(group -> group.getAllBlocks().stream())
+        .mapToLong(DeletedBlock::getSize).sum();
+
+    assertEquals(QuotaUtil.getReplicatedSize(KEY_SIZE, replicationConfig), totalUsedBytes);
+    assertEquals(KEY_SIZE, totalUnreplicatedBytes);
   }
 }
