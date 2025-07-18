763a2892aed2bd496e7a10e4a1665d7223f079f5
tejaskriya <tejaskriya@gmail.com>
2025-07-18 11:48:39 +0530

    add size comparisons

--- hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/repair/ldb/TestLdbRepair.java
+++ hadoop-ozone/tools/src/test/java/org/apache/hadoop/ozone/repair/ldb/TestLdbRepair.java
@@ -19,12 +19,16 @@ package org.apache.hadoop.ozone.repair.ldb;
 
 import static org.apache.ozone.test.IntLambda.withTextFromSystemIn;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
 import org.apache.hadoop.hdds.utils.db.CodecBuffer;
@@ -77,8 +81,12 @@ public class TestLdbRepair {
 
   /**
    * Test manual compaction of RocksDB.
-   * This test creates a large number of keys, deletes them, and then triggers compaction.
-   * The sizes after each step is compared to ensure that compaction reduces the size of the database.
+   * This test creates a large number of keys, deletes them (creating tombstones),
+   * and then runs manual compaction via the CLI tool.
+   * It verifies the following:
+   *    Compaction command executes successfully.
+   *    Tombstones are removed (i.e., numDeletions in SST files is 0).
+   *    Size of the db reduces after compaction.
    */
   @Test
   public void testRocksDBManualCompaction() throws Exception {
@@ -91,6 +99,7 @@ public class TestLdbRepair {
       testTable.put(key.getBytes(StandardCharsets.UTF_8), value.getBytes(StandardCharsets.UTF_8));
     }
     rdbStore.flushDB();
+    long sizeAfterKeysCreate = calculateSstFileSize(dbPath);
 
     // Delete all keys
     for (int i = 0; i < NUM_KEYS; i++) {
@@ -98,6 +107,7 @@ public class TestLdbRepair {
       testTable.delete(key.getBytes(StandardCharsets.UTF_8));
     }
     rdbStore.flushDB();
+    long sizeAfterKeysDelete = calculateSstFileSize(dbPath);
     rdbStore.close();
 
     // Trigger compaction of the table
@@ -110,7 +120,19 @@ public class TestLdbRepair {
     int exitCode = withTextFromSystemIn("y")
         .execute(() -> cmd.execute(args));
     assertEquals(0, exitCode, "Compaction command should execute successfully");
+    long sizeAfterCompaction = calculateSstFileSize(dbPath);
 
+    System.out.printf("Size after creating keys: %d, after deleting keys: %d, after compaction: %d %n",
+        sizeAfterKeysCreate, sizeAfterKeysDelete, sizeAfterCompaction);
+
+    // Deletes should increase the size due to tombstones
+    assertTrue(sizeAfterKeysCreate < sizeAfterKeysDelete,
+        String.format("sizeAfterKeysCreate (%d) is not lesser than sizeAfterKeysDelete (%d), ",
+            sizeAfterKeysCreate, sizeAfterKeysDelete));
+    // Compaction should reduce the size below original
+    assertTrue(sizeAfterCompaction < sizeAfterKeysCreate,
+        String.format("sizeAfterCompaction (%d) is not lesser than sizeAfterKeysCreate (%d), ",
+            sizeAfterCompaction, sizeAfterKeysCreate));
     // check all tombstones were removed
     List<ColumnFamilyHandle> cfHandleList = new ArrayList<>();
     List<ColumnFamilyDescriptor> cfDescList = RocksDBUtils.getColumnFamilyDescriptors(dbPath.toString());
@@ -118,9 +140,29 @@ public class TestLdbRepair {
       List<LiveFileMetaData> liveFileMetaDataList = RdbUtil
           .getLiveSSTFilesForCFs(db, Collections.singletonList(TEST_CF_NAME));
       for (LiveFileMetaData liveMetadata : liveFileMetaDataList) {
-        assertEquals(0,liveMetadata.numDeletions(),
-            "Compaction did not remove all tombstones, unlike what was expected");
+        assertEquals(0, liveMetadata.numDeletions(),
+            "Tombstones found in file: " + liveMetadata.fileName());
       }
+    }
+  }
+
+  private long calculateSstFileSize(Path db) throws IOException {
+    if (!Files.exists(db)) {
+      return 0;
+    }
+
+    try (Stream<Path> paths = Files.walk(db)) {
+      return paths
+          .filter(Files::isRegularFile)
+          .filter(path -> path.toString().endsWith(".sst"))
+          .mapToLong(path -> {
+            try {
+              return Files.size(path);
+            } catch (IOException e) {
+              throw new IllegalStateException(e);
+            }
+          })
+          .sum();
     }
   }
 }
