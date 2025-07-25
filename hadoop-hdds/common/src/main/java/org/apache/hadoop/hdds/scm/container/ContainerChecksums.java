242f2e410943ea9503c8c90fe98fc3e56d594ed9
echonesis <echonesis@gmail.com>
2025-07-25 11:30:50 +0800

    placeholder update

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/container/ContainerChecksums.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/container/ContainerChecksums.java
@@ -28,10 +28,15 @@ import net.jcip.annotations.Immutable;
 public final class ContainerChecksums {
 
   private static final ContainerChecksums UNKNOWN =
-      new ContainerChecksums(0, 0L);
+      new ContainerChecksums(-1L, -1L);
 
-  private final long dataChecksum;
-  private final long metadataChecksum;
+  // Checksum of the data within the wrapper.
+  private long dataChecksum;
+  private static final long UNSET_DATA_CHECKSUM = -1;
+
+  // Checksum of the metadata within the wrapper.
+  private long metadataChecksum;
+  private static final long UNSET_METADATA_CHECKSUM = -1;
 
   private ContainerChecksums(long dataChecksum, long metadataChecksum) {
     this.dataChecksum = dataChecksum;
@@ -45,13 +50,42 @@ public final class ContainerChecksums {
   public static ContainerChecksums of(long dataChecksum, long metadataChecksum) {
     return new ContainerChecksums(dataChecksum, metadataChecksum);
   }
+  public void setDataChecksum(long dataChecksum) {
+    if (dataChecksum < 0) {
+      throw new IllegalArgumentException("Data checksum cannot be set to a negative number.");
+    }
+    this.dataChecksum = dataChecksum;
+  }
 
   public long getDataChecksum() {
+    // UNSET_DATA_CHECKSUM is an internal placeholder, it should not be used outside this class.
+    if (needsDataChecksum()) {
+      return 0;
+    }
     return dataChecksum;
   }
 
+  public boolean needsDataChecksum() {
+    return dataChecksum == UNSET_DATA_CHECKSUM;
+  }
+
+  public void setMetadataChecksum(long metadataChecksum) {
+    if (metadataChecksum < 0) {
+      throw new IllegalArgumentException("Metadata checksum cannot be set to a negative number.");
+    }
+    this.metadataChecksum = metadataChecksum;
+  }
+
   public long getMetadataChecksum() {
+    // UNSET_DATA_CHECKSUM is an internal placeholder, it should not be used outside this class.
+    if (needsMetadataChecksum()) {
+      return 0;
+    }
     return metadataChecksum;
+  }
+
+  public boolean needsMetadataChecksum() {
+    return metadataChecksum == UNSET_METADATA_CHECKSUM;
   }
 
   @Override
@@ -63,8 +97,8 @@ public final class ContainerChecksums {
       return false;
     }
     ContainerChecksums that = (ContainerChecksums) obj;
-    return dataChecksum == that.dataChecksum &&
-        metadataChecksum == that.metadataChecksum;
+    return getDataChecksum() == that.getDataChecksum() &&
+        getMetadataChecksum() == that.getMetadataChecksum();
   }
 
   @Override
@@ -74,7 +108,7 @@ public final class ContainerChecksums {
 
   @Override
   public String toString() {
-    return "data=" + Long.toHexString(dataChecksum) +
-        ", metadata=" + Long.toHexString(metadataChecksum);
+    return "data=" + Long.toHexString(getDataChecksum()) +
+        ", metadata=" + Long.toHexString(getMetadataChecksum());
   }
 }
