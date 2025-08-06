6b5ee3c832011917358c42e8ae14b6cf619fc859
Doroszlai, Attila <adoroszlai@apache.org>
2025-08-06 09:49:26 +0200

    extract SONAR_MAVEN_PLUGIN_VERSION

--- hadoop-ozone/dev-support/checks/sonar.sh
+++ hadoop-ozone/dev-support/checks/sonar.sh
@@ -23,7 +23,8 @@ if [ ! "$SONAR_TOKEN" ]; then
   exit 1
 fi
 
+: "${SONAR_MAVEN_PLUGIN_VERSION:=5.1.0.4751}"
 
 mvn -V -B -DskipShade -DskipTests -DskipRecon --no-transfer-progress \
   -Dsonar.coverage.jacoco.xmlReportPaths="$(pwd)/target/coverage/all.xml" \
-  verify org.sonarsource.scanner.maven:sonar-maven-plugin:5.1.0.4751:sonar
+  verify "org.sonarsource.scanner.maven:sonar-maven-plugin:${SONAR_MAVEN_PLUGIN_VERSION}:sonar"
