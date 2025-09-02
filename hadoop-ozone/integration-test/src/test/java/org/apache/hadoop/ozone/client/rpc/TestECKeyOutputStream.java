b10dfabe2f03a001e20c6653273c09a9bb485d58
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2025-09-01 21:45:48 -0400

    HDDS-11821. Debug
    Change-Id: I0a759a936b4cce14f6d7839ff937e0fbfb7ef8e3

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestECKeyOutputStream.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestECKeyOutputStream.java
@@ -243,15 +243,10 @@ public class TestECKeyOutputStream {
               .findFirst().get();
       Mockito.when(handlers.get(dnWithReplicaIndex1.getUuidString()).getDatanodeId())
           .thenAnswer(i -> {
-            if (!failed.get()) {
-              // Change dnId for one write chunk request.
-              System.out.println("Swaminathan1 Getting Datanode 1 total data written \t");
-              new RuntimeException().printStackTrace();
-              failed.set(true);
-              return dnWithReplicaIndex1.getUuidString() + "_failed";
-            } else {
-              return dnWithReplicaIndex1.getUuidString();
-            }
+            System.out.println("Swaminathan1 Getting Datanode 1 total data written \t");
+            new RuntimeException().printStackTrace();
+            failed.set(true);
+            return dnWithReplicaIndex1.getUuidString() + "_failed";
           });
       locationInfoList = groupOutputStream.getLocationInfoList();
       AtomicInteger count = new AtomicInteger(0);
