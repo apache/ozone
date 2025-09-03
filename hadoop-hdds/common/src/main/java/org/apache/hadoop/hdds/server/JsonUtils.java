0839c2468e7a4a77720fbb8ae4209ed1599c8d64
Ethan Rose <33912936+errose28@users.noreply.github.com>
2025-09-02 23:58:31 -0400

    HDDS-12078. Improve container reconciliation CLIs (#7944)

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/server/JsonUtils.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/server/JsonUtils.java
@@ -18,21 +18,26 @@
 package org.apache.hadoop.hdds.server;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.util.List;
+import org.apache.hadoop.hdds.HddsUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -76,6 +81,23 @@ public final class JsonUtils {
     return MAPPER.writeValueAsString(obj);
   }
 
+  /**
+   * Returns a {@link SequenceWriter} that will write to and close the provided output stream when it is closed.
+   * If the sequence is being written to stdout and more stdout output is needed later, use
+   * {@link #getStdoutSequenceWriter} instead.
+   */
+  public static SequenceWriter getSequenceWriter(OutputStream stream) throws IOException {
+    return WRITER.writeValuesAsArray(stream);
+  }
+
+  /**
+   * Returns a {@link SequenceWriter} that will write to stdout but not close stdout for more output once the sequence
+   * writer is closed.
+   */
+  public static SequenceWriter getStdoutSequenceWriter() throws IOException {
+    return getSequenceWriter(new NonClosingOutputStream(System.out));
+  }
+
   public static String toJsonStringWIthIndent(Object obj)  {
     try {
       return INDENT_OUTPUT_MAPPER.writeValueAsString(obj);
@@ -107,6 +129,10 @@ public final class JsonUtils {
     return MAPPER.readValue(reader, valueType);
   }
 
+  public static ObjectMapper getDefaultMapper() {
+    return MAPPER;
+  }
+
   /**
    * Utility to sequentially write a large collection of items to a file.
    */
@@ -132,4 +158,47 @@ public final class JsonUtils {
     }
   }
 
+  /**
+   * Serializes a checksum stored as a long into its json string representation.
+   */
+  public static class ChecksumSerializer extends JsonSerializer<Long> {
+    @Override
+    public void serialize(Long value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+      gen.writeString(HddsUtils.checksumToString(value));
+    }
+  }
+
+  private static class NonClosingOutputStream extends OutputStream {
+
+    private final OutputStream delegate;
+
+    NonClosingOutputStream(OutputStream delegate) {
+      this.delegate = delegate;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      delegate.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+      delegate.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+      delegate.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+      delegate.flush();
+    }
+
+    @Override
+    public void close() {
+      // Ignore close to keep the underlying stream open
+    }
+  }
 }
