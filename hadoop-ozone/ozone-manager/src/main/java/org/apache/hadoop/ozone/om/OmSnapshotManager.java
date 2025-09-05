36c5606a6e5dc921755c717fddb023b333297cbc
Mahesh kumar Gaddam <mahi.ramuk@gmail.com>
2025-09-04 22:54:01 -0500

    HDDS-13604. Add volume and bucket to checkpoint existence/creation logs. (#8976)
    Co-authored-by: maheshkumargaddam <gaddam.mahesh@outlook.com>

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmSnapshotManager.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmSnapshotManager.java
@@ -530,12 +530,13 @@ public final class OmSnapshotManager implements AutoCloseable {
         snapshotInfo.getVolumeName(), snapshotInfo.getBucketName(), batchOperation);
 
     if (dbCheckpoint != null && snapshotDirExist) {
-      LOG.info("Checkpoint : {} for snapshot {} already exists.",
-          dbCheckpoint.getCheckpointLocation(), snapshotInfo.getName());
+      LOG.info("Checkpoint: {} for snapshot {} already exists.",
+          dbCheckpoint.getCheckpointLocation(), snapshotInfo.getTableKey());
       return dbCheckpoint;
     } else if (dbCheckpoint != null) {
-      LOG.info("Created checkpoint : {} for snapshot {}",
-          dbCheckpoint.getCheckpointLocation(), snapshotInfo.getName());
+      LOG.info("Created checkpoint: {} for snapshot {}",
+          dbCheckpoint.getCheckpointLocation(),
+          snapshotInfo.getTableKey());
     }
 
     return dbCheckpoint;
