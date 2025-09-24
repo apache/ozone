28288d8f34dfb0bad87dda9a82d405f3c26f0205
Bolin Lin <linbolin1230@gmail.com>
2025-09-23 21:41:29 -0400

    feat: support CodecBuffer in KeyPrefixContainer

--- hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/spi/impl/KeyPrefixContainerCodec.java
+++ hadoop-ozone/recon/src/main/java/org/apache/hadoop/ozone/recon/spi/impl/KeyPrefixContainerCodec.java
@@ -22,8 +22,14 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import com.google.common.base.Preconditions;
 import com.google.common.primitives.Longs;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetDecoder;
+import jakarta.annotation.Nonnull;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.hadoop.hdds.utils.db.Codec;
+import org.apache.hadoop.hdds.utils.db.CodecBuffer;
+import org.apache.hadoop.hdds.utils.db.CodecException;
 import org.apache.hadoop.ozone.recon.api.types.KeyPrefixContainer;
 
 /**
@@ -48,6 +54,134 @@ public final class KeyPrefixContainerCodec
   @Override
   public Class<KeyPrefixContainer> getTypeClass() {
     return KeyPrefixContainer.class;
+  }
+
+  @Override
+  public boolean supportCodecBuffer() {
+    return true;
+  }
+
+  @Override
+  public CodecBuffer toCodecBuffer(@Nonnull KeyPrefixContainer object, CodecBuffer.Allocator allocator) throws CodecException {
+    Preconditions.checkNotNull(object, "Null object can't be converted to CodecBuffer.");
+
+    final byte[] keyPrefixBytes = object.getKeyPrefix().getBytes(UTF_8);
+    int totalSize = keyPrefixBytes.length;
+
+    if (object.getKeyVersion() != -1) {
+      totalSize += KEY_DELIMITER.getBytes(UTF_8).length + Long.BYTES;
+    }
+    if (object.getContainerId() != -1) {
+      totalSize += KEY_DELIMITER.getBytes(UTF_8).length + Long.BYTES;
+    }
+
+    final CodecBuffer buffer = allocator.apply(totalSize);
+    buffer.put(ByteBuffer.wrap(keyPrefixBytes));
+
+    if (object.getKeyVersion() != -1) {
+      buffer.put(ByteBuffer.wrap(KEY_DELIMITER.getBytes(UTF_8)));
+      buffer.putLong(object.getKeyVersion());
+    }
+
+    if (object.getContainerId() != -1) {
+      buffer.put(ByteBuffer.wrap(KEY_DELIMITER.getBytes(UTF_8)));
+      buffer.putLong(object.getContainerId());
+    }
+
+    return buffer;
+  }
+
+  @Override
+  public KeyPrefixContainer fromCodecBuffer(@Nonnull CodecBuffer buffer) throws CodecException {
+
+    Object wrapped = buffer.getWrapped();
+    if (wrapped instanceof byte[]) {
+      return fromPersistedFormat((byte[]) wrapped);
+    }
+
+    final ByteBuffer byteBuffer = buffer.asReadOnlyByteBuffer();
+    final int totalLength = byteBuffer.remaining();
+    final int startPosition = byteBuffer.position();
+
+    final byte[] delimiterBytes = KEY_DELIMITER.getBytes(UTF_8);
+
+    int firstDelimiterIndex = findDelimiterInBuffer(byteBuffer, delimiterBytes);
+
+    byteBuffer.position(startPosition);
+
+    // If no delimiter found, entire buffer is key prefix
+    if (firstDelimiterIndex == -1) {
+      String keyPrefix = decodeStringFromBuffer(byteBuffer, totalLength);
+      return KeyPrefixContainer.get(keyPrefix, -1, -1);
+    }
+
+    // Decode key prefix without copying
+    String keyPrefix = decodeStringFromBuffer(byteBuffer, firstDelimiterIndex);
+
+    // Skip delimiter
+    byteBuffer.position(byteBuffer.position() + delimiterBytes.length);
+
+    long version = -1;
+    long containerId = -1;
+
+    if (byteBuffer.remaining() >= Long.BYTES) {
+      version = byteBuffer.getLong();
+
+      if (byteBuffer.remaining() >= delimiterBytes.length + Long.BYTES) {
+        // Skip delimiter
+        byteBuffer.position(byteBuffer.position() + delimiterBytes.length);
+        containerId = byteBuffer.getLong();
+      }
+    }
+
+    return KeyPrefixContainer.get(keyPrefix, version, containerId);
+  }
+
+  /**
+   * Find delimiter in ByteBuffer without copying data.
+   * Returns relative position of delimiter, or -1 if not found.
+   */
+  private int findDelimiterInBuffer(ByteBuffer buffer, byte[] delimiter) {
+    final int startPos = buffer.position();
+    final int limit = buffer.limit();
+
+    for (int i = startPos; i <= limit - delimiter.length; i++) {
+      boolean found = true;
+      for (int j = 0; j < delimiter.length; j++) {
+        if (buffer.get(i + j) != delimiter[j]) {
+          found = false;
+          break;
+        }
+      }
+      if (found) {
+        return i - startPos;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Decode string from ByteBuffer without copying to intermediate byte array.
+   * Uses CharsetDecoder for efficient decoding.
+   */
+  private String decodeStringFromBuffer(ByteBuffer buffer, int length) throws CodecException {
+    if (length == 0) {
+      return "";
+    }
+
+    try {
+      ByteBuffer slice = buffer.duplicate();
+      slice.limit(buffer.position() + length);
+
+      buffer.position(buffer.position() + length);
+
+      CharsetDecoder decoder = UTF_8.newDecoder();
+      CharBuffer charBuffer = decoder.decode(slice);
+      return charBuffer.toString();
+
+    } catch (CharacterCodingException e) {
+      throw new CodecException("Failed to decode UTF-8 string from buffer", e);
+    }
   }
 
   @Override
