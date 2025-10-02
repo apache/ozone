0e4ff245fa62798b6982f34551e8e8d2b90234e7
Doroszlai, Attila <adoroszlai@apache.org>
2025-10-02 12:08:17 +0200

    backport HADOOP-18691

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ipc_/Server.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ipc_/Server.java
@@ -897,6 +897,11 @@ public abstract class Server {
     }
 
     @Override
+    public CallerContext getCallerContext() {
+      return this.callerContext;
+    }
+
+    @Override
     public int getPriorityLevel() {
       return this.priorityLevel;
     }
