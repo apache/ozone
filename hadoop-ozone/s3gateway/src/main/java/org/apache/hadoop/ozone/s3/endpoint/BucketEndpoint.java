fe72a2d701cb7aab3a5c4be98bd2b21c46b5c209
rich7420 <rc910420@gmail.com>
2025-10-13 23:32:44 +0800

    rename methids and test class

--- hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/endpoint/BucketEndpoint.java
+++ hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/endpoint/BucketEndpoint.java
@@ -124,7 +124,6 @@ public class BucketEndpoint extends EndpointBase {
       // Handle ACL requests
       final String aclMarker = queryParams().get(QueryParams.ACL);
       if (aclMarker != null) {
-        s3GAction = S3GAction.GET_ACL;
         return handleGetBucketAcl(bucketName, startNanos);
       }
 
@@ -309,6 +308,10 @@ public class BucketEndpoint extends EndpointBase {
       String bucketName, String delimiter, String encodingType, String marker,
       int maxKeys, String prefix, String continueToken, String startAfter)
       throws OS3Exception, IOException {
+    
+    // If you specify the encoding-type request parameter, should return encoded key name values 
+    // in the following response elements: Delimiter, Prefix, Key, and StartAfter.
+    // For detail refer: https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html#AmazonS3-ListObjectsV2-response-EncodingType
     
     // Validate encoding type
     if (encodingType != null && !encodingType.equals(ENCODING_TYPE)) {
