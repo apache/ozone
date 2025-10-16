ff49170554fe3b7d80a86136cb57d382e81fcc70
Eric Chang <e850506@gmail.com>
2025-10-16 11:50:11 +0800

    minor tweaks

--- hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/utils/TestHttpServletUtils.java
+++ hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/utils/TestHttpServletUtils.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TestHttpServletUtils {
-
   public static Stream<Arguments> provideGetResponseFormatTestCases() {
     return Stream.of(
         Arguments.of("text/plain", HttpServletUtils.ResponseFormat.XML),
