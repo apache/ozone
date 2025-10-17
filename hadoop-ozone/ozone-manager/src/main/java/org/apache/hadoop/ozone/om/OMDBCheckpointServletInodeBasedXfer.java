ca36ef92f332821c230fcc2173caf204bcb36969
Wei-Chiu Chuang <weichiu@apache.org>
2025-10-17 10:15:08 -0700

    Findbugs.
    Change-Id: I3442fdc585a1c1a59f994e2a6003ed3022085a63

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OMDBCheckpointServletInodeBasedXfer.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OMDBCheckpointServletInodeBasedXfer.java
@@ -49,6 +49,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
