a7a20491c9bc79c264ea9c5364d502f7a580d555
Fabian Morgan <fmorg-git@gmail.com>
2025-10-28 19:16:26 -0700

    update Javadoc

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/security/acl/AssumeRoleRequest.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/security/acl/AssumeRoleRequest.java
@@ -22,14 +22,13 @@ import java.util.AbstractMap;
 import java.util.Objects;
 import java.util.Set;
 import net.jcip.annotations.Immutable;
-import org.apache.hadoop.ozone.security.acl.iam.IamSessionPolicyResolver;
 import org.apache.hadoop.security.UserGroupInformation;
 
 /**
  * Represents an S3 AssumeRole request that needs to be authorized by an IAccessAuthorizer.
- * The grants parameter can be obtained via a call to
- * {@link IamSessionPolicyResolver#resolve(String, String, IamSessionPolicyResolver.AuthorizerType)},
- * or it can be null if the access must not be limited beyond the role.
+ * The grants parameter can be null if the access must not be limited beyond the role.
+ * Note that if the grants is parameter is the empty set, this means the access should
+ * be the intersection of the role and the empty set, meaning no access will be granted.
  */
 @Immutable
 public class AssumeRoleRequest {
