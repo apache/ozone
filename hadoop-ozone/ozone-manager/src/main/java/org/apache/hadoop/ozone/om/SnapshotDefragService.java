a810cc1b4b13636040b0bf248290382f5eccf2e1
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2025-10-30 18:45:20 -0400

    HDDS-13785. Fix findbugs
    Change-Id: Ic2aa9091a9c463bb238b5af6c347df3d080b28f9

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/SnapshotDefragService.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/SnapshotDefragService.java
@@ -134,8 +134,6 @@ public class SnapshotDefragService extends BackgroundService
     try (OmSnapshotLocalDataManager.ReadableOmSnapshotLocalDataProvider readableOmSnapshotLocalDataProvider =
              ozoneManager.getOmSnapshotManager().getSnapshotLocalDataManager().getOmSnapshotLocalData(snapshotInfo)) {
       // Read snapshot local metadata from YAML
-      OmSnapshotLocalData snapshotLocalData = readableOmSnapshotLocalDataProvider.getSnapshotLocalData();
-
       // Check if snapshot needs compaction (defragmentation)
       boolean needsDefrag = readableOmSnapshotLocalDataProvider.needsDefrag();
       LOG.debug("Snapshot {} needsDefragmentation field value: {}", snapshotInfo.getName(), needsDefrag);
