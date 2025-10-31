8a4ca7bbffc986ada31d9e24874c242608da8c45
rich7420 <101171023+rich7420@users.noreply.github.com>
2025-10-31 18:27:53 +0800

    Format getTxIdFromObjectId method for readability

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/OmUtils.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/OmUtils.java
@@ -667,7 +667,8 @@ public final class OmUtils {
    */
   @VisibleForTesting
   public static long getTxIdFromObjectId(long objectId) {
-    return ((Long.MAX_VALUE >> REVERSE_EPOCH_ID_SHIFT) & objectId) >> TRANSACTION_ID_SHIFT;
+    return ((Long.MAX_VALUE >> REVERSE_EPOCH_ID_SHIFT) & objectId)
+        >> TRANSACTION_ID_SHIFT;
   }
 
   /**
