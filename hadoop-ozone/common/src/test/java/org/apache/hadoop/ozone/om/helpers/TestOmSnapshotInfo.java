41b7cfbcba3250090b6a3c3e0c54ea169f4705fa
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2025-11-01 17:22:32 -0400

    HDDS-13830. Fix pmd
    Change-Id: If9cd8e82083f90997d7ce052408b33648d9b2c2a

--- hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/om/helpers/TestOmSnapshotInfo.java
+++ hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/om/helpers/TestOmSnapshotInfo.java
@@ -45,7 +45,6 @@ public class TestOmSnapshotInfo {
   private static final UUID GLOBAL_PREVIOUS_SNAPSHOT_ID =
       PATH_PREVIOUS_SNAPSHOT_ID;
   private static final String SNAPSHOT_PATH = "test/path";
-  private static final String CHECKPOINT_DIR = "checkpoint.testdir";
   private static final long DB_TX_SEQUENCE_NUMBER = 12345L;
 
   private SnapshotInfo createSnapshotInfo() {
