cc5cf0282160738849d02f2106640cbed3d29c9d
Abhishek Pal <pal.abhishek03012001@gmail.com>
2025-11-03 12:00:25 +0530

    HDDS-13183. Create Cluster Capacity page UI (#9022).

---
+++ hadoop-ozone/recon/src/main/resources/webapps/recon/ozone-recon-web/src/v2/pages/capacity/constants/styles.constants.tsx
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const cardHeadStyle: React.CSSProperties = {
+  width: '100%',
+  flexGrow: 0,
+  fontFamily: 'Roboto',
+  fontSize: '16px',
+  fontWeight: 500,
+  fontStretch: 'normal',
+  fontStyle: 'normal',
+  lineHeight: 1.5,
+  letterSpacing: 'normal',
+  textAlign: 'left',
+  color: '#5a656d'
+};
+
+export const statisticValueStyle: React.CSSProperties = {
+  fontFamily: 'Roboto',
+  fontSize: '24px',
+  fontWeight: 'normal',
+  fontStretch: 'normal',
+  fontStyle: 'normal',
+  lineHeight: 1.33,
+  letterSpacing: 'normal',
+  textAlign: 'left',
+  color: '#1b2329'
+};
