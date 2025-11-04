1963a009f9694a060bb2a8c574f137b7ed81aa23
rich7420 <rc910420@gmail.com>
2025-11-04 13:36:28 +0800

    fix waiting for a flush that might never occur

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/service/TestKeyDeletingService.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/service/TestKeyDeletingService.java
@@ -623,7 +623,6 @@ class TestKeyDeletingService extends OzoneTestBase {
         assertTableRowCount(snap3deletedTable, initialDeletedCount + 10, metadataManager);
 
         writeClient.deleteSnapshot(volumeName, bucketName, snap2);
-        om.awaitDoubleBufferFlush();
         keyDeletingService.runPeriodicalTaskNow();
         directoryDeletingService.runPeriodicalTaskNow();
         om.awaitDoubleBufferFlush();
