b77698e010d9c361fd176a2d6cbfb32ffb4c5fe6
peterxcli <peterxcli@gmail.com>
2025-11-07 18:16:28 +0800

    Revert "HDDS-13861. Intermittent failure in testOverWriteKeyWithAndWithOutVersioning (#9234)"
    This reverts commit 1fa7e569f7cd225be7d49ae4cccf8a9e4384fbf8.

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/OzoneRpcClientTests.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/OzoneRpcClientTests.java
@@ -4637,14 +4637,8 @@ abstract class OzoneRpcClientTests extends OzoneTestBase {
 
   private void checkExceptedResultForVersioningTest(String volumeName,
       String bucketName, String keyName, int expectedCount) throws Exception {
-    // Get actual bucket layout from the bucket itself to avoid mismatch
-    String bucketKey = cluster.getOzoneManager().getMetadataManager()
-        .getBucketKey(volumeName, bucketName);
-    BucketLayout actualLayout = cluster.getOzoneManager().getMetadataManager()
-        .getBucketTable().get(bucketKey).getBucketLayout();
-    
     OmKeyInfo omKeyInfo = cluster.getOzoneManager().getMetadataManager()
-        .getKeyTable(actualLayout).get(
+        .getKeyTable(getBucketLayout()).get(
             cluster.getOzoneManager().getMetadataManager()
                 .getOzoneKey(volumeName, bucketName, keyName));
 
@@ -4656,21 +4650,13 @@ abstract class OzoneRpcClientTests extends OzoneTestBase {
     cluster.getOzoneManager().awaitDoubleBufferFlush();
 
     if (expectedCount == 1) {
-      // Wait for deleted key to become visible in deletedTable
-      String ozoneKey = cluster.getOzoneManager().getMetadataManager()
-          .getOzoneKey(volumeName, bucketName, keyName);
-      GenericTestUtils.waitFor((CheckedSupplier<Boolean, IOException>) () -> {
-        List<? extends Table.KeyValue<String, RepeatedOmKeyInfo>> rangeKVs
-            = cluster.getOzoneManager().getMetadataManager().getDeletedTable()
-            .getRangeKVs(null, 100, ozoneKey);
-        return !rangeKVs.isEmpty();
-      }, 100, 60000);
-
       List<? extends Table.KeyValue<String, RepeatedOmKeyInfo>> rangeKVs
           = cluster.getOzoneManager().getMetadataManager().getDeletedTable()
-          .getRangeKVs(null, 100, ozoneKey);
+          .getRangeKVs(null, 100,
+              cluster.getOzoneManager().getMetadataManager()
+              .getOzoneKey(volumeName, bucketName, keyName));
 
-      assertThat(rangeKVs).isNotEmpty();
+      assertThat(rangeKVs.size()).isGreaterThan(0);
       assertEquals(expectedCount,
           rangeKVs.get(0).getValue().getOmKeyInfoList().size());
     } else {
