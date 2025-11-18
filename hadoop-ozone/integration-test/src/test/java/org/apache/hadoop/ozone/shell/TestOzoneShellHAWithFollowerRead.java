438128f20ac509131a7112eb110bc503d11002c0
Yiyang Zhou <yiyang.zhou@shopee.com>
2025-11-18 17:11:17 +0800

    HDDS-13953. Add TestOzoneShellHAWithFollowerRead

---
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/shell/TestOzoneShellHAWithFollowerRead.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.ozone.shell;
+
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.ozone.om.ratis.OzoneManagerRatisServerConfig;
+import org.apache.ratis.server.RaftServerConfigKeys;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * This class tests Ozone sh shell command with FollowerRead.
+ * Inspired by TestS3Shell
+ */
+public class TestOzoneShellHAWithFollowerRead extends TestOzoneShellHA {
+
+  @BeforeAll
+  @Override
+  public void init() throws Exception {
+    OzoneConfiguration conf = new OzoneConfiguration();
+
+    OzoneManagerRatisServerConfig omHAConfig =
+        conf.getObject(OzoneManagerRatisServerConfig.class);
+    omHAConfig.setReadOption(RaftServerConfigKeys.Read.Option.LINEARIZABLE.name());
+
+    conf.setFromObject(omHAConfig);
+    startKMS();
+    startCluster(conf);
+  }
+}
