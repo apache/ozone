da75f565e4d5bbcea32d4e0020c63bbf06c92c1b
Wei-Chiu Chuang <weichiu@apache.org>
2025-11-19 12:56:59 -0800

    Revert "HDDS-13228. Take snapshot cache lock during the last iteration of tarball transfer. (#8678)"
    This reverts commit c706c7ac0d7f8614d9fffd6d390219a0479b0f2a.
    
     Conflicts:
    	hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/TestOMDbCheckpointServletInodeBasedXfer.java
    	hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OMDBCheckpointServletInodeBasedXfer.java
    	hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmSnapshotManager.java

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/codec/OMDBDefinition.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/codec/OMDBDefinition.java
@@ -17,8 +17,6 @@
 
 package org.apache.hadoop.ozone.om.codec;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import org.apache.hadoop.hdds.utils.TransactionInfo;
 import org.apache.hadoop.hdds.utils.db.DBColumnFamilyDefinition;
@@ -359,14 +357,6 @@ public final class OMDBDefinition extends DBDefinition.WithMap {
   @Override
   public String getLocationConfigKey() {
     return OMConfigKeys.OZONE_OM_DB_DIRS;
-  }
-
-  public static List<String> getAllColumnFamilies() {
-    List<String> columnFamilies = new ArrayList<>();
-    COLUMN_FAMILIES.values().forEach(cf -> {
-      columnFamilies.add(cf.getName());
-    });
-    return columnFamilies;
   }
 }
 
