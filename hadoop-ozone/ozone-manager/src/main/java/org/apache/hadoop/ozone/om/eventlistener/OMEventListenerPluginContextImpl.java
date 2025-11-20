b698ce088eaf177ddee2a923e1d2790ee368c8eb
Colm Dougan <colm.dougan@cirata.com>
2025-11-20 14:40:51 +0000

    HDDS-14005. EventNotification: Added the concept of dynamically configurable EventListener plugins

---
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/eventlistener/OMEventListenerPluginContextImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.ozone.om.eventlistener;
+
+import org.apache.hadoop.ozone.om.OzoneManager;
+
+/**
+ * A narrow set of functionality we are ok with exposing to plugin
+ * implementations.
+ */
+public final class OMEventListenerPluginContextImpl implements OMEventListenerPluginContext {
+  private final OzoneManager ozoneManager; // NOPMD: unused.  will be used soon
+
+  public OMEventListenerPluginContextImpl(OzoneManager ozoneManager) {
+    this.ozoneManager = ozoneManager;
+  }
+
+  // TODO: fill this out with capabilities we would like to expose to
+  // plugin implementations.
+}
