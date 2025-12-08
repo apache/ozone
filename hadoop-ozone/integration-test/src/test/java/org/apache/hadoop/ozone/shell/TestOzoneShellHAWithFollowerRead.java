7ef052bf15b2b30447521ace44b4a72725de055d
Symious <yiyang0203@gmail.com>
2025-12-08 17:48:18 +0800

    HDDS-13954. Fix checkstyle

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/shell/TestOzoneShellHAWithFollowerRead.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/shell/TestOzoneShellHAWithFollowerRead.java
@@ -17,7 +17,7 @@
 
 package org.apache.hadoop.ozone.shell;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
