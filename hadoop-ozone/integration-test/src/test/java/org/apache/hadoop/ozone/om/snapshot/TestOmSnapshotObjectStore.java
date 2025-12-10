a2b10a1ee2e111fe1a2ea3ea30ee2ecdfae8df55
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2025-12-09 23:59:06 -0500

    HDDS-13466. Fix Unhealthy Snapshot Test
    Change-Id: Ia1bb3f9bdd199bf7c652b25f158556c8b0b24c10

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/snapshot/TestOmSnapshotObjectStore.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/om/snapshot/TestOmSnapshotObjectStore.java
@@ -19,12 +19,9 @@ package org.apache.hadoop.ozone.om.snapshot;
 
 import static org.apache.hadoop.ozone.om.helpers.BucketLayout.OBJECT_STORE;
 
-import org.apache.ozone.test.tag.Unhealthy;
-
 /**
  * Test OmSnapshot for Object Store bucket type.
  */
-@Unhealthy("HDDS-13466")
 public class TestOmSnapshotObjectStore extends TestOmSnapshot {
 
   public TestOmSnapshotObjectStore() throws Exception {
