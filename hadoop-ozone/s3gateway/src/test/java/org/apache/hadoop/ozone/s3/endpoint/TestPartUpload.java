a530932302e9024f834b855c55b5f4b22b9dfe93
Doroszlai, Attila <adoroszlai@apache.org>
2026-01-05 13:19:45 +0100

    reduce duplication in TestPartUpload

--- hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/endpoint/TestPartUpload.java
+++ hadoop-ozone/s3gateway/src/test/java/org/apache/hadoop/ozone/s3/endpoint/TestPartUpload.java
@@ -17,18 +17,19 @@
 
 package org.apache.hadoop.ozone.s3.endpoint;
 
-import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.hadoop.ozone.s3.endpoint.EndpointTestUtils.assertErrorResponse;
+import static org.apache.hadoop.ozone.s3.endpoint.EndpointTestUtils.assertSucceeds;
 import static org.apache.hadoop.ozone.s3.endpoint.EndpointTestUtils.initiateMultipartUpload;
+import static org.apache.hadoop.ozone.s3.endpoint.EndpointTestUtils.put;
 import static org.apache.hadoop.ozone.s3.util.S3Consts.DECODED_CONTENT_LENGTH_HEADER;
 import static org.apache.hadoop.ozone.s3.util.S3Consts.STORAGE_CLASS_HEADER;
 import static org.apache.hadoop.ozone.s3.util.S3Consts.X_AMZ_CONTENT_SHA256;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
@@ -37,7 +38,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -46,40 +46,56 @@ import java.util.UUID;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.ozone.OzoneConfigKeys;
 import org.apache.hadoop.ozone.OzoneConsts;
 import org.apache.hadoop.ozone.client.OzoneClient;
 import org.apache.hadoop.ozone.client.OzoneClientStub;
 import org.apache.hadoop.ozone.client.OzoneMultipartUploadPartListParts;
 import org.apache.hadoop.ozone.s3.exception.OS3Exception;
+import org.apache.hadoop.ozone.s3.exception.S3ErrorTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.Parameter;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
 
 /**
  * This class tests Upload part request.
  */
+@ParameterizedClass
+@ValueSource(booleans = {false, true})
 public class TestPartUpload {
 
   private ObjectEndpoint rest;
   private OzoneClient client;
 
+  private HttpHeaders headers;
+
+  @Parameter
+  private boolean enableDataStream;
+
   @BeforeEach
   public void setUp() throws Exception {
-
     client = new OzoneClientStub();
     client.getObjectStore().createS3Bucket(OzoneConsts.S3_BUCKET);
 
-
-    HttpHeaders headers = mock(HttpHeaders.class);
+    headers = mock(HttpHeaders.class);
     when(headers.getHeaderString(STORAGE_CLASS_HEADER)).thenReturn(
         "STANDARD");
     when(headers.getHeaderString(X_AMZ_CONTENT_SHA256))
         .thenReturn("mockSignature");
 
-    rest = EndpointBuilder.newObjectEndpointBuilder()
+    OzoneConfiguration conf = new OzoneConfiguration();
+    conf.setBoolean(OzoneConfigKeys.HDDS_CONTAINER_RATIS_DATASTREAM_ENABLED, enableDataStream);
+
+    rest = spy(EndpointBuilder.newObjectEndpointBuilder()
         .setHeaders(headers)
         .setClient(client)
-        .build();
+        .setConfig(conf)
+        .build());
+    assertEquals(enableDataStream, rest.isDatastreamEnabled());
   }
 
   @Test
@@ -87,137 +103,74 @@ public class TestPartUpload {
     String uploadID = initiateMultipartUpload(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY);
 
     String content = "Multipart Upload";
-    ByteArrayInputStream body =
-        new ByteArrayInputStream(content.getBytes(UTF_8));
-    Response response = rest.put(OzoneConsts.S3_BUCKET, OzoneConsts.KEY,
-        content.length(), 1, uploadID, null, null, body);
-
-    assertNotNull(response.getHeaderString(OzoneConsts.ETAG));
-
-  }
-
-  @Test
-  public void testPartUploadWithOverride() throws Exception {
-    String uploadID = initiateMultipartUpload(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY);
-
-    String content = "Multipart Upload";
-    ByteArrayInputStream body =
-        new ByteArrayInputStream(content.getBytes(UTF_8));
-    Response response = rest.put(OzoneConsts.S3_BUCKET, OzoneConsts.KEY,
-        content.length(), 1, uploadID, null, null, body);
-
-    assertNotNull(response.getHeaderString(OzoneConsts.ETAG));
-
-    String eTag = response.getHeaderString(OzoneConsts.ETAG);
+    String eTag;
+    try (Response response = put(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY, 1, uploadID, content)) {
+      eTag = response.getHeaderString(OzoneConsts.ETAG);
+      assertNotNull(eTag);
+    }
+    assertContentLength(uploadID, OzoneConsts.KEY, content.length());
 
     // Upload part again with same part Number, the ETag should be changed.
-    content = "Multipart Upload Changed";
-    response = rest.put(OzoneConsts.S3_BUCKET, OzoneConsts.KEY,
-        content.length(), 1, uploadID, null, null, body);
-    assertNotNull(response.getHeaderString(OzoneConsts.ETAG));
-    assertNotEquals(eTag, response.getHeaderString(OzoneConsts.ETAG));
-
+    String newContent = "Multipart Upload Changed";
+    try (Response response = put(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY, 1, uploadID, newContent)) {
+      String newETag = response.getHeaderString(OzoneConsts.ETAG);
+      assertNotNull(newETag);
+      assertNotEquals(eTag, newETag);
+    }
   }
 
   @Test
-  public void testPartUploadWithIncorrectUploadID() throws Exception {
-    OS3Exception ex = assertThrows(OS3Exception.class, () -> {
-      String content = "Multipart Upload With Incorrect uploadID";
-      ByteArrayInputStream body =
-          new ByteArrayInputStream(content.getBytes(UTF_8));
-      rest.put(OzoneConsts.S3_BUCKET, OzoneConsts.KEY, content.length(), 1,
-          "random", null, null, body);
-    });
-    assertEquals("NoSuchUpload", ex.getCode());
-    assertEquals(HTTP_NOT_FOUND, ex.getHttpCode());
+  public void testPartUploadWithIncorrectUploadID() {
+    assertErrorResponse(S3ErrorTable.NO_SUCH_UPLOAD,
+        () -> put(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY, 1, "random", "any"));
   }
 
   @Test
   public void testPartUploadStreamContentLength()
       throws IOException, OS3Exception {
-    HttpHeaders headers = mock(HttpHeaders.class);
-    when(headers.getHeaderString(X_AMZ_CONTENT_SHA256))
-        .thenReturn("mockSignature");
-    ObjectEndpoint objectEndpoint = EndpointBuilder.newObjectEndpointBuilder()
-        .setHeaders(headers)
-        .setClient(client)
-        .build();
     String keyName = UUID.randomUUID().toString();
 
+    int contentLength = 15;
     String chunkedContent = "0a;chunk-signature=signature\r\n"
         + "1234567890\r\n"
         + "05;chunk-signature=signature\r\n"
         + "abcde\r\n";
-    when(headers.getHeaderString("x-amz-content-sha256"))
+    when(headers.getHeaderString(X_AMZ_CONTENT_SHA256))
         .thenReturn("STREAMING-AWS4-HMAC-SHA256-PAYLOAD");
     when(headers.getHeaderString(DECODED_CONTENT_LENGTH_HEADER))
-        .thenReturn("15");
+        .thenReturn(String.valueOf(contentLength));
 
     String uploadID = initiateMultipartUpload(rest, OzoneConsts.S3_BUCKET, keyName);
-    long contentLength = chunkedContent.length();
 
-    objectEndpoint.put(OzoneConsts.S3_BUCKET, keyName, contentLength, 1,
-        uploadID, null, null, new ByteArrayInputStream(chunkedContent.getBytes(UTF_8)));
-    assertContentLength(uploadID, keyName, 15);
-  }
-
-  @Test
-  public void testPartUploadContentLength() throws IOException, OS3Exception {
-    // The contentLength specified when creating the Key should be the same as
-    // the Content-Length, the key Commit will compare the Content-Length with
-    // the actual length of the data written.
-
-    String keyName = UUID.randomUUID().toString();
-    String uploadID = initiateMultipartUpload(rest, OzoneConsts.S3_BUCKET, keyName);
-    String content = "Multipart Upload";
-    long contentLength = content.length();
-
-    ByteArrayInputStream body =
-        new ByteArrayInputStream(content.getBytes(UTF_8));
-    rest.put(OzoneConsts.S3_BUCKET, keyName,
-        contentLength, 1, uploadID, null, null, body);
-    assertContentLength(uploadID, keyName, content.length());
+    assertSucceeds(() -> put(rest, OzoneConsts.S3_BUCKET, keyName, 1, uploadID, chunkedContent));
+    assertContentLength(uploadID, keyName, contentLength);
   }
 
   @Test
   public void testPartUploadMessageDigestResetDuringException() throws IOException, OS3Exception {
-    OzoneClient clientStub = new OzoneClientStub();
-    clientStub.getObjectStore().createS3Bucket(OzoneConsts.S3_BUCKET);
-
-
-    HttpHeaders headers = mock(HttpHeaders.class);
-    when(headers.getHeaderString(X_AMZ_CONTENT_SHA256))
-        .thenReturn("mockSignature");
-    when(headers.getHeaderString(STORAGE_CLASS_HEADER)).thenReturn(
-        "STANDARD");
-
-    ObjectEndpoint objectEndpoint = EndpointBuilder.newObjectEndpointBuilder()
-        .setHeaders(headers)
-        .setClient(clientStub)
-        .build();
-
-    objectEndpoint = spy(objectEndpoint);
-
-    String uploadID = initiateMultipartUpload(objectEndpoint, OzoneConsts.S3_BUCKET, OzoneConsts.KEY);
+    String uploadID = initiateMultipartUpload(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY);
 
     MessageDigest messageDigest = mock(MessageDigest.class);
     when(messageDigest.getAlgorithm()).thenReturn("MD5");
     MessageDigest sha256Digest = mock(MessageDigest.class);
     when(sha256Digest.getAlgorithm()).thenReturn("SHA-256");
-    try (MockedStatic<IOUtils> mocked = mockStatic(IOUtils.class)) {
-      // Add the mocked methods only during the copy request
-      when(objectEndpoint.getMessageDigestInstance()).thenReturn(messageDigest);
-      when(objectEndpoint.getSha256DigestInstance()).thenReturn(sha256Digest);
-      mocked.when(() -> IOUtils.copyLarge(any(InputStream.class), any(OutputStream.class), anyLong(),
-              anyLong(), any(byte[].class)))
-          .thenThrow(IOException.class);
+    try (MockedStatic<IOUtils> ioutils = mockStatic(IOUtils.class);
+        MockedStatic<ObjectEndpointStreaming> streaming = mockStatic(ObjectEndpointStreaming.class)) {
+      // Add the mocked methods only during part upload
+      when(rest.getMessageDigestInstance()).thenReturn(messageDigest);
+      when(rest.getSha256DigestInstance()).thenReturn(sha256Digest);
+      if (enableDataStream) {
+        streaming.when(() -> ObjectEndpointStreaming.createMultipartKey(
+            any(), any(), anyLong(), anyInt(), any(), anyInt(), any(), any()))
+                .thenThrow(IOException.class);
+      } else {
+        ioutils.when(() -> IOUtils.copyLarge(any(InputStream.class), any(OutputStream.class), anyLong(),
+                anyLong(), any(byte[].class)))
+            .thenThrow(IOException.class);
+      }
 
       String content = "Multipart Upload";
-      ByteArrayInputStream body =
-          new ByteArrayInputStream(content.getBytes(UTF_8));
-      try {
-        objectEndpoint.put(OzoneConsts.S3_BUCKET, OzoneConsts.KEY,
-            content.length(), 1, uploadID, null, null, body);
+      try (Response ignored = put(rest, OzoneConsts.S3_BUCKET, OzoneConsts.KEY, 1, uploadID, content)) {
         fail("Should throw IOException");
       } catch (IOException ignored) {
         // Verify that the message digest is reset so that the instance can be reused for the
