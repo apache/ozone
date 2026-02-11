bc6bc9cc8e38a1a43fbd6efd9af35609db88daa9
Russole <850905junior@gmail.com>
2026-02-12 00:01:11 +0800

    Revert previous two commits to re-implement the feature

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/impl/ChunkManagerDummyImpl.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/impl/ChunkManagerDummyImpl.java
@@ -73,11 +73,7 @@ public class ChunkManagerDummyImpl implements ChunkManager {
 
       try (FileChannel ch = FileChannel.open(backingFile,
           StandardOpenOption.READ, StandardOpenOption.WRITE)) {
-        long currentSize = ch.size();
-        if (currentSize < newSize) {
-          ch.position(newSize - 1L);
-          ch.write(ByteBuffer.wrap(new byte[]{0}));
-        } else if (currentSize > newSize) {
+        if (ch.size() < newSize) {
           ch.truncate(newSize);
         }
         mapped = ch.map(FileChannel.MapMode.READ_ONLY, 0, newSize);
