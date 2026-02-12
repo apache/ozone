c3854982a84eff95254a64dbbc42381be7bc7a98
Sergey Soldatov <ssa@apache.org>
2026-02-12 08:21:29 -0800

    A new helper to record volume stats

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueHandler.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/keyvalue/KeyValueHandler.java
@@ -2093,14 +2093,11 @@ public class KeyValueHandler extends Handler {
     try {
       final long startTime = Time.monotonicNow();
       final long bytesRead = readBlockImpl(request, blockFile, kvContainer, streamObserver, false);
-      long endTime = Time.monotonicNow();
       KeyValueContainerData containerData = (KeyValueContainerData) kvContainer
           .getContainerData();
       HddsVolume volume = containerData.getVolume();
       if (volume != null) {
-        volume.getVolumeIOStats().incReadTime(endTime - startTime);
-        volume.getVolumeIOStats().incReadOpCount();
-        volume.getVolumeIOStats().incReadBytes(bytesRead);
+        volume.getVolumeIOStats().recordReadOperation(startTime, bytesRead);
       }
       metrics.incContainerBytesStats(Type.ReadBlock, bytesRead);
     } catch (StorageContainerException ex) {
