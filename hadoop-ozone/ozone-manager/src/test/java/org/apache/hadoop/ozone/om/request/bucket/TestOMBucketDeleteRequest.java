80240828362aa19f7da22b5b115c3cc0f560ff4f
Artemiy Murashev <144871779+art9440@users.noreply.github.com>
2026-02-17 16:12:43 +0700

    Update hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/bucket/TestOMBucketDeleteRequest.java
    Co-authored-by: Gargi Jaiswal <134698352+Gargi-jais11@users.noreply.github.com>

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/bucket/TestOMBucketDeleteRequest.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/bucket/TestOMBucketDeleteRequest.java
@@ -78,11 +78,11 @@ public class TestOMBucketDeleteRequest extends TestBucketRequest {
 
     OMBucketDeleteRequest req = new OMBucketDeleteRequest(originalRequest) {
       @Override
-      public void checkAcls(OzoneManager ozoneManager,
-                            OzoneObj.ResourceType resType,
-                            OzoneObj.StoreType storeType,
-                            IAccessAuthorizer.ACLType aclType,
-                            String vol, String bucket, String key) throws IOException {
+      public void checkAcls(OzoneManager ozoneManager, 
+          OzoneObj.ResourceType resType,
+          OzoneObj.StoreType storeType,
+          IAccessAuthorizer.ACLType aclType,
+          String vol, String bucket, String key) throws IOException 
         throw new OMException("denied",
             OMException.ResultCodes.PERMISSION_DENIED);
       }
