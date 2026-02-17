7fb5e381f8de1965f3ba68b0f8c292fe0f694acb
Artemiy Murashev <144871779+art9440@users.noreply.github.com>
2026-02-17 16:13:38 +0700

    Update hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/volume/TestOMVolumeDeleteRequest.java
    Co-authored-by: Gargi Jaiswal <134698352+Gargi-jais11@users.noreply.github.com>

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/volume/TestOMVolumeDeleteRequest.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/request/volume/TestOMVolumeDeleteRequest.java
@@ -66,11 +66,11 @@ public class TestOMVolumeDeleteRequest extends TestOMVolumeRequest {
 
     OMVolumeDeleteRequest req = new OMVolumeDeleteRequest(originalRequest) {
       @Override
-      public void checkAcls(OzoneManager ozoneManager,
-                            OzoneObj.ResourceType resType,
-                            OzoneObj.StoreType storeType,
-                            IAccessAuthorizer.ACLType aclType,
-                            String vol, String bucket, String key) throws IOException {
+      public void checkAcls(OzoneManager ozoneManager, 
+          OzoneObj.ResourceType resType,
+          OzoneObj.StoreType storeType,
+          IAccessAuthorizer.ACLType aclType,
+          String vol, String bucket, String key) throws IOException 
 
 
         throw new OMException("denied", OMException.ResultCodes.PERMISSION_DENIED);
