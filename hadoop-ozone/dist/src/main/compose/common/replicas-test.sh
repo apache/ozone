8ab8f58eca25eff44e1f0e7d50b1ee8faedd932a
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2026-03-11 03:16:13 -0700

    Fix replicas-test.sh attempt 2

--- hadoop-ozone/dist/src/main/compose/common/replicas-test.sh
+++ hadoop-ozone/dist/src/main/compose/common/replicas-test.sh
@@ -82,18 +82,16 @@ if [ ! -e "${backup_container_db}" ]; then
   echo "Failed to locate backup for ${target_container_db} on ${container}" >&2
   exit 1
 fi
-# Replace the whole RocksDB directory so stale files (eg. old WAL/MANIFEST)
-# are not left behind when restoring the backup.
+target_container_dir=$(dirname "${target_container_db}")
 echo "Restoring backup at ${target_container_db} on ${container}"
-docker exec "${container}" mv "${target_container_db}" "${target_container_db}.orig" \
-  && docker cp "${backup_container_db}" "${container}:${target_container_db}" \
-  && docker exec "${container}" sudo chown -R hadoop:hadoop "${target_container_db}" \
-  && docker exec "${container}" rm -rf "${target_container_db}.orig" \
+docker cp "${backup_container_db}" "${container}:${target_container_dir}/" \
+  && docker exec "${container}" sudo chown -R hadoop:hadoop "${target_container_dir}" \
   || exit 1
 
 docker stop "${container}"
 
 wait_for_datanode "${container}" STALE 60
+
 execute_robot_test ${SCM} -v "PREFIX:${prefix}" -v "STALE_DATANODE:${host}" debug/stale-datanode-checksum.robot
 
 docker start "${container}"
