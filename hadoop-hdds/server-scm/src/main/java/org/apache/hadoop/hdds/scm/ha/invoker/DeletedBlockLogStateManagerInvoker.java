cbc44e0dece4d5f80f65b7e9000e5b3b1882fe64
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2026-03-29 09:11:26 -0700

    Revert DeletedBlockLogStateManagerInvoker

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/invoker/DeletedBlockLogStateManagerInvoker.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/invoker/DeletedBlockLogStateManagerInvoker.java
@@ -24,7 +24,9 @@ import org.apache.hadoop.hdds.scm.block.DeletedBlockLogStateManager;
 import org.apache.hadoop.hdds.scm.ha.ScmInvoker;
 import org.apache.hadoop.hdds.utils.db.Table;
 
-/** Code generated for DeletedBlockLogStateManager.  Do not modify. */
+/**
+ * Invoker for DeletedBlockLogStateManager local (non-@Replicate) methods.
+ */
 public class DeletedBlockLogStateManagerInvoker implements ScmInvoker<DeletedBlockLogStateManager> {
   private final DeletedBlockLogStateManager impl;
 
@@ -47,37 +49,43 @@ public class DeletedBlockLogStateManagerInvoker implements ScmInvoker<DeletedBlo
     return impl;
   }
 
+  // Code generated for DeletedBlockLogStateManager.  Do not modify.
   @SuppressWarnings("unchecked")
   @Override
   public Object invokeLocal(String methodName, Object[] params) throws Exception {
     switch (methodName) {
-      case "addTransactionsToDB":
-        final ArrayList arg0 = params.length > 0 ? (ArrayList) params[0] : null;
-        final DeletedBlocksTransactionSummary arg1 = params.length > 1 ? (DeletedBlocksTransactionSummary) params[1] : null;
-        impl.addTransactionsToDB(arg0, arg1);
-        return null;
+    case "onFlush":
+      impl.onFlush();
+      return null;
 
-      case "getReadOnlyIterator":
-        return impl.getReadOnlyIterator();
+    case "addTransactionsToDB":
+      final ArrayList arg0 =
+          params.length > 0 ? (ArrayList) params[0] : null;
+      final DeletedBlocksTransactionSummary arg1 =
+          params.length > 1 ? (DeletedBlocksTransactionSummary) params[1] : null;
+      impl.addTransactionsToDB(arg0, arg1);
+      return null;
 
-      case "onFlush":
-        impl.onFlush();
-        return null;
+    case "removeTransactionsFromDB":
+      final ArrayList arg2 =
+          params.length > 0 ? (ArrayList) params[0] : null;
+      final DeletedBlocksTransactionSummary arg3 =
+          params.length > 1 ? (DeletedBlocksTransactionSummary) params[1] : null;
+      impl.removeTransactionsFromDB(arg2, arg3);
+      return null;
 
-      case "reinitialize":
-        final Table arg2 = params.length > 0 ? (Table) params[0] : null;
-        final Table arg3 = params.length > 1 ? (Table) params[1] : null;
-        impl.reinitialize(arg2, arg3);
-        return null;
+    case "getReadOnlyIterator":
+      return impl.getReadOnlyIterator();
 
-      case "removeTransactionsFromDB":
-        final ArrayList arg4 = params.length > 0 ? (ArrayList) params[0] : null;
-        final DeletedBlocksTransactionSummary arg5 = params.length > 1 ? (DeletedBlocksTransactionSummary) params[1] : null;
-        impl.removeTransactionsFromDB(arg4, arg5);
-        return null;
+    case "reinitialize":
+      final Table arg4 = params.length > 0 ? (Table) params[0] : null;
+      final Table arg5 = params.length > 1 ? (Table) params[1] : null;
+      impl.reinitialize(arg4, arg5);
+      return null;
 
-      default:
-        throw new IllegalArgumentException("Method not found: " + methodName + " in DeletedBlockLogStateManager");
+    default:
+      throw new IllegalArgumentException("Method not found: " + methodName);
     }
   }
+
 }
