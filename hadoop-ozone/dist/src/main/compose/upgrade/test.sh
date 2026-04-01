7e2c25ead70e2f6200d9b6c7bc74ec558d24daaa
Zita Dombi <zitadombi98@gmail.com>
2026-04-01 13:34:54 +0200

    Comment out rolling upgrade run

--- hadoop-ozone/dist/src/main/compose/upgrade/test.sh
+++ hadoop-ozone/dist/src/main/compose/upgrade/test.sh
@@ -33,7 +33,7 @@ RESULT_DIR="$ALL_RESULT_DIR" create_results_dir
 
 # This is the version of Ozone that should use the runner image to run the
 # code that was built. Other versions will pull images from docker hub.
-# run_test ha     non-rolling-upgrade 2.1.0 "$OZONE_CURRENT_VERSION"
+run_test ha     non-rolling-upgrade 2.1.0 "$OZONE_CURRENT_VERSION"
 # run_test ha     non-rolling-upgrade 2.0.0 "$OZONE_CURRENT_VERSION"
 #run_test non-ha non-rolling-upgrade 1.4.1 "$OZONE_CURRENT_VERSION"
 #run_test ha     non-rolling-upgrade 1.4.1 "$OZONE_CURRENT_VERSION"
@@ -43,7 +43,7 @@ RESULT_DIR="$ALL_RESULT_DIR" create_results_dir
 # run_test om-ha non-rolling-upgrade 1.1.0 "$OZONE_CURRENT_VERSION"
 
 # Rolling upgrade test, commented out for now
-run_test ha     rolling-upgrade "$OZONE_CURRENT_VERSION" "$OZONE_CURRENT_VERSION"
+# run_test ha     rolling-upgrade "$OZONE_CURRENT_VERSION" "$OZONE_CURRENT_VERSION"
 
 generate_report "upgrade" "$ALL_RESULT_DIR"
 
