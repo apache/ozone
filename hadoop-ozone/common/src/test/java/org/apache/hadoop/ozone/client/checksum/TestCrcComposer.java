d734e0c7f65cabd1e1df2e8684a775b77bbf34f3
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2026-04-25 11:45:39 -0700

    Fix findbugs

--- hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/client/checksum/TestCrcComposer.java
+++ hadoop-ozone/common/src/test/java/org/apache/hadoop/ozone/client/checksum/TestCrcComposer.java
@@ -35,16 +35,16 @@ import org.junit.jupiter.api.Timeout;
 /** Test {@link CrcComposer}. */
 @Timeout(10)
 public class TestCrcComposer {
+  private static final int DATA_SIZE = 75;
+  private static final int CELL_SIZE = 20;
+  private static final int CHUNK_SIZE = 10;
 
   private final Random rand = new Random(1234);
 
   private final DataChecksum.Type type = DataChecksum.Type.CRC32C;
   private final DataChecksum checksum = Objects.requireNonNull(
       DataChecksum.newDataChecksum(type, Integer.MAX_VALUE));
-  private final int dataSize = 75;
-  private final byte[] data = new byte[dataSize];
-  private final int chunkSize = 10;
-  private final int cellSize = 20;
+  private final byte[] data = new byte[DATA_SIZE];
 
   private int fullCrc;
   private int[] crcsByChunk;
@@ -55,23 +55,23 @@ public class TestCrcComposer {
   @BeforeEach
   public void setup() {
     rand.nextBytes(data);
-    fullCrc = getRangeChecksum(data, 0, dataSize);
+    fullCrc = getRangeChecksum(data, 0, DATA_SIZE);
 
     // 7 chunks of size chunkSize, 1 chunk of size (dataSize % chunkSize).
     crcsByChunk = new int[8];
     for (int i = 0; i < 7; ++i) {
-      crcsByChunk[i] = getRangeChecksum(data, i * chunkSize, chunkSize);
+      crcsByChunk[i] = getRangeChecksum(data, i * CHUNK_SIZE, CHUNK_SIZE);
     }
     crcsByChunk[7] = getRangeChecksum(
-        data, (crcsByChunk.length - 1) * chunkSize, dataSize % chunkSize);
+        data, (crcsByChunk.length - 1) * CHUNK_SIZE, DATA_SIZE % CHUNK_SIZE);
 
     // 3 cells of size cellSize, 1 cell of size (dataSize % cellSize).
     int[] crcsByCell = new int[4];
     for (int i = 0; i < 3; ++i) {
-      crcsByCell[i] = getRangeChecksum(data, i * cellSize, cellSize);
+      crcsByCell[i] = getRangeChecksum(data, i * CELL_SIZE, CELL_SIZE);
     }
     crcsByCell[3] = getRangeChecksum(
-        data, (crcsByCell.length - 1) * cellSize, dataSize % cellSize);
+        data, (crcsByCell.length - 1) * CELL_SIZE, DATA_SIZE % CELL_SIZE);
 
     crcBytesByChunk = intArrayToByteArray(crcsByChunk);
     crcBytesByCell = intArrayToByteArray(crcsByCell);
@@ -93,13 +93,13 @@ public class TestCrcComposer {
 
   @Test
   public void testUnstripedIncorrectChunkSize() {
-    CrcComposer digester = CrcComposer.newCrcComposer(type, chunkSize);
+    CrcComposer digester = CrcComposer.newCrcComposer(type, CHUNK_SIZE);
 
     // If we incorrectly specify that all CRCs ingested correspond to chunkSize
     // when the last CRC in the array actually corresponds to
     // dataSize % chunkSize then we expect the resulting CRC to not be equal to
     // the fullCrc.
-    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length, chunkSize);
+    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length, CHUNK_SIZE);
     byte[] digest = digester.digest();
     assertEquals(4, digest.length);
     int calculatedCrc = CrcUtil.readInt(digest, 0);
@@ -108,9 +108,9 @@ public class TestCrcComposer {
 
   @Test
   public void testUnstripedByteArray() {
-    CrcComposer digester = CrcComposer.newCrcComposer(type, chunkSize);
-    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length - 4, chunkSize);
-    digester.update(crcBytesByChunk, crcBytesByChunk.length - 4, 4, dataSize % chunkSize);
+    CrcComposer digester = CrcComposer.newCrcComposer(type, CHUNK_SIZE);
+    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length - 4, CHUNK_SIZE);
+    digester.update(crcBytesByChunk, crcBytesByChunk.length - 4, 4, DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertEquals(4, digest.length);
@@ -120,10 +120,10 @@ public class TestCrcComposer {
 
   @Test
   public void testUnstripedDataInputStream() throws IOException {
-    CrcComposer digester = CrcComposer.newCrcComposer(type, chunkSize);
+    CrcComposer digester = CrcComposer.newCrcComposer(type, CHUNK_SIZE);
     DataInputStream input = new DataInputStream(new ByteArrayInputStream(crcBytesByChunk));
-    digester.update(input, crcsByChunk.length - 1, chunkSize);
-    digester.update(input, 1, dataSize % chunkSize);
+    digester.update(input, crcsByChunk.length - 1, CHUNK_SIZE);
+    digester.update(input, 1, DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertEquals(4, digest.length);
@@ -133,11 +133,11 @@ public class TestCrcComposer {
 
   @Test
   public void testUnstripedSingleCrcs() {
-    CrcComposer digester = CrcComposer.newCrcComposer(type, chunkSize);
+    CrcComposer digester = CrcComposer.newCrcComposer(type, CHUNK_SIZE);
     for (int i = 0; i < crcsByChunk.length - 1; ++i) {
-      digester.update(crcsByChunk[i], chunkSize);
+      digester.update(crcsByChunk[i], CHUNK_SIZE);
     }
-    digester.update(crcsByChunk[crcsByChunk.length - 1], dataSize % chunkSize);
+    digester.update(crcsByChunk[crcsByChunk.length - 1], DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertEquals(4, digest.length);
@@ -148,10 +148,10 @@ public class TestCrcComposer {
   @Test
   public void testStripedByteArray() {
     CrcComposer digester =
-        CrcComposer.newStripedCrcComposer(type, chunkSize, cellSize);
-    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length - 4, chunkSize);
+        CrcComposer.newStripedCrcComposer(type, CHUNK_SIZE, CELL_SIZE);
+    digester.update(crcBytesByChunk, 0, crcBytesByChunk.length - 4, CHUNK_SIZE);
     digester.update(
-        crcBytesByChunk, crcBytesByChunk.length - 4, 4, dataSize % chunkSize);
+        crcBytesByChunk, crcBytesByChunk.length - 4, 4, DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertArrayEquals(crcBytesByCell, digest);
@@ -160,11 +160,11 @@ public class TestCrcComposer {
   @Test
   public void testStripedDataInputStream() throws IOException {
     CrcComposer digester =
-        CrcComposer.newStripedCrcComposer(type, chunkSize, cellSize);
+        CrcComposer.newStripedCrcComposer(type, CHUNK_SIZE, CELL_SIZE);
     DataInputStream input =
         new DataInputStream(new ByteArrayInputStream(crcBytesByChunk));
-    digester.update(input, crcsByChunk.length - 1, chunkSize);
-    digester.update(input, 1, dataSize % chunkSize);
+    digester.update(input, crcsByChunk.length - 1, CHUNK_SIZE);
+    digester.update(input, 1, DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertArrayEquals(crcBytesByCell, digest);
@@ -173,11 +173,11 @@ public class TestCrcComposer {
   @Test
   public void testStripedSingleCrcs() {
     CrcComposer digester =
-        CrcComposer.newStripedCrcComposer(type, chunkSize, cellSize);
+        CrcComposer.newStripedCrcComposer(type, CHUNK_SIZE, CELL_SIZE);
     for (int i = 0; i < crcsByChunk.length - 1; ++i) {
-      digester.update(crcsByChunk[i], chunkSize);
+      digester.update(crcsByChunk[i], CHUNK_SIZE);
     }
-    digester.update(crcsByChunk[crcsByChunk.length - 1], dataSize % chunkSize);
+    digester.update(crcsByChunk[crcsByChunk.length - 1], DATA_SIZE % CHUNK_SIZE);
 
     byte[] digest = digester.digest();
     assertArrayEquals(crcBytesByCell, digest);
@@ -186,23 +186,23 @@ public class TestCrcComposer {
   @Test
   public void testMultiStageMixed() throws IOException {
     CrcComposer digester =
-        CrcComposer.newStripedCrcComposer(type, chunkSize, cellSize);
+        CrcComposer.newStripedCrcComposer(type, CHUNK_SIZE, CELL_SIZE);
 
     // First combine chunks into cells.
     DataInputStream input =
         new DataInputStream(new ByteArrayInputStream(crcBytesByChunk));
-    digester.update(input, crcsByChunk.length - 1, chunkSize);
-    digester.update(input, 1, dataSize % chunkSize);
+    digester.update(input, crcsByChunk.length - 1, CHUNK_SIZE);
+    digester.update(input, 1, DATA_SIZE % CHUNK_SIZE);
     byte[] digest = digester.digest();
 
     // Second, individually combine cells into full crc.
     digester =
-        CrcComposer.newCrcComposer(type, cellSize);
+        CrcComposer.newCrcComposer(type, CELL_SIZE);
     for (int i = 0; i < digest.length - 4; i += 4) {
       int cellCrc = CrcUtil.readInt(digest, i);
-      digester.update(cellCrc, cellSize);
+      digester.update(cellCrc, CELL_SIZE);
     }
-    digester.update(digest, digest.length - 4, 4, dataSize % cellSize);
+    digester.update(digest, digest.length - 4, 4, DATA_SIZE % CELL_SIZE);
     digest = digester.digest();
     assertEquals(4, digest.length);
     int calculatedCrc = CrcUtil.readInt(digest, 0);
@@ -212,24 +212,24 @@ public class TestCrcComposer {
   @Test
   public void testUpdateMismatchesStripe() {
     CrcComposer digester =
-        CrcComposer.newStripedCrcComposer(type, chunkSize, cellSize);
+        CrcComposer.newStripedCrcComposer(type, CHUNK_SIZE, CELL_SIZE);
 
-    digester.update(crcsByChunk[0], chunkSize);
+    digester.update(crcsByChunk[0], CHUNK_SIZE);
 
     // Going from chunkSize to chunkSize + cellSize will cross a cellSize
     // boundary in a single CRC, which is not allowed, since we'd lack a
     // CRC corresponding to the actual cellSize boundary.
     assertThrows(IllegalStateException.class,
-        () -> digester.update(crcsByChunk[1], cellSize),
+        () -> digester.update(crcsByChunk[1], CELL_SIZE),
         "stripe");
   }
 
   @Test
   public void testUpdateByteArrayLengthUnalignedWithCrcSize() {
-    CrcComposer digester = CrcComposer.newCrcComposer(type, chunkSize);
+    CrcComposer digester = CrcComposer.newCrcComposer(type, CHUNK_SIZE);
 
     assertThrows(IllegalArgumentException.class,
-        () -> digester.update(crcBytesByChunk, 0, 6, chunkSize),
+        () -> digester.update(crcBytesByChunk, 0, 6, CHUNK_SIZE),
         "length");
   }
 }
