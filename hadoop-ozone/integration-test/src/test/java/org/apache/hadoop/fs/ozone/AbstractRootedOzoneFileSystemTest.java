213969082d06c780636de1f931485b4d6925a885
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-05-07 23:10:07 +0800

    HDDS-14043. Clean up EC bucket in testContentSummaryErasureCodingPolicyOnEcBucket

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/AbstractRootedOzoneFileSystemTest.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/AbstractRootedOzoneFileSystemTest.java
@@ -2032,9 +2032,14 @@ abstract class AbstractRootedOzoneFileSystemTest extends OzoneFileSystemTestBase
     String ecBuck = UUID.randomUUID().toString();
     TestDataUtil.createVolumeAndBucket(client, ecVol, ecBuck, ecBucketArgs);
 
-    Path ecBucketPath = new Path(new Path(OZONE_URI_DELIMITER, ecVol), ecBuck);
-    assertEquals(ecConfig.getReplication(),
-        fs.getContentSummary(ecBucketPath).getErasureCodingPolicy());
+    Path ecVolPath = new Path(OZONE_URI_DELIMITER, ecVol);
+    Path ecBucketPath = new Path(ecVolPath, ecBuck);
+    try {
+      assertEquals(ecConfig.getReplication(),
+          fs.getContentSummary(ecBucketPath).getErasureCodingPolicy());
+    } finally {
+      fs.delete(ecVolPath, true);
+    }
   }
 
   @Test
