8847638cf5303d9b7be0ce0246d284cb705127cf
S O'Donnell <sodonnell@cloudera.com>
2026-05-08 16:13:03 +0100

    Fix checkstyle

--- hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/upgrade/TestOMUpgradeFinalizeService.java
+++ hadoop-ozone/ozone-manager/src/test/java/org/apache/hadoop/ozone/om/upgrade/TestOMUpgradeFinalizeService.java
@@ -17,10 +17,8 @@
 
 package org.apache.hadoop.ozone.om.upgrade;
 
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
