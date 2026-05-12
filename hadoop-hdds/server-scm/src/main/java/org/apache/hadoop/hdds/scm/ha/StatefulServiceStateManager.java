778df2d6e09e480e6d7d82cb942ca7efd95aaab1
Bolin Lin <linbolin1230@gmail.com>
2026-05-11 23:36:38 -0400

    feat: add StatefulServiceStateManagerInvoker

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/StatefulServiceStateManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/StatefulServiceStateManager.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.hdds.scm.ha;
 import com.google.protobuf.ByteString;
 import java.io.IOException;
 import org.apache.hadoop.hdds.protocol.proto.SCMRatisProtocol.RequestType;
+import org.apache.hadoop.hdds.scm.ha.invoker.ScmInvokerCodeGenerator;
 import org.apache.hadoop.hdds.scm.metadata.Replicate;
 import org.apache.hadoop.hdds.utils.db.Table;
 
@@ -70,5 +71,9 @@ public interface StatefulServiceStateManager extends SCMHandler {
   @Override
   default RequestType getType() {
     return RequestType.STATEFUL_SERVICE_CONFIG;
+  }
+
+  static void main(String[] args) {
+    ScmInvokerCodeGenerator.generate(StatefulServiceStateManager.class, true);
   }
 }
