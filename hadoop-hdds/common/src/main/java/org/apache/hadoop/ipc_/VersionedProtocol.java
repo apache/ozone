0926e9f0ef61ebe516fbed50f7462a1e915db959
Doroszlai, Attila <adoroszlai@apache.org>
2026-05-12 08:15:26 +0200

    HDDS-15247. Remove unused ProtocolMetaInterface

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/ipc_/VersionedProtocol.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/ipc_/VersionedProtocol.java
@@ -37,18 +37,4 @@ public interface VersionedProtocol {
   public long getProtocolVersion(String protocol,
                                  long clientVersion) throws IOException;
 
-  /**
-   * Return protocol version corresponding to protocol interface.
-   * @param protocol The classname of the protocol interface
-   * @param clientVersion The version of the protocol that the client speaks
-   * @param clientMethodsHash the hashcode of client protocol methods
-   * @return the server protocol signature containing its version and
-   *         a list of its supported methods
-   * @see ProtocolSignature#getProtocolSignature(VersionedProtocol, String, 
-   *                long, int) for a default implementation
-   * @throws IOException raised on errors performing I/O.
-   */
-  public ProtocolSignature getProtocolSignature(String protocol, 
-                                 long clientVersion,
-                                 int clientMethodsHash) throws IOException;
 }
