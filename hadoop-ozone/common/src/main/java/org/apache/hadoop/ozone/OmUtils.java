2689c6e4de2e3616af97a808f4dfd099e5a5543e
Gargi Jaiswal <gargi.jaiswal@cloudera.com>
2026-06-09 12:43:24 +0530

    add protobuf schema and OmBucketInfo storage for S3 bucket tags

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/OmUtils.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/OmUtils.java
@@ -259,6 +259,8 @@ public final class OmUtils {
       // keeping it here for compatibility
     case GetSnapshotInfo:
     case GetObjectTagging:
+    case GetBucketTagging:
+      return true;
     case GetQuotaRepairStatus:
     case StartQuotaRepair:
       return true;
@@ -322,6 +324,9 @@ public final class OmUtils {
     case QuotaRepair:
     case PutObjectTagging:
     case DeleteObjectTagging:
+    case PutBucketTagging:
+    case DeleteBucketTagging:
+      return false;
     case UnknownCommand:
       return false;
     case EchoRPC:
@@ -376,6 +381,8 @@ public final class OmUtils {
     case GetKeyInfo:
     case GetSnapshotInfo:
     case GetObjectTagging:
+      return true;
+    case GetBucketTagging:
       return true;
     case CreateVolume:
     case SetVolumeProperty:
@@ -437,6 +444,8 @@ public final class OmUtils {
     case QuotaRepair:
     case PutObjectTagging:
     case DeleteObjectTagging:
+    case PutBucketTagging:
+    case DeleteBucketTagging:
     case ServiceList: // OM leader should have the most up-to-date OM service list info
     case RangerBGSync: // Ranger Background Sync task is only run on leader
     case SnapshotDiff:
