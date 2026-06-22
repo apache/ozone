3dc4aa4f9263260d71b6f3809736dc0198506a9c
Om Kenge <88768848+omkenge@users.noreply.github.com>
2026-06-22 15:59:14 +0000

    feat: add pipeline info detail

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineStateMap.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineStateMap.java
@@ -114,10 +114,14 @@ class PipelineStateMap {
    */
   PipelineInfo getPipeline(PipelineID pipelineID) throws PipelineNotFoundException {
     Objects.requireNonNull(pipelineID, "pipelineID == null");
-    final PipelineInfo info = pipelineMap.get(pipelineID);
+    final PipelineInfo info = pipelineMap.remove(pipelineID);
+
     if (info == null) {
-      throw new PipelineNotFoundException("Pipeline not found: " + pipelineID);
+      throw new PipelineNotFoundException(
+          "Pipeline not found: " + pipelineID);
     }
+
+    final Pipeline pipeline = info.getPipeline();
     return info;
   }
 
