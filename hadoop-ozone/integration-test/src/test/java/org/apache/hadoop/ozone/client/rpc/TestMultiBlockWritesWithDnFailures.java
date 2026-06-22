5dac1bf61d1e7f6036132aa112654939ab007206
Chun-Hung Tseng <henrybear327@users.noreply.github.com>
2026-06-22 10:40:17 +0200

    HDDS-15636. Remove comment about "the easiest way to create an open container" (#10568)

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestMultiBlockWritesWithDnFailures.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestMultiBlockWritesWithDnFailures.java
@@ -103,7 +103,6 @@ public class TestMultiBlockWritesWithDnFailures {
         .setNumDatanodes(datanodes)
         .build();
     cluster.waitForClusterToBeReady();
-    //the easiest way to create an open container is creating a key
     client = OzoneClientFactory.getRpcClient(conf);
     objectStore = client.getObjectStore();
     keyString = UUID.randomUUID().toString();
