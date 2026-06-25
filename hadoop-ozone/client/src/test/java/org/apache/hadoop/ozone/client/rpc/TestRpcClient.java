6355f092b6b9b09ec64852a2cdba3d10f6730c99
Ethan Rose <erose@apache.org>
2026-06-25 19:14:25 -0400

    Checkstyle

--- hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/rpc/TestRpcClient.java
+++ hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/rpc/TestRpcClient.java
@@ -22,7 +22,6 @@ import static org.apache.hadoop.ozone.client.rpc.RpcClient.validateOmVersion;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
