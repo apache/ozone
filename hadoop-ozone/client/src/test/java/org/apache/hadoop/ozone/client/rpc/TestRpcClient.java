14aa099c20e0f3a73a925046c389e316e15b5082
Ethan Rose <erose@apache.org>
2026-06-25 17:29:18 -0400

    Add test cases

--- hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/rpc/TestRpcClient.java
+++ hadoop-ozone/client/src/test/java/org/apache/hadoop/ozone/client/rpc/TestRpcClient.java
@@ -17,15 +17,21 @@
 
 package org.apache.hadoop.ozone.client.rpc;
 
+import static org.apache.hadoop.ozone.client.rpc.RpcClient.getOmVersion;
 import static org.apache.hadoop.ozone.client.rpc.RpcClient.validateOmVersion;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
 import org.apache.hadoop.ozone.OzoneManagerVersion;
 import org.apache.hadoop.ozone.om.helpers.ServiceInfo;
+import org.apache.hadoop.ozone.om.helpers.ServiceInfoEx;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -169,6 +175,33 @@ public class TestRpcClient {
         OzoneManagerVersion.SOFTWARE_VERSION,
         OzoneManagerVersion.SOFTWARE_VERSION,
         OzoneManagerVersion.UNKNOWN_VERSION,
+        true),
+
+    // An intermediate expected version is supported only by OMs at or above that version.
+    INTERMEDIATE_EXPECTED_ONE_OLDER_OM(
+        OzoneManagerVersion.HBASE_SUPPORT,
+        OzoneManagerVersion.ATOMIC_REWRITE_KEY,
+        null,
+        false),
+    INTERMEDIATE_EXPECTED_ONE_EXACT_OM(
+        OzoneManagerVersion.HBASE_SUPPORT,
+        OzoneManagerVersion.HBASE_SUPPORT,
+        null,
+        true),
+    INTERMEDIATE_EXPECTED_ONE_NEWER_OM(
+        OzoneManagerVersion.HBASE_SUPPORT,
+        OzoneManagerVersion.LIGHTWEIGHT_LIST_STATUS,
+        null,
+        true),
+    INTERMEDIATE_EXPECTED_ONE_NEWER_ONE_OLDER_OM(
+        OzoneManagerVersion.HBASE_SUPPORT,
+        OzoneManagerVersion.LIGHTWEIGHT_LIST_STATUS,
+        OzoneManagerVersion.ATOMIC_REWRITE_KEY,
+        false),
+    INTERMEDIATE_EXPECTED_TWO_NEWER_OM(
+        OzoneManagerVersion.HBASE_SUPPORT,
+        OzoneManagerVersion.LIGHTWEIGHT_LIST_STATUS,
+        OzoneManagerVersion.S3_OBJECT_TAGGING_API,
         true);
 
     private final OzoneManagerVersion expectedVersion;
@@ -214,5 +247,66 @@ public class TestRpcClient {
     assertThrows(
         IllegalArgumentException.class,
         () -> validateOmVersion(OzoneManagerVersion.UNKNOWN_VERSION, null));
+  }
+
+  @Test
+  public void testValidateOmVersionFailsWhenNoOmPresent() {
+    // At least one OM must be present. An empty list should fail validation.
+    assertFalse(validateOmVersion(OzoneManagerVersion.SOFTWARE_VERSION, Collections.emptyList()));
+  }
+
+  @Test
+  public void testGetOmVersionWithNoOmDefaultsToSoftwareVersion() {
+    // Default software version should be returned if OM list is empty.
+    assertEquals(OzoneManagerVersion.SOFTWARE_VERSION, getOmVersion(serviceInfoEx()));
+  }
+
+  @Test
+  public void testGetOmVersionReturnsSingleOmVersion() {
+    assertEquals(OzoneManagerVersion.HBASE_SUPPORT, getOmVersion(serviceInfoEx(om(OzoneManagerVersion.HBASE_SUPPORT))));
+  }
+
+  @Test
+  public void testGetOmVersionReturnsMinimumAcrossOms() {
+    // The lowest version among all OMs is returned, regardless of ordering.
+    assertEquals(OzoneManagerVersion.HBASE_SUPPORT, // version 7
+        getOmVersion(serviceInfoEx(
+            om(OzoneManagerVersion.LIGHTWEIGHT_LIST_STATUS), // version 8
+            om(OzoneManagerVersion.HBASE_SUPPORT)))); // version 7
+    assertEquals(OzoneManagerVersion.DEFAULT_VERSION, // version 0
+        getOmVersion(serviceInfoEx(
+            om(OzoneManagerVersion.DEFAULT_VERSION), // version 0
+            om(OzoneManagerVersion.SOFTWARE_VERSION)))); // largest concrete version.
+  }
+
+  @Test
+  public void testGetOmVersionWithFutureOM() {
+    // A future (unknown) OM version must never be returned as the version to use, even if it is the only one present.
+    // The client's latest known OM version should be used in this case, which should still be less than the future
+    // versions.
+    assertEquals(OzoneManagerVersion.SOFTWARE_VERSION,
+        getOmVersion(serviceInfoEx(
+            om(OzoneManagerVersion.UNKNOWN_VERSION),
+            om(OzoneManagerVersion.SOFTWARE_VERSION))));
+    assertEquals(OzoneManagerVersion.SOFTWARE_VERSION,
+        getOmVersion(serviceInfoEx(
+            om(OzoneManagerVersion.UNKNOWN_VERSION),
+            om(OzoneManagerVersion.UNKNOWN_VERSION))));
+  }
+
+  private static ServiceInfo om(OzoneManagerVersion version) {
+    return node(HddsProtos.NodeType.OM, version);
+  }
+
+  private static ServiceInfo node(HddsProtos.NodeType nodeType, OzoneManagerVersion version) {
+    return new ServiceInfo.Builder()
+        .setNodeType(nodeType)
+        .setHostname("localhost")
+        .setOmVersion(version)
+        .build();
+  }
+
+  private static ServiceInfoEx serviceInfoEx(ServiceInfo... serviceInfos) {
+    return new ServiceInfoEx(Arrays.asList(serviceInfos), null, null);
   }
 }
