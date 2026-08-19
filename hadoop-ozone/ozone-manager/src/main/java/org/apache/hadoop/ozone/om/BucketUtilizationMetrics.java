61ed72c2050c14ae03c34294d0c2d94a07780192
sravani-revuri <rvsravani24@gmail.com>
2026-08-19 13:56:44 +0530

    rearranged

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/BucketUtilizationMetrics.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/BucketUtilizationMetrics.java
@@ -38,8 +38,8 @@ import org.apache.hadoop.ozone.om.helpers.OmBucketInfo;
  * <ul>
  *   <li>Bytes used in bucket.
  *   <li>Bucket quota bytes held in snapshots.
- *   <li>Bucket quota in bytes. If bucket quota is not set then this metric shows -1 as value.
- *   <li>Bucket quota in namespace. If bucket quota is not set then this metric shows -1 as value.
+ *   <li>Bucket quota in bytes.
+ *   <li>Bucket quota in namespace.
  *   <li>Bucket used namespace (number of keys/directories in bucket).
  *   <li>Bucket available space. Calculated from difference between used bytes in bucket and bucket quota.
  *   If bucket quota is not set then this metric shows -1 as value.
