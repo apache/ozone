8db77e6e076e140b8e7e797fc65e610a6d76a33d
Ethan Rose <erose@apache.org>
2026-08-19 15:13:48 -0400

    Update register endpoint task as well

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/states/datanode/RunningDatanodeState.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/states/datanode/RunningDatanodeState.java
@@ -150,7 +150,6 @@ public class RunningDatanodeState implements DatanodeState {
           .setConfig(conf)
           .setEndpointStateMachine(endpoint)
           .setContext(context)
-          .setDatanodeDetails(context.getParent().getDatanodeDetails())
           .setOzoneContainer(context.getParent().getContainer())
           .build();
     case HEARTBEAT:
