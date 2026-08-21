f1400032843d768ea74d9c98da983165c32fa8c6
Wei-Chiu Chuang <weichiu@apache.org>
2026-08-21 10:20:03 -0700

    Improve Ozone Manager test coverage
    Change-Id: I95046cdfc254d77b8402a3c909199d6d838ed5e0
    (cherry picked from commit b264f854f1b9a3cf68f5956d054e378b6cab290b)

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
@@ -1281,7 +1281,8 @@ public final class OzoneManager extends ServiceRuntimeInfoImpl
   }
 
   @Override
-  public UUID refetchSecretKey() {
+  public UUID refetchSecretKey() throws IOException {
+    checkAdminUserPrivilege("refetch secret key.");
     secretKeyClient.refetchSecretKey();
     return secretKeyClient.getCurrentSecretKey().getId();
   }
