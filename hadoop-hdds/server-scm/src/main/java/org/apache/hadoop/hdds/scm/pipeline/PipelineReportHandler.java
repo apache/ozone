4779634ab4f87cdf12928e360ab3022c4619f384
Ethan Rose <erose@apache.org>
2026-09-03 14:01:05 -0400

    Add more specific logs

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineReportHandler.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/PipelineReportHandler.java
@@ -85,6 +85,9 @@ public class PipelineReportHandler implements
         if (!isNotLeaderException(e)) {
           LOGGER.error("Could not process pipeline report={} from dn={}.",
               report, dn, e);
+        } else {
+          LOGGER.info("NLE prevented pipeline from being processed on {}",
+              scmContext.getScm().getScmNodeDetails().getNodeId(), e);
         }
       }
     }
@@ -128,8 +131,10 @@ public class PipelineReportHandler implements
             dn);
       }
       if (pipeline.isHealthy()) {
+        String nodeID = scmContext.getScm().getScmNodeDetails().getNodeId();
+        LOGGER.info("Attempting to open pipeline {} on {}", pipeline, nodeID);
         pipelineManager.openPipeline(pipelineID);
-        LOGGER.info("Opened pipeline {}", pipelineID);
+        LOGGER.info("Opened pipeline {} on {}", pipeline, nodeID);
       }
     }
     if (pipeline.isHealthy()) {
