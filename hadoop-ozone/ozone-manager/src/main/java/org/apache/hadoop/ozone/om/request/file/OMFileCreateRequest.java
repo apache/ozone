ad891243da51bbbf58aeb967c8bcd2abe8c014b8
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-09-07 21:37:07 +0800

    HDDS-16161. Publish bucket usage from a copy in file create requests

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/file/OMFileCreateRequest.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/file/OMFileCreateRequest.java
@@ -227,7 +227,7 @@ public class OMFileCreateRequest extends OMKeyRequest {
 
       // do open key
       omBucketInfo =
-          getBucketInfo(omMetadataManager, volumeName, bucketName);
+          getBucketInfoForUpdate(omMetadataManager, volumeName, bucketName);
       final ReplicationConfig repConfig = OzoneConfigUtil
           .resolveReplicationConfigPreference(keyArgs.getType(),
               keyArgs.getFactor(), keyArgs.getEcReplicationConfig(),
@@ -276,6 +276,9 @@ public class OMFileCreateRequest extends OMKeyRequest {
       OMFileRequest.addKeyTableCacheEntries(omMetadataManager, volumeName,
           bucketName, omBucketInfo.getBucketLayout(),
           null, missingParentInfos, trxnLogIndex);
+
+      omMetadataManager.getBucketTable().addCacheEntry(
+          omMetadataManager.getBucketKey(volumeName, bucketName), omBucketInfo, trxnLogIndex);
 
       // Prepare response
       omResponse.setCreateFileResponse(CreateFileResponse.newBuilder()
