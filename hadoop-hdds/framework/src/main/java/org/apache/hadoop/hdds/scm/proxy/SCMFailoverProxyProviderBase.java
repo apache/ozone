04d5eec30ba0800265f5c1e2a735de41fc751692
Ivan Andika <ivandika@apache.org>
2026-09-15 10:16:48 +0800

    HDDS-16382. Preserve SCM proxy field ordering

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/scm/proxy/SCMFailoverProxyProviderBase.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/scm/proxy/SCMFailoverProxyProviderBase.java
@@ -94,6 +94,8 @@ public abstract class SCMFailoverProxyProviderBase<T> implements FailoverProxyPr
   @Nullable
   private final SocketFactory socketFactory;
 
+  private String updatedLeaderNodeID = null;
+
   /**
    * When true, on each connection-class failure the provider re-resolves
    * the cached SCM hostname and rebuilds the proxy if the IP has changed
@@ -101,8 +103,6 @@ public abstract class SCMFailoverProxyProviderBase<T> implements FailoverProxyPr
    * intent of HADOOP-17068 / HDFS-14118.
    */
   private final boolean resolveOnFailureEnabled;
-
-  private String updatedLeaderNodeID = null;
 
   public SCMFailoverProxyProviderBase(Class<T> protocol, ConfigurationSource conf,
       UserGroupInformation userGroupInformation) {
