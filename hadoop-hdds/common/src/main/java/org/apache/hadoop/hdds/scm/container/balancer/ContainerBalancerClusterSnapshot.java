edf26381e08df4950bb2806f7d0cdf9b0184df6b
sravani-revuri <rvsravani24@gmail.com>
2026-09-16 11:24:24 +0530

    added comment

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/container/balancer/ContainerBalancerClusterSnapshot.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/scm/container/balancer/ContainerBalancerClusterSnapshot.java
@@ -37,6 +37,9 @@ public final class ContainerBalancerClusterSnapshot {
   private final int targetCount;
   private final long totalOverUtilizedBytes;
   private final long totalUnderUtilizedBytes;
+
+  // Same value as totalOverUtilizedBytes. The analyzer sets it once via
+  // setTotalOverUtilizedBytes(); bytesToMove is assigned here for assessment output.
   private final long bytesToMove;
   private final double imbalance;
   private final List<NodeUtilization> topSourceNodes;
